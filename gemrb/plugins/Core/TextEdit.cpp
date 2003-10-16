bb332b44c37d9750e03e665dd9855ab05325366e
Balrog994 <Balrog994@yahoo.com>
2003-10-16 19:48:42 +0000

    Added Support for Text Edit Control
    svn-id: r32

---
+++ gemrb/plugins/Core/TextEdit.cpp
@@ -0,0 +1,78 @@
+#include "../../includes/win32def.h"
+#include "TextEdit.h"
+#include "Interface.h"
+
+extern Interface * core;
+
+TextEdit::TextEdit(unsigned short maxLength)
+{
+	max = maxLength;
+	Buffer = (unsigned char*)malloc(max+1);
+	font = NULL;
+	Cursor = NULL;
+	Back = NULL;
+	CurPos = 0;
+	strncpy((char*)Buffer, "Text Edit", max);
+}
+
+TextEdit::~TextEdit(void)
+{
+	free(Buffer);
+}
+
+/** Draws the Control on the Output Display */
+void TextEdit::Draw(unsigned short x, unsigned short y)
+{
+	Color hi = {0xff, 0xff, 0xff, 0x00}, low = {0x00, 0x00, 0x00, 0x00};
+	if(hasFocus)
+		font->Print(Region(x+XPos, y+YPos, Width, Height), Buffer, &hi, &low, IE_FONT_ALIGN_LEFT | IE_FONT_ALIGN_MIDDLE, true, NULL, NULL, Cursor, CurPos);
+	else
+		font->Print(Region(x+XPos, y+YPos, Width, Height), Buffer, &hi, &low, IE_FONT_ALIGN_LEFT | IE_FONT_ALIGN_MIDDLE, true);
+}
+
+/** Set Font */
+void TextEdit::SetFont(Font * f)
+{
+	if(f != NULL)
+		font = f;
+}
+
+/** Set Cursor */
+void TextEdit::SetCursor(Sprite2D * cur)
+{
+	if(cur != NULL)
+		Cursor = cur;
+}
+
+/** Set BackGround */
+void TextEdit::SetBackGround(Sprite2D * back)
+{
+	//if 'back' is NULL then no BackGround will be drawn
+	Back = back;
+}
+
+/** Key Press Event */
+void TextEdit::OnKeyPress(unsigned char Key, unsigned short Mod)
+{
+	if(Key >= 0x20) {
+		int len = strlen((char*)Buffer);
+		if(len+1 < max) {
+			for(int i = len; i > CurPos; i--) {
+				Buffer[i] = Buffer[i-1];
+			}
+			Buffer[CurPos] = Key;
+			CurPos++;
+		}
+	}
+	else if(Key == '\b') {
+		if(CurPos != 0) {
+			int len = strlen((char*)Buffer);
+			for(int i = CurPos; i < len; i++) {
+				Buffer[i-1] = Buffer[i];
+			}
+			Buffer[len-1] = 0;
+			CurPos--;
+		}
+	}
+	//else if(key == '     
+}
---
+++ gemrb/plugins/Core/TextEdit.h
@@ -0,0 +1,68 @@
+/***************************************************************************
+                          TextArea.h  -  description
+                             -------------------
+    begin                : dom ott 12 2003
+    copyright            : (C) 2003 by GemRB Developement Team
+    email                : Balrog994@yahoo.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef TEXTEDIT_H
+#define TEXTEDIT_H
+
+#include "Control.h"
+#include "../../includes/RGBAColor.h"
+#include "Font.h"
+
+#ifdef WIN32
+
+#ifdef GEM_BUILD_DLL
+#define GEM_EXPORT __declspec(dllexport)
+#else
+#define GEM_EXPORT __declspec(dllimport)
+#endif
+
+#else
+#define GEM_EXPORT
+#endif
+
+class GEM_EXPORT TextEdit : public Control
+{
+public:
+	TextEdit(unsigned short maxLength);
+	~TextEdit(void);
+	/** Draws the Control on the Output Display */
+	void Draw(unsigned short x, unsigned short y);
+	/** Set Font */
+	void SetFont(Font * f);
+	/** Set Cursor */
+	void SetCursor(Sprite2D * cur);
+	/** Set BackGround */
+	void SetBackGround(Sprite2D * back);
+private:
+	/** Text Editing Cursor Sprite */
+	Sprite2D * Cursor;
+	/** Text Font */
+	Font * font;
+	/** Background */
+	Sprite2D * Back;
+	/** Max Edit Text Length */
+	unsigned short max;
+	/** Text Buffer */
+	unsigned char * Buffer;
+	/** Cursor Position */
+	unsigned short CurPos;
+public: //Events
+	/** Key Press Event */
+	void OnKeyPress(unsigned char Key, unsigned short Mod);
+};
+
+#endif
