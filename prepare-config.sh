6444e8d28836745a1ccef882319b9d952f54392e
Balrog994 <Balrog994@yahoo.com>
2003-10-21 15:48:11 +0000

    Added a Linux Script for correcting a CVS Bug: Running this script you can restore some missing files and permissions, so you can run 'configure'
    svn-id: r72

---
+++ prepare-config.sh
@@ -0,0 +1,7 @@
+cp admin/old-ltcf-c.sh admin/ltcf-c.sh
+cp admin/old-ltcf-cxx.sh admin/ltcf-cxx.sh
+cp admin/old-ltcf-gcj.sh admin/ltcf-gcj.sh
+cp admin/old-ltconfig admin/ltconfig
+cp admin/old-ltmain.sh admin/ltmain.sh
+cp admin/old-libtool.m4.in admin/libtool.m4.in
+chmod 744 *.sh
