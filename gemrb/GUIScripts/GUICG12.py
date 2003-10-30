d9d1e8c695e84c39a208a50a35ca9c915b8928d6
Balrog994 <Balrog994@yahoo.com>
2003-10-30 18:28:51 +0000

    Missing GUICH12.py script changes
    svn-id: r260

--- gemrb/GUIScripts/GUICG12.py
+++ gemrb/GUIScripts/GUICG12.py
@@ -3,12 +3,17 @@ import GemRB
 
 AppearanceWindow = 0
 PortraitButton = 0
+PortraitsTable = 0
+LastPortrait = 0
 
 def OnLoad():
-	global AppearanceWindow, PortraitButton
+	global AppearanceWindow, PortraitButton, PortraitsTable, LastPortrait
 	
 	GemRB.LoadWindowPack("GUICG")
 	AppearanceWindow = GemRB.LoadWindow(11)
+
+	#Load the Portraits Table
+	PortraitsTable = GemRB.LoadTable("PORTRAIT")
 
 	TextAreaControl = GemRB.GetControl(AppearanceWindow, 7)
 	GemRB.SetText(AppearanceWindow, TextAreaControl,"") # why is this here?
@@ -29,12 +34,22 @@ def OnLoad():
 	GemRB.SetEvent(AppearanceWindow,RightButton,IE_GUI_BUTTON_ON_PRESS,"RightPress")
 	GemRB.SetEvent(AppearanceWindow,DoneButton,IE_GUI_BUTTON_ON_PRESS,"NextPress")
 	GemRB.SetEvent(AppearanceWindow,BackButton,IE_GUI_BUTTON_ON_PRESS,"BackPress")
+	
+	PortraitName = GemRB.GetTableRowName(PortraitsTable, LastPortrait)
+	print "Loading",PortraitName
+	GemRB.SetButtonPicture(AppearanceWindow, PortraitButton, PortraitName)
+	
 	GemRB.SetVisible(AppearanceWindow,1)
 	return
 
 def RightPress():
+	global AppearanceWindow, PortraitButton, PortraitsTable, LastPortrait
 #Portrait=GetNextPortrait()
 #SetPicture(AppearanceWindow, PortraitButton, Portrait)
+	LastPortrait = (LastPortrait + 2) % 64
+	PortraitName = GemRB.GetTableRowName(PortraitsTable, LastPortrait)
+	print "Loading",PortraitName
+	GemRB.SetButtonPicture(AppearanceWindow, PortraitButton, PortraitName)
 	return
 
 def LeftPress():
