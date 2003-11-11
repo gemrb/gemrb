f631e2accffc98349f43efe921fa31f6ec40e234
Avenger <avenger_teambg@users.sourceforge.net>
2003-11-11 20:44:48 +0000

    continued work on proficiency screen, almost working
    svn-id: r453

--- gemrb/GUIScripts/bg2/GUICG9.py
+++ gemrb/GUIScripts/bg2/GUICG9.py
@@ -8,6 +8,7 @@ SkillTable = 0
 TopIndex = 0
 RowCount = 0
 PointsLeft = 0
+ProfColumn = 0
 
 def RedrawSkills():
 	SumLabel = GemRB.GetControl(SkillWindow, 0x10000009)
@@ -15,8 +16,28 @@ def RedrawSkills():
 
 	for i in range(0,8):
 		Pos=TopIndex+i
-		SkillName = GemRB.GetTableValue(SkillTable, Pos+7, 1) #we add the bg1 skill count offset
-		GemRB.SetText(SkillWindow, 0x1000001+i, SkillName)
+		MaxProf = GemRB.GetTableValue(SkillTable, Pos+8, ProfColumn) #we add the bg1 skill count offset
+		print "MaxProf:",MaxProf," row:",Pos+8," in column:",ProfColumn
+		Label=GemRB.GetControl(SkillWindow, i+69)
+		Button1=GemRB.GetControl(SkillWindow, i*2+11)
+		Button2=GemRB.GetControl(SkillWindow, i*2+12)
+		if MaxProf == 0:
+			GemRB.SetButtonState(SkillWindow, Label, IE_GUI_BUTTON_DISABLED)
+			GemRB.SetButtonState(SkillWindow, Button1, IE_GUI_BUTTON_DISABLED)
+			GemRB.SetButtonState(SkillWindow, Button2, IE_GUI_BUTTON_DISABLED)
+			GemRB.SetButtonFlags(SkillWindow, Button1, IE_GUI_BUTTON_NO_IMAGE,OP_OR)
+			GemRB.SetButtonFlags(SkillWindow, Button2, IE_GUI_BUTTON_NO_IMAGE,OP_OR)
+		else:
+			GemRB.SetButtonState(SkillWindow, Label, IE_GUI_BUTTON_ENABLED)
+			GemRB.SetButtonState(SkillWindow, Button1, IE_GUI_BUTTON_ENABLED)
+			GemRB.SetButtonState(SkillWindow, Button2, IE_GUI_BUTTON_ENABLED)
+			GemRB.SetButtonFlags(SkillWindow, Button1, IE_GUI_BUTTON_NO_IMAGE,OP_NAND)
+			GemRB.SetButtonFlags(SkillWindow, Button2, IE_GUI_BUTTON_NO_IMAGE,OP_NAND)
+		
+		SkillName = GemRB.GetTableValue(SkillTable, Pos+8, 1) #we add the bg1 skill count offset
+		Label=GemRB.GetControl(SkillWindow, 0x10000001+i)
+		GemRB.SetText(SkillWindow, Label, SkillName)
+
 		ActPoint = GemRB.GetVar("Proficiency "+str(Pos) )
 		for j in range(0,5):  #5 is maximum distributable
 			Star=GemRB.GetControl(SkillWindow, i*5+j+27)
@@ -26,14 +47,9 @@ def RedrawSkills():
 				GemRB.SetButtonFlags(SkillWindow, Star, IE_GUI_BUTTON_NO_IMAGE,OP_OR)
 	return
 
-def ScrollBarPress():
-	#redraw skill list
-	RedrawSkills()
-	return
-
 def OnLoad():
 	global SkillWindow, TextAreaControl, DoneButton, TopIndex
-	global SkillTable, PointsLeft
+	global SkillTable, PointsLeft, ProfColumn
 	
 	ClassTable = GemRB.LoadTable("classes")
 	Class = GemRB.GetVar("Class")-1
@@ -41,9 +57,13 @@ def OnLoad():
 	Kit = GemRB.GetVar("Class Kit")
 	if Kit == 0:
 		KitName = ClassName
+		ProfColumn = Class + 3
 	else:
 		KitList = GemRB.LoadTable("kitlist")
-		KitName = GemRB.GetTableValue(KitList, Kit, 0) #rowname is just a number
+		#rowname is just a number, the kitname is the first data column
+		KitName = GemRB.GetTableValue(KitList, Kit, 0)
+		#this is the proficiency column number in kitlist
+		ProfColumn = GemRB.GetTableValue(KitList, Kit, 5)
 
 	SkillTable = GemRB.LoadTable("profs")
 	Level = GemRB.GetVar("Level")-1
@@ -57,6 +77,10 @@ def OnLoad():
 	SkillWindow = GemRB.LoadWindow(9)
 
 	for i in range(0,8):
+		Button=GemRB.GetControl(SkillWindow, i+69)
+		GemRB.SetVarAssoc(SkillWindow, Button, "Skill", i)
+		GemRB.SetEvent(SkillWindow, Button, IE_GUI_BUTTON_ON_PRESS, "JustPress")
+
 		Button=GemRB.GetControl(SkillWindow, i*2+11)
 		GemRB.SetVarAssoc(SkillWindow, Button, "Skill", i)
 		GemRB.SetEvent(SkillWindow, Button, IE_GUI_BUTTON_ON_PRESS, "LeftPress")
@@ -94,19 +118,36 @@ def ScrollBarPress():
 	RedrawSkills()
 	return
 
+def JustPress():
+	Pos = GemRB.GetVar("Skill")+TopIndex
+	GemRB.SetText(SkillWindow, TextAreaControl, GemRB.GetTableValue(SkillTable, Pos+8, 2) )
+	return
+	
 def RightPress():
 	Pos = GemRB.GetVar("Skill")+TopIndex
-	GemRB.SetText(SkillWindow, TextAreaControl, GemRB.GetTableValue(SkillTable, Pos+7, 1) )
-	#
+	GemRB.SetText(SkillWindow, TextAreaControl, GemRB.GetTableValue(SkillTable, Pos+8, 2) )
+	ActPoint = GemRB.GetVar("Skill "+str(Pos) )
+	if ActPoint <= 0:
+		return
+	GemRB.SetVar("Skill "+str(Pos),ActPoint-1)
 	PointsLeft = PointsLeft + 1
 	RedrawSkills()
 	return
 
 def LeftPress():
 	Pos = GemRB.GetVar("Skill")+TopIndex
-	GemRB.SetText(SkillWindow, TextAreaControl, GemRB.GetTableValue(SkillTable, Pos+7, 1) )
+	GemRB.SetText(SkillWindow, TextAreaControl, GemRB.GetTableValue(SkillTable, Pos+8, 2) )
 	if PointsLeft == 0:
 		return
+	MaxProf = GemRB.GetTableValue(SkillTable, Pos+8, ProfColumn) #we add the bg1 skill count offset
+	if MaxProf>5:
+		MaxProf = 5
+
+	ActPoint = GemRB.GetVar("Skill "+str(Pos) )
+	if ActPoint >= MaxProf:
+		return
+	GemRB.SetVar("Skill "+str(Pos),ActPoint+1)
+	PointsLeft = PointsLeft - 1
 	RedrawSkills()
 	return
 
