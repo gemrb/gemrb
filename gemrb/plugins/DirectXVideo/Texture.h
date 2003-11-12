9f590ad2f0230b362c4e480765e24bf79d160332
Balrog994 <Balrog994@yahoo.com>
2003-11-12 22:32:56 +0000

    First Import: DirectXVideo Module : Texture handling class for Direct3D 9 Accelerated video output
    svn-id: r464

---
+++ gemrb/plugins/DirectXVideo/Texture.h
@@ -0,0 +1,28 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include "DirectXVideoDriver.h"
+#include "../../includes/RGBAColor.h"
+#include "d3d9.h"
+
+class Texture
+{
+	
+public:
+	Texture() : pTexture(NULL) { Clear(); }
+	Texture(void * pixels, int w, int h, int bpp, void * palette = NULL, bool cK = false, int index = 0) : pTexture(NULL) { Init(pixels, w, h, bpp, palette, cK, index); }
+	~Texture() { Clear(); }
+	void Init (void * pixels, int w, int h, int bpp, void * palette = NULL, bool cK = false, int index = 0);
+	void Clear();
+
+	LPDIRECT3DTEXTURE9	pTexture;
+	int m_Width;
+	int m_Height;
+	int m_Pitch;
+	void * palette;
+	bool hasCK;
+	int colorKey;
+
+};
+
+#endif
