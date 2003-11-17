27beaff8aafdfa737cc9b60ed2c6cea137c3d423
Balrog994 <Balrog994@yahoo.com>
2003-11-17 14:46:47 +0000

    IWD2 GUI Scripts: Added "Single Player" Party Formation Screen
    svn-id: r566

---
+++ gemrb/GUIScripts/iwd2/SPPartyFormation.py
@@ -0,0 +1,68 @@
+#Single Player Party Formation
+import GemRB
+
+PartyFormationWindow = 0
+ExitWindow = 0
+
+def OnLoad():
+	global PartyFormationWindow
+	GemRB.LoadWindowPack("GUISP")
+	
+	PartySelectWindow = GemRB.LoadWindow(0)
+	
+	ExitButton = GemRB.GetControl(PartyFormationWindow, 30)
+	GemRB.SetText(PartyFormationWindow, ExitButton, 13906)
+	GemRB.SetEvent(PartyFormationWindow, ExitButton, IE_GUI_BUTTON_ON_PRESS, "ExitPress")
+	ModifyCharactersButton = GemRB.GetControl(PartyFormationWindow, 43)
+	GemRB.SetText(PartyFormationWindow, ModifyCharactersButton, 18816)
+	GemRB.SetButtonState(PartyFormationWindow, ModifyCharactersButton, IE_GUI_BUTTON_DISABLED)
+	DoneButton = GemRB.GetControl(PartyFormationWindow, 28)
+	GemRB.SetText(PartyFormationWindow, DoneButton, 11973)
+	GemRB.SetButtonState(PartyFormationWindow, DoneButton, IE_GUI_BUTTON_DISABLED)
+	
+	for i in range(18,24):
+		Button = GemRB.GetControl(PartyFormationWindow,i)
+		GemRB.SetText(PartyFormationWindow, Button, 10264)
+		GemRB.SetEvent(PartyFormationWindow, Button, IE_GUI_BUTTON_ON_PRESS, "GeneratePress")
+	
+	GemRB.SetVisible(PartyFormationWindow, 1)
+	
+	return
+	
+def ExitPress():
+	global PartyFormationWindow, ExitWindow
+	GemRB.SetVisible(PartyFormationWindow, 0)
+	ExitWindow = GemRB.LoadWindow(7)
+	
+	TextArea = GemRB.GetControl(ExitWindow, 0)
+	GemRB.SetText(ExitWindow, TextArea, 11329)
+	
+	CancelButton = GemRB.GetControl(ExitWindow, 2)
+	GemRB.SetText(ExitWindow, CancelButton, 13727)
+	GemRB.SetEvent(ExitWindow, CancelButton, IE_GUI_BUTTON_ON_PRESS, "ExitCancelPress")
+	
+	DoneButton = GemRB.GetControl(ExitWindow, 1)
+	GemRB.SetText(ExitWindow, DoneButton, 11973)
+	GemRB.SetEvent(ExitWindow, DoneButton, IE_GUI_BUTTON_ON_PRESS, "ExitDonePress")
+	
+	GemRB.SetVisible(ExitWindow, 1)
+	return
+	
+def ExitDonePress():
+	global PartyFormationWindow, ExitWindow
+	GemRB.UnloadWindow(ExitWindow);
+	GemRB.UnloadWindow(PartyFormationWindow)
+	GemRB.SetNextScript("Start")
+	return
+	
+def ExitCancelPress():
+	global PartyFormationWindow, ExitWindow
+	GemRB.UnloadWindow(ExitWindow)
+	GemRB.SetVisible(PartyFormationWindow, 1)
+	return
+	
+def GeneratePress():
+	global PartyFormationWindow
+	GemRB.UnloadWindow(PartyFormationWindow)
+	GemRB.SetNextScript("CharGen")
+	return
