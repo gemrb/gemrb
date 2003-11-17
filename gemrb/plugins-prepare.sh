4f91705a7201c59a89f9d0d61285f6d6031044b0
Avenger <avenger_teambg@users.sourceforge.net>
2003-11-17 20:55:33 +0000

    copies all plugins without need of update
    svn-id: r584

--- gemrb/plugins-prepare.sh
+++ gemrb/plugins-prepare.sh
@@ -1,20 +1,4 @@
-cp plugins/2DAImporter/.libs/lib2DAImporter.so plugins/
-cp plugins/ACMImporter/.libs/libACMImporter.so plugins/
-cp plugins/AREImporter/.libs/libAREImporter.so plugins/
-cp plugins/BAMImporter/.libs/libBAMImporter.so plugins/
-cp plugins/BG2HCAnim/.libs/libBG2HCAnim.so plugins/
-cp plugins/BIFImporter/.libs/libBIFImporter.so plugins/
-cp plugins/BMPImporter/.libs/libBMPImporter.so plugins/
-cp plugins/CHUImporter/.libs/libCHUImporter.so plugins/
-cp plugins/CREImporter/.libs/libCREImporter.so plugins/
-cp plugins/GUIScript/.libs/libGUIScript.so plugins/
-cp plugins/IDSImporter/.libs/libIDSImporter.so plugins/
-cp plugins/KEYImporter/.libs/libKEYImporter.so plugins/
-cp plugins/MOSImporter/.libs/libMOSImporter.so plugins/
-cp plugins/MUSImporter/.libs/libMUSImporter.so plugins/
-cp plugins/MVEPlayer/.libs/libMVEPlayer.so plugins/
-cp plugins/SDLVideo/.libs/libSDLVideo.so plugins/
-cp plugins/TISImporter/.libs/libTISImporter.so plugins/
-cp plugins/TLKImporter/.libs/libTLKImporter.so plugins/
-cp plugins/WEDImporter/.libs/libWEDImporter.so plugins/
-cp plugins/ZLibMgr/.libs/libZLibMgr.so plugins/
+#copy all plugins
+cp -f plugins/*/.libs/lib*.so plugins/
+#remove the cuckoo's egg
+rm plugins/libcore.so
