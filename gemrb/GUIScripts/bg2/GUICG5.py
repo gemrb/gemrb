c87f4e30794eb799412df83a5319416a750d8de6
Avenger <avenger_teambg@users.sourceforge.net>
2003-11-22 23:14:52 +0000

    Default button (gender is a default button on the 1. cg screen)
    svn-id: r619

--- gemrb/GUIScripts/bg2/GUICG5.py
+++ gemrb/GUIScripts/bg2/GUICG5.py
@@ -3,9 +3,10 @@ import GemRB
 
 NameWindow = 0
 NameField = 0
+DoneButton = 0
 
 def OnLoad():
-	global NameWindow, NameField
+	global NameWindow, NameField, DoneButton
 	
 	GemRB.LoadWindowPack("GUICG")
 	NameWindow = GemRB.LoadWindow(5)
@@ -15,11 +16,13 @@ def OnLoad():
 
 	DoneButton = GemRB.GetControl(NameWindow,0)
 	GemRB.SetText(NameWindow, DoneButton, 11973)
+	GemRB.SetButtonState(NameWindow, DoneButton, IE_GUI_BUTTON_DISABLED)
 
 	NameField = GemRB.GetControl(NameWindow, 2)
 
 	GemRB.SetEvent(NameWindow, DoneButton, IE_GUI_BUTTON_ON_PRESS,"NextPress")
 	GemRB.SetEvent(NameWindow, BackButton, IE_GUI_BUTTON_ON_PRESS,"BackPress")
+	GemRB.SetEvent(NameWindow, NameField, IE_GUI_EDIT_ON_CHANGE,"EditChange")
 	GemRB.SetVisible(NameWindow,1)
 	return
 
@@ -37,3 +40,10 @@ def NextPress():
 	GemRB.SetNextScript("CharGen9")
 	return
 
+def EditChange():
+	Name = GemRB.QueryText(NameWindow, NameField)
+	if len(Name)==0:
+		GemRB.SetButtonState(NameWindow, DoneButton, IE_GUI_BUTTON_DISABLED)
+	else:
+		GemRB.SetButtonState(NameWindow, DoneButton, IE_GUI_BUTTON_ENABLED)
+	return
