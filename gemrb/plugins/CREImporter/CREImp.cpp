2c866c5b1fe9a28d0ab5d0a717e6a4a6f8d79799
Balrog994 <Balrog994@yahoo.com>
2003-11-25 18:58:39 +0000

    Added Feature: CREImp.cpp : Now CREImp initializes correctly the DrawCircle variable
    svn-id: r702

--- gemrb/plugins/CREImporter/CREImp.cpp
+++ gemrb/plugins/CREImporter/CREImp.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/CREImporter/CREImp.cpp,v 1.7 2003/11/25 13:48:02 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/CREImporter/CREImp.cpp,v 1.8 2003/11/25 18:58:39 balrog994 Exp $
  *
  */
 
@@ -244,6 +244,8 @@ Actor * CREImp::GetActor()
 		break;
 	}
 	act->SetAnimationID(act->BaseStats[IE_ANIMATION_ID]);
+	if(act->anims)
+		act->anims->DrawCircle = false;
 
 	return act;
 }
