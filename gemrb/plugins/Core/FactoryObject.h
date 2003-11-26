e682871204c84c6e50e438b00b62c55cc601f868
Balrog994 <Balrog994@yahoo.com>
2003-11-26 18:14:50 +0000

    Bug Fixed : FactoryObject.h : Missing 'virtual' on destructor
    svn-id: r751

--- gemrb/plugins/Core/FactoryObject.h
+++ gemrb/plugins/Core/FactoryObject.h
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/FactoryObject.h,v 1.2 2003/11/25 13:48:02 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/FactoryObject.h,v 1.3 2003/11/26 18:14:50 balrog994 Exp $
  *
  */
 
@@ -42,7 +42,7 @@ public:
 	SClass_ID SuperClassID;
 	char ResRef[9];
 	FactoryObject(const char * ResRef, SClass_ID SuperClassID);
-	~FactoryObject(void);
+	virtual ~FactoryObject(void);
 };
 
 #endif
