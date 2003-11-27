b7228023d142aec08adb66bffc5eddd3faea950b
Balrog994 <Balrog994@yahoo.com>
2003-11-27 22:20:49 +0000

    Added Feature : Polygon.cpp / .h : First Import
    svn-id: r784

---
+++ gemrb/plugins/Core/Polygon.cpp
@@ -0,0 +1,60 @@
+#include "../../includes/win32def.h"
+#include "Polygon.h"
+#include "Interface.h"
+
+extern Interface * core;
+
+Gem_Polygon::Gem_Polygon(Point * points, int count, bool precalculate, Color * color)
+{
+	this->points = (Point*)malloc(count*sizeof(Point));
+	memcpy(this->points, points, count*sizeof(Point));
+	this->count = count;
+	if(precalculate) {
+		fill = core->GetVideoDriver()->PrecalculatePolygon(points, count, *color);
+	}
+	else
+		fill = NULL;
+}
+
+Gem_Polygon::~Gem_Polygon(void)
+{
+	free(points);
+	if(fill)
+		core->GetVideoDriver()->FreeSprite(fill);
+}
+
+bool Gem_Polygon::PointIn(unsigned short x, unsigned short y)
+{
+	register int   j, yflag0, yflag1, inside_flag, xflag0 , index;
+	register long ty, tx;
+	Point *vtx0, *vtx1;
+
+	index = 0;
+
+	tx = x;
+	ty = y;
+
+	vtx0 = &points[count-1];
+	yflag0 = ( vtx0->y >= ty );
+	vtx1 = &points[index];
+
+	inside_flag = 0;
+
+	for( j = count+1; --j ; ) {
+		yflag1 = ( vtx1->y >= ty );
+		if(yflag0 != yflag1) {
+			xflag0 = ( vtx0->x >= tx );
+			if( xflag0 == ( vtx1->x >= tx ) ) {
+				if( xflag0 ) inside_flag = !inside_flag;
+			} else {
+				if( ( vtx1->x - ( vtx1->y-ty ) * ( vtx0->x-vtx1->x ) / ( vtx0->y-vtx1->y )) >= tx ) {
+					inside_flag = !inside_flag;
+				}
+			}
+		}
+		yflag0 = yflag1;
+		vtx0 = vtx1;
+		vtx1 = &points[++index];
+	}
+	return (bool)inside_flag;
+}
---
+++ gemrb/plugins/Core/Polygon.h
@@ -0,0 +1,36 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+
+#include "Sprite2D.h"
+#include "Region.h"
+#include "../../includes/RGBAColor.h"
+
+typedef struct Point {
+	short x,y;
+} Point;
+
+#ifdef WIN32
+
+#ifdef GEM_BUILD_DLL
+#define GEM_EXPORT __declspec(dllexport)
+#else
+#define GEM_EXPORT __declspec(dllimport)
+#endif
+
+#else
+#define GEM_EXPORT
+#endif
+
+class GEM_EXPORT Gem_Polygon
+{
+public:
+	Gem_Polygon(Point * points, int count, bool precalculate = false, Color * color = NULL);
+	~Gem_Polygon(void);
+	Region BBox;
+	Point *points;
+	int count;
+	Sprite2D * fill;
+	bool PointIn(unsigned short x, unsigned short y);
+};
+
+#endif
