ef23b2b35543115107de4308a8bd8d74e2ae7b2c
Balrog994 <Balrog994@yahoo.com>
2003-11-27 21:58:22 +0000

    Added Feature : WEDImp.cpp / .h : Reading Secondary Header and Doors
    svn-id: r770

--- gemrb/plugins/WEDImporter/WEDImp.cpp
+++ gemrb/plugins/WEDImporter/WEDImp.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/WEDImporter/WEDImp.cpp,v 1.3 2003/11/25 13:47:59 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/WEDImporter/WEDImp.cpp,v 1.4 2003/11/27 21:58:22 balrog994 Exp $
  *
  */
 
@@ -50,7 +50,6 @@ bool WEDImp::Open(DataStream * stream, bool autoFree)
 		printf("[WEDImporter]: This file is not a valid WED File\n");
 		return false;
 	}
-	unsigned long OverlaysCount, DoorsCount, OverlaysOffset, SecHeaderOffset, DoorsOffset, DoorTilesOffset;
 	str->Read(&OverlaysCount, 4);
 	str->Read(&DoorsCount, 4);
 	str->Read(&OverlaysOffset, 4);
@@ -68,6 +67,13 @@ bool WEDImp::Open(DataStream * stream, bool autoFree)
 		str->Read(&o.TILOffset, 4);
 		overlays.push_back(o);
 	}
+	//Reading the Secondary Header
+	str->Seek(SecHeaderOffset, GEM_STREAM_START);
+	str->Read(&WallPolygonsCount, 4);
+	str->Read(&PolygonsOffset, 4);
+	str->Read(&VerticesOffset, 4);
+	str->Read(&WallGroupsOffset, 4);
+	str->Read(&PILTOffset, 4);
 	return true;
 }
 
@@ -86,19 +92,96 @@ TileMap * WEDImp::GetTileMap()
 	for(int y = 0; y < overlays[0].Height; y++) {
 		for(int x = 0; x < overlays[0].Width; x++) {
 			str->Seek(overlays[0].TilemapOffset + (y*overlays[0].Width*10) + (x*10), GEM_STREAM_START);
-			unsigned short	startindex, count;
+			unsigned short	startindex, count, secondary;
+			unsigned char overlaymask;
 			str->Read(&startindex, 2);
 			str->Read(&count, 2);
+			str->Read(&secondary, 2);
+			str->Read(&overlaymask, 1);
 			//TODO: Consider Alternative Tile and Overlay Mask
 			str->Seek(overlays[0].TILOffset + (startindex*2), GEM_STREAM_START);
 			unsigned short *indexes = (unsigned short*)malloc(count*2);
 			str->Read(indexes, count*2);
-			Tile * tile = tis->GetTile(indexes, count);
+			Tile * tile;
+			if(secondary == 0xffff)
+				tile = tis->GetTile(indexes, count);
+			else {
+				tile = tis->GetTile(indexes, 1, &secondary);
+			}
+			tile->om = overlaymask;
 			over->AddTile(tile);
 			free(indexes);
 		}
 	}
 	tm->AddOverlay(over);
+	for(int d = 0; d < DoorsCount; d++) {
+		str->Seek(DoorsOffset + (d*0x1A), GEM_STREAM_START);
+		unsigned short DoorClosed, DoorTileStart, DoorTileCount, *DoorTiles;
+		unsigned short OpenPolyCount, ClosedPolyCount;
+		unsigned long OpenPolyOffset, ClosedPolyOffset;
+		char Name[9];
+		str->Read(Name, 8);
+		Name[8] = 0;
+		str->Read(&DoorClosed, 2);
+		str->Read(&DoorTileStart, 2);
+		str->Read(&DoorTileCount, 2);
+		str->Read(&OpenPolyCount, 2);
+		str->Read(&ClosedPolyCount, 2);
+		str->Read(&OpenPolyOffset, 4);
+		str->Read(&ClosedPolyOffset, 4);
+		//Reading Door Tile Cells
+		str->Seek(DoorTilesOffset + (DoorTileStart*2), GEM_STREAM_START);
+		DoorTiles = (unsigned short*)malloc(DoorTileCount*sizeof(unsigned short));
+		memset(DoorTiles, 0, DoorTileCount*sizeof(unsigned short));
+		str->Read(DoorTiles, DoorTileCount*sizeof(unsigned short));
+		//Reading the Open Polygon
+		str->Seek(OpenPolyOffset, GEM_STREAM_START);
+		unsigned long StartingVertex, VerticesCount;
+		unsigned short BitFlag, MinX, MaxX, MinY, MaxY;
+		str->Read(&StartingVertex, 4);
+		str->Read(&VerticesCount, 4);
+		str->Read(&BitFlag, 2);
+		str->Read(&MinX, 2);
+		str->Read(&MaxX, 2);
+		str->Read(&MinY, 2);
+		str->Read(&MaxY, 2);
+		//Reading Vertices
+		str->Seek(VerticesOffset + (StartingVertex*4), GEM_STREAM_START);
+		Point * points = (Point*)malloc(VerticesCount*sizeof(Point));
+		for(int i = 0; i < VerticesCount; i++) {
+			str->Read(&points[i].x, 2);
+			str->Read(&points[i].y, 2);
+		}
+		Gem_Polygon * open = new Gem_Polygon(points, VerticesCount);
+		free(points);
+		open->BBox.x = MinX;
+		open->BBox.y = MinY;
+		open->BBox.w = MaxX-MinX;
+		open->BBox.h = MaxY-MinY;
+		//Reading the closed Polygon
+		str->Seek(ClosedPolyOffset, GEM_STREAM_START);
+		str->Read(&StartingVertex, 4);
+		str->Read(&VerticesCount, 4);
+		str->Read(&BitFlag, 2);
+		str->Read(&MinX, 2);
+		str->Read(&MaxX, 2);
+		str->Read(&MinY, 2);
+		str->Read(&MaxY, 2);
+		//Reading Vertices
+		str->Seek(VerticesOffset + (StartingVertex*4), GEM_STREAM_START);
+		points = (Point*)malloc(VerticesCount*sizeof(Point));
+		for(int i = 0; i < VerticesCount; i++) {
+			str->Read(&points[i].x, 2);
+			str->Read(&points[i].y, 2);
+		}
+		Gem_Polygon * closed = new Gem_Polygon(points, VerticesCount);
+		free(points);
+		closed->BBox.x = MinX;
+		closed->BBox.y = MinY;
+		closed->BBox.w = MaxX-MinX;
+		closed->BBox.h = MaxY-MinY;
+		tm->AddDoor(Name, DoorClosed, DoorTiles, DoorTileCount, open, closed);
+	}
 	core->FreeInterface(tis);
 	return tm;
 }
--- gemrb/plugins/WEDImporter/WEDImp.h
+++ gemrb/plugins/WEDImporter/WEDImp.h
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/WEDImporter/WEDImp.h,v 1.3 2003/11/25 13:47:59 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/WEDImporter/WEDImp.h,v 1.4 2003/11/27 21:58:22 balrog994 Exp $
  *
  */
 
@@ -38,6 +38,8 @@ private:
 	std::vector<Overlay> overlays;
 	DataStream * str;
 	bool autoFree;
+	unsigned long OverlaysCount, DoorsCount, OverlaysOffset, SecHeaderOffset, DoorsOffset, DoorTilesOffset;
+	unsigned long WallPolygonsCount, PolygonsOffset, VerticesOffset, WallGroupsOffset, PILTOffset;
 public:
 	WEDImp(void);
 	~WEDImp(void);
