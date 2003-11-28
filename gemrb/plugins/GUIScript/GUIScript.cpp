d7aadf82c0de4fdbe12abd7bc00832fe6ee07ed9
Balrog994 <Balrog994@yahoo.com>
2003-11-28 17:28:47 +0000

    Bug Fixed : GUIScript.cpp : Now calling EnterGame sets the current focused object correctly
    svn-id: r799

--- gemrb/plugins/GUIScript/GUIScript.cpp
+++ gemrb/plugins/GUIScript/GUIScript.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/GUIScript/GUIScript.cpp,v 1.73 2003/11/27 22:14:55 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/GUIScript/GUIScript.cpp,v 1.74 2003/11/28 17:28:47 balrog994 Exp $
  *
  */
 
@@ -72,6 +72,7 @@ static PyObject * GemRB_EnterGame(PyObject *, PyObject *args)
 	gc->ControlType = IE_GUI_GAMECONTROL;
 	gamewin->AddControl(gc);
 	core->AddWindow(gamewin);
+	gamewin->SetFocused(gc);
 	core->SetVisible(0, 1);
 	int start = core->LoadTable("STARTARE");
 	TableMgr * tm = core->GetTable(start);
