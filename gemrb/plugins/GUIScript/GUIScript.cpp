a5684bc3d32f320016dce153571d26a027b09659
Avenger <avenger_teambg@users.sourceforge.net>
2003-11-29 22:16:37 +0000

    Fix: oops, fixed the check on area open
    svn-id: r848

--- gemrb/plugins/GUIScript/GUIScript.cpp
+++ gemrb/plugins/GUIScript/GUIScript.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/GUIScript/GUIScript.cpp,v 1.75 2003/11/29 22:08:11 avenger_teambg Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/GUIScript/GUIScript.cpp,v 1.76 2003/11/29 22:16:37 avenger_teambg Exp $
  *
  */
 
@@ -84,7 +84,7 @@ static PyObject * GemRB_EnterGame(PyObject *, PyObject *args)
 	if(!am) {
 		return NULL;
 	}
-	if(am->Open(str, true)) {
+	if(!am->Open(str, true)) {
 		printMessage("GUIScript", "Can't load area.\n", LIGHT_RED);
 		return NULL;
 	}
