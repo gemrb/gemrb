8a2bbbc56f80824553619efea7e2a44e4adfbf3f
Avenger <avenger_teambg@users.sourceforge.net>
2003-11-30 18:11:06 +0000

    Add: dummy GetPixelIndex for MOSImp
    svn-id: r879

--- gemrb/plugins/MOSImporter/MOSImp.h
+++ gemrb/plugins/MOSImporter/MOSImp.h
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/MOSImporter/MOSImp.h,v 1.5 2003/11/26 13:59:41 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/MOSImporter/MOSImp.h,v 1.6 2003/11/30 18:11:06 avenger_teambg Exp $
  *
  */
 
@@ -38,6 +38,11 @@ public:
 	Sprite2D * GetImage();
 	/** No descriptions */
 	void GetPalette(int index, int colors, Color * pal);
+	/** Gets a Pixel index */
+	unsigned long GetPixelIndex(int x, int y)
+	{
+		return 0;
+	}
 	/** Gets a Pixel from the Image */
 	Color GetPixel(int x, int y)
 	{
