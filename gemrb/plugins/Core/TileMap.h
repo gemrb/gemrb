dc5e70560f1761df77c05b196b3c38f120e81958
Balrog994 <Balrog994@yahoo.com>
2003-11-30 00:37:56 +0000

    Added Feature : TileMap.h : Extended InfoPoints to support Info Text
    svn-id: r852

--- gemrb/plugins/Core/TileMap.h
+++ gemrb/plugins/Core/TileMap.h
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/TileMap.h,v 1.8 2003/11/29 20:32:32 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/TileMap.h,v 1.9 2003/11/30 00:37:56 balrog994 Exp $
  *
  */
 
@@ -57,6 +57,9 @@ typedef struct InfoPoint {
 	unsigned short Type;
 	Gem_Polygon * outline;
 	unsigned long Cursor;
+	char * String;
+	unsigned long startDisplayTime;
+	unsigned char textDisplaying;
 } InfoPoint;
 
 #ifdef WIN32
