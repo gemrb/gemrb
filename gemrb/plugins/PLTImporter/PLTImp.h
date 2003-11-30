2ec0f6f24875a3cbb4edaac2cce39f8cf94b3a3e
Avenger <avenger_teambg@users.sourceforge.net>
2003-11-30 18:09:27 +0000

    Add: dummy GetPixelIndex to PLTImp
    svn-id: r876

--- gemrb/plugins/PLTImporter/PLTImp.h
+++ gemrb/plugins/PLTImporter/PLTImp.h
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/PLTImporter/PLTImp.h,v 1.5 2003/11/26 13:59:40 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/PLTImporter/PLTImp.h,v 1.6 2003/11/30 18:09:27 avenger_teambg Exp $
  *
  */
 
@@ -41,6 +41,11 @@ public:
 	Sprite2D * GetImage();
 	/** No descriptions */
 	void GetPalette(int index, int colors, Color * pal);
+	/** Gets a Pixel Index from the Image */
+        unsigned long GetPixelIndex(int x, int y)
+        {
+                return 0;
+        }
 	/** Gets a Pixel from the Image */
 	Color GetPixel(int x, int y)
 	{
