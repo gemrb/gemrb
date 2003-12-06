8c090106c765be4264106a31bc209c206cbfd49a
Balrog994 <Balrog994@yahoo.com>
2003-12-06 17:32:01 +0000

    Added Feature : Interface.h : The Strings interface is now Public
    svn-id: r962

--- gemrb/plugins/Core/Interface.h
+++ gemrb/plugins/Core/Interface.h
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Interface.h,v 1.54 2003/12/04 23:37:27 avenger_teambg Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Interface.h,v 1.55 2003/12/06 17:32:01 balrog994 Exp $
  *
  */
 
@@ -87,7 +87,9 @@ private:
 	PluginMgr * plugin;
 	Video * video;
 	ResourceMgr * key;
+public:
 	StringMgr *strings;
+private:
 	HCAnimationSeq * hcanims;
 	Factory * factory;
 	ImageMgr * pal256;
