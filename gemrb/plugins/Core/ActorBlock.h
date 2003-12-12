c4cc4b035e5687184e4949acb49629c93160d8ac
Balrog994 <Balrog994@yahoo.com>
2003-12-12 23:16:27 +0000

    Added Feature : ActorBlock.h : Moved the ActorBlock Struct in a single file
    svn-id: r1048

---
+++ gemrb/plugins/Core/ActorBlock.h
@@ -0,0 +1,27 @@
+#ifndef ACTORBLOCK_H
+#define ACTORBLOCK_H
+
+#include "Sprite2D.h"
+#include "Actor.h"
+#include "PathFinder.h"
+#include "GameScript.h"
+
+typedef struct ActorBlock {
+	unsigned short XPos, YPos;
+	unsigned short XDes, YDes;
+	unsigned short MinX, MaxX, MinY, MaxY;
+	unsigned char Orientation;
+	unsigned char AnimID;
+	Sprite2D * lastFrame;
+	Actor * actor;
+	bool Selected;
+	PathNode * path;
+	PathNode * step;
+	unsigned long timeStartStep;
+	char * overHeadText;
+	unsigned char textDisplaying;
+	unsigned long timeStartDisplaying;
+	GameScript * Scripts[MAX_SCRIPTS];
+} ActorBlock;
+
+#endif
