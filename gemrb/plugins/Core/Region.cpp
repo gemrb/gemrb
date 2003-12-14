efcdf66cfd04d07f056b6e25d34002f609062aef
Avenger <avenger_teambg@users.sourceforge.net>
2003-12-14 09:17:17 +0000

    Fix: added const to some prototypes, this fixed a compile problem in GlobalTimer::SetScreenShake
    svn-id: r1100

--- gemrb/plugins/Core/Region.cpp
+++ gemrb/plugins/Core/Region.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Region.cpp,v 1.5 2003/11/25 13:48:03 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Region.cpp,v 1.6 2003/12/14 09:17:17 avenger_teambg Exp $
  *
  */
 
@@ -50,7 +50,7 @@ Region::Region(const Region & rgn)
 DEBUG=rgn.DEBUG;
 }
 
-Region & Region::operator=(Region & rgn)
+Region & Region::operator=(const Region & rgn)
 {
 	x = rgn.x;
 	y = rgn.y;
@@ -60,14 +60,14 @@ DEBUG=rgn.DEBUG;
 	return *this;
 }
 
-bool Region::operator==(Region & rgn)
+bool Region::operator==(const Region & rgn)
 {
 	if((x == rgn.x) && (y == rgn.y) && (w == rgn.w) && (h == rgn.h))
 		return true;
 	return false;
 }
 
-bool Region::operator!=(Region & rgn)
+bool Region::operator!=(const Region & rgn)
 {
 	if((x != rgn.x) || (y != rgn.y) || (w != rgn.w) || (h != rgn.h))
 		return true;
--- gemrb/plugins/Core/Region.h
+++ gemrb/plugins/Core/Region.h
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Region.h,v 1.4 2003/11/25 13:48:03 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Region.h,v 1.5 2003/12/14 09:17:17 avenger_teambg Exp $
  *
  */
 
@@ -46,9 +46,9 @@ public:
 int DEBUG;
 	Region(const Region & rgn);
 	//Region(Region & rgn);
-	Region & operator=(Region & rgn);
-	bool operator==(Region & rgn);
-	bool operator!=(Region & rgn);
+	Region & operator=(const Region & rgn);
+	bool operator==(const Region & rgn);
+	bool operator!=(const Region & rgn);
 	Region(int x, int y, int w, int h, int DEBUG=0);
 };
 
