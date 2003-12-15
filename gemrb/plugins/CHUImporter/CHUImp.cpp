ca7f56dd01fdc791c938f81fd82c146837ef566c
Balrog994 <Balrog994@yahoo.com>
2003-12-15 23:52:28 +0000

    Bug Fixed : CHUImp.cpp : Error while casting to (unsigned short)
    svn-id: r1166

--- gemrb/plugins/CHUImporter/CHUImp.cpp
+++ gemrb/plugins/CHUImporter/CHUImp.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/CHUImporter/CHUImp.cpp,v 1.21 2003/12/15 09:37:21 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/CHUImporter/CHUImp.cpp,v 1.22 2003/12/15 23:52:28 balrog994 Exp $
  *
  */
 
@@ -117,7 +117,7 @@ Window * CHUImp::GetWindow(unsigned long i)
 		str->Seek(COffset, GEM_STREAM_START);
 		str->Read(&ControlID, 4);
 		//str->Read(&BufferLength, 2);
-		BufferLength = (unsigned short)(ControlID & 0xffff0000)>>16;
+		BufferLength = (unsigned short)((ControlID & 0xffff0000)>>16);
 		str->Read(&XPos, 2);
 		str->Read(&YPos, 2);
 		str->Read(&Width, 2);
