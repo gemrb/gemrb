9bd7fa02925b74762a3bc508c2092ccf623ed939
Avenger <avenger_teambg@users.sourceforge.net>
2003-12-17 18:38:21 +0000

    Add: SaveGameMgr
    svn-id: r1172

---
+++ gemrb/plugins/Core/SaveGameMgr.h
@@ -0,0 +1,27 @@
+#ifndef SAVEGAMEMGR_H
+#define SAVEGAMEMGR_H
+
+#include "Plugin.h"
+#include "DataStream.h"
+
+#ifdef WIN32
+
+#ifdef GEM_BUILD_DLL
+#define GEM_EXPORT __declspec(dllexport)
+#else
+#define GEM_EXPORT __declspec(dllimport)
+#endif
+
+#else
+#define GEM_EXPORT
+#endif
+
+class GEM_EXPORT SaveGameMgr : public Plugin
+{
+public:
+	SaveGameMgr(void);
+	virtual ~SaveGameMgr(void);
+	virtual bool Open(DataStream * stream, bool autoFree = true) = 0;
+};
+
+#endif
