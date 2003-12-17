ea3da2b89e57c2d3986bd9c8175f610508422714
Avenger <avenger_teambg@users.sourceforge.net>
2003-12-17 17:40:55 +0000

    Add: Empty GAMImporter plugin
    svn-id: r1170

---
+++ gemrb/plugins/GAMImporter/GAMImpCD.cpp
@@ -0,0 +1,50 @@
+/* GemRB - Infinity Engine Emulator
+ * Copyright (C) 2003 The GemRB Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ */
+
+#include "GAMImpCD.h"
+#include "GAMImp.h"
+
+GAMImpCD::GAMImpCD(void)
+{
+}
+
+GAMImpCD::~GAMImpCD(void)
+{
+}
+
+void * GAMImpCD::Create(void)
+{
+	return new GAMImp();
+}
+
+const char * GAMImpCD::ClassName(void)
+{
+	return "GAMImporter";
+}
+
+SClass_ID GAMImpCD::SuperClassID(void)
+{
+	return IE_GAM_CLASS_ID;
+}
+
+Class_ID GAMImpCD::ClassID(void)
+{
+	return Class_ID(0x9a5d7c10, 0xca5a7a43);
+}
