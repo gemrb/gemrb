1c9253d8735e8308d779d3828613cbbdc03abbf9
Balrog994 <Balrog994@yahoo.com>
2003-12-17 19:53:06 +0000

    Added Feature : GAMImp.cpp : Added CVS Header informations
    svn-id: r1175

--- gemrb/plugins/GAMImporter/GAMImp.cpp
+++ gemrb/plugins/GAMImporter/GAMImp.cpp
@@ -15,6 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/GAMImporter/GAMImp.cpp,v 1.2 2003/12/17 19:53:06 balrog994 Exp $
  *
  */
 
