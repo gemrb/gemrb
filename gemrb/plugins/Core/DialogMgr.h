e2592f4caed68e97d6db902f1159116737b7d8fa
Balrog994 <Balrog994@yahoo.com>
2003-12-18 20:04:06 +0000

    First Import : DialogMgr.cpp / .h / Dialog.cpp / .h : Empty Base classes for Dialog Handling
    svn-id: r1188

---
+++ gemrb/plugins/Core/DialogMgr.h
@@ -0,0 +1,50 @@
+/* GemRB - Infinity Engine Emulator
+ * Copyright (C) 2003 The GemRB Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/DialogMgr.h,v 1.1 2003/12/18 20:04:06 balrog994 Exp $
+ *
+ */
+
+#ifndef DIALOGMGR_H
+#define DIALOGMGR_H
+
+#include "Plugin.h"
+#include "DataStream.h"
+#include "Dialog.h"
+
+#ifdef WIN32
+
+#ifdef GEM_BUILD_DLL
+#define GEM_EXPORT __declspec(dllexport)
+#else
+#define GEM_EXPORT __declspec(dllimport)
+#endif
+
+#else
+#define GEM_EXPORT
+#endif
+
+class GEM_EXPORT DialogMgr : public Plugin
+{
+public:
+	DialogMgr(void);
+	virtual ~DialogMgr(void);
+	virtual bool Open(DataStream * stream, bool autoFree = true) = 0;
+	virtual Dialog * GetDialog() = 0;
+};
+
+#endif
