05d708c2d8e5d562641a689b9c690871a281fddf
Avenger <avenger_teambg@users.sourceforge.net>
2003-12-21 18:20:11 +0000

    Add: improved PST character generation script
    svn-id: r1255

--- gemrb/GUIScripts/pst/NewLife.py
+++ gemrb/GUIScripts/pst/NewLife.py
@@ -9,14 +9,19 @@ ConLabel = 0
 WisLabel = 0
 IntLabel = 0
 ChaLabel = 0
+TotLabel = 0
+AcLabel = 0
+HpLabel = 0
 
-TotPoints = 0
 Str = 0
 Dex = 0
 Con = 0
 Wis = 0
 Int = 0
 Cha = 0
+TotPoints = 0
+AcPoints = 0
+HpPoints = 0
 
 IE_SEX =        	35
 IE_HATEDRACE =  	49
@@ -33,7 +38,9 @@ IE_HAIR_COLOR =		214
 IE_ALIGNMENT =  	217
 
 def OnLoad():
-	global NewLifeWindow, TotPoints, Str, Dex, Con, Wis, Int, Cha
+	global NewLifeWindow, TotPoints, AcPoints, HpPoints
+	global Str, Dex, Con, Wis, Int, Cha
+	global TotLabel, AcLabel, HpLabel
 	global StrLabel, DexLabel, ConLabel, WisLabel, IntLabel, ChaLabel
 	global TextArea
 
@@ -46,6 +53,7 @@ def OnLoad():
 	Wis = 9
 	Int = 9
 	Cha = 9
+	TotPoints = 21
 	
 	StrLabel = GemRB.GetControl(NewLifeWindow, 0x10000018)
 	DexLabel = GemRB.GetControl(NewLifeWindow, 0x1000001B)
@@ -66,6 +74,75 @@ def OnLoad():
 	GemRB.SetButtonFlags(NewLifeWindow, Button, IE_GUI_BUTTON_NO_IMAGE, OP_SET)
 	Button = GemRB.GetControl(NewLifeWindow, 7)
 	GemRB.SetButtonFlags(NewLifeWindow, Button, IE_GUI_BUTTON_NO_IMAGE, OP_SET)
+
+	Button = GemRB.GetControl(NewLifeWindow, 8)
+	GemRB.SetButtonFlags(NewLifeWindow, Button, IE_GUI_BUTTON_RADIOBUTTON, OP_SET)
+	GemRB.SetButtonState(NewLifeWindow, Button, IE_GUI_BUTTON_SELECTED)
+	GemRB.SetButtonSprites(NewLifeWindow, Button, "", 0, 0, 0, 0, 0)
+	GemRB.SetText(NewLifeWindow, Button, 5025)
+	GemRB.SetEvent(NewLifeWindow,Button, IE_GUI_BUTTON_ON_PRESS, "PointPress")
+
+	Button = GemRB.GetControl(NewLifeWindow, 9)
+	GemRB.SetButtonFlags(NewLifeWindow, Button, IE_GUI_BUTTON_RADIOBUTTON, OP_SET)
+	GemRB.SetButtonState(NewLifeWindow, Button, IE_GUI_BUTTON_SELECTED)
+	GemRB.SetButtonSprites(NewLifeWindow, Button, "", 0, 0, 0, 0, 0)
+	GemRB.SetText(NewLifeWindow, Button, 5026)
+	GemRB.SetEvent(NewLifeWindow,Button, IE_GUI_BUTTON_ON_PRESS, "AcPress")
+
+	Button = GemRB.GetControl(NewLifeWindow, 10)
+	GemRB.SetButtonFlags(NewLifeWindow, Button, IE_GUI_BUTTON_RADIOBUTTON, OP_SET)
+	GemRB.SetButtonState(NewLifeWindow, Button, IE_GUI_BUTTON_SELECTED)
+	GemRB.SetButtonSprites(NewLifeWindow, Button, "", 0, 0, 0, 0, 0)
+	GemRB.SetText(NewLifeWindow, Button, 5027)
+	GemRB.SetEvent(NewLifeWindow,Button, IE_GUI_BUTTON_ON_PRESS, "HpPress")
+
+	Button = GemRB.GetControl(NewLifeWindow, 11) #str +
+	GemRB.SetEvent(NewLifeWindow, Button, IE_GUI_BUTTON_ON_PRESS, "IncreasePress")
+	GemRB.SetVarAssoc(NewLifeWindow, Button, "Pressed", 0)
+	
+	Button = GemRB.GetControl(NewLifeWindow, 13) #int +
+	GemRB.SetEvent(NewLifeWindow, Button,IE_GUI_BUTTON_ON_PRESS,  "IncreasePress")
+	GemRB.SetVarAssoc(NewLifeWindow, Button, "Pressed", 1)
+	
+	Button = GemRB.GetControl(NewLifeWindow, 15) #wis +
+	GemRB.SetEvent(NewLifeWindow, Button,IE_GUI_BUTTON_ON_PRESS,  "IncreasePress")
+	GemRB.SetVarAssoc(NewLifeWindow, Button, "Pressed", 2)
+	
+	Button = GemRB.GetControl(NewLifeWindow, 17) #dex +
+	GemRB.SetEvent(NewLifeWindow, Button,IE_GUI_BUTTON_ON_PRESS,  "IncreasePress")
+	GemRB.SetVarAssoc(NewLifeWindow, Button, "Pressed", 3)
+	
+	Button = GemRB.GetControl(NewLifeWindow, 19) #con +
+	GemRB.SetEvent(NewLifeWindow, Button,IE_GUI_BUTTON_ON_PRESS, "IncreasePress")
+	GemRB.SetVarAssoc(NewLifeWindow, Button, "Pressed", 4)
+	
+	Button = GemRB.GetControl(NewLifeWindow, 21) #chr +
+	GemRB.SetEvent(NewLifeWindow, Button,IE_GUI_BUTTON_ON_PRESS, "IncreasePress")
+	GemRB.SetVarAssoc(NewLifeWindow, Button, "Pressed", 5)
+	
+	Button = GemRB.GetControl(NewLifeWindow, 12) #str +
+	GemRB.SetEvent(NewLifeWindow, Button,IE_GUI_BUTTON_ON_PRESS, "DecreasePress")
+	GemRB.SetVarAssoc(NewLifeWindow, Button, "Pressed", 0)
+	
+	Button = GemRB.GetControl(NewLifeWindow, 14) #int +
+	GemRB.SetEvent(NewLifeWindow, Button,IE_GUI_BUTTON_ON_PRESS, "DecreasePress")
+	GemRB.SetVarAssoc(NewLifeWindow, Button, "Pressed", 1)
+	
+	Button = GemRB.GetControl(NewLifeWindow, 16) #wis +
+	GemRB.SetEvent(NewLifeWindow, Button,IE_GUI_BUTTON_ON_PRESS, "DecreasePress")
+	GemRB.SetVarAssoc(NewLifeWindow, Button, "Pressed", 2)
+	
+	Button = GemRB.GetControl(NewLifeWindow, 18) #dex +
+	GemRB.SetEvent(NewLifeWindow, Button,IE_GUI_BUTTON_ON_PRESS, "DecreasePress")
+	GemRB.SetVarAssoc(NewLifeWindow, Button, "Pressed", 3)
+	
+	Button = GemRB.GetControl(NewLifeWindow,  20) #con +
+	GemRB.SetEvent(NewLifeWindow, Button,IE_GUI_BUTTON_ON_PRESS, "DecreasePress")
+	GemRB.SetVarAssoc(NewLifeWindow, Button, "Pressed", 4)
+	
+	Button = GemRB.GetControl(NewLifeWindow,  22) #chr +
+	GemRB.SetEvent(NewLifeWindow, Button,IE_GUI_BUTTON_ON_PRESS, "DecreasePress")
+	GemRB.SetVarAssoc(NewLifeWindow, Button, "Pressed", 5)
 	
 	NewLifeLabel = GemRB.GetControl(NewLifeWindow, 0x10000023)
 	GemRB.SetText(NewLifeWindow, NewLifeLabel, 1899)
@@ -73,13 +150,10 @@ def OnLoad():
 	TextArea = GemRB.GetControl(NewLifeWindow, 23)
 	GemRB.SetText(NewLifeWindow, TextArea, 18495)
 	
-	Label = GemRB.GetControl(NewLifeWindow, 0x10000020)
-	GemRB.SetText(NewLifeWindow, Label, 5027)
-	Label = GemRB.GetControl(NewLifeWindow, 0x1000001E)
-	GemRB.SetText(NewLifeWindow, Label, 5025)
-	Label = GemRB.GetControl(NewLifeWindow, 0x1000001F)
-	GemRB.SetText(NewLifeWindow, Label, 5026)
-	
+	TotLabel = GemRB.GetControl(NewLifeWindow, 0x10000020)
+	AcLabel = GemRB.GetControl(NewLifeWindow, 0x1000001E)
+	HpLabel = GemRB.GetControl(NewLifeWindow, 0x1000001F)
+
 	Label = GemRB.GetControl(NewLifeWindow, 0x10000021)
 	GemRB.SetText(NewLifeWindow, Label, 254)
 	
@@ -94,6 +168,7 @@ def OnLoad():
 	
 	CancelButton = GemRB.GetControl(NewLifeWindow, 1)
 	GemRB.SetText(NewLifeWindow, CancelButton, 4196)	
+	GemRB.SetEvent(NewLifeWindow, AcceptButton, IE_GUI_BUTTON_ON_PRESS, "CancelPress")
 	
 	UpdateLabels()
 	
@@ -101,12 +176,25 @@ def OnLoad():
 	return
 	
 def UpdateLabels():
+	global AcPoints, HpPoints
+
 	GemRB.SetText(NewLifeWindow, StrLabel, str(Str))
 	GemRB.SetText(NewLifeWindow, DexLabel, str(Dex))
 	GemRB.SetText(NewLifeWindow, ConLabel, str(Con))
 	GemRB.SetText(NewLifeWindow, WisLabel, str(Wis))
 	GemRB.SetText(NewLifeWindow, IntLabel, str(Int))
 	GemRB.SetText(NewLifeWindow, ChaLabel, str(Cha))
+	GemRB.SetText(NewLifeWindow, TotLabel, str(TotPoints))
+	AcPoints = 10
+	if Dex>14:
+		AcPoints = AcPoints + (Dex-14)
+
+	HpPoints = 20
+	if Con>14:
+		HpPoints = HpPoints + (Con-14)
+
+	GemRB.SetText(NewLifeWindow, AcLabel, str(AcPoints))
+	GemRB.SetText(NewLifeWindow, HpLabel, str(HpPoints))
 	return
 	
 def AcceptPress():
@@ -117,3 +205,95 @@ def AcceptPress():
 	#LETS PLAY!!
 	GemRB.EnterGame()
 	return
+
+def CancelPress():
+	GemRB.UnloadWindow(NewLifeWindow)
+	GemRB.SetNextScript("Start")
+	return
+
+def PointPress():
+	GemRB.SetText(NewLifeWindow, TextArea, 18492)
+	return
+
+def AcPress():
+	GemRB.SetText(NewLifeWindow, TextArea, 18493)
+	return
+
+def HpPress():
+	GemRB.SetText(NewLifeWindow, TextArea, 18494)
+	return
+
+def DecreasePress():
+	global TotPoints
+	global Str, Int, Wis, Dex, Con, Chr
+
+	Pressed = GemRB.GetVar("Pressed")
+	if Pressed == 0:
+		Sum = Str
+	if Pressed == 1:
+		Sum = Int
+	if Pressed == 2:
+		Sum = Wis 
+	if Pressed == 3:
+		Sum = Dex
+	if Pressed == 4:
+		Sum = Con
+	if Pressed == 5:
+		Sum = Chr
+	if Sum<=9:
+		return
+	TotPoints = TotPoints+1
+	Sum = Sum-1
+	if Pressed == 0:
+		Str = Sum
+	if Pressed == 1:
+		Int = Sum
+	if Pressed == 2:
+		Wis = Sum
+	if Pressed == 3:
+		Dex = Sum
+	if Pressed == 4:
+		Con = Sum
+	if Pressed == 5:
+		Chr = Sum
+	UpdateLabels()
+	return
+
+def IncreasePress():
+	global TotPoints
+	global Str, Int, Wis, Dex, Con, Chr
+
+	if TotPoints<=0:
+		return
+	Pressed = GemRB.GetVar("Pressed")
+	if Pressed == 0:
+		Sum = Str
+	if Pressed == 1:
+		Sum = Int
+	if Pressed == 2:
+		Sum = Wis 
+	if Pressed == 3:
+		Sum = Dex
+	if Pressed == 4:
+		Sum = Con
+	if Pressed == 5:
+		Sum = Chr
+	if Sum>=18:
+		return
+	TotPoints = TotPoints-1
+	Sum = Sum+1
+	if Pressed == 0:
+		Str = Sum
+	if Pressed == 1:
+		Int = Sum
+	if Pressed == 2:
+		Wis = Sum
+	if Pressed == 3:
+		Dex = Sum
+	if Pressed == 4:
+		Con = Sum
+	if Pressed == 5:
+		Chr = Sum
+	UpdateLabels()
+	return
+
