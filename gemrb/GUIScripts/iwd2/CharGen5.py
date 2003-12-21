74c4ebeac42344276925ad169ca9da0cfc169c29
Avenger <avenger_teambg@users.sourceforge.net>
2003-12-21 14:39:45 +0000

    Add: chargen4/chargen5 screens
    svn-id: r1253

---
+++ gemrb/GUIScripts/iwd2/CharGen5.py
@@ -0,0 +1,106 @@
+#character generation (GUICG 0)
+import GemRB
+
+CharGenWindow = 0
+TextAreaControl = 0
+
+def OnLoad():
+	global CharGenWindow, TextAreaControl
+
+	GemRB.SetVar("Class",0) #class
+	GemRB.SetVar("Class Kit",0) #class kit
+
+	GemRB.LoadWindowPack("GUICG")
+        CharGenWindow = GemRB.LoadWindow(0)
+	PortraitButton = GemRB.GetControl(CharGenWindow, 12)
+	GemRB.SetButtonFlags(CharGenWindow, PortraitButton, IE_GUI_BUTTON_PICTURE|IE_GUI_BUTTON_NO_IMAGE,OP_SET)
+	PortraitTable = GemRB.LoadTable("pictures")
+	PortraitName = GemRB.GetTableRowName(PortraitTable,GemRB.GetVar("PortraitIndex") )
+	GemRB.SetButtonPicture(CharGenWindow,PortraitButton, PortraitName+"L")
+
+	RaceTable = GemRB.LoadTable("races")
+
+	GenderButton = GemRB.GetControl(CharGenWindow,0)
+	GemRB.SetText(CharGenWindow,GenderButton,11956)
+	GemRB.SetButtonState(CharGenWindow,GenderButton,IE_GUI_BUTTON_DISABLED)
+
+	RaceButton = GemRB.GetControl(CharGenWindow,1)
+	GemRB.SetText(CharGenWindow,RaceButton, 11957)
+	GemRB.SetButtonState(CharGenWindow,RaceButton,IE_GUI_BUTTON_DISABLED)
+
+	ClassButton = GemRB.GetControl(CharGenWindow,2)
+	GemRB.SetText(CharGenWindow,ClassButton, 11959)
+	GemRB.SetButtonState(CharGenWindow,ClassButton,IE_GUI_BUTTON_DISABLED)
+
+	AlignmentButton = GemRB.GetControl(CharGenWindow,3)
+	GemRB.SetText(CharGenWindow,AlignmentButton, 11958)
+	GemRB.SetButtonState(CharGenWindow,AlignmentButton,IE_GUI_BUTTON_DISABLED)
+
+	AbilitiesButton = GemRB.GetControl(CharGenWindow,4)
+	GemRB.SetText(CharGenWindow,AbilitiesButton, 11960)
+	GemRB.SetButtonState(CharGenWindow,AbilitiesButton,IE_GUI_BUTTON_ENABLED)
+
+	SkillButton = GemRB.GetControl(CharGenWindow,5)
+	GemRB.SetText(CharGenWindow,SkillButton, 17372)
+	GemRB.SetButtonState(CharGenWindow,SkillButton,IE_GUI_BUTTON_DISABLED)
+
+	AppearanceButton = GemRB.GetControl(CharGenWindow,6)
+	GemRB.SetText(CharGenWindow,AppearanceButton, 11961)
+	GemRB.SetButtonState(CharGenWindow,AppearanceButton,IE_GUI_BUTTON_DISABLED)
+
+	NameButton = GemRB.GetControl(CharGenWindow,7)
+	GemRB.SetText(CharGenWindow,NameButton, 11963)
+	GemRB.SetButtonState(CharGenWindow,NameButton,IE_GUI_BUTTON_DISABLED)
+
+	BackButton = GemRB.GetControl(CharGenWindow, 11)
+	GemRB.SetText(CharGenWindow, BackButton, 15416)
+	GemRB.SetButtonState(CharGenWindow,BackButton,IE_GUI_BUTTON_ENABLED)
+
+	AcceptButton = GemRB.GetControl(CharGenWindow, 8)
+	GemRB.SetText(CharGenWindow, AcceptButton, 11962)
+	GemRB.SetButtonState(CharGenWindow,AcceptButton,IE_GUI_BUTTON_DISABLED)
+
+	ImportButton = GemRB.GetControl(CharGenWindow, 13)
+	GemRB.SetText(CharGenWindow, ImportButton, 13955)
+	GemRB.SetButtonState(CharGenWindow,ImportButton,IE_GUI_BUTTON_DISABLED)
+
+        CancelButton = GemRB.GetControl(CharGenWindow, 15)
+        GemRB.SetText(CharGenWindow, CancelButton, 36788)
+	GemRB.SetButtonState(CharGenWindow,CancelButton,IE_GUI_BUTTON_ENABLED)
+
+	BiographyButton = GemRB.GetControl(CharGenWindow, 16)
+	GemRB.SetText(CharGenWindow, BiographyButton, 18003)
+	GemRB.SetButtonState(CharGenWindow,BiographyButton,IE_GUI_BUTTON_DISABLED)
+
+	TextAreaControl= GemRB.GetControl(CharGenWindow,9)
+	GemRB.SetText(CharGenWindow, TextAreaControl, 12135)
+	GemRB.TextAreaAppend(CharGenWindow, TextAreaControl,": ")
+	if GemRB.GetVar("Gender") == 1:
+		GemRB.TextAreaAppend(CharGenWindow, TextAreaControl, 1050)
+	else:
+		GemRB.TextAreaAppend(CharGenWindow, TextAreaControl, 1051)
+	GemRB.TextAreaAppend(CharGenWindow, TextAreaControl,1048,-1) # new line
+	GemRB.TextAreaAppend(CharGenWindow, TextAreaControl,": ")
+	GemRB.TextAreaAppend(CharGenWindow, TextAreaControl,GemRB.GetTableValue(RaceTable,GemRB.GetVar("Race")-1,2))
+
+        GemRB.SetEvent(CharGenWindow, CancelButton, IE_GUI_BUTTON_ON_PRESS, "CancelPress")
+        GemRB.SetEvent(CharGenWindow, BackButton, IE_GUI_BUTTON_ON_PRESS, "BackPress")
+        GemRB.SetEvent(CharGenWindow, ClassButton, IE_GUI_BUTTON_ON_PRESS, "NextPress")
+	GemRB.SetVisible(CharGenWindow,1)
+	return
+	
+def NextPress():
+        GemRB.UnloadWindow(CharGenWindow)
+	GemRB.SetNextScript("Class") #class
+	return
+
+def CancelPress():
+        GemRB.UnloadWindow(CharGenWindow)
+        GemRB.SetNextScript("CharGen")
+        return
+
+def BackPress():
+        GemRB.UnloadWindow(CharGenWindow)
+	GemRB.SetNextScript("CharGen2") #appearance
+	return
+
