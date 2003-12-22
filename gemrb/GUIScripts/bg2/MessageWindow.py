283a89ad7ba12ae906c1819730401986e2a52409
Balrog994 <Balrog994@yahoo.com>
2003-12-22 22:08:03 +0000

    Added Feature : MessageWindow.py on BG2 : Added Message Window
    svn-id: r1275

--- gemrb/GUIScripts/bg2/MessageWindow.py
+++ gemrb/GUIScripts/bg2/MessageWindow.py
@@ -5,18 +5,18 @@ def OnLoad():
 	ActionsWindow = GemRB.LoadWindow(3)
 	PortraitWindow = GemRB.LoadWindow(1)
 	OptionsWindow = GemRB.LoadWindow(0)
-	TopWindow = GemRB.LoadWindow(2)
+	MessageWindow = GemRB.LoadWindow(4)
 	GemRB.SetVar("PortraitWindow", PortraitWindow)
 	GemRB.SetVar("ActionsWindow", ActionsWindow)
 	GemRB.SetVar("OptionsWindow", OptionsWindow)
-	GemRB.SetVar("TopWindow", TopWindow)
+	GemRB.SetVar("MessageWindow", MessageWindow)
 	GemRB.SetVar("PortraitPosition", 2) #Right
-	GemRB.SetVar("ActionsPosition", 1) #Left
-	GemRB.SetVar("OptionsPosition", 0) #Bottom
-	GemRB.SetVar("TopWindow", 3) #Top
+	GemRB.SetVar("ActionsPosition", 4) #BottomAdded
+	GemRB.SetVar("OptionsPosition", 0) #Left
+	GemRB.SetVar("MessagePosition", 4) #BottomAdded
 	
 	GemRB.SetVisible(ActionsWindow, 1)
 	GemRB.SetVisible(PortraitWindow, 1)
 	GemRB.SetVisible(OptionsWindow, 1)
-	GemRB.SetVisible(TopWindow, 1)
+	GemRB.SetVisible(MessageWindow, 1)
 	return
