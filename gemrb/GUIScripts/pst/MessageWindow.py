00d312399dc86bb121a3c9d401e0111ff8d9bf79
Balrog994 <Balrog994@yahoo.com>
2003-12-22 18:58:56 +0000

    Added Feature : MessageWindow.py on PST : Added some other Interface Windows
    svn-id: r1267

--- gemrb/GUIScripts/pst/MessageWindow.py
+++ gemrb/GUIScripts/pst/MessageWindow.py
@@ -1,16 +1,18 @@
 import GemRB
 
-PictureWindow = 0
-
 def OnLoad():
-	global PictureWindow
 	GemRB.LoadWindowPack("GUIMG")
-	TimeWindow = GemRB.LoadWindow(0)
-	PictureWindow = GemRB.LoadWindow(1)
-	ButtonWindow = GemRB.LoadWindow(2)
-	GemRB.SetVar("MessageWindow", PictureWindow)		
+	ActionsWindow = GemRB.LoadWindow(0)
+	PortraitWindow = GemRB.LoadWindow(1)
+	OptionsWindow = GemRB.LoadWindow(2)
+	GemRB.SetVar("PortraitWindow", PortraitWindow)
+	GemRB.SetVar("ActionsWindow", ActionsWindow)
+	GemRB.SetVar("OptionsWindow", OptionsWindow)
+	GemRB.SetVar("PortraitPosition", 1) #Bottom
+	GemRB.SetVar("ActionsPosition", 1) #Bottom
+	GemRB.SetVar("OptionsPosition", 1) #Bottom
 	
-	GemRB.SetVisible(TimeWindow, 1)
-	GemRB.SetVisible(PictureWindow, 1)
-	GemRB.SetVisible(ButtonWindow, 1)
+	GemRB.SetVisible(ActionsWindow, 1)
+	GemRB.SetVisible(PortraitWindow, 1)
+	GemRB.SetVisible(OptionsWindow, 1)
 	return
