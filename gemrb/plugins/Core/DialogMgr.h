fab1cf96101594c11242ead6614c83b22303ceee
Balrog994 <Balrog994@yahoo.com>
2003-12-23 23:43:02 +0000

    Added Feature : DialogMgr.h : Added basic functions for dialog handling
    svn-id: r1305

--- gemrb/plugins/Core/DialogMgr.h
+++ gemrb/plugins/Core/DialogMgr.h
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/DialogMgr.h,v 1.1 2003/12/18 20:04:06 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/DialogMgr.h,v 1.2 2003/12/23 23:43:02 balrog994 Exp $
  *
  */
 
@@ -45,6 +45,7 @@ public:
 	virtual ~DialogMgr(void);
 	virtual bool Open(DataStream * stream, bool autoFree = true) = 0;
 	virtual Dialog * GetDialog() = 0;
+	virtual DialogState * GetDialogState(int index) = 0;
 };
 
 #endif
