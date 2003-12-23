1d75fd3c02e502f665d38c12979026c5ddedcc80
Balrog994 <Balrog994@yahoo.com>
2003-12-23 23:30:46 +0000

    Bug Fixed : MessageWindow.py on IWD2 : Now the MessageWindow works again
    svn-id: r1299

--- gemrb/GUIScripts/iwd2/MessageWindow.py
+++ gemrb/GUIScripts/iwd2/MessageWindow.py
@@ -8,9 +8,15 @@ def OnLoad():
 	GemRB.LoadWindowPack("GUIW08")
 	MessageWindow = GemRB.LoadWindow(22)
 	MessageTextArea = GemRB.GetControl(MessageWindow, 1)
-	#GemRB.SetText(MessageWindow, MessageTextArea, "")
+	PortraitWindow = GemRB.LoadWindow(1)
+	
 	GemRB.SetVar("MessageWindow", MessageWindow)
 	GemRB.SetVar("MessageTextArea", MessageTextArea)
+	GemRB.SetVar("PortraitWindow", PortraitWindow)
+	
+	GemRB.SetVar("MessagePosition", 4)
+	GemRB.SetVar("PortraitPosition", 4)
 	
 	GemRB.SetVisible(MessageWindow, 1)
+	GemRB.SetVisible(PortraitWindow, 1)
 	return
