42a25232934556925cd4f4dc35e94d9a8ecbddb8
Edheldil <edheldil@users.sourceforge.net>
2003-12-29 19:50:35 +0000

    Added auto* support for building Config utility
    svn-id: r1363

--- gemrb/Config/Config.cpp
+++ gemrb/Config/Config.cpp
@@ -1,6 +1,29 @@
-#include "windows.h"
+/* GemRB - Infinity Engine Emulator
+ * Copyright (C) 2003 The GemRB Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/Config/Config.cpp,v 1.3 2003/12/29 19:50:35 edheldil Exp $
+ *
+ */
+
+// Config.cpp : Simple utility to create (and maybe modify) GemRB.cfg
 
 #ifdef WIN32
+#include "windows.h"
+
 HANDLE hConsole;
 #endif
 
