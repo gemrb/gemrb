53ad16642ef2118342749b84333f005f4b1db9af
Balrog994 <Balrog994@yahoo.com>
2004-01-02 00:50:53 +0000

    GUIScript.cpp: - Using the new ActionQueue
    svn-id: r1422

--- gemrb/plugins/GUIScript/GUIScript.cpp
+++ gemrb/plugins/GUIScript/GUIScript.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/GUIScript/GUIScript.cpp,v 1.105 2004/01/01 15:50:40 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/GUIScript/GUIScript.cpp,v 1.106 2004/01/02 00:50:53 balrog994 Exp $
  *
  */
 
@@ -1809,9 +1809,7 @@ static PyObject * GemRB_ExecuteString(PyObject * /*self*/, PyObject *args)
 		printMessage("GUIScript","Syntax Error: ExecuteString(String)\n", LIGHT_RED);
 		return NULL;
 	}
-	core->GetGame()->GetMap(0)->Scripts[0]->ExecuteString(String);
-	core->GetDictionary()->SetAt("MessageText", 13775);	
-	core->GetGUIScriptEngine()->RunFunction("AddMessage");
+	GameScript::ExecuteString(core->GetGame()->GetMap(0), String);
 	Py_INCREF(Py_None);
 	return Py_None;
 }
@@ -1824,7 +1822,7 @@ static PyObject * GemRB_EvaluateString(PyObject * /*self*/, PyObject *args)
 		printMessage("GUIScript","Syntax Error: EvaluateString(String)\n", LIGHT_RED);
 		return NULL;
 	}
-	if(core->GetGame()->GetMap(0)->Scripts[0]->EvaluateString(String))
+	if(GameScript::EvaluateString(core->GetGame()->GetMap(0), String))
 		printf("%s returned True\n", String);
 	else
 		printf("%s returned False\n", String);
