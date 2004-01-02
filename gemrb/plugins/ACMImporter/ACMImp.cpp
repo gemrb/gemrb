9c951c6eaefcea8c0da874c62d657076314e946a
Balrog994 <Balrog994@yahoo.com>
2004-01-02 16:00:40 +0000

    ACMImp.cpp : - Removed an Include
    svn-id: r1435

--- gemrb/plugins/ACMImporter/ACMImp.cpp
+++ gemrb/plugins/ACMImporter/ACMImp.cpp
@@ -15,14 +15,13 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/ACMImporter/ACMImp.cpp,v 1.27 2004/01/02 15:52:26 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/ACMImporter/ACMImp.cpp,v 1.28 2004/01/02 16:00:40 balrog994 Exp $
  *
  */
 
 #include "../../includes/win32def.h"
 #include "../Core/Interface.h"
 #include "ACMImp.h"
-#include "acmsound.h"
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
