b42ce9dc537212bc23813758a0c23bd12b3a8889
Edheldil <edheldil@users.sourceforge.net>
2004-01-05 14:42:11 +0000

    Use FONTDLG instead of TRMTFONT for buttons
    svn-id: r1492

--- gemrb/Config/Config.cpp
+++ gemrb/Config/Config.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/Config/Config.cpp,v 1.4 2004/01/04 00:27:08 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/Config/Config.cpp,v 1.5 2004/01/05 14:42:11 edheldil Exp $
  *
  */
 
@@ -494,7 +494,7 @@ void InitGames()
 		"CARET",			//CursorBAM
 		"",					//GameDataPath
 		"",					//GameOverridePath
-		"TRMTFONT",			//ButtonFont
+		"FONTDLG",			//ButtonFont
 		"",					//GemRBPath
 		"",					//CachePath
 		"",					//GUIScriptsPath
