307b1717576e79ff30b988975bfbb798ba622980
Balrog994 <Balrog994@yahoo.com>
2004-01-07 20:38:36 +0000

    Dialog.cpp / .h : - Added Release Method
    svn-id: r1529

--- gemrb/plugins/Core/Dialog.cpp
+++ gemrb/plugins/Core/Dialog.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Dialog.cpp,v 1.4 2003/12/28 20:37:43 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Dialog.cpp,v 1.5 2004/01/07 20:38:36 balrog994 Exp $
  *
  */
 
@@ -63,9 +63,11 @@ void Dialog::FreeDialogState(DialogState *ds)
 
 void Dialog::FreeDialogString(DialogString *ds)
 {
-	for(int i = 0; i < ds->count; i++) {
-		if(ds->strings[i])
+	for(int i = 0; i < ds->count; i++) { 
+		if(ds->strings[i]) {
+			printf("Freeing String 0x%08X\n", ds->strings[i]);
 			free(ds->strings[i]);
+		}
 	}
 	free(ds->strings);
 	delete(ds);
--- gemrb/plugins/Core/Dialog.h
+++ gemrb/plugins/Core/Dialog.h
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Dialog.h,v 1.3 2003/12/25 23:50:04 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Dialog.h,v 1.4 2004/01/07 20:38:36 balrog994 Exp $
  *
  */
 
@@ -72,6 +72,10 @@ public:
 private:
 	void FreeDialogState(DialogState *ds);
 	void FreeDialogString(DialogString *ds);
+public:
+	void Release() {
+		delete this;
+	}
 };
 
 #endif
