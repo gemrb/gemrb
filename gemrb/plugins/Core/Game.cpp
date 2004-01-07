b51d284728297a28c652d8507b5c95b05f26f611
Balrog994 <Balrog994@yahoo.com>
2004-01-07 22:00:47 +0000

    Game.cpp : - Fixed a Bug in LoadMap function
    svn-id: r1532

--- gemrb/plugins/Core/Game.cpp
+++ gemrb/plugins/Core/Game.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Game.cpp,v 1.9 2004/01/07 20:37:32 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Game.cpp,v 1.10 2004/01/07 22:00:47 balrog994 Exp $
  *
  */
 
@@ -105,7 +105,7 @@ int Game::LoadMap(char *ResRef)
 	if(!newMap)
 		return -1;
 	for(int i = 0; i < PCs.size(); i++) {
-		if(stricmp(NPCs[i]->Area, ResRef) == 0)
+		if(stricmp(PCs[i]->Area, ResRef) == 0)
 			newMap->AddActor(PCs[i]);
 	}
 	for(int i = 0; i < NPCs.size(); i++) {
