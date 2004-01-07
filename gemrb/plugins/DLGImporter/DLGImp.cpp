27e164565bc00f8cf81b204511dd177532828ff5
Balrog994 <Balrog994@yahoo.com>
2004-01-07 20:38:04 +0000

    DLGImp.cpp : - Fixed 2 Memory Leaks
    svn-id: r1528

--- gemrb/plugins/DLGImporter/DLGImp.cpp
+++ gemrb/plugins/DLGImporter/DLGImp.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/DLGImporter/DLGImp.cpp,v 1.2 2003/12/23 23:34:51 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/DLGImporter/DLGImp.cpp,v 1.3 2004/01/07 20:38:04 balrog994 Exp $
  *
  */
 
@@ -139,6 +139,7 @@ DialogString * DLGImp::GetTrigger(int index)
 	str->Read(string, offset.Length);
 	string[offset.Length] = 0;
 	ds->strings = GetStrings(string, ds->count);
+	free(string);
 	return ds;
 }
 
@@ -155,6 +156,7 @@ DialogString * DLGImp::GetAction(int index)
 	str->Read(string, offset.Length);
 	string[offset.Length] = 0;
 	ds->strings = GetStrings(string, ds->count);
+	free(string);
 	return ds;
 }
 
