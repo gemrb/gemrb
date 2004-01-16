39ad4087f8b53f0e7275fa66ffae2c5c0077e4ac
Balrog994 <Balrog994@yahoo.com>
2004-01-16 22:57:26 +0000

    GUIScript.cpp : - Fixed a missing MySelf in EnterGame()
    svn-id: r1565

--- gemrb/plugins/GUIScript/GUIScript.cpp
+++ gemrb/plugins/GUIScript/GUIScript.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/GUIScript/GUIScript.cpp,v 1.109 2004/01/07 20:22:55 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/GUIScript/GUIScript.cpp,v 1.110 2004/01/16 22:57:26 balrog994 Exp $
  *
  */
 
@@ -111,6 +111,7 @@ static PyObject * GemRB_EnterGame(PyObject *, PyObject *args)
 		core->GetGame()->GetMap(0)->AddActor(MyActor);
 		strcpy(MyActor->Area, StartArea);
 		MyActor->FromGame = true;
+		MyActor->MySelf = MyActor;
 	}
 	core->DelTable(start);
 	if(core->ConsolePopped) {
