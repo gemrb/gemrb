125dfb03f90685e5b83e92b4f9b8f49acdb64261
Edheldil <edheldil@users.sourceforge.net>
2004-01-25 13:49:10 +0000

    Set the installation directory as a prefix dir Small fix when run with plain 'sh'
    svn-id: r1604

--- admin/setup.sh
+++ admin/setup.sh
@@ -4,11 +4,13 @@
 #configure script. This script compensates for the fact that GemRB 
 #expects certain files to be in paths relative to GemRB.
 
-if [ "$1" == "" ]; then
+if [ "$1" = "" ]; then
 	echo "Usage: $0 [installation directory]";
 	echo "Example: $0 $HOME/GemRB"
 	exit 1
 else
-	sh configure --bindir=$1/ --datadir=$1/ --libdir=$1/plugins 
-	echo "Configure was invoked as: sh configure --bindir=$1/ --datadir=$1 --libdir=$1/plugins"
+	cmd="sh configure --prefix=$1/ --bindir=$1/ --datadir=$1/ --libdir=$1/plugins"
+	$cmd
+	echo
+	echo "Configure was invoked as: $cmd"
 fi
