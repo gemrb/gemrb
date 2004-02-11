8d1515d7ec7f57cd892563a273a1effda46070bf
Edheldil <edheldil@users.sourceforge.net>
2004-02-11 21:37:58 +0000

    Added header file with definition of primitive types used in IE data structures
    svn-id: r1659

---
+++ gemrb/includes/ie_types.h
@@ -0,0 +1,33 @@
+/* GemRB - Infinity Engine Emulator
+ * Copyright (C) 2003 The GemRB Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/includes/ie_types.h,v 1.1 2004/02/11 21:37:58 edheldil Exp $
+ *
+ */
+
+// ie_types.h : defines data types used to load IE structures
+
+#ifndef IE_TYPES_H
+#define IE_TYPES_H
+
+typedef unsigned char ieByte;
+typedef unsigned short ieWord;
+typedef unsigned long ieDword;
+typedef ieDword ieStrRef; 
+typedef char ieResRef[8];
+
+#endif  //! IE_TYPES_H
