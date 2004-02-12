9572f6d0a65a2a9ce989dc52370633a0769a8e26
Avenger <avenger_teambg@users.sourceforge.net>
2004-02-12 16:38:20 +0000

    Fix: fixed a missing ; in globals.h
    svn-id: r1678

--- gemrb/includes/globals.h
+++ gemrb/includes/globals.h
@@ -83,7 +83,7 @@ typedef unsigned long DWORD;
 GEM_EXPORT bool dir_exists(const char *path);
 GEM_EXPORT int strlench(const char *string, char ch);
 #ifndef HAVE_STRNDUP
-GEM_EXPORT char * strndup(const char * s, int l)
+GEM_EXPORT char * strndup(const char * s, int l);
 #endif
 
 #ifdef WIN32
