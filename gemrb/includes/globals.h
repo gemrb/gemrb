f6dc4bd9ce0c9dc1873dba672d901175ed0b7e4a
Edheldil <edheldil@users.sourceforge.net>
2004-02-19 01:12:18 +0000

    Added IgnoreButtonFrames feature
    svn-id: r1734

--- gemrb/includes/globals.h
+++ gemrb/includes/globals.h
@@ -65,6 +65,7 @@ typedef unsigned long DWORD;
 #define  GF_LOWER_LABEL_TEXT		5 //bg2
 #define  GF_HAS_PARTY_INI               6 //iwd2
 #define  GF_SOUNDFOLDERS                7 //iwd2
+#define  GF_IGNORE_BUTTON_FRAMES        8 // pst?
 
 /////AI global defines
 #define AI_UPDATE_TIME	30
