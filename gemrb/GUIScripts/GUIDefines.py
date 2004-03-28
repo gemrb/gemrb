631a32670ee349ca792a723cffad8ff453c1dfb6
Edheldil <edheldil@users.sourceforge.net>
2004-03-28 11:20:49 +0000

    Added boilerplate to GUIDefines.py
    svn-id: r1934

--- gemrb/GUIScripts/GUIDefines.py
+++ gemrb/GUIScripts/GUIDefines.py
@@ -1,3 +1,26 @@
+# -*-python-*-
+# GemRB - Infinity Engine Emulator
+# Copyright (C) 2003 The GemRB Project
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GUIScripts/GUIDefines.py,v 1.14 2004/03/28 11:20:49 edheldil Exp $
+
+
+# GUIDefines.py - common definitions of GUI-related constants for GUIScripts
+
 #button flags
 IE_GUI_BUTTON_NORMAL     = 0x00000004   #default button, doesn't stick
 IE_GUI_BUTTON_NO_IMAGE   = 0x00000001
