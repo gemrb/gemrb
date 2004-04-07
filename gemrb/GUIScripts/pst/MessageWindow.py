89719f28ac573844c85c2516e7a46dbd3d67dd05
Edheldil <edheldil@users.sourceforge.net>
2004-04-07 09:47:27 +0000

    Added boilerplate
    svn-id: r1965

--- gemrb/GUIScripts/pst/MessageWindow.py
+++ gemrb/GUIScripts/pst/MessageWindow.py
@@ -1,3 +1,26 @@
+# -*-python-*-
+# GemRB - Infinity Engine Emulator
+# Copyright (C) 2003 The GemRB Project
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GUIScripts/pst/MessageWindow.py,v 1.14 2004/04/07 09:47:27 edheldil Exp $
+
+
+# MessageWindow.py - scripts and GUI for main (walk) window
+
 import GemRB
 
 from GUICommonWindows import *
@@ -147,6 +170,6 @@ def PopulatePortraitWindow ():
 			Button = GemRB.GetControl (Window, i)
 			#GemRB.SetButtonSprites (Window, Button, pic, 0, 0, 1, 0, 0)
 			GemRB.SetButtonBAM (Window, Button, pic, 0, 0, 0)
-			GemRB.SetButtonFlags(Window, Button, IE_GUI_BUTTON_PICTURE, OP_SET)
+			GemRB.SetButtonFlags(Window, Button, IE_GUI_BUTTON_PICTURE | IE_GUI_BUTTON_ANIMATED, OP_SET)
 		else:
 			pass
