11e586cb656a8cebc45b6bb403f06d47907d0f43
Dark-Star <doc_wagon@users.sourceforge.net>
2004-04-13 23:26:23 +0000

    STOImporter/STOImp.cpp: fixed another unsigned-signed-warning
    svn-id: r2009

--- gemrb/plugins/STOImporter/STOImp.cpp
+++ gemrb/plugins/STOImporter/STOImp.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/STOImporter/STOImp.cpp,v 1.2 2004/02/24 22:20:38 balrog994 Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/STOImporter/STOImp.cpp,v 1.3 2004/04/13 23:26:23 doc_wagon Exp $
  *
  */
 
@@ -172,7 +172,7 @@ ieDword* STOImp::GetPurchasedCategories(Store* s)
 	int size = s->PurchasedCategoriesCount * sizeof( ieDword );
 	ieDword* pc = ( ieDword* ) malloc( size );
 
-	for (int i = 0; i < s->PurchasedCategoriesCount; i++) {
+	for (unsigned int i = 0; i < s->PurchasedCategoriesCount; i++) {
 		str->Read( &pc[i], 4 );
 	}
 
