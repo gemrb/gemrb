ffe6af8f8f4bb37bdad0cb6c6a2b13c25f1be586
Avenger <avenger_teambg@users.sourceforge.net>
2004-04-14 12:20:22 +0000

    Fixed a small leak in GemRB.cpp (palette for the fps display wasn't freed)
    svn-id: r2010

--- gemrb/GemRB.cpp
+++ gemrb/GemRB.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GemRB.cpp,v 1.23 2004/03/14 14:29:16 avenger_teambg Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GemRB.cpp,v 1.24 2004/04/14 12:20:22 avenger_teambg Exp $
  *
  */
 
@@ -59,8 +59,7 @@ int main(int argc, char** argv)
 	GetTime(timebase);
 	double frames = 0.0;
 	Region bg( 0, 0, 100, 30 );
-	Color* palette = core->GetVideoDriver()->CreatePalette( fpscolor,
-												fpsblack );
+	Color* palette = core->GetVideoDriver()->CreatePalette( fpscolor, fpsblack );
 	do {
 		if (core->ChangeScript) {
 			core->GetGUIScriptEngine()->LoadScript( core->NextScript );
@@ -81,6 +80,7 @@ int main(int argc, char** argv)
 				( unsigned char * ) fpsstring, palette,
 				IE_FONT_ALIGN_LEFT | IE_FONT_ALIGN_MIDDLE, true );
 	} while (core->GetVideoDriver()->SwapBuffers() == GEM_OK);
+	delete( palette );
 	delete( core );
 	return 0;
 }
