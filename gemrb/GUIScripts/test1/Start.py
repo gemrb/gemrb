46cecc4c4f59c97990369d66b0383c8a55063c01
Avenger <avenger_teambg@users.sourceforge.net>
2004-07-25 11:16:08 +0000

    test1 update
    svn-id: r2204

--- gemrb/GUIScripts/test1/Start.py
+++ gemrb/GUIScripts/test1/Start.py
@@ -2,14 +2,23 @@ import GemRB
 
 def OnLoad():
 	print "Testing basic guiscript functions"
-	print "1. Testing SetToken"
+	#case insensitive on token key, but case sensitive on value
+	print "1. Testing Set/GetToken"
 	GemRB.SetToken("CHARname","Avenger")
-	print "2. Testing GetToken"
 	print "Avenger == ",GemRB.GetToken("charNAME")
-	print "3. Testing SetVariable & GetVariable"
-	GemRB.SetVariable("Strref On",1)
-	print "Strref On was turned to ",GemRB.GetVariable("Strref On")
-
-
+	#case insensitive on key
+	print "2. Testing SetVar & GetVar"
+	GemRB.SetVar("Strref On",1)
+	print "Strref On was turned to ",GemRB.GetVar("STRREF ON")
+	#actually this breaks the script atm
+	#print "3. Testing PlaySound - non-existent resource"
+	#GemRB.PlaySound("nothere")
+	print "4. Testing PlaySound, uncompressed wav"
+	GemRB.PlaySound("alivad12")
+	print "5. Testing PlaySound, compressed wav"
+	GemRB.PlaySound("compress")
+	print "6. Enabling cheatkeys"
+	GemRB.EnableCheatKeys(1)
+	#should quit without error :)
 	print "99. quit"
 	GemRB.Quit()
