e02038bef48a3083118bc1482fa316a1c9f86d4a
Edheldil <edheldil@users.sourceforge.net>
2004-07-26 09:23:12 +0000

    Create default files, check for errors
    svn-id: r2220

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,35 @@
-cp admin/* .
-aclocal
-autoconf
-autoheader
-automake
-./setup.sh $HOME/GemRB
+#!/bin/sh
+
+#export CXXFLAGS="-ggdb -I/usr/local/include"
+#export LDFLAGS="-ggdb -L/usr/local/lib"
+
+if [ "$1" = "" ]; then
+  dest=$HOME/GemRB
+else
+  dest=$1
+  shift
+fi
+
+echo Running libtoolize
+libtoolize || exit 1
+
+echo Running aclocal
+aclocal || exit 1
+
+echo Running autoconf
+autoconf || exit 1
+
+echo Running autoheader
+autoheader || exit 1
+
+echo Running automake
+automake --add-missing || exit 1
+
+
+echo Running configure
+
+cmd="sh configure --prefix=$dest/ --bindir=$dest/ --sysconfdir=$dest/ --datadir=$dest/ --libdir=$dest/plugins/ --disable-subdirs"
+$cmd
+
+echo
+echo "Configure was invoked as: $cmd"
