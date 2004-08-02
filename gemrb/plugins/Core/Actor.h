bf5d371039e3899d3dfd3a53f15139c3da9de178
Avenger <avenger_teambg@users.sourceforge.net>
2004-08-02 22:13:59 +0000

    LongName/ShortName displaying based on TalkCount, if name selector is set to -1 (default) in GetText(..), used in dialogs
    svn-id: r2269

--- gemrb/plugins/Core/Actor.h
+++ gemrb/plugins/Core/Actor.h
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Actor.h,v 1.41 2004/08/02 21:26:54 guidoj Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Actor.h,v 1.42 2004/08/02 22:13:59 avenger_teambg Exp $
  *
  */
 
@@ -207,6 +207,7 @@ public:
 	/** Gets the Character Long Name/Short Name */
 	char* GetName(int which)
 	{
+		if(which==-1) which=!TalkCount;
 		if (which) {
 			return LongName;
 		}
