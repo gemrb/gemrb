dfb1920508339daa797160cb88f59d886df2a963
Avenger <avenger_teambg@users.sourceforge.net>
2004-08-19 12:43:14 +0000

    fixed GUIScript::QuitGame missing param
    svn-id: r2374

--- gemrb/plugins/GUIScript/GUIScript.cpp
+++ gemrb/plugins/GUIScript/GUIScript.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/GUIScript/GUIScript.cpp,v 1.187 2004/08/10 19:55:47 avenger_teambg Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/GUIScript/GUIScript.cpp,v 1.188 2004/08/19 12:43:14 avenger_teambg Exp $
  *
  */
 
@@ -230,7 +230,7 @@ PyDoc_STRVAR( GemRB_QuitGame__doc,
 "Stops the current game.");
 static PyObject* GemRB_QuitGame(PyObject*, PyObject* args)
 {
-	core->QuitGame();
+	core->QuitGame(false);
 	Py_INCREF( Py_None );
 	return Py_None;
 }
