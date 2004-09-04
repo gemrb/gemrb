279d14e636808360fe80215ff35216252aeec784
Avenger <avenger_teambg@users.sourceforge.net>
2004-09-04 15:44:45 +0000

    fixed small glitches with iwd2 loadscreen
    svn-id: r2472

--- gemrb/GUIScripts/iwd2/LoadScreen.py
+++ gemrb/GUIScripts/iwd2/LoadScreen.py
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
-# $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GUIScripts/iwd2/LoadScreen.py,v 1.1 2004/09/04 15:34:50 avenger_teambg Exp $
+# $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GUIScripts/iwd2/LoadScreen.py,v 1.2 2004/09/04 15:44:45 avenger_teambg Exp $
 
 # LoadScreen.py - display Loading screen
 
@@ -45,7 +45,7 @@ def StartLoadScreen ():
 	return
 
 def EndLoadScreen ():
-	Skull = GemRB.GetControl (LoadScreen, 1)
+	Skull = GemRB.GetControl (LoadScreen, 3)
 	GemRB.SetButtonMOS (LoadScreen, Skull, "GTRBPSK2")
 	GemRB.SetVisible (LoadScreen, 1)
 	return
