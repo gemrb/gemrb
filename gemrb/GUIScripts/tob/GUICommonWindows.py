deed3ecc52cb849eca1579564571219737d23290
Avenger <avenger_teambg@users.sourceforge.net>
2004-10-10 14:33:03 +0000

    Printing hp on the portrait's bottom in tob
    svn-id: r2600

--- gemrb/GUIScripts/tob/GUICommonWindows.py
+++ gemrb/GUIScripts/tob/GUICommonWindows.py
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
-# $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GUIScripts/tob/GUICommonWindows.py,v 1.8 2004/10/02 11:05:50 avenger_teambg Exp $
+# $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GUIScripts/tob/GUICommonWindows.py,v 1.9 2004/10/10 14:33:03 avenger_teambg Exp $
 
 
 # GUICommonWindows.py - functions to open common windows in lower part of the screen
@@ -200,7 +200,7 @@ def PopulatePortraitWindow (Window):
 
 		pic = GemRB.GetPlayerPortrait (i+1,1)
 		GemRB.SetButtonPicture(Window, Button, pic)
-		GemRB.SetButtonFlags(Window, Button, IE_GUI_BUTTON_PICTURE, OP_SET)
+		GemRB.SetButtonFlags(Window, Button, IE_GUI_BUTTON_ALIGN_BOTTOM|IE_GUI_BUTTON_PICTURE, OP_SET)
 
 		GemRB.SetButtonBorder (Window, Button, FRAME_PC_SELECTED, 1, 1,
 2, 2, 0, 255, 0, 255)
