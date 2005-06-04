b0dde55da260495d2fb7d742ab45a6e335d28778
Avenger <avenger_teambg@users.sourceforge.net>
2005-06-04 17:10:56 +0000

    guiscript:Fixed ShowModal to not require a subsequent SetVisible(Window, 1) to redraw the window without darkening
    svn-id: r3274

--- gemrb/plugins/Core/Interface.cpp
+++ gemrb/plugins/Core/Interface.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Interface.cpp,v 1.313 2005/05/29 22:16:22 avenger_teambg Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Interface.cpp,v 1.314 2005/06/04 17:10:56 avenger_teambg Exp $
  *
  */
 
@@ -1850,10 +1850,10 @@ int Interface::ShowModal(unsigned short WindowIndex, int Shadow)
 	evntmgr->Clear();
 	SetOnTop( WindowIndex );
 	evntmgr->AddWindow( win );
-	win->Invalidate();
 
 	ModalWindow = NULL;
 	DrawWindows();
+	win->Invalidate();
 
 	Color gray = {
 		0, 0, 0, 128
