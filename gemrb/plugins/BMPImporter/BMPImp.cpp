08799931e7c7e37af7c77abef83feb2dd3553aaa
Avenger <avenger_teambg@users.sourceforge.net>
2005-08-14 17:00:33 +0000

    fixed a possible crasher caused by incorrect colour gradients (didn't fix the bug which corrupts the gradient)
    svn-id: r3445

--- gemrb/plugins/BMPImporter/BMPImp.cpp
+++ gemrb/plugins/BMPImporter/BMPImp.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/BMPImporter/BMPImp.cpp,v 1.20 2005/06/22 15:55:23 avenger_teambg Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/BMPImporter/BMPImp.cpp,v 1.21 2005/08/14 17:00:33 avenger_teambg Exp $
  *
  */
 
@@ -214,6 +214,9 @@ Sprite2D* BMPImp::GetImage()
 /** No descriptions */
 void BMPImp::GetPalette(int index, int colors, Color* pal)
 {
+	if ((unsigned int) index>=Height) {
+		index = 0;
+	}
 	if (BitCount == 24) {
 		unsigned char * p = ( unsigned char * ) pixels;
 		p += PaddedRowLength * index;
