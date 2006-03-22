e6641387583f7a073f60d7b210707ef237412892
Avenger <avenger_teambg@users.sourceforge.net>
2006-03-22 15:34:40 +0000

    added missing damages.h
    svn-id: r3815

---
+++ gemrb/plugins/Core/damages.h
@@ -0,0 +1,19 @@
+//damage types
+#define DAMAGE_ACID     1
+#define DAMAGE_COLD     2
+#define DAMAGE_ELECTRICITY 4
+#define DAMAGE_FIRE     8
+#define DAMAGE_MAGIC 0x100
+#define DAMAGE_MAGICFIRE 0x200
+#define DAMAGE_MAGICCOLD 0x400
+#define DAMAGE_STUNNING 0x800
+
+//damage levels
+#define DL_BLOOD  0
+#define DL_CRITICAL 3
+#define DL_FIRE   4
+#define DL_ELECTRICITY 7
+#define DL_COLD   11
+#define DL_ACID   14
+#define DL_DISINTEGRATE 17
+
