5c01a5359b1d8b402ea9f93ce6da2038f71dd5ce
Avenger <avenger_teambg@users.sourceforge.net>
2006-06-30 06:58:43 +0000

    fixed crasher when an attacker stops attacking
    svn-id: r3925

--- gemrb/plugins/Core/Game.cpp
+++ gemrb/plugins/Core/Game.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Game.cpp,v 1.116 2006/06/26 10:28:54 avenger_teambg Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/Core/Game.cpp,v 1.117 2006/06/30 06:58:43 avenger_teambg Exp $
  *
  */
 
@@ -1107,7 +1107,10 @@ void Game::OutAttack(ieDword globalID)
 	std::vector< ieDword>::iterator idx;
 
 	for(idx=Attackers.begin(); idx!=Attackers.end();idx++) {
-		if (*idx==globalID) Attackers.erase(idx);
+		if (*idx==globalID) {
+			Attackers.erase(idx);
+			break;
+		}
 	}
 }
 
