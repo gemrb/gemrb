83b4c5dfaac28386aca293217ffebcf961287820
Avenger <avenger_teambg@users.sourceforge.net>
2006-12-27 21:26:38 +0000

    fixed border highlights
    svn-id: r4261

--- gemrb/GUIScripts/bg1/GUIINV.py
+++ gemrb/GUIScripts/bg1/GUIINV.py
@@ -16,7 +16,7 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
-#$Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GUIScripts/bg1/GUIINV.py,v 1.28 2006/12/27 21:19:20 avenger_teambg Exp $
+#$Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GUIScripts/bg1/GUIINV.py,v 1.29 2006/12/27 21:26:38 avenger_teambg Exp $
 
 #GUIINV.py - scripts to control inventory windows from GUIINV winpack
 
@@ -122,9 +122,12 @@ def OpenInventoryWindow ():
 			GemRB.SetEvent (Window, Button, IE_GUI_MOUSE_LEAVE_BUTTON, "MouseLeaveSlot")
 
 			GemRB.SetVarAssoc (Window, Button, "ItemButton", slot+1)
+			#keeping 2 in the original place, because it is how
+			#the gui resource has it, but setting the other cycles
+			GemRB.SetButtonSprites (Window, Button, "STONSLOT",0,0,1,2,3)
 			GemRB.SetButtonFont (Window, Button, "NUMBER")
 			GemRB.SetButtonFlags (Window, Button, IE_GUI_BUTTON_ALIGN_RIGHT | IE_GUI_BUTTON_ALIGN_TOP | IE_GUI_BUTTON_PICTURE, OP_OR)
-			GemRB.SetButtonBorder (Window, Button, 0,0,0,0,0,128,128,255,64,0,1)
+			#GemRB.SetButtonBorder (Window, Button, 0,0,0,0,0,128,128,255,64,0,1)
 
 	GemRB.UnhideGUI()
 	GemRB.SetVar ("TopIndex", 0)
