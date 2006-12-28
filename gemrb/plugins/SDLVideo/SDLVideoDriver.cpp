23f876270fe1ecf01dcd79452a1a9d37c146431f
Willem Jan Palenstijn <wjpalenstijn@users.sourceforge.net>
2006-12-28 09:28:15 +0000

    removed some spurious DOS endlines
    svn-id: r4266

--- gemrb/plugins/SDLVideo/SDLVideoDriver.cpp
+++ gemrb/plugins/SDLVideo/SDLVideoDriver.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/SDLVideo/SDLVideoDriver.cpp,v 1.149 2006/12/09 15:00:26 avenger_teambg Exp $
+ * $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/plugins/SDLVideo/SDLVideoDriver.cpp,v 1.150 2006/12/28 09:28:15 wjpalenstijn Exp $
  *
  */
 
