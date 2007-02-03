d7f4f0d5893afd7cf58a33ca40bc856bb6fe34d0
Avenger <avenger_teambg@users.sourceforge.net>
2007-02-03 19:52:05 +0000

    implemented #1650997 for bg2/tob
    svn-id: r4407

--- gemrb/GUIScripts/bg2/GUICommonWindows.py
+++ gemrb/GUIScripts/bg2/GUICommonWindows.py
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
-# $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GUIScripts/bg2/GUICommonWindows.py,v 1.41 2006/12/30 22:29:20 avenger_teambg Exp $
+# $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GUIScripts/bg2/GUICommonWindows.py,v 1.42 2007/02/03 19:52:04 avenger_teambg Exp $
 
 
 # GUICommonWindows.py - functions to open common
@@ -465,6 +465,7 @@ def RunSelectionChangeHandler ():
 	if SelectionChangeHandler:
 		SelectionChangeHandler ()
 
+
 def OpenPortraitWindow (needcontrols):
 	global PortraitWindow
 
@@ -492,6 +493,9 @@ def OpenPortraitWindow (needcontrols):
 	for i in range (PARTY_SIZE):
 		Button = GemRB.GetControl (Window, i)
 		GemRB.SetVarAssoc (Window, Button, "PressedPortrait", i)
+		if (needcontrols):
+			GemRB.SetEvent (Window, Button, IE_GUI_BUTTON_ON_RIGHT_PRESS, "OpenInventoryWindowClick")
+
 		GemRB.SetEvent (Window, Button, IE_GUI_BUTTON_ON_PRESS, "PortraitButtonOnPress")
 		GemRB.SetEvent (Window, Button, IE_GUI_BUTTON_ON_SHIFT_PRESS, "PortraitButtonOnShiftPress")
 		GemRB.SetEvent (Window, Button, IE_GUI_BUTTON_ON_DRAG_DROP, "OnDropItemToPC")
