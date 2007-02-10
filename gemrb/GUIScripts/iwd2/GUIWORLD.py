9eee1fe8e425ccd098e432fc1ec1a7bbfad5bcfb
Avenger <avenger_teambg@users.sourceforge.net>
2007-02-10 17:38:10 +0000

    fixed EndGame in iwd2 (recent bug)
    svn-id: r4442

--- gemrb/GUIScripts/iwd2/GUIWORLD.py
+++ gemrb/GUIScripts/iwd2/GUIWORLD.py
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
-# $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GUIScripts/iwd2/GUIWORLD.py,v 1.11 2007/02/10 14:29:19 avenger_teambg Exp $
+# $Header: /data/gemrb/cvs2svn/gemrb/gemrb/gemrb/GUIScripts/iwd2/GUIWORLD.py,v 1.12 2007/02/10 17:38:10 avenger_teambg Exp $
 
 
 # GUIW.py - scripts to control some windows from GUIWORLD winpack
@@ -342,7 +342,7 @@ def DeathWindow():
 	#no death movie, but music is changed
 	GemRB.LoadMusicPL ("Theme.mus",1)
 	GemRB.HideGUI ()
-	GemRB.SetVar("QuitGame3", 16498)
+	GemRB.SetVar("QuitGame1", 16498)
 	GemRB.SetTimedEvent ("DeathWindowEnd", 10)
 	return
 
@@ -354,7 +354,7 @@ def DeathWindowEnd ():
 
 	#reason for death
 	Label = GemRB.GetControl (Window, 0x0fffffff)
-	strref = GemRB.GetVar ("QuitGame3")
+	strref = GemRB.GetVar ("QuitGame1")
 	GemRB.SetText (Window, Label, strref)
 
 	#load
