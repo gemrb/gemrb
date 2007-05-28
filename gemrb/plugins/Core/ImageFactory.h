9315470fe01ed6cf93cda338b5cd496815d5ecf1
Willem Jan Palenstijn <wjpalenstijn@users.sourceforge.net>
2007-05-28 13:53:41 +0000

    Create ImageFactory so images can be cached by KeyImporter as well. Only used for BMP and in SetButtonPicture at the moment.
    svn-id: r4672

---
+++ gemrb/plugins/Core/ImageFactory.h
@@ -0,0 +1,51 @@
+/* GemRB - Infinity Engine Emulator
+ * Copyright (C) 2007 The GemRB Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id: AnimationFactory.h 4503 2007-02-27 16:43:46Z wjpalenstijn $
+ *
+ */
+
+#ifndef IMAGEFACTORY_H
+#define IMAGEFACTORY_H
+
+#include "FactoryObject.h"
+#include "../../includes/globals.h"
+#include "Sprite2D.h"
+
+#ifdef WIN32
+
+#ifdef GEM_BUILD_DLL
+#define GEM_EXPORT __declspec(dllexport)
+#else
+#define GEM_EXPORT __declspec(dllimport)
+#endif
+
+#else
+#define GEM_EXPORT
+#endif
+
+class GEM_EXPORT ImageFactory : public FactoryObject {
+private:
+	Sprite2D* bitmap;
+public:
+	ImageFactory(const char* ResRef, Sprite2D* bitmap);
+	~ImageFactory(void);
+
+	Sprite2D* GetImage() const;
+};
+
+#endif
