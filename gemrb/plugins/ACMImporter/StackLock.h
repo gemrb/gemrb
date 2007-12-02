f2eabecd2d29c148cc2cb660a65f2f255298cc5d
Willem Jan Palenstijn <wjpalenstijn@users.sourceforge.net>
2007-12-02 15:18:53 +0000

    utility class: StackLock for locking mutexes while object is in scope
    svn-id: r4853

---
+++ gemrb/plugins/ACMImporter/StackLock.h
@@ -0,0 +1,39 @@
+/* GemRB - Infinity Engine Emulator
+ * Copyright (C) 2007 The GemRB Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id$
+ *
+ */
+
+#include "SDL_mutex.h"
+
+// Copied from ScummVM, mutex.h
+
+/**
+ * Auxillary class to (un)lock a mutex on the stack.
+ */
+class StackLock {
+	SDL_mutex* _mutex;
+	const char *_mutexName;
+
+	void lock();
+	void unlock();
+public:
+	StackLock(SDL_mutex* mutex, const char *mutexName = NULL);
+	~StackLock();
+};
+
