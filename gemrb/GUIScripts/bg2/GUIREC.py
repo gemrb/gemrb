8187a902ab746fb39a2a13db8f8e4d93c225110c
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2008-01-19 19:57:13 +0000

    removing a bad line, this is not impletented in bg2
    svn-id: r4993

--- gemrb/GUIScripts/bg2/GUIREC.py
+++ gemrb/GUIScripts/bg2/GUIREC.py
@@ -76,7 +76,6 @@ def OpenRecordsWindow ():
 	#saving the original portrait window
 	OldOptionsWindow = GUICommonWindows.OptionsWindow
 	OptionsWindow = GemRB.LoadWindow(0)
-	MarkMenuButton (OptionsWindow)
 	SetupMenuWindowControls (OptionsWindow, 0, "OpenRecordsWindow")
 	GemRB.SetWindowFrame (OptionsWindow)
 	OldPortraitWindow = GUICommonWindows.PortraitWindow
