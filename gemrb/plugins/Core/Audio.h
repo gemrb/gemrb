f89eabb36992c16a79c5640b1461468da5843863
Willem Jan Palenstijn <wjpalenstijn@users.sourceforge.net>
2008-02-17 17:15:12 +0000

    GPL header; cleanup
    svn-id: r5056

--- gemrb/plugins/Core/Audio.h
+++ gemrb/plugins/Core/Audio.h
@@ -1,3 +1,24 @@
+/* GemRB - Infinity Engine Emulator
+ * Copyright (C) 2003-2004 The GemRB Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id: OpenALAudio.cpp 5035 2008-02-10 23:33:21Z wjpalenstijn $
+ *
+ */
+
 #ifndef AUDIO_H_INCLUDED
 #define AUDIO_H_INCLUDED
 
@@ -25,27 +46,27 @@
 
 class GEM_EXPORT Audio : public Plugin {
 public:
-    Audio(void) ;
-    virtual ~Audio() ;
-    virtual bool Init(void) = 0 ;
-    virtual unsigned int Play(const char* ResRef, int XPos = 0, int YPos = 0, unsigned int flags = GEM_SND_RELATIVE) = 0 ;
-    virtual bool IsSpeaking() = 0 ;
-    virtual AmbientMgr* GetAmbientMgr() { return ambim ; } ;
+    Audio(void);
+    virtual ~Audio();
+    virtual bool Init(void) = 0;
+    virtual unsigned int Play(const char* ResRef, int XPos = 0, int YPos = 0, unsigned int flags = GEM_SND_RELATIVE) = 0;
+    virtual bool IsSpeaking() = 0;
+    virtual AmbientMgr* GetAmbientMgr() { return ambim; };
     virtual void UpdateVolume(unsigned int flags = GEM_SND_VOL_MUSIC | GEM_SND_VOL_AMBIENTS) = 0;
-    virtual bool CanPlay() = 0 ;
-    virtual void ResetMusics() = 0 ;
-    virtual bool Play() = 0 ;
-    virtual bool Stop() = 0 ;
-    virtual int StreamFile(const char* fileName ) = 0 ;
+    virtual bool CanPlay() = 0;
+    virtual void ResetMusics() = 0;
+    virtual bool Play() = 0;
+    virtual bool Stop() = 0;
+    virtual int StreamFile(const char* fileName ) = 0;
     virtual void UpdateListenerPos(int XPos, int YPos ) = 0;
-    virtual void GetListenerPos(int &XPos, int &YPos ) = 0 ;
-    virtual bool ReleaseAmbientStream(int stream, bool HardStop=false ) = 0 ;
+    virtual void GetListenerPos(int &XPos, int &YPos ) = 0;
+    virtual bool ReleaseAmbientStream(int stream, bool HardStop=false ) = 0;
     virtual int SetupAmbientStream( ieWord x, ieWord y, ieWord z,
-                    ieWord gain, bool point ) = 0 ;
+                    ieWord gain, bool point ) = 0;
     virtual int QueueAmbient(int stream, const char* sound) = 0;
-    virtual void SetAmbientStreamVolume(int stream, int volume) = 0 ;
+    virtual void SetAmbientStreamVolume(int stream, int volume) = 0;
 protected:
-    AmbientMgr* ambim ;
+    AmbientMgr* ambim;
 
 };
 
