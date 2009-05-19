3cc024609139204597a8f4daa47451c26052c64e
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2009-05-19 18:48:25 +0000

    plugins-prepare.sh: added cmake support
    svn-id: r6009

--- gemrb/plugins-prepare.sh
+++ gemrb/plugins-prepare.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 #copy all plugins
-cd `dirname $0`/plugins
-ln -sf */.libs/lib*.so .
+dir=$1
+if test -z "$dir"; then
+  cd `dirname $0`/plugins
+else
+  cd "$dir"
+fi
+
+if test -d Core/.libs; then
+  ln -sf */.libs/lib*.so .
+else
+  # cmake; expect to be in the build dir since it is arbitrary
+  if test -z "$dir"; then
+    echo missing dir parameter - pass the path to the build dir
+    exit 1
+  fi
+  cd gemrb/plugins
+  ln -sf */lib*.so .
+fi
 
 #remove the cuckoo's egg
 rm libgemrb_core.so
