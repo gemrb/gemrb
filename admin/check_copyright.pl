cda99e1b6626671716a7e5167063996cdfb8011b
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2009-05-19 10:21:38 +0000

    updated fsf address
    svn-id: r6005

--- admin/check_copyright.pl
+++ admin/check_copyright.pl
@@ -23,10 +23,6 @@ use strict;
 
 my $TGT_DIR = "gemrb";
 
-#
-# TODO: possibly replace the FSF address with:
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
 # TODO: use list of exceptions, i.e. pairs file, boilerplate to use
 # 
 
@@ -47,7 +43,7 @@ my $copyright_cpp = <<EOT;
 
  \\* You should have received a copy of the GNU General Public License
  \\* along with this program; if not, write to the Free Software
- \\* Foundation, Inc\\., 59 Temple Place - Suite 330, Boston, MA  02111\\-1307, USA\\.
+ \\* Foundation, Inc\\., 51 Franklin Street, Fifth Floor, Boston, MA 02111\\-1301, USA\\.
  \\*
  \\* \\\$[I]d:.* \\\$
  \\*
@@ -77,7 +73,7 @@ my $copyright_py = <<EOT;
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc\\., 59 Temple Place \\- Suite 330, Boston, MA  02111\\-1307, USA\\.
+# Foundation, Inc\\., 51 Franklin Street, Fifth Floor, Boston, MA 02111\\-1301, USA\\.
 #
 # \\\$[I]d: .* \\\$
 EOT
--- admin/make_formation.py
+++ admin/make_formation.py
@@ -15,7 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 # This generates a formatio.2da file for GemRB, because it was becoming
 # far too tedious to do this by hand. The output files have pairs of
