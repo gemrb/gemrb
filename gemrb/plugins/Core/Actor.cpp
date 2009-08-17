3cd8c8cf1b618a5ba655404d871e617d64926a87
Alyssa Milburn <fuzzie@users.sourceforge.net>
2009-08-17 16:42:39 +0000

    actions are apparently not cleared on death
    svn-id: r6901

--- gemrb/plugins/Core/Actor.cpp
+++ gemrb/plugins/Core/Actor.cpp
@@ -2650,7 +2650,7 @@ void Actor::Die(Scriptable *killer)
 	game->SelectActor(this, false, SELECT_NORMAL);
 	game->OutAttack(GetID());
 
-	ClearActions();
+	// actions are NOT cleared on death
 	ClearPath();
 	SetModal( 0 );
 	DisplayStringCore(this, VB_DIE, DS_CONSOLE|DS_CONST );
