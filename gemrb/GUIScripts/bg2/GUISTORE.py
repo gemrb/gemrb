4f41c27f2ac01204d2eb8a056a427bd60721d664
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2009-08-26 16:22:59 +0000

    bg2: don't allow selling to temples
    svn-id: r7057

--- gemrb/GUIScripts/bg2/GUISTORE.py
+++ gemrb/GUIScripts/bg2/GUISTORE.py
@@ -224,7 +224,8 @@ def OpenStoreShoppingWindow ():
 
 		Button = Window.GetControl (i+13)
 		Button.SetBorder (0,0,0,0,0,0,0,128,160,0,1)
-		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, "SelectSell")
+		if Store['StoreType'] != 3: # can't sell to temples
+			Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, "SelectSell")
 		Button.SetEvent (IE_GUI_BUTTON_ON_RIGHT_PRESS, "InfoRightWindow")
 
 	# Buy
@@ -243,7 +244,8 @@ def OpenStoreShoppingWindow ():
 		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, "ToBagPressed")
 	else:
 		Button.SetText (13704)
-		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, "SellPressed")
+		if Store['StoreType'] != 3: # can't sell to temples
+			Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, "SellPressed")
 
 	# inactive button
 	Button = Window.GetControl (50)
