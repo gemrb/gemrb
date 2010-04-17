53936c82aaee0755b65caa1ba943e2e5b28b7226
Tom Prince <tom.prince@ualberta.net>
2010-04-17 12:56:06 -0400

    win32: Comment out unused paramaters to DllMain.
    Signed-off-by: Tom Prince <tom.prince@ualberta.net>

--- gemrb/includes/plugindef.h
+++ gemrb/includes/plugindef.h
@@ -51,8 +51,8 @@ Resource* CreateResource(DataStream *str)
 #ifdef WIN32
 #include <windows.h>
 
-BOOL APIENTRY DllMain(HANDLE hModule, DWORD  ul_reason_for_call,
-	LPVOID lpReserved)
+BOOL APIENTRY DllMain(HANDLE /*hModule*/, DWORD  /*ul_reason_for_call*/,
+	LPVOID /*lpReserved*/)
 {
 	return TRUE;
 }
