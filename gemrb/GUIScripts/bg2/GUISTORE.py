4b6a4d3c8c1ac5f724c6b1e5a2b4bae1788c8461
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2010-06-22 18:23:36 +0200

    bg2: fixed bag closing

--- gemrb/GUIScripts/bg2/GUISTORE.py
+++ gemrb/GUIScripts/bg2/GUISTORE.py
@@ -87,6 +87,7 @@ def CloseWindows ():
 
 def CloseStoreWindow ():
 	import GUICommonWindows
+	import GUIINV
 	global StoreWindow, ActionWindow, PortraitWindow
 	global OldPortraitWindow
 
@@ -102,7 +103,7 @@ def CloseStoreWindow ():
 	GemRB.LeaveStore ()
 	GUICommonWindows.PortraitWindow = OldPortraitWindow
 	if Inventory:
-		GemRB.RunEventHandler("OpenInventoryWindow")
+		GUIINV.OpenInventoryWindow ()
 	else:
 		GUICommon.GameWindow.SetVisible(WINDOW_VISIBLE) #enabling the game control screen
 		GemRB.UnhideGUI () #enabling the other windows
