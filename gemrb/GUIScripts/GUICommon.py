gemrb/gemrb

commit 232be72debcca97772d88f3d5eb98e4016121109

bg2: removed two now unneeded snippets for spell learning

gemrb/GUIScripts/GUICommon.py
@@ -327,11 +327,7 @@ def AddClassAbilities (pc, table, Level=1, LevelDiff=1, align=-1):
 				if ab[:2] == "AP":
 					GemRB.ApplySpell (pc, ab[3:])
 				elif ab[:2] == "GA":
-					SpellIndex = HasSpell (pc, IE_SPELL_TYPE_INNATE, 0, ab[3:])
-					if SpellIndex < 0: # don't know it yet
-						GemRB.LearnSpell (pc, ab[3:], LS_MEMO)
-					else: # memorize another one
-						GemRB.MemorizeSpell (pc, IE_SPELL_TYPE_INNATE, 0, SpellIndex)
+					GemRB.LearnSpell (pc, ab[3:], LS_MEMO)
 				else:
 					print "ERROR, unknown class ability (type): ", ab
 

gemrb/GUIScripts/bg2/CharGenEnd.py
@@ -49,10 +49,6 @@ def OnLoad():
 	LUCommon.SetupLore (MyChar)
 	LUCommon.SetupHP (MyChar)
 
-	# no table for innates, so we make it a high value
-	# to guarantee innate, class ability and HLA memorization works
-	GemRB.SetMemorizableSpellsCount (MyChar, 50, IE_SPELL_TYPE_INNATE, 0)
-
 	# mage spells
 	TableName = CommonTables.ClassSkills.GetValue (Class, 2, 0)
 	if TableName != "*":

commit 505ccb5185ea51426d660126dea13948983d42de

fxied AddClassAbilities to properly memorize already known spells fixes a bug and a regression from 232be72deb

gemrb/GUIScripts/GUICommon.py
@@ -327,7 +327,15 @@ def AddClassAbilities (pc, table, Level=1, LevelDiff=1, align=-1):
 				if ab[:2] == "AP":
 					GemRB.ApplySpell (pc, ab[3:])
 				elif ab[:2] == "GA":
-					GemRB.LearnSpell (pc, ab[3:], LS_MEMO)
+					SpellIndex = HasSpell (pc, IE_SPELL_TYPE_INNATE, 0, ab[3:])
+					if SpellIndex == -1:
+						GemRB.LearnSpell (pc, ab[3:], LS_MEMO)
+					else:
+						# make room for one more memorization
+						max_mem_cnt = GemRB.GetMemorizableSpellsCount (pc, IE_SPELL_TYPE_INNATE, 0, 0)
+						GemRB.SetMemorizableSpellsCount (pc, max_mem_cnt+1, IE_SPELL_TYPE_INNATE, 0)
+						# memorize another spell instance
+						GemRB.MemorizeSpell (pc, IE_SPELL_TYPE_INNATE, 0, SpellIndex)
 				else:
 					print "ERROR, unknown class ability (type): ", ab
 