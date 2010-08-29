d4ed0f4da72cb9b09b25a2d2b78e6e9ce7fb82d6
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2010-08-29 18:14:20 +0200

    SetupWishCore: fixed table lookup being out of bounds for the wisest pcs

--- gemrb/core/GameScript/GSUtils.cpp
+++ gemrb/core/GameScript/GSUtils.cpp
@@ -2260,7 +2260,7 @@ retry:
 	for (i = 0; i < picks; i++) {
 		if (selects[i]<0)
 			continue;
-		int spnum = atoi( tm->QueryField( selects[i], column ) );
+		int spnum = atoi( tm->QueryField( selects[i]-1, column-1 ) );
 		snprintf(varname,32,"wishpower%02d", spnum);
 		SetVariable(Sender, varname, "GLOBAL",1);
 	}
