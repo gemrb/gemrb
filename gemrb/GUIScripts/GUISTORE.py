gemrb/gemrb

commit f95b2129ba39f5228a732b0c3af1ec5008095761

adjust the price too  (TODO: change the amount)

gemrb/GUIScripts/GUISTORE.py
@@ -1105,6 +1105,7 @@ def SetupItems (pc, Slot, Button, Label, i, type, idx, steal=0):
 		Button.SetFlags (IE_GUI_BUTTON_NO_IMAGE, OP_OR)
 		Button.SetFlags (IE_GUI_BUTTON_PICTURE, OP_NAND)
 		Label.SetText ("")
+		Button.SetText ("")
 	else:
 		LeftTopIndex = idx[0]
 		RightTopIndex = idx[1]
@@ -1179,10 +1180,16 @@ def SetupItems (pc, Slot, Button, Label, i, type, idx, steal=0):
 		else:
 			GemRB.SetToken ("ITEMCOST", str(Price) )
 			Label.SetText (10162)
+	return
 
 def GetRealPrice (pc, mode, Item, Slot):
 	# get the base from the item
 	price = Item['Price']
+
+	# FIXME: actually, the price for wands depends on their charge
+	# how ...
+	if Item['StackAmount']>1:
+		price = price * Slot['Usages0']
 
 	# modifier from store properties (in percent)
 	if mode == "buy":

commit 970007877c279fb6c2c632d9940ea711f08d2888

GUISTORE: fixed identifying regression from f95b2129ba39

gemrb/GUIScripts/GUISTORE.py
@@ -881,7 +881,10 @@ def RedrawStoreIdentifyWindow ():
 			Flags = GemRB.IsValidStoreItem (pc, inventory_slots[TopIndex+i], ITEM_PC)
 			Item = GemRB.GetItem (Slot['ItemResRef'])
 			Button.SetItemIcon (Slot['ItemResRef'], 0)
-			Button.SetText (str(Item['Count']) )
+			if Item['StackAmount'] > 1:
+				Button.SetText (str(Slot['Usages0']))
+			else:
+				Button.SetText ("")
 			Button.SetFlags (IE_GUI_BUTTON_NO_IMAGE, OP_NAND)
 			Button.SetFlags (IE_GUI_BUTTON_PICTURE, OP_OR)
 			if Flags & SHOP_ID: