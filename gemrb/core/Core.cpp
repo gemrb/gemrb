gemrb/gemrb

commit 315b8f2ef04218e0fea80600551cb33192b963ee

fixed variables to be uppercase (PST chokes on lowercase variables)

gemrb/core/Core.cpp
@@ -76,12 +76,12 @@ void strnuprcpy(char* dest, const char *source, int count)
 	*dest=0;
 }
 
-// this one also filters spaces
+// this one also filters spaces, used to copy variables 
 void strnspccpy(char* dest, const char *source, int count)
 {
 	memset(dest,0,count);
 	while(count--) {
-		char c = pl_lowercase[(ieByte) *source];
+		char c = pl_uppercase[(ieByte) *source];
 		if (c!=' ') {
 			*dest++=c;
 		}

commit c8fa61fe6b4514e9ce31cc277a2a63a555aa46b8

fix bug with uppercased area names, introduced by 315b8f2e

gemrb/core/Core.cpp
@@ -76,12 +76,16 @@ void strnuprcpy(char* dest, const char *source, int count)
 	*dest=0;
 }
 
-// this one also filters spaces, used to copy variables 
-void strnspccpy(char* dest, const char *source, int count)
+// this one also filters spaces, used to copy resrefs and variables
+void strnspccpy(char* dest, const char *source, int count, bool upper)
 {
 	memset(dest,0,count);
 	while(count--) {
-		char c = pl_uppercase[(ieByte) *source];
+		char c;
+		if (upper)
+			c = pl_uppercase[(ieByte) *source];
+		else
+			c = pl_lowercase[(ieByte) *source];
 		if (c!=' ') {
 			*dest++=c;
 		}

gemrb/core/Variables.cpp
@@ -500,7 +500,8 @@ void Variables::LoadInitialValues(const char* name)
 		// is it the type we want? if not, skip
 		if (strnicmp(buffer, name, 6)) continue;
 		// copy variable (types got 2 extra spaces, and the name is padded too)
-		strnspccpy(varname,buffer+8,32);
+		// (true = uppercase, needed for original engine save compat, see 315b8f2e)
+		strnspccpy(varname,buffer+8,32, true);
 		SetAt(varname, value);
 	}  
 }

gemrb/includes/globals.h
@@ -173,7 +173,7 @@ extern unsigned char pl_lowercase[256];
 
 GEM_EXPORT void strnlwrcpy(char* d, const char *s, int l);
 GEM_EXPORT void strnuprcpy(char* d, const char *s, int l);
-GEM_EXPORT void strnspccpy(char* d, const char *s, int l);
+GEM_EXPORT void strnspccpy(char* d, const char *s, int l, bool upper = false);
 #ifndef HAVE_STRNLEN
 GEM_EXPORT int strnlen(const char* string, int maxlen);
 #endif