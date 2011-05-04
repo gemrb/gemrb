002bc22efd4e029c6f0d7370a8ee7fc50001309b
dhewg <dhewg@wiibrew.org>
2011-05-04 20:57:27 +0200

    CORE: Add a HashMap template
    Based on the Cache.cpp/Dictionary.cpp/Variables.cpp implementation.
    Comes with a hash implementations for std::string and fixed char
    arrays.

---
+++ gemrb/includes/HashMap.h
@@ -0,0 +1,479 @@
+/* GemRB - Infinity Engine Emulator
+ * Copyright (C) 2011 The GemRB Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *
+ */
+
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
+#include <cassert>
+#include <cstring>
+#include <deque>
+
+#include "System/Logging.h"
+
+// dumps stats upon destruction
+//#define HASHMAP_DEBUG
+
+template<typename T>
+struct HashKey {
+	static inline unsigned int hash(const T &key);
+	static inline bool equals(const T &a, const T &b);
+	static inline void copy(T &a, const T &b);
+};
+
+#define HASHMAP_DEFINE_TRIVIAL_HASHKEY(T)			\
+template<>							\
+struct HashKey<T> {						\
+	static inline unsigned int hash(const T &key)		\
+	{							\
+		return static_cast<unsigned int>(key);		\
+	}							\
+	static inline bool equals(const T &a, const T &b)	\
+	{							\
+		return a == b;					\
+	}							\
+	static inline void copy(T &a, const T &b)		\
+	{							\
+		a = b;						\
+	}							\
+}
+
+// MSVC6 is convinced that this the same as char[1] from StringMap.h
+//HASHMAP_DEFINE_TRIVIAL_HASHKEY(char);
+//HASHMAP_DEFINE_TRIVIAL_HASHKEY(signed char);
+//HASHMAP_DEFINE_TRIVIAL_HASHKEY(unsigned char);
+
+HASHMAP_DEFINE_TRIVIAL_HASHKEY(short);
+HASHMAP_DEFINE_TRIVIAL_HASHKEY(unsigned short);
+HASHMAP_DEFINE_TRIVIAL_HASHKEY(int);
+HASHMAP_DEFINE_TRIVIAL_HASHKEY(unsigned int);
+HASHMAP_DEFINE_TRIVIAL_HASHKEY(long);
+HASHMAP_DEFINE_TRIVIAL_HASHKEY(unsigned long);
+
+#undef HASHMAP_DEFINE_TRIVIAL_HASHKEY
+
+template<typename Key, typename Value, typename HashKey = HashKey<Key> >
+class HashMap {
+public:
+	HashMap();
+	~HashMap();
+
+	void init(unsigned int tableSize, unsigned int blockSize);
+	void setDescription(const char *description);
+
+	// sets a value and returns true if an existing entry has been replaced
+	bool set(const Key &key, const Value &value);
+	const Value *get(const Key &key) const;
+	bool has(const Key &key) const;
+
+	bool remove(const Key &key);
+	void clear();
+
+protected:
+	struct Entry {
+		Key key;
+		Value value;
+		Entry *next;
+	};
+
+	HashKey _hash;
+
+	inline bool isInitialized() const;
+	Value *take(const Key &key);
+
+	inline Entry *popAvailable();
+	inline void pushAvailable(Entry *e);
+	inline unsigned int getMapPosByHash(unsigned int hash) const;
+	inline unsigned int getMapPosByKey(const Key &key) const;
+	inline Entry *getBucketByHash(unsigned int hash) const;
+	inline Entry *getBucketByKey(const Key &key) const;
+
+	// walks from e and looks for key. if a match is found, its in
+	// result->next, else result is the end of the list.
+	inline Entry *findPredecessor(const Key &key, Entry *e) const;
+	// just for stats, usable by derived classes
+	inline void incAccesses() const;
+
+private:
+	unsigned int _tableSize;
+	unsigned int _blockSize;
+	std::deque<Entry *> _blocks;
+	Entry **_buckets;
+	Entry *_available;
+
+#ifdef HASHMAP_DEBUG
+	struct Debug {
+		unsigned int allocs;
+		unsigned int accesses;
+	};
+
+	mutable Debug _debug;
+	void dumpStats();
+#endif
+
+	char *_description;
+
+	void allocBlock();
+};
+
+template<typename Key, typename Value, typename HashKey>
+HashMap<Key, Value, HashKey>::HashMap() :
+		_tableSize(0),
+		_blockSize(0),
+		_blocks(),
+		_buckets(NULL),
+		_available(NULL),
+		_description(NULL)
+{
+#ifdef HASHMAP_DEBUG
+	memset(&_debug, 0, sizeof(_debug));
+#endif
+}
+
+template<typename Key, typename Value, typename HashKey>
+HashMap<Key, Value, HashKey>::~HashMap()
+{
+	clear();
+	free(_description);
+}
+
+template<typename Key, typename Value, typename HashKey>
+void HashMap<Key, Value, HashKey>::init(unsigned int tableSize, unsigned int blockSize)
+{
+	clear();
+
+	if (tableSize < 1)
+		return;
+
+	_tableSize = tableSize;
+	if (_tableSize < 16)
+		_tableSize = 16;
+
+	// even sucks for distribution
+	_tableSize |= 1u;
+
+	_blockSize = blockSize;
+	if (_blockSize < 4)
+		_blockSize = 4;
+
+	_buckets = new Entry *[_tableSize];
+	if (!_buckets)
+		error("HashMap", "%s: Out of memory\n", _description);
+
+	memset(_buckets, 0, sizeof(Entry *) * _tableSize);
+
+#ifdef HASHMAP_DEBUG
+	memset(&_debug, 0, sizeof(_debug));
+#endif
+}
+
+template<typename Key, typename Value, typename HashKey>
+void HashMap<Key, Value, HashKey>::setDescription(const char *description)
+{
+	free(_description);
+	_description = strdup(description);
+}
+
+template<typename Key, typename Value, typename HashKey>
+bool HashMap<Key, Value, HashKey>::set(const Key &key, const Value &value)
+{
+	if (!isInitialized())
+		error("HashMap", "%s: Not initialized\n", _description);
+
+	unsigned int p = getMapPosByKey(key);
+	Entry *e;
+
+	// set as root if empty
+	if (!_buckets[p]) {
+		e = popAvailable();
+		_hash.copy(e->key, key);
+		e->value = value;
+
+		_buckets[p] = e;
+
+		return false;
+	}
+
+	e = _buckets[p];
+
+	// check root
+	if (_hash.equals(e->key, key)) {
+		e->value = value;
+		return true;
+	}
+
+	// walk list
+	e = findPredecessor(key, e);
+	Entry *hit = e->next;
+
+	// replace match
+	if (hit) {
+		hit->value = value;
+		return true;
+	}
+
+	// append new
+	hit = popAvailable();
+	_hash.copy(hit->key, key);
+	hit->value = value;
+	e->next = hit;
+
+	return false;
+}
+
+template<typename Key, typename Value, typename HashKey>
+const Value *HashMap<Key, Value, HashKey>::get(const Key &key) const
+{
+	if (!isInitialized())
+		return NULL;
+
+	incAccesses();
+
+	for (Entry *e = getBucketByKey(key); e; e = e->next)
+		if (_hash.equals(e->key, key))
+			return &e->value;
+
+	return NULL;
+}
+
+template<typename Key, typename Value, typename HashKey>
+bool HashMap<Key, Value, HashKey>::has(const Key &key) const
+{
+	return get(key) != NULL;
+}
+
+template<typename Key, typename Value, typename HashKey>
+inline bool HashMap<Key, Value, HashKey>::remove(const Key &key)
+{
+	return take(key) != NULL;
+}
+
+template<typename Key, typename Value, typename HashKey>
+void HashMap<Key, Value, HashKey>::clear()
+{
+	if (!isInitialized())
+		return;
+
+#ifdef HASHMAP_DEBUG
+	dumpStats();
+#endif
+
+	_available = NULL;
+
+	delete[] _buckets;
+	_buckets = NULL;
+
+	while (!_blocks.empty()) {
+		delete[] _blocks.front();
+		_blocks.pop_front();
+	}
+}
+
+template<typename Key, typename Value, typename HashKey>
+bool inline HashMap<Key, Value, HashKey>::isInitialized() const
+{
+	return _buckets != NULL;
+}
+
+template<typename Key, typename Value, typename HashKey>
+Value *HashMap<Key, Value, HashKey>::take(const Key &key)
+{
+	if (!isInitialized())
+		return NULL;
+
+	unsigned int p = getMapPosByKey(key);
+	Entry *e = _buckets[p];
+
+	if (!e)
+		return NULL;
+
+	// check root
+	if (_hash.equals(e->key, key)) {
+		_buckets[p] = e->next;
+		pushAvailable(e);
+
+		return &e->value;
+	}
+
+	// walk the list
+	e = findPredecessor(key, e);
+	Entry *hit = e->next;
+
+	if (!hit)
+		return NULL;
+
+	e->next = hit->next;
+	pushAvailable(hit);
+
+	return &hit->value;
+}
+
+template<typename Key, typename Value, typename HashKey>
+inline typename HashMap<Key, Value, HashKey>::Entry *HashMap<Key, Value, HashKey>::popAvailable()
+{
+	if (!_available)
+		allocBlock();
+
+	Entry *e = _available;
+	_available = e->next;
+
+	e->next = NULL;
+
+	return e;
+}
+
+template<typename Key, typename Value, typename HashKey>
+inline void HashMap<Key, Value, HashKey>::pushAvailable(Entry *e) {
+	e->next = _available;
+	_available = e;
+}
+
+template<typename Key, typename Value, typename HashKey>
+inline unsigned int HashMap<Key, Value, HashKey>::getMapPosByHash(unsigned int hash) const
+{
+	return hash % _tableSize;
+}
+
+template<typename Key, typename Value, typename HashKey>
+inline unsigned int HashMap<Key, Value, HashKey>::getMapPosByKey(const Key &key) const
+{
+	return getMapPosByHash(_hash.hash(key));
+}
+
+template<typename Key, typename Value, typename HashKey>
+inline typename HashMap<Key, Value, HashKey>::Entry *HashMap<Key, Value, HashKey>::getBucketByHash(unsigned int hash) const
+{
+	return _buckets[getMapPosByHash(hash)];
+}
+
+template<typename Key, typename Value, typename HashKey>
+inline typename HashMap<Key, Value, HashKey>::Entry *HashMap<Key, Value, HashKey>::getBucketByKey(const Key &key) const
+{
+	return _buckets[getMapPosByKey(key)];
+}
+
+template<typename Key, typename Value, typename HashKey>
+inline typename HashMap<Key, Value, HashKey>::Entry *HashMap<Key, Value, HashKey>::findPredecessor(const Key &key, Entry *e) const
+{
+	for (; e->next; e = e->next)
+		if (_hash.equals(e->next->key, key))
+			break;
+
+	return e;
+}
+
+template<typename Key, typename Value, typename HashKey>
+inline void HashMap<Key, Value, HashKey>::incAccesses() const
+{
+#ifdef HASHMAP_DEBUG
+	_debug.accesses++;
+#endif
+}
+
+template<typename Key, typename Value, typename HashKey>
+void HashMap<Key, Value, HashKey>::allocBlock()
+{
+	Entry *block = new Entry[_blockSize];
+	if (!block)
+		error("HashMap", "%s: Out of memory\n", _description);
+
+	_blocks.push_back(block);
+
+	for (unsigned int i = 0; i < _blockSize; ++i)
+		pushAvailable(block++);
+
+#ifdef HASHMAP_DEBUG
+	_debug.allocs++;
+#endif
+}
+
+#ifdef HASHMAP_DEBUG
+template<typename Key, typename Value, typename HashKey>
+void HashMap<Key, Value, HashKey>::dumpStats()
+{
+	if (!isInitialized())
+		return;
+
+	unsigned int entries = 0;
+	unsigned int collisions = 0;
+	unsigned int empty = 0;
+	unsigned int eq1 = 0;
+	unsigned int eq2 = 0;
+	unsigned int gt2 = 0;
+	unsigned int gt4 = 0;
+	unsigned int gt8 = 0;
+	unsigned int largest = 0;
+
+	Entry **b = _buckets;
+	for (unsigned int i = 0; i < _tableSize; ++i, ++b) {
+		if (!(*b)) {
+			empty++;
+			continue;
+		}
+
+		unsigned int c = 0;
+		for (Entry *e = *b; e; c++, entries++, e = e->next)
+			;
+
+		if (c > 8)
+			gt8++;
+		else if (c > 4)
+			gt4++;
+		else if (c > 2)
+			gt2++;
+		else if (c > 1)
+			eq2++;
+		else
+			eq1++;
+
+		if (c > 1)
+			collisions += c;
+
+		if (c > largest)
+			largest++;
+	}
+
+	unsigned int bytes = sizeof(*this) +
+		_tableSize * sizeof(Entry *) +
+		_blocks.size() * sizeof(Entry) * _blockSize;
+
+	printMessage("HashMap", "stats for %s:\n"
+			"size\t\t%u\n"
+			"allocs\t\t%u\n"
+			"accesses\t%u\n"
+			"entries\t\t%u\n"
+			"collisions\t%u\n"
+			"empty buckets\t%u\n"
+			"=1 buckets\t%u\n"
+			"=2 buckets\t%u\n"
+			">2 buckets\t%u\n"
+			">4 buckets\t%u\n"
+			">8 buckets\t%u\n"
+			"largest bucket\t%u\n"
+			"memsize\t\t%ukb\n",
+			DEFAULT, _description,
+			_tableSize, _debug.allocs, _debug.accesses,
+			entries, collisions, empty, eq1, eq2, gt2,
+			gt4, gt8, largest, bytes / 1024);
+
+	memset(&_debug, 0, sizeof(_debug));
+}
+#endif
+
+#endif
---
+++ gemrb/includes/StringMap.h
@@ -0,0 +1,127 @@
+/* GemRB - Infinity Engine Emulator
+ * Copyright (C) 2011 The GemRB Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *
+ */
+
+#ifndef STRINGMAP_H
+#define STRINGMAP_H
+
+#include <string>
+
+#include "ie_types.h"
+
+#include "System/String.h"
+#include "HashMap.h"
+
+// Use "StringMap" for mapping std::strings to std::strings.
+// This does not limit the length of either the keys nor values, but at the
+// cost of (re)allocs for each string.
+//
+// Use HashMap<char[size], ...> to use fixed-sized char arrays as keys.
+// This has the advantage that HashMap will alloc the char array as part of
+// its blocks.
+
+template<>
+struct HashKey<std::string> {
+	// hash without std::string construction
+	static inline unsigned int hash(const char *key)
+	{
+		unsigned int h = 0;
+
+		while (*key)
+			h = (h << 5) + h + tolower(*key++);
+
+		return h;
+	}
+
+	static inline unsigned int hash(const std::string &key)
+	{
+		return hash(key.c_str());
+	}
+
+	// equal check without std::string construction
+	static inline bool equals(const std::string &a, const char *b)
+	{
+		return stricmp(a.c_str(), b) == 0;
+	}
+
+	static inline bool equals(const std::string &a, const std::string &b)
+	{
+		return equals(a, b.c_str());
+	}
+
+	static inline void copy(std::string &a, const std::string &b)
+	{
+		a = b;
+	}
+};
+
+class StringMap : public HashMap<std::string, std::string> {
+public:
+	// lookup without std::string construction
+	const std::string *get(const char *key) const
+	{
+		if (!isInitialized())
+			return NULL;
+
+		incAccesses();
+
+		for (Entry *e = getBucketByHash(_hash.hash(key)); e; e = e->next)
+			if (_hash.equals(e->key, key))
+				return &e->value;
+
+		return NULL;
+	}
+
+	// lookup without std::string construction
+	bool has(const char *key) const
+	{
+		return get(key) != NULL;
+	}
+};
+
+// disabled, msvc6 hates it
+#if 0
+template<unsigned int size>
+struct HashKey<char[size]> {
+	typedef char array[size];
+
+	static inline unsigned int hash(const array &key)
+	{
+		unsigned int h = 0;
+		const char *c = key;
+
+		for (unsigned int i = 0; *c && i < size; ++i)
+			h = (h << 5) + h + tolower(*c++);
+
+		return h;
+	}
+
+	static inline bool equals(const array &a, const array &b)
+	{
+		return strnicmp(a, b, size) == 0;
+	}
+
+	static inline void copy(array &a, const array &b)
+	{
+		strncpy(a, b, size);
+	}
+};
+#endif
+
+#endif
