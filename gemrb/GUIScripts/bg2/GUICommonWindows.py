16b7f7b49473dd4a1489b37ca1198ea73877a78b
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2011-07-24 11:56:57 +0200

    Revert "bg2: let dead pcs be visible only in temple type of stores" too unreliable
    This reverts commit 155ca694ef74507a9293807034a9ab7353c1ac2a.
    
    Conflicts:
    
    	gemrb/GUIScripts/bg2/GUICommonWindows.py

--- gemrb/GUIScripts/bg2/GUICommonWindows.py
+++ gemrb/GUIScripts/bg2/GUICommonWindows.py
@@ -750,12 +750,6 @@ def UpdatePortraitWindow ():
 		if Inventory and pc != portid+1:
 			pic = None
 
-		if pic and GemRB.GetPlayerStat(portid+1, IE_STATE_ID) & STATE_DEAD:
-			import GUISTORE
-			# dead pcs are hidden in all stores but temples
-			if GUISTORE.StoreWindow and not GUISTORE.StoreHealWindow:
-				pic = None
-
 		if not pic:
 			Button.SetFlags (IE_GUI_BUTTON_NO_IMAGE, OP_SET)
 			Button.SetState (IE_GUI_BUTTON_DISABLED)
