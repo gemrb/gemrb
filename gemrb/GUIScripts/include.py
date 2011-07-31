217b8218e92186b62ccea0b0edbccb1300c8f3ac
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2011-07-31 21:29:50 +0200

    include.py: added some shorthand forms for easier debugging

--- gemrb/GUIScripts/include.py
+++ gemrb/GUIScripts/include.py
@@ -1,2 +1,9 @@
 # this file is executed at gemrb startup, for the Console
 from ie_stats import *
+
+# /handy/ shorthand forms
+def gps(stat, base=0):
+	print GemRB.GetPlayerStat(GemRB.GameGetFirstSelectedPC(), stat, base)
+
+def sps(stat, value, pcf=1):
+	GemRB.SetPlayerStat(GemRB.GameGetFirstSelectedPC(), stat, value, pcf)
