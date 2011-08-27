c360e8f95b724db45b00190491cc977143dc731e
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2011-08-27 23:32:10 +0200

    include.py: added a few more shorthand helpers

--- gemrb/GUIScripts/include.py
+++ gemrb/GUIScripts/include.py
@@ -7,3 +7,12 @@ def gps(stat, base=0):
 
 def sps(stat, value, pcf=1):
 	GemRB.SetPlayerStat(GemRB.GameGetFirstSelectedPC(), stat, value, pcf)
+
+def mta(area):
+	GemRB.MoveToArea(area)
+
+def cc(cre, px=-1, py=-1):
+	GemRB.CreateCreature(GemRB.GameGetFirstSelectedPC(), cre, px, py)
+
+def ci(item, slot=-1, c0=1, c1=0, c2=0):
+	GemRB.CreateItem(GemRB.GameGetFirstSelectedPC(), item, slot, c0, c1, c2)
