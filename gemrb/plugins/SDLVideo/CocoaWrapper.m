138333fba635dfb398128295f09e5c58ff6bc3f8
Brad Allred <bradallred@me.com>
2011-11-01 22:04:30 -0600

    CocoaWrapper - SDLVideo: add an accessory view to the soft keyboard with a ctrl toggle button. Now cheat keys can be used on iOS.

--- gemrb/plugins/SDLVideo/CocoaWrapper.m
+++ gemrb/plugins/SDLVideo/CocoaWrapper.m
@@ -19,7 +19,88 @@
  */
 #import <SDL.h>
 #import "CocoaWrapper.h"
-#if TARGET_OS_MAC >= 1
+
+// SDL 1.3 has its own App Delegate, but curently all it does is what this delegate category is doing
+// SDL is nice enough to check if a delegate exists prior to assigning its own.
+
+#if TARGET_OS_IPHONE > 0
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface SDL_UIKit_ModifierKeyButton : UIButton {
+@private
+    SDLMod _modifierKey;
+}
+@property(nonatomic, assign, getter = modifierKey, setter = setModifierKey:) SDLMod _modifierKey;
+
+- (void)toggleModifier;
+@end
+
+@implementation SDL_UIKit_ModifierKeyButton
+@synthesize _modifierKey;
+#pragma mark Parent Method Overrides
+
+- (id)initWithFrame:(CGRect)frame
+{
+	self = [super initWithFrame:frame];
+	if (self) {
+		_modifierKey = KMOD_NONE;
+
+		//custom drawing
+		self.backgroundColor = [UIColor colorWithRed:0.196 green:0.3098 blue:0.52 alpha:1.0];
+		self.titleLabel.textColor = [UIColor grayColor];
+		self.titleLabel.font = [UIFont systemFontOfSize:24];
+		self.layer.cornerRadius = 10;
+		self.layer.borderWidth = 2;
+		self.layer.borderColor = [UIColor grayColor].CGColor;
+		self.clipsToBounds = YES;
+
+		[self addTarget:self action:@selector(toggleModifier) forControlEvents:UIControlEventTouchDown];
+	}
+	return self;
+}
+
+- (BOOL)selected
+{
+	return (BOOL)(SDL_GetModState() & _modifierKey);
+}
+
+- (void)setSelected:(BOOL)selected
+{
+	if (selected) SDL_SetModState(SDL_GetModState() | _modifierKey);
+	else SDL_SetModState(SDL_GetModState() & ~_modifierKey);
+}
+
+- (void)toggleModifier
+{
+	[self setSelected:!self.selected];
+}
+
+@end
+
+// need a custom accessory view for the keybaord (for ctrl key modifier)
+@interface UITextField (KeyboardAccesory)
+-(UIView *)inputAccessoryView;// override UIKit and force our own
+@end
+
+@implementation UITextField (KeyboardAccesory)
+- (UIView *)inputAccessoryView
+{
+	static UIView* accessoryView = nil;
+	if (!accessoryView){
+		CGRect accessFrame = CGRectMake(0.0, 0.0, [UIScreen mainScreen].bounds.size.width, 77.0);
+        accessoryView = [[UIView alloc] initWithFrame:accessFrame];
+        accessoryView.backgroundColor = [UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:0.5];
+        SDL_UIKit_ModifierKeyButton *ctrlButton = [[SDL_UIKit_ModifierKeyButton alloc] initWithFrame:CGRectMake(20.0, 20.0, 120.0, 40.0)];
+		ctrlButton.modifierKey = KMOD_LCTRL;
+        [ctrlButton setTitle: @"Ctrl" forState:UIControlStateNormal];
+        [accessoryView addSubview:ctrlButton];
+		[ctrlButton release];
+	}
+    return accessoryView;
+}
+@end
+#elif TARGET_OS_MAC
 @interface CocoaWrapper (SDLTerminate)
 - (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender;
 @end
