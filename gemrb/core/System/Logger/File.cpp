b5d132e584e9c5991b1576ade0698c7f38b553ef
Tom Prince <tom.prince@ualberta.net>
2012-02-15 00:07:29 -0500

    Add file logging.

---
+++ gemrb/core/System/Logger/File.cpp
@@ -0,0 +1,48 @@
+/* GemRB - Infinity Engine Emulator
+ * Copyright (C) 2011 The GemRB Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "System/Logger/File.h"
+
+#include "System/DataStream.h"
+#include "System/Logging.h"
+
+#include <cstdio>
+
+FileLogger::FileLogger(DataStream* log_file)
+	: StdioLogger(false), log_file(log_file)
+{}
+
+FileLogger::~FileLogger()
+{
+	delete log_file;
+}
+
+void FileLogger::vprint(const char *message, va_list ap)
+{
+	// Don't try to be smart.
+	// Assume this is long enough. If not, message will be truncated.
+	// MSVC6 has old vsnprintf that doesn't give length
+	char buff[_MAX_PATH];
+	vsnprintf(buff, _MAX_PATH, message, ap);
+	log_file->Write(buff, strlen(buff));
+}
+
+Logger* createFileLogger(DataStream* log_file)
+{
+	return new FileLogger(log_file);
+}
---
+++ gemrb/core/System/Logger/File.h
@@ -0,0 +1,39 @@
+/* GemRB - Infinity Engine Emulator
+ * Copyright (C) 2003 The GemRB Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef LOGGER_FILE_H
+#define LOGGER_FILE_H
+
+#include "System/Logger/Stdio.h"
+
+class DataStream;
+
+class GEM_EXPORT FileLogger : public StdioLogger {
+public:
+	FileLogger(DataStream*);
+	virtual ~FileLogger();
+
+	void vprint(const char* message, va_list ap);
+
+private:
+	DataStream* log_file;
+};
+
+Logger* createFileLogger(DataStream*);
+
+#endif
