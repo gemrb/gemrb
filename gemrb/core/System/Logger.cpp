6983e90977f79bd0348378c01d3a56814688886e
Tom Prince <tom.prince@ualberta.net>
2012-02-16 17:33:22 -0500

    Remove Apple logger.
    All this does is override display color, which can better be handled
    by passing NOCOLOR to cmake.

--- gemrb/core/System/Logger.cpp
+++ gemrb/core/System/Logger.cpp
@@ -38,11 +38,6 @@ void Logger::destroy()
 #include "System/Logger/Android.h"
 Logger* (*createDefaultLogger)() = createAndroidLogger;
 
-#elif defined (__APPLE__)
-
-#include "System/Logger/Apple.h"
-Logger* (*createDefaultLogger)() = createAppleLogger;
-
 #elif defined(WIN32) && !defined(WIN32_USE_STDIO)
 
 #include "System/Logger/Win32Console.h"
