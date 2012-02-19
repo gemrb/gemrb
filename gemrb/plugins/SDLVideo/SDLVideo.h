be14b86278ee4617057f9ba88071b324b5e3075e
Tom Prince <tom.prince@ualberta.net>
2012-02-19 16:57:10 -0500

    Remove some inline keywords from SDLVideo.
    The keywords don't have any affect. The functions are either
    virtual, in which case they can't be inlined, or the compiler will
    use its own heuristics to inline them anyway.

--- gemrb/plugins/SDLVideo/SDLVideo.h
+++ gemrb/plugins/SDLVideo/SDLVideo.h
@@ -122,9 +122,9 @@ public:
 		const Color& color, bool clipped = true);
 	/** This function Draws a Polygon on the Screen */
 	void DrawPolyline(Gem_Polygon* poly, const Color& color, bool fill = false);
-	inline void DrawHLine(short x1, short y, short x2, const Color& color, bool clipped = false);
-	inline void DrawVLine(short x, short y1, short y2, const Color& color, bool clipped = false);
-	inline void DrawLine(short x1, short y1, short x2, short y2, const Color& color, bool clipped = false);
+	void DrawHLine(short x1, short y, short x2, const Color& color, bool clipped = false);
+	void DrawVLine(short x, short y1, short y2, const Color& color, bool clipped = false);
+	void DrawLine(short x1, short y1, short x2, short y2, const Color& color, bool clipped = false);
 	/** Blits a Sprite filling the Region */
 	void BlitTiled(Region rgn, const Sprite2D* img, bool anchor = false);
 	/** Send a Quit Signal to the Event Queue */
