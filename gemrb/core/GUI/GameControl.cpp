1ecf087dfde7f6e8b82e0f86aad9659bae8cddad
Brad Allred <bradallred@me.com>
2012-08-10 10:41:26 -0600

    GameControl: implement directional arrow for indicating offscreen position of characters when their portrait is hovered.

--- gemrb/core/GUI/GameControl.cpp
+++ gemrb/core/GUI/GameControl.cpp
@@ -562,6 +562,11 @@ void GameControl::Draw(unsigned short x, unsigned short y)
 		}
 	}
 
+	if (lastActorID) {
+		Actor* actor = GetLastActor();
+		DrawArrowMarker(screen, actor->Pos, viewport, green);
+	}
+
 	if (ScreenFlags & SF_DISABLEMOUSE)
 		return;
 	Point p(lastMouseX, lastMouseY);
