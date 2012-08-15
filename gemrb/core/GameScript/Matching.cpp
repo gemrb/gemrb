gemrb/gemrb

commit 58c5d0b9261695da8c369509de28756652a04b1d

moved shared code to IsInvisibleTo and fixed a silly crash on the way

gemrb/core/GameScript/Matching.cpp
@@ -96,21 +96,8 @@ static inline bool DoObjectChecks(Map *map, Scriptable *Sender, Actor *target, i
 		Actor *source = (Actor *)Sender;
 
 		// Detect() ignores invisibility completely
-		if (!ignoreinvis) {
-			// TODO: move this stuff into a shared function so it can be used elsewhere?
-
-			// SEEINVISIBLE skips these checks :-)
-			if (source->Modified[IE_SEEINVISIBLE] == 0) {
-				ieDword state = target->Modified[IE_STATE_ID];
-				// check for invisibility
-				if ((state & STATE_INVISIBLE) != 0) return false;
-				// check for improved invisibility? probably not
-				//if ((state & STATE_INVIS2) != 0) return false;
-			}
-
-			// maybe this should be setting an invis flag?
-			// TODO: should SEEINVISIBLE ignore this? Detect()?
-			if (target->Modified[IE_AVATARREMOVAL]) return false;
+		if (!ignoreinvis && source->IsInvisibleTo(target, 1)) {
+			return false;
 		}
 
 		// visual range check

gemrb/core/Scriptable/Actor.cpp
@@ -4834,16 +4834,7 @@ bool Actor::ValidTarget(int ga_flags, Scriptable *checker) const
 	//scripts can still see this type of actor
 
 	if (ga_flags&GA_NO_HIDDEN) {
-		if (Modified[IE_AVATARREMOVAL]) return false;
-
-		bool seer = false;
-		if (checker->Type == ST_ACTOR) {
-			seer = ((Actor *) checker)->GetSafeStat(IE_SEEINVISIBLE);
-		}
-
-		if ((Modified[IE_EA]>EA_GOODCUTOFF) && (Modified[IE_STATE_ID] & state_invisible) && !seer) {
-			return false;
-		}
+		if (IsInvisibleTo(checker)) return false;
 	}
 
 	if (ga_flags&GA_NO_ALLY) {
@@ -8603,6 +8594,25 @@ int Actor::GetArmorFailure() const
 	}
 
 	return -penalty;
+}
+
+// checks whether the actor is visible to another scriptable
+// if flags is 1, it skips the EA check for STATE_INVISIBLE
+bool Actor::IsInvisibleTo(Scriptable *checker, int flags) const
+{
+	if (Modified[IE_AVATARREMOVAL]) return true;
+
+	bool seer = false;
+	if (checker && checker->Type == ST_ACTOR) {
+		seer = ((Actor *) checker)->GetSafeStat(IE_SEEINVISIBLE);
+	}
+	if (!seer && (Modified[IE_STATE_ID] & state_invisible)) {
+		if ((flags&1) || (Modified[IE_EA]>EA_GOODCUTOFF)) {
+			return true;
+		}
+	}
+
+	return false;
 }
 
 }

gemrb/core/Scriptable/Actor.h
@@ -823,6 +823,7 @@ public:
 	/* returns the armor check penalty */
 	int GetArmorFailure() const;
 	bool IsDead() const;
+	bool IsInvisibleTo(Scriptable *checker, int flags=0) const;
 };
 }
 

commit ec5c58a1e23262cd90b8d20817c1b86187a50cf4

fixed regression from 58c5d0b92, thanks fuzzie

gemrb/core/GameScript/Matching.cpp
@@ -96,7 +96,7 @@ static inline bool DoObjectChecks(Map *map, Scriptable *Sender, Actor *target, i
 		Actor *source = (Actor *)Sender;
 
 		// Detect() ignores invisibility completely
-		if (!ignoreinvis && source->IsInvisibleTo(target)) {
+		if (!ignoreinvis && target->IsInvisibleTo(source)) {
 			return false;
 		}
 