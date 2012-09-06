gemrb/gemrb

commit ca9334719e500f727d0d9af9ad2dc99298d5d4d5

CommonTables: preload aligns.2da

gemrb/GUIScripts/iwd/CharGen.py
@@ -67,7 +67,6 @@ KitDoneButton = 0
 
 AlignmentButton = 0
 AlignmentWindow = 0
-AlignmentTable = 0
 AlignmentTextArea = 0
 AlignmentDoneButton = 0
 
@@ -156,13 +155,12 @@ def OnLoad():
 	global CharGenWindow, CharGenState, TextArea, PortraitButton, AcceptButton
 	global GenderButton, RaceButton, ClassButton, AlignmentButton
 	global AbilitiesButton, SkillsButton, AppearanceButton, BiographyButton, NameButton
-	global KitTable, ProficienciesTable, AlignmentTable, RacialEnemyTable
+	global KitTable, ProficienciesTable, RacialEnemyTable
 	global AbilitiesTable, SkillsTable, PortraitsTable
 	global MyChar, ImportedChar
 
 	KitTable = GemRB.LoadTable ("magesch")
 	ProficienciesTable = GemRB.LoadTable ("weapprof")
-	AlignmentTable = GemRB.LoadTable ("aligns")
 	RacialEnemyTable = GemRB.LoadTable ("haterace")
 	AbilitiesTable = GemRB.LoadTable ("ability")
 	SkillsTable = GemRB.LoadTable ("skills")
@@ -371,7 +369,7 @@ def AcceptPress():
 	LUSkillsSelection.SkillsSave (MyChar)
 
 	TmpTable = GemRB.LoadTable ("repstart")
-	t = AlignmentTable.FindValue (3, t)
+	t = CommonTables.Aligns.FindValue (3, t)
 	t = TmpTable.GetValue (t, 0) * 10
 	GemRB.SetPlayerStat (MyChar, IE_REPUTATION, t)
 	# set the party rep if this in the main char
@@ -474,8 +472,8 @@ def SetCharacterDescription():
 	if CharGenState > 3:
 		TextArea.Append (1049, -1)
 		TextArea.Append (": ")
-		Alignment = AlignmentTable.FindValue (3, GemRB.GetPlayerStat(MyChar, IE_ALIGNMENT) )
-		TextArea.Append (AlignmentTable.GetValue (Alignment, 2) )
+		Alignment = CommonTables.Aligns.FindValue (3, GemRB.GetPlayerStat(MyChar, IE_ALIGNMENT))
+		TextArea.Append (CommonTables.Aligns.GetValue (Alignment, 2))
 	if CharGenState > 4:
 		strextra = GemRB.GetPlayerStat (MyChar, IE_STREXTRA)
 		TextArea.Append ("", -1)
@@ -1223,7 +1221,7 @@ def AlignmentPress():
 		else:
 			AlignmentSelectButton.SetState (IE_GUI_BUTTON_ENABLED)
 		AlignmentSelectButton.SetEvent (IE_GUI_BUTTON_ON_PRESS, AlignmentSelectPress)
-		AlignmentSelectButton.SetText (AlignmentTable.GetValue (i, 0) )
+		AlignmentSelectButton.SetText (CommonTables.Aligns.GetValue (i, 0))
 		AlignmentSelectButton.SetVarAssoc ("Alignment", i + 1)
 
 	AlignmentTextArea = AlignmentWindow.GetControl (11)
@@ -1248,7 +1246,7 @@ def AlignmentSelectPress():
 	global AlignmentWindow, AlignmentTextArea, AlignmentDoneButton
 
 	Alignment = GemRB.GetVar ("Alignment") - 1
-	AlignmentTextArea.SetText (AlignmentTable.GetValue (Alignment, 1))
+	AlignmentTextArea.SetText (CommonTables.Aligns.GetValue (Alignment, 1))
 	AlignmentDoneButton.SetState (IE_GUI_BUTTON_ENABLED)
 	return
 
@@ -1263,7 +1261,7 @@ def AlignmentDonePress():
 	AbilitiesButton.SetState (IE_GUI_BUTTON_ENABLED)
 	AbilitiesButton.SetFlags (IE_GUI_BUTTON_DEFAULT, OP_OR)
 
-	Alignment = AlignmentTable.GetValue (GemRB.GetVar ("Alignment")-1, 3)
+	Alignment = CommonTables.Aligns.GetValue (GemRB.GetVar ("Alignment")-1, 3)
 	GemRB.SetPlayerStat (MyChar, IE_ALIGNMENT, Alignment )
 
 	CharGenState = 4
@@ -2202,7 +2200,7 @@ def PriestSpellsMemorize(SpellTable, Level, SpellLevel):
 
 	CharGenWindow.SetVisible (WINDOW_INVISIBLE)
 	PriestMemorizeWindow = GemRB.LoadWindow (17)
-	t = AlignmentTable.GetValue ( GemRB.GetVar ("Alignment")-1, 3)
+	t = CommonTables.Aligns.GetValue (GemRB.GetVar ("Alignment")-1, 3)
 	Learnable = Spellbook.GetLearnablePriestSpells( ClassFlag, t, SpellLevel)
 
 	MaxSpellsPriestTable = GemRB.LoadTable (SpellTable)

gemrb/GUIScripts/CommonTables.py
@@ -17,17 +17,18 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 # the place for preloading the most commonly used tables
+# helps with code deduplication, reduced log spam and tiny lookup savings
 import GemRB
 from ie_restype import RES_2DA
 
 # these two are only used in SetEncumbranceLabels, but that is called very often
 StrMod = StrModEx = None
 Classes = KitList = ClassSkills = Races = NextLevel = None
-Pdolls = SpellDisplay = None
+Pdolls = SpellDisplay = Aligns = None
 
 def Load():
 	global Classes, KitList, ClassSkills, Races, NextLevel
-	global Pdolls, StrModEx, StrMod, SpellDisplay
+	global Pdolls, StrModEx, StrMod, SpellDisplay, Aligns
 
 	print # so the following output isn't appended to an existing line
 	if not Classes:
@@ -47,3 +48,5 @@ def Load():
 		StrModEx = GemRB.LoadTable ("strmodex")
 	if not SpellDisplay:
 		SpellDisplay = GemRB.LoadTable ("spldisp")
+	if not Aligns and GemRB.HasResource("aligns", RES_2DA):
+		Aligns = GemRB.CommonTables.Aligns

gemrb/GUIScripts/DualClass.py
@@ -383,10 +383,9 @@ def CanDualInto (index):
 
 	# make sure we aren't restricted by alignment
 	AlignmentTable = GemRB.LoadTable ("alignmnt")
-	AlignsTable = GemRB.LoadTable ("aligns")
 	Alignment = GemRB.GetPlayerStat (pc, IE_ALIGNMENT) # our alignment
-	Alignment = AlignsTable.FindValue (3, Alignment)
-	Alignment = AlignsTable.GetValue (Alignment, 4) # convert the alignment
+	Alignment = CommonTables.Aligns.FindValue (3, Alignment)
+	Alignment = CommonTables.Aligns.GetValue (Alignment, 4) # convert the alignment
 	if not AlignmentTable.GetValue (DCClasses[index], Alignment, 1): # check it
 		return 0
 

gemrb/GUIScripts/GUICommon.py
@@ -650,10 +650,9 @@ def CanDualClass(actor):
 		return 1
 
 	AlignmentTable = GemRB.LoadTable ("alignmnt")
-	AlignsTable = GemRB.LoadTable ("aligns")
 	Alignment = GemRB.GetPlayerStat (actor, IE_ALIGNMENT)
-	AlignmentColName = AlignsTable.FindValue (3, Alignment)
-	AlignmentColName = AlignsTable.GetValue (AlignmentColName, 4)
+	AlignmentColName = CommonTables.Aligns.FindValue (3, Alignment)
+	AlignmentColName = CommonTables.Aligns.GetValue (AlignmentColName, 4)
 	if AlignmentColName == "*":
 		print "CannotDualClass: extraordinary character alignment"
 		return 1

gemrb/GUIScripts/GUIREC.py
@@ -263,12 +263,13 @@ def UpdateRecordsWindow ():
 	Label = Window.GetControl (0x1000000f)
 	Label.SetText (text)
 
-	Table = GemRB.LoadTable ("aligns")
-
-	text = Table.GetValue (Table.FindValue ( 3, GemRB.GetPlayerStat (pc, IE_ALIGNMENT) ), 0)
+	# alignment
+	text = CommonTables.Aligns.FindValue (3, GemRB.GetPlayerStat (pc, IE_ALIGNMENT))
+	text = CommonTables.Aligns.GetValue (text, 0)
 	Label = Window.GetControl (0x10000010)
 	Label.SetText (text)
 
+	# gender
 	Label = Window.GetControl (0x10000011)
 	if GemRB.GetPlayerStat (pc, IE_SEX) == 1:
 		Label.SetText (7198)

gemrb/GUIScripts/Spellbook.py
@@ -298,9 +298,8 @@ def SetupSpellIcons(Window, BookType, Start=0, Offset=0):
 def GetMageSpells (Kit, Alignment, Level):
 	MageSpells = []
 	SpellType = 99
-	Table = GemRB.LoadTable ("aligns")
-	v = Table.FindValue (3, Alignment)
-	Usability = Kit | Table.GetValue(v, 5)
+	v = CommonTables.Aligns.FindValue (3, Alignment)
+	Usability = Kit | CommonTables.Aligns.GetValue(v, 5)
 
 	SpellsTable = GemRB.LoadTable ("spells")
 	for i in range(SpellsTable.GetValue ("MAGE", str(Level), 1) ):
@@ -336,10 +335,9 @@ def GetLearnableMageSpells (Kit, Alignment, Level):
 def GetLearnablePriestSpells (Class, Alignment, Level):
 	Learnable =[]
 
-	Table=GemRB.LoadTable("aligns")
-	v = Table.FindValue(3, Alignment)
+	v = CommonTables.Aligns.FindValue(3, Alignment)
 	#usability is the bitset we look for
-	Usability=Table.GetValue(v, 5)
+	Usability = CommonTables.Aligns.GetValue(v, 5)
 
 	SpellsTable = GemRB.LoadTable ("spells")
 	for i in range(SpellsTable.GetValue ("PRIEST", str (Level), 1) ):

gemrb/GUIScripts/bg1/CharGenGui.py
@@ -84,15 +84,13 @@ def unsetAlignment():
 	GemRB.SetPlayerStat (MyChar, IE_ALIGNMENT,0)
 	
 def getAlignment(area):
-	AlignmentTable = GemRB.LoadTable("aligns")
-	
 	MyChar = GemRB.GetVar ("Slot")
 	AllignID = GemRB.GetPlayerStat (MyChar, IE_ALIGNMENT)
 	
 	area.Append(1049, -1)
 	area.Append(": ")
-	AllignIndex = AlignmentTable.FindValue(3,AllignID)
-	AllignCap = AlignmentTable.GetValue(AllignIndex,2)
+	AllignIndex = CommonTables.Aligns.FindValue (3, AllignID)
+	AllignCap = CommonTables.Aligns.GetValue (AllignIndex, 2)
 	area.Append(AllignCap)
 	area.Append("\n")
 
@@ -342,7 +340,6 @@ def setAccept():
 	AllignID = GemRB.GetPlayerStat (MyChar, IE_ALIGNMENT)
 	
 	TmpTable=GemRB.LoadTable ("repstart")
-	AlignmentTable = GemRB.LoadTable ("aligns")
 	t = TmpTable.GetValue (AllignID,0) * 10
 	GemRB.SetPlayerStat (MyChar, IE_REPUTATION, t)
 

gemrb/GUIScripts/bg1/GUICG3.py
@@ -28,11 +28,9 @@ import CommonTables
 AlignmentWindow = 0
 TextAreaControl = 0
 DoneButton = 0
-AlignmentTable = 0
 
 def OnLoad():
 	global AlignmentWindow, TextAreaControl, DoneButton
-	global AlignmentTable
 
 	if GUICommon.CloseOtherWindow (OnLoad):
 		if(AlignmentWindow):
@@ -51,14 +49,13 @@ def OnLoad():
 	AlignmentOk = GemRB.LoadTable("ALIGNMNT")
 
 	GemRB.LoadWindowPack("GUICG", 640, 480)
-	AlignmentTable = GemRB.LoadTable("aligns")
 	AlignmentWindow = GemRB.LoadWindow(3)
 
 	for i in range(9):
 		Button = AlignmentWindow.GetControl(i+2)
 		Button.SetFlags(IE_GUI_BUTTON_RADIOBUTTON,OP_OR)
 		Button.SetState(IE_GUI_BUTTON_DISABLED)
-		Button.SetText(AlignmentTable.GetValue(i,0) )
+		Button.SetText (CommonTables.Aligns.GetValue (i,0))
 
 	# This section enables or disables different alignment selections
 	# based on Class, and depends on the ALIGNMNT.2DA table
@@ -66,7 +63,7 @@ def OnLoad():
 	# For now, we just enable all buttons
 	for i in range(9):
 		Button = AlignmentWindow.GetControl(i+2)
-		if AlignmentOk.GetValue(KitName, AlignmentTable.GetValue(i, 4) ) != 0:
+		if AlignmentOk.GetValue (KitName, CommonTables.Aligns.GetValue (i, 4)) != 0:
 			Button.SetState(IE_GUI_BUTTON_ENABLED)
 		else:
 			Button.SetState(IE_GUI_BUTTON_DISABLED)
@@ -91,9 +88,9 @@ def OnLoad():
 
 def AlignmentPress():
 	Alignment = GemRB.GetVar("Alignment")
-	TextAreaControl.SetText(AlignmentTable.GetValue(Alignment,1) )
+	TextAreaControl.SetText (CommonTables.Aligns.GetValue (Alignment,1))
 	DoneButton.SetState(IE_GUI_BUTTON_ENABLED)
-	GemRB.SetVar("Alignment",AlignmentTable.GetValue(Alignment,3) )
+	GemRB.SetVar ("Alignment", CommonTables.Aligns.GetValue (Alignment,3))
 	return
 
 def NextPress():

commit 7d87835934e4099b1c3c23d9efcc20167a3ef3c2

iwd: fixed regression from ca9334719e

gemrb/GUIScripts/iwd/CharGen.py
@@ -1216,7 +1216,7 @@ def AlignmentPress():
 
 	for i in range (9):
 		AlignmentSelectButton = AlignmentWindow.GetControl (i + 2)
-		if ClassAlignmentTable.GetValue (ClassName, AlignmentTable.GetValue(i, 4)) == 0:
+		if ClassAlignmentTable.GetValue (ClassName, CommonTables.Aligns.GetValue(i, 4)) == 0:
 			AlignmentSelectButton.SetState (IE_GUI_BUTTON_DISABLED)
 		else:
 			AlignmentSelectButton.SetState (IE_GUI_BUTTON_ENABLED)