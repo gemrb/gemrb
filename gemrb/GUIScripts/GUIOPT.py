a28b943c4a12ff42c3334d660895d14f2d823ebf
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2012-09-11 19:21:39 +0200

    GUIOPT: make the quit win's exit button the default also makes exiting quicker

--- gemrb/GUIScripts/GUIOPT.py
+++ gemrb/GUIScripts/GUIOPT.py
@@ -774,6 +774,7 @@ def OpenQuitMsgWindow ():
 	Button = Window.GetControl (1)
 	Button.SetText (15417)
 	Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, QuitGamePress)
+	Button.SetFlags (IE_GUI_BUTTON_DEFAULT, OP_OR)
 
 	# Cancel
 	Button = Window.GetControl (2)
