gemrb/gemrb

commit 782faaf77c8c6e387da12e0db02d463881552209

SeeCore: ignore invisibles

gemrb/core/GameScript/GSUtils.cpp
@@ -547,12 +547,12 @@ int CanSee(Scriptable* Sender, Scriptable* target, bool range, int seeflag)
 int SeeCore(Scriptable* Sender, Trigger* parameters, int justlos)
 {
 	//see dead
-	int flags;
+	int flags = GA_NO_HIDDEN;
 
 	if (parameters->int0Parameter) {
-		flags = GA_DETECT;
+		flags |= GA_DETECT;
 	} else {
-		flags = GA_NO_DEAD;
+		flags |= GA_NO_DEAD;
 	}
 	Scriptable* tar = GetActorFromObject( Sender, parameters->objectParameter, flags );
 	/* don't set LastSeen if this isn't an actor */

commit b3a53f38559e10b90bf503879884a60f9f975da0

SeeCore: don't ignore invisible Senders
bug introduced in 782faaf77c8c6e and caused idleness in invisible enemies

gemrb/core/GameScript/GSUtils.cpp
@@ -547,7 +547,7 @@ int CanSee(Scriptable* Sender, Scriptable* target, bool range, int seeflag)
 int SeeCore(Scriptable* Sender, Trigger* parameters, int justlos)
 {
 	//see dead
-	int flags = GA_NO_HIDDEN;
+	int flags = 0;
 
 	if (parameters->int0Parameter) {
 		flags |= GA_DETECT;
@@ -566,6 +566,9 @@ int SeeCore(Scriptable* Sender, Trigger* parameters, int justlos)
 		return 0;
 	}
 
+	// ignore invisible targets
+	flags |= GA_NO_HIDDEN;
+	
 	//both are actors
 	if (CanSee(Sender, tar, true, flags) ) {
 		if (justlos) {