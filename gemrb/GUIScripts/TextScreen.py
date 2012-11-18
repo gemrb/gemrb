gemrb/gemrb

commit d781e9411a333c28dd41a0c57aa6dd90e13e89f5

fixed double textscreen and not playing textscreen in chapter 2 in bg2

gemrb/GUIScripts/TextScreen.py
@@ -54,8 +54,10 @@ def StartTextScreen ():
 	#fixme: we always assume there isn't for non-bg2
 	if GUICommon.GameIsBG2():
 		if TableName == "":
-			Chapter = GemRB.GetGameVar ("CHAPTER") & 0x7fffffff
-			TableName = "CHPTXT"+str(Chapter)
+			#apparently, BG2 doesn't always use IncrementChapter to trigger a TextScreen
+			#Chapter = GemRB.GetGameVar ("CHAPTER") & 0x7fffffff
+			#TableName = "CHPTXT"+str(Chapter)
+			return
 		ID = 62
 	else:
 		ID = GemRB.GetGameVar("CHAPTER") & 0x7fffffff
@@ -81,7 +83,6 @@ def StartTextScreen ():
 
 	TextArea = TextScreen.GetControl (2)
 	TextArea.SetFlags (IE_GUI_TEXTAREA_SMOOTHSCROLL)
-	TextArea.SetEvent (IE_GUI_TEXTAREA_OUT_OF_TEXT, FeedScroll)
 
 	#caption
 	Table = GemRB.LoadTable (TableName)
@@ -117,7 +118,7 @@ def StartTextScreen ():
 	GUICommon.GameWindow.SetVisible(WINDOW_INVISIBLE) #removing the gamecontrol screen
 	TextScreen.SetVisible (WINDOW_VISIBLE)
 
-	TextArea.Rewind ()
+	ReplayTextScreen()
 	return
 
 def FeedScroll ():

commit c758780d53a9c7e0ca8c6cb93d926a509c5b4e11

fixed regression from d781e9411a3: restore the gamecontrol

gemrb/GUIScripts/TextScreen.py
@@ -57,6 +57,7 @@ def StartTextScreen ():
 			#apparently, BG2 doesn't always use IncrementChapter to trigger a TextScreen
 			#Chapter = GemRB.GetGameVar ("CHAPTER") & 0x7fffffff
 			#TableName = "CHPTXT"+str(Chapter)
+			EndTextScreen ()
 			return
 		ID = 62
 	else:
@@ -146,8 +147,8 @@ def FeedScroll ():
 def EndTextScreen ():
 	global TextScreen
 
-	TextScreen.SetVisible (WINDOW_INVISIBLE)
 	if TextScreen:
+		TextScreen.SetVisible (WINDOW_INVISIBLE)
 		TextScreen.Unload ()
 		GemRB.PlaySound(None, 0, 0, 4)
 	GUICommon.GameWindow.SetVisible(WINDOW_VISIBLE) # enable the gamecontrol screen