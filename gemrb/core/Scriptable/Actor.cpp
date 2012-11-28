gemrb/gemrb

commit 80f9468a95da96f2f7553a3df3ad989abc0f18c7

added favourite weapon registering

gemrb/core/Scriptable/Actor.cpp
@@ -5832,13 +5832,18 @@ void Actor::PerformAttack(ieDword gameTime)
 	ITMExtHeader *header = NULL;
 	ITMExtHeader *hittingheader = NULL;
 	int tohit;
-	//ieDword Flags;
 	int DamageBonus, CriticalBonus;
 	int speed, style;
 
 	//will return false on any errors (eg, unusable weapon)
 	if (!GetCombatDetails(tohit, leftorright, wi, header, hittingheader, DamageBonus, speed, CriticalBonus, style, target)) {
 		return;
+	}
+
+	if (PCStats) {
+		CREItem *slot = inventory.GetUsedWeapon(leftorright, wi.slot);
+		//if slot was null, then GetCombatDetails returned false
+		PCStats->RegisterFavourite(slot->ItemResRef, FAV_WEAPON);
 	}
 
 	//if this is the first call of the round, we need to update next attack
@@ -8519,34 +8524,34 @@ inline void HideFailed(Actor* actor)
 //checks if we are seen, or seeing anyone
 bool Actor::SeeAnyOne(bool enemy, bool seenby)
 {
-  Map *area = GetCurrentArea();
-  if (!area) return false;
+	Map *area = GetCurrentArea();
+	if (!area) return false;
 
-  int flag = seenby?GA_NO_DEAD:GA_NO_DEAD|GA_NO_HIDDEN;
-  if (enemy) {
-    ieDword ea = GetSafeStat(IE_EA);
-    if (ea>=EA_EVILCUTOFF) {
-      flag|=GA_NO_ENEMY|GA_NO_NEUTRAL;
-    } else if (ea<=EA_GOODCUTOFF) {
-      flag|=GA_NO_ALLY|GA_NO_NEUTRAL;
-    } else return false; //neutrals got no enemy
-  } 
-  Actor** visActors = area->GetAllActorsInRadius(Pos, flag, seenby?15*10:GetSafeStat(IE_VISUALRANGE)*10, this);
+	int flag = seenby?GA_NO_DEAD:GA_NO_DEAD|GA_NO_HIDDEN;
+	if (enemy) {
+		ieDword ea = GetSafeStat(IE_EA);
+		if (ea>=EA_EVILCUTOFF) {
+			flag|=GA_NO_ENEMY|GA_NO_NEUTRAL;
+		} else if (ea<=EA_GOODCUTOFF) {
+			flag|=GA_NO_ALLY|GA_NO_NEUTRAL;
+		} else return false; //neutrals got no enemy
+	}
+	Actor** visActors = area->GetAllActorsInRadius(Pos, flag, seenby?15*10:GetSafeStat(IE_VISUALRANGE)*10, this);
 
 	Actor** poi = visActors;
 	bool seeEnemy = false;
 
-  //we need to look harder if we look for seenby anyone
+	//we need to look harder if we look for seenby anyone
 	while (*poi && !seeEnemy) {
 		Actor *toCheck = *poi++;
-    if (toCheck==this) continue;
-    if (seenby) {
-      if(ValidTarget(GA_NO_HIDDEN, toCheck) && (toCheck->Modified[IE_VISUALRANGE]*10<PersonalDistance(toCheck, this) ) ) seeEnemy=true;
-    }
-    else seeEnemy = true;
+		if (toCheck==this) continue;
+		if (seenby) {
+			if(ValidTarget(GA_NO_HIDDEN, toCheck) && (toCheck->Modified[IE_VISUALRANGE]*10<PersonalDistance(toCheck, this) ) ) seeEnemy=true;
+		}
+		else seeEnemy = true;
 	}
 	free(visActors);
-  return seeEnemy;
+	return seeEnemy;
 }
 
 bool Actor::TryToHide()
@@ -8564,7 +8569,7 @@ bool Actor::TryToHide()
 		return false;
 	}
 
-  bool seen = SeeAnyOne(true, true);
+	bool seen = SeeAnyOne(true, true);
 
 	if (seen) {
 		HideFailed(this);

commit 17669a78ce2d2553c9382985e134ce1fc044e37b

fixed regression from 80f9468 - unwanted weapon slot change
caused infinite ammo among other things

gemrb/core/Scriptable/Actor.cpp
@@ -5841,7 +5841,9 @@ void Actor::PerformAttack(ieDword gameTime)
 	}
 
 	if (PCStats) {
-		CREItem *slot = inventory.GetUsedWeapon(leftorright && IsDualWielding(), wi.slot);
+		// make a copy of wi.slot, since GetUsedWeapon can modify it
+		int wislot = wi.slot;
+		CREItem *slot = inventory.GetUsedWeapon(leftorright && IsDualWielding(), wislot);
 		//if slot was null, then GetCombatDetails returned false
 		PCStats->RegisterFavourite(slot->ItemResRef, FAV_WEAPON);
 	}