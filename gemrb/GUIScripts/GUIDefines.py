8244c82d7cd36ea2e72929352d39fb4f731f3078
chiv <chilvence@gmail.com>
2012-12-09 00:07:53 +0100

    new GUIEnhancement to skip the container description and open it directly

--- gemrb/GUIScripts/GUIDefines.py
+++ gemrb/GUIScripts/GUIDefines.py
@@ -307,6 +307,7 @@ GEMRB_VERSION = -1
 # GUIEnhancements bits
 GE_SCROLLBARS = 1
 GE_TRY_IDENTIFY_ON_TRANSFER = 2
+GE_ALWAYS_OPEN_CONTAINER_ITEMS = 4
 
 # Log Levels
 # !!! Keep this synchronized with System/Logging.h !!!
