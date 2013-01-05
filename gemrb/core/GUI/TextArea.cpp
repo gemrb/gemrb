gemrb/gemrb

commit 7e74cd319e79308f343ab4309f286f72b92347b0

TextArea: we no loger have use of OutOfText handler

gemrb/core/GUI/TextArea.cpp
@@ -54,7 +54,6 @@ TextArea::TextArea(Color hitextcolor, Color initcolor, Color lowtextcolor)
 	seltext = -1;
 	Value = 0xffffffff;
 	ResetEventHandler( TextAreaOnChange );
-	ResetEventHandler( TextAreaOutOfText );
 	PortraitResRef[0]=0;
 	palette = core->CreatePalette( hitextcolor, lowtextcolor );
 	initpalette = core->CreatePalette( initcolor, lowtextcolor );
@@ -229,13 +228,7 @@ void TextArea::Draw(unsigned short x, unsigned short y)
 			//the buffer is filled enough
 			return;
 		}
-		if (RunEventHandler( TextAreaOutOfText )) {
-			return;
-		}
-		if (linesize==lines.size()) {
-			ResetEventHandler( TextAreaOutOfText );
-			return;
-		}
+
 		AppendText("\n",-1);
 		return;
 	}
@@ -920,9 +913,6 @@ bool TextArea::SetEvent(int eventType, EventHandler handler)
 	switch (eventType) {
 	case IE_GUI_TEXTAREA_ON_CHANGE:
 		TextAreaOnChange = handler;
-		break;
-	case IE_GUI_TEXTAREA_OUT_OF_TEXT:
-		TextAreaOutOfText = handler;
 		break;
 	default:
 		return false;

gemrb/GUIScripts/GUIDefines.py
@@ -91,7 +91,6 @@ IE_GUI_EDIT_ON_CHANGE       = 0x03000000
 IE_GUI_EDIT_ON_DONE         = 0x03000001
 IE_GUI_EDIT_ON_CANCEL       = 0x03000002
 IE_GUI_TEXTAREA_ON_CHANGE   = 0x05000000
-IE_GUI_TEXTAREA_OUT_OF_TEXT = 0x05000001
 IE_GUI_LABEL_ON_PRESS       = 0x06000000
 IE_GUI_SCROLLBAR_ON_CHANGE  = 0x07000000
 IE_GUI_WORLDMAP_ON_PRESS    = 0x08000000

gemrb/core/GUI/TextArea.h
@@ -40,7 +40,6 @@ namespace GemRB {
 // Keep these synchronized with GUIDefines.py
 // 0x05 is the control type of TextArea
 #define IE_GUI_TEXTAREA_ON_CHANGE   0x05000000
-#define IE_GUI_TEXTAREA_OUT_OF_TEXT 0x05000001
 
 // TextArea flags, keep these in sync too
 // the control type is intentionally left out
@@ -180,8 +179,6 @@ public: //Events
 	void SetFocus(bool focus);
 	/** OnChange Scripted Event Function Name */
 	EventHandler TextAreaOnChange;
-	/** OutOfText Scripted Event Function Name */
-	EventHandler TextAreaOutOfText;
 };
 
 }

commit 3a292fdf34069a7df13dcf1b49aa513a53ed47ec

TextArea: fixed regression from 7e74cd319e79308

gemrb/core/GUI/TextArea.cpp
@@ -228,7 +228,9 @@ void TextArea::Draw(unsigned short x, unsigned short y)
 			//the buffer is filled enough
 			return;
 		}
-
+		if (linesize==lines.size()) {
+			return;
+		}
 		AppendText("\n",-1);
 		return;
 	}