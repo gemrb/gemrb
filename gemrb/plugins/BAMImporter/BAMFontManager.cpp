7017a69bd7f436c8a9173faa0950c7781c3ebb59
Brad Allred <bradallred@me.com>
2013-01-14 15:21:10 -0700

    BAMFont: fix offset states and initials + numeric fonts
    numeric fonts begin at ascii code for 0.
    when a font has < 256 construct the font with cycles otherwise loop though the frames of each cycle
    this will allow us to use languages with more than 256 characters without breaking the standard data

--- gemrb/plugins/BAMImporter/BAMFontManager.cpp
+++ gemrb/plugins/BAMImporter/BAMFontManager.cpp
@@ -49,7 +49,6 @@ Font* BAMFontManager::GetFont(unsigned short /*ptSize*/,
 							  FontStyle /*style*/, Palette* pal)
 {
 	AnimationFactory* af = bamImp->GetAnimationFactory("dummy"); // FIXME: how does this get released?
-	//int glyphIndexOffset = 0, limit = 0, Count = 0, glyphCount = 0;
 	size_t glyphCount = 0, cycleCount = af->GetCycleCount();
 
 	// FIXME: we will end up with many useless (blank) glyphs
@@ -59,29 +58,40 @@ Font* BAMFontManager::GetFont(unsigned short /*ptSize*/,
 
 	Sprite2D** glyphs = (Sprite2D**)malloc( glyphCount * sizeof(Sprite2D*) );
 	ieWord glyphIndex = 0;
+	ieWord firstChar = 1;
 
-	for (size_t i = 0; i < cycleCount; i++) {
-		for (int j = 0; j < af->GetCycleSize(i); j++) {
-			glyphs[glyphIndex] = af->GetFrame(j, i);
-
-			if (isStateFont) {
-				// Hack to work around original data where some status icons have inverted x and y positions (ie level up icon)
-				// isStateFont is set in Open() and simply compares the first 6 characters of the file with "STATES"
-
-				// since state icons should all be the same size/position we can just take the position of the first one
-				glyphs[glyphIndex]->YPos = glyphs[0]->YPos;
-			} else if (cycleCount == 1) {
-				// this is a numeric font
-				// we want them to have the same baseline as the rest
-				glyphs[glyphIndex]->YPos = 13 - glyphs[glyphIndex]->Height;
+	if (cycleCount > 1) {
+		for (size_t i = 0; i < cycleCount; i++) {
+			if (af->GetFrameCount() < 255) {
+				glyphs[glyphIndex] = af->GetFrame(0, i);
+				if (isStateFont) {
+					// Hack to work around original data where some status icons have inverted x and y positions (ie level up icon)
+					// isStateFont is set in Open() and simply compares the first 6 characters of the file with "STATES"
+					
+					// since state icons should all be the same size/position we can just take the position of the first one
+					glyphs[glyphIndex]->YPos = glyphs[0]->YPos;
+					//firstChar = 0;
+				}
+				glyphIndex++;
+			} else {
+				for (int j = 0; j < af->GetCycleSize(i); j++) {
+					glyphs[glyphIndex++] = af->GetFrame(j, i);
+				}
 			}
-			glyphIndex++;
+		}
+	} else {
+		// this is a numeric font
+		firstChar = '0';
+		for (glyphIndex = 0; glyphIndex < af->GetFrameCount(); glyphIndex++) {
+			glyphs[glyphIndex] = af->GetFrameWithoutCycle(glyphIndex);
+			// we want them to have the same baseline as the rest
+			glyphs[glyphIndex]->YPos = 13 - glyphs[glyphIndex]->Height;
 		}
 	}
 
 	// assume all sprites have same palette
 	Palette* palette = glyphs[0]->GetPalette();
-	Font* fnt = new Font(glyphs, 1, glyphCount, palette);
+	Font* fnt = new Font(glyphs, firstChar, firstChar + glyphIndex - 1, palette);
 	palette->Release();
 	if (pal) {
 		fnt->SetPalette(pal);
