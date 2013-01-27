gemrb/gemrb

commit 8ec2dc99c4e2b07ba1d1ed443a9e6122f753cbd3

Actor: split out IWD2GemrbQslot converter function

gemrb/core/Scriptable/Actor.cpp
@@ -7524,26 +7524,32 @@ void Actor::GetActionButtonRow(ActionButtonRow &ar)
 	CreateStats();
 	InitButtons(GetStat(IE_CLASS), false);
 	for(int i=0;i<GUIBT_COUNT;i++) {
-		ieByte tmp=PCStats->QSlots[i];
-		//the first three buttons are hardcoded in gemrb
-		//don't mess with them
-		if (QslotTranslation && i>2) {
-			if (tmp>=110) { //quick songs
-				tmp = ACT_IWDQSONG+tmp%10;
-			} else if (tmp>=90) { //quick abilities
-				tmp=ACT_IWDQSPEC+tmp%10;
-			} else if (tmp>=80) { //quick items
-				tmp=ACT_IWDQITEM+tmp%10;
-			} else if (tmp>=70) { //quick spells
-				tmp=ACT_IWDQSPELL+tmp%10;
-			} else if (tmp>=50) { //spellbooks
-				tmp=ACT_BARD+tmp%10;
-			} else {
-				tmp=iwd2gemrb[tmp];
-			}
-		}
-		ar[i]=tmp;
+		ar[i] = IWD2GemrbQslot(i);
 	}
+}
+
+int Actor::IWD2GemrbQslot (int slotindex)
+{
+	//the first three buttons are hardcoded in gemrb
+	//don't mess with them
+	if (QslotTranslation && slotindex>2) {
+		ieByte tmp = PCStats->QSlots[slotindex];
+		if (tmp>=110) { //quick songs
+			tmp = ACT_IWDQSONG + tmp%10;
+		} else if (tmp>=90) { //quick abilities
+			tmp = ACT_IWDQSPEC + tmp%10;
+		} else if (tmp>=80) { //quick items
+			tmp = ACT_IWDQITEM + tmp%10;
+		} else if (tmp>=70) { //quick spells
+			tmp = ACT_IWDQSPELL + tmp%10;
+		} else if (tmp>=50) { //spellbooks
+			tmp = ACT_BARD + tmp%10;
+		} else {
+			tmp = iwd2gemrb[tmp];
+		}
+		return tmp;
+	}
+	return slotindex;
 }
 
 void Actor::SetPortrait(const char* ResRef, int Which)

gemrb/core/Scriptable/Actor.h
@@ -674,6 +674,8 @@ public:
 	void SetActionButtonRow(ActionButtonRow &ar);
 	/* updates the quick slots */
 	void GetActionButtonRow(ActionButtonRow &qs);
+	/* converts the iwd2 qslot index to our internal representation */
+	int IWD2GemrbQslot (int slotindex);
 
 	/* Handling automatic stance changes */
 	bool HandleActorStance();

commit 3ebc470062f62d8efaff9845378026b63859e500

fixed my regression from 8ec2dc99c

gemrb/core/Scriptable/Actor.cpp
@@ -7536,10 +7536,10 @@ void Actor::GetActionButtonRow(ActionButtonRow &ar)
 
 int Actor::IWD2GemrbQslot (int slotindex)
 {
+	ieByte tmp = PCStats->QSlots[slotindex];
 	//the first three buttons are hardcoded in gemrb
 	//don't mess with them
 	if (QslotTranslation && slotindex>2) {
-		ieByte tmp = PCStats->QSlots[slotindex];
 		if (tmp>=110) { //quick songs
 			tmp = ACT_IWDQSONG + tmp%10;
 		} else if (tmp>=90) { //quick abilities
@@ -7553,9 +7553,8 @@ int Actor::IWD2GemrbQslot (int slotindex)
 		} else {
 			tmp = iwd2gemrb[tmp];
 		}
-		return tmp;
 	}
-	return slotindex;
+	return tmp;
 }
 
 void Actor::SetPortrait(const char* ResRef, int Which)