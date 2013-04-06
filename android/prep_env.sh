9dba6d6ab266169f4eabcbffe2d21668c5020ca7
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2013-04-06 12:36:01 +0200

    android: use the new glowing icons

--- android/prep_env.sh
+++ android/prep_env.sh
@@ -127,9 +127,9 @@ function move_and_edit_projectfiles {
   cp $ENVROOT/packaged.GemRB.cfg $ENVROOT/build/gemrb/assets
 
   # copy the icons
-  cp $GEMRB_GIT_PATH/artwork/gemrb-logo-36px.png $ENVROOT/build/gemrb/res/drawable-ldpi/icon.png
-  cp $GEMRB_GIT_PATH/artwork/gemrb-logo-48px.png $ENVROOT/build/gemrb/res/drawable-mdpi/icon.png
-  cp $GEMRB_GIT_PATH/artwork/gemrb-logo-72px.png $ENVROOT/build/gemrb/res/drawable-hdpi/icon.png
+  cp $GEMRB_GIT_PATH/artwork/gemrb-logo-glow-36px.png $ENVROOT/build/gemrb/res/drawable-ldpi/icon.png
+  cp $GEMRB_GIT_PATH/artwork/gemrb-logo-glow-48px.png $ENVROOT/build/gemrb/res/drawable-mdpi/icon.png
+  cp $GEMRB_GIT_PATH/artwork/gemrb-logo-glow-72px.png $ENVROOT/build/gemrb/res/drawable-hdpi/icon.png
 
   # copy the makefile
   cp $ENVROOT/GEMRB_Android.mk $ENVROOT/build/gemrb/jni/src/Android.mk
