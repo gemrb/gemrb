f6907e2fb814f30cab62d76c4fe526fa6a78e0bb
Jens Granseuer <jensgr@gmx.net>
2013-05-05 21:00:10 +0200

    rewrite OpenStoreRentWindow so as not to require UpdateVarAssoc

--- gemrb/plugins/GUIScript/GUIScript.cpp
+++ gemrb/plugins/GUIScript/GUIScript.cpp
@@ -1927,29 +1927,6 @@ static PyObject* GemRB_Control_SetVarAssoc(PyObject * /*self*/, PyObject* args)
 	return Py_None;
 }
 
-PyDoc_STRVAR( GemRB_Window_UpdateVarAssoc__doc,
-"UpdateVarAssoc(WindowIndex)\n\n"
-"Redraw controls in the given Window associated with a variable." );
-
-static PyObject* GemRB_Window_UpdateVarAssoc(PyObject * /*self*/, PyObject* args)
-{
-	int WindowIndex;
-	ieDword Value;
-	char* VarName;
-
-	if (!PyArg_ParseTuple( args, "is", &WindowIndex, &VarName )) {
-		return AttributeError( GemRB_Window_UpdateVarAssoc__doc );
-	}
-
-	Value = 0;
-	core->GetDictionary()->Lookup( VarName, Value );
-	Window* win = core->GetWindow( WindowIndex );
-	win->RedrawControls(VarName, Value);
-
-	Py_INCREF( Py_None );
-	return Py_None;
-}
-
 PyDoc_STRVAR( GemRB_Window_Unload__doc,
 "UnloadWindow(WindowIndex)\n\n"
 "Unloads a previously Loaded Window." );
@@ -10895,7 +10872,6 @@ static PyMethodDef GemRBInternalMethods[] = {
 	METHOD(Window_SetupEquipmentIcons, METH_VARARGS),
 	METHOD(Window_ShowModal, METH_VARARGS),
 	METHOD(Window_Unload, METH_VARARGS),
-	METHOD(Window_UpdateVarAssoc, METH_VARARGS),
 	METHOD(WorldMap_AdjustScrolling, METH_VARARGS),
 	METHOD(WorldMap_GetDestinationArea, METH_VARARGS),
 	METHOD(WorldMap_SetTextColor, METH_VARARGS),
