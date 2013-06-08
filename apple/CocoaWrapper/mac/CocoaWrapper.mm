91f04868a32f66713bbd758b103777f790c1e9b0
Brad Allred <bradallred@me.com>
2013-06-08 17:27:56 -0600

    CocoaWrapper: remove old garbage

--- apple/CocoaWrapper/mac/CocoaWrapper.mm
+++ apple/CocoaWrapper/mac/CocoaWrapper.mm
@@ -27,13 +27,6 @@
 
 using namespace GemRB;
 
-/* For some reaon, Apple removed setAppleMenu from the headers in 10.4,
- but the method still is there and works. To avoid warnings, we declare
- it ourselves here. */
-@interface NSApplication(Missing_Methods)
-- (void)setAppleMenu:(NSMenu *)menu;
-@end
-
 /* The main class of the application, the application's delegate */
 @implementation CocoaWrapper
 @synthesize configWindow=_configWindow;
