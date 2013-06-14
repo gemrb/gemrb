bb674c2ae5aac195eeb2967702f651d3dd4ea7f1
Jens Granseuer <jensgr@gmx.net>
2013-06-14 19:33:54 +0200

    Revert "when initiating dialog immediately suspend script execution"
    This reverts commit 7007465932b777a922d408e9de7df4a6ee675c31.

--- gemrb/core/GameScript/GSUtils.cpp
+++ gemrb/core/GameScript/GSUtils.cpp
@@ -1148,8 +1148,6 @@ void BeginDialog(Scriptable* Sender, Action* parameters, int Flags)
 	}
 
 	Sender->ReleaseCurrentAction();
-	//force ProcessActions to suspend until dialog has started
-	Sender->SetWait(1);
 
 	if (!ret) {
 		if (Flags & BD_NOEMPTY) {
