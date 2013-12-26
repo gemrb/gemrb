gemrb/gemrb

commit 16c81f763279362da9b6c07ddf270aa9cf00f3f1

Control: factor out common draw code to base class

gemrb/core/GUI/Progressbar.cpp
@@ -57,16 +57,8 @@ Progressbar::~Progressbar()
 }
 
 /** Draws the Control on the Output Display */
-void Progressbar::Draw(unsigned short x, unsigned short y)
+void Progressbar::DrawInternal(Region& rgn)
 {
-	//it is unlikely that a floating window is above us, but...
-	if (!Changed && !(Owner->Flags&WF_FLOAT) ) {
-		return;
-	}
-	Changed = false;
-	if (XPos == 65535) {
-		return;
-	}
 	Sprite2D *bcksprite;
 
 	if((Value >= 100) && KnobStepsCount && BackGround2) {
@@ -76,9 +68,8 @@ void Progressbar::Draw(unsigned short x, unsigned short y)
 		bcksprite=BackGround;
 	}
 	if (bcksprite) {
-		Region r( x + XPos, y + YPos, Width, Height );
 		core->GetVideoDriver()->BlitSprite( bcksprite,
-			x + XPos, y + YPos, true, &r );
+			rgn.x, rgn.y, true, &rgn );
 		if( bcksprite==BackGround2) {
 			return; //done for animated progbar
 		}
@@ -92,12 +83,12 @@ void Progressbar::Draw(unsigned short x, unsigned short y)
 		int h = BackGround2->Height;
 		//this is the PST/IWD specific part
 		Count = Value*w/100;
-		Region r( x + XPos + KnobXPos, y + YPos + KnobYPos, Count, h );
+		Region r( rgn.x + KnobXPos, rgn.y + KnobYPos, Count, h );
 		core->GetVideoDriver()->BlitSprite( BackGround2, 
 			r.x, r.y, true, &r );
 
 		core->GetVideoDriver()->BlitSprite( PBarCap,
-			x+XPos+CapXPos+Count-PBarCap->Width, y+YPos+CapYPos, true );
+			rgn.x+CapXPos+Count-PBarCap->Width, rgn.y+CapYPos, true );
 		return;
 	}
 
@@ -105,7 +96,7 @@ void Progressbar::Draw(unsigned short x, unsigned short y)
 	Count=Value*KnobStepsCount/100;
 	for(unsigned int i=0; i<Count ;i++ ) {
 		Sprite2D *Knob = PBarAnim->GetFrame(i);
-		core->GetVideoDriver()->BlitSprite( Knob, x , y , true );
+		core->GetVideoDriver()->BlitSprite( Knob, 0, 0, true );
 	}
 }
 

gemrb/core/GUI/Button.cpp
@@ -161,16 +161,8 @@ void Button::CloseUpColor()
 }
 
 /** Draws the Control on the Output Display */
-void Button::Draw(unsigned short x, unsigned short y)
+void Button::DrawInternal(Region& rgn)
 {
-	if (!Changed && !(Owner->Flags&WF_FLOAT) ) {
-		return;
-	}
-	Changed = false;
-	if (XPos == 65535 || Width == 0) {
-		return;
-	}
-
 	Video * video = core->GetVideoDriver();
 
 	// Button image
@@ -203,26 +195,26 @@ void Button::Draw(unsigned short x, unsigned short y)
 			int xOffs = ( Width / 2 ) - ( Image->Width / 2 );
 			int yOffs = ( Height / 2 ) - ( Image->Height / 2 );
 
-			video->BlitSprite( Image, x + XPos + xOffs, y + YPos + yOffs, true );
+			video->BlitSprite( Image, rgn.x + xOffs, rgn.y + yOffs, true );
 		}
 	}
 
 	if (State == IE_GUI_BUTTON_PRESSED) {
 		//shift the writing/border a bit
-		x+= PushOffset.x;
-		y+= PushOffset.y;
+		rgn.x += PushOffset.x;
+		rgn.y += PushOffset.y;
 	}
 
 	// Button picture
 	if (AnimPicture) {
 		int xOffs = ( Width / 2 ) - ( AnimPicture->Width / 2 );
 		int yOffs = ( Height / 2 ) - ( AnimPicture->Height / 2 );
-		Region r( x + XPos + xOffs, y + YPos + yOffs, (int)(AnimPicture->Width * Clipping), AnimPicture->Height );
+		Region r( rgn.x + xOffs, rgn.y + yOffs, (int)(AnimPicture->Width * Clipping), AnimPicture->Height );
 
 		if (Flags & IE_GUI_BUTTON_CENTER_PICTURES) {
-			video->BlitSprite( AnimPicture, x + XPos + xOffs + AnimPicture->XPos, y + YPos + yOffs + AnimPicture->YPos, true, &r );
+			video->BlitSprite( AnimPicture, rgn.x + xOffs + AnimPicture->XPos, rgn.y + yOffs + AnimPicture->YPos, true, &r );
 		} else {
-			video->BlitSprite( AnimPicture, x + XPos + xOffs, y + YPos + yOffs, true, &r );
+			video->BlitSprite( AnimPicture, rgn.x + xOffs, rgn.y + yOffs, true, &r );
 		}
 	}
 
@@ -230,8 +222,8 @@ void Button::Draw(unsigned short x, unsigned short y)
 	int picXPos = 0, picYPos = 0;
 	if (Picture  && (Flags & IE_GUI_BUTTON_PICTURE) ) {
 		// Picture is drawn centered
-		picXPos = ( Width / 2 ) - ( Picture->Width / 2 ) + x + XPos;
-		picYPos = ( Height / 2 ) - ( Picture->Height / 2 ) + y + YPos;
+		picXPos = ( rgn.w / 2 ) - ( Picture->Width / 2 ) + rgn.x;
+		picYPos = ( rgn.h / 2 ) - ( Picture->Height / 2 ) + rgn.y;
 		if (Flags & IE_GUI_BUTTON_HORIZONTAL) {
 			picXPos += Picture->XPos;
 			picYPos += Picture->YPos;
@@ -281,7 +273,7 @@ void Button::Draw(unsigned short x, unsigned short y)
 		}
 
 		for (; iter != PictureList.end(); ++iter) {
-			video->BlitSprite( *iter, x + XPos + xOffs, y + YPos + yOffs, true );
+			video->BlitSprite( *iter, rgn.x + xOffs, rgn.y + yOffs, true );
 		}
 	}
 
@@ -317,11 +309,11 @@ void Button::Draw(unsigned short x, unsigned short y)
 		Region r;
 		if (Picture && (Flags & IE_GUI_BUTTON_PORTRAIT) == IE_GUI_BUTTON_PORTRAIT) {
 			// constrain the label (status icons) to the picture bounds
-			// we are subtracting IE_FONT_PADDING because Font indents 5px, but we dont want that here
+			// FIXME: we are subtracting IE_FONT_PADDING because Font indents 5px, but we dont want that here
 			r = Region(picXPos - IE_FONT_PADDING, picYPos + IE_FONT_PADDING,
 					   Picture->Width + IE_FONT_PADDING, Picture->Height);
 		} else {
-			r = Region( x + XPos, y + YPos, Width - 2, Height - 2);
+			r = Region( rgn.x, rgn.y, rgn.w - 2, rgn.h - 2);
 		}
 
 		font->Print( r, ( unsigned char * ) Text, ppoi, (ieByte) align, true );
@@ -332,7 +324,7 @@ void Button::Draw(unsigned short x, unsigned short y)
 			ButtonBorder *fr = &borders[i];
 			if (! fr->enabled) continue;
 
-			Region r = Region( x + XPos + fr->dx1, y + YPos + fr->dy1, Width - (fr->dx1 + fr->dx2 + 1), Height - (fr->dy1 + fr->dy2 + 1) );
+			Region r = Region( rgn.x + fr->dx1, rgn.y + fr->dy1, rgn.w - (fr->dx1 + fr->dx2 + 1), rgn.h - (fr->dy1 + fr->dy2 + 1) );
 			video->DrawRect( r, fr->color, fr->filled );
 		}
 	}

gemrb/core/GUI/Button.h
@@ -130,14 +130,15 @@ enum BUTTON_IMAGE_TYPE {
  */
 
 class GEM_EXPORT Button : public Control {
+protected:
+	/** Draws the Control on the Output Display */
+	void DrawInternal(Region& drawRegion);
 public: 
 	Button(Region& frame);
 	~Button();
 	/** Sets the 'type' Image of the Button to 'img'.
 	see 'BUTTON_IMAGE_TYPE' */
 	void SetImage(BUTTON_IMAGE_TYPE, Sprite2D* img);
-	/** Draws the Control on the Output Display */
-	void Draw(unsigned short x, unsigned short y);
 	/** Sets the Button State */
 	void SetState(unsigned char state);
 	/** Sets the Text of the current control */

gemrb/core/GUI/Console.cpp
@@ -62,15 +62,14 @@ Console::~Console(void)
 }
 
 /** Draws the Console on the Output Display */
-void Console::Draw(unsigned short x, unsigned short y)
+void Console::DrawInternal(Region& drawFrame)
 {
 	if (Back) {
-		core->GetVideoDriver()->BlitSprite( Back, 0, y, true );
+		core->GetVideoDriver()->BlitSprite( Back, 0, drawFrame.y, true );
 	}
 
-	Region r( (short)x + XPos, (short)y + YPos, Width, Height );
-	core->GetVideoDriver()->DrawRect( r, ColorBlack );
-	font->Print( r, Buffer, palette,
+	core->GetVideoDriver()->DrawRect( drawFrame, ColorBlack );
+	font->Print( drawFrame, Buffer, palette,
 			IE_FONT_ALIGN_LEFT | IE_FONT_ALIGN_MIDDLE, true, NULL,
 			Cursor, CurPos, true );
 }

gemrb/core/GUI/Console.h
@@ -50,8 +50,6 @@ class Console : public Control {
 public:
 	Console(const Region& frame);
 	~Console(void);
-	/** Draws the Console on the Output Display */
-	void Draw(unsigned short x, unsigned short y);
 	/** Set Font */
 	void SetFont(Font* f);
 	/** Set Cursor */
@@ -60,6 +58,9 @@ public:
 	void SetBackGround(Sprite2D* back);
 	/** Sets the Text of the current control */
 	void SetText(const char* string);
+protected:
+	/** Draws the Console on the Output Display */
+	void DrawInternal(Region& drawFrame);
 private:
 	/** Text Editing Cursor Sprite */
 	Sprite2D* Cursor;

gemrb/core/GUI/Control.cpp
@@ -79,6 +79,25 @@ Region Control::ControlFrame()
 	return Region(XPos, YPos, Width, Height);
 }
 
+void Control::Draw(unsigned short x, unsigned short y)
+{
+	// FIXME: Draw shouldnt be getting called on controls that are offscreen...
+	if (XPos == 65535) {
+		return;
+	}
+	// no point in drawing something with a 0 w/h
+	if (!Width || !Height) {
+		return;
+	}
+	//it is unlikely that a floating window is above us, but...
+	if (!Changed && (Owner && !(Owner->Flags&WF_FLOAT)) ) {
+		return;
+	}
+	Region drawFrame = Region(x + XPos, y + YPos, Width, Height);
+	DrawInternal(drawFrame);
+	Changed = false; // set *after* calling DrawInternal
+}
+
 /** Sets the Tooltip text of the current control */
 int Control::SetTooltip(const char* string)
 {

gemrb/core/GUI/Control.h
@@ -66,12 +66,13 @@ class GEM_EXPORT Control {
 protected:
 	/** Focused Control */
 	bool hasFocus;
+	virtual void DrawInternal(Region& drawFrame)=0;
 public:
 	Control(const Region& frame);
 	virtual ~Control();
 	Region ControlFrame();
 	/** Draws the Control on the Output Display */
-	virtual void Draw(unsigned short x, unsigned short y) = 0;
+	void Draw(unsigned short x, unsigned short y);
 	/** Sets the Text of the current control */
 	virtual void SetText(const char* string);
 	/** Sets the Tooltip text of the current control */

gemrb/core/GUI/GameControl.cpp
@@ -369,7 +369,7 @@ void GameControl::DrawTargetReticle(Point p, int size, bool animate, bool flash,
 }
 
 /** Draws the Control on the Output Display */
-void GameControl::Draw(unsigned short x, unsigned short y)
+void GameControl::DrawInternal(Region& screen)
 {
 	bool update_scripts = !(DialogueFlags & DF_FREEZE_SCRIPTS);
 
@@ -377,11 +377,6 @@ void GameControl::Draw(unsigned short x, unsigned short y)
 	if (!game)
 		return;
 
-	if (((short) Width) <=0 || ((short) Height) <= 0) {
-		return;
-	}
-
-	Region screen( x + XPos, y + YPos, Width, Height );
 	Map *area = core->GetGame()->GetCurrentArea();
 	Video* video = core->GetVideoDriver();
 	if (!area) {
@@ -1567,6 +1562,7 @@ void GameControl::UpdateScrolling() {
 	video->FreeSprite(cursor);
 
 	numScrollCursor = (numScrollCursor+1) % 15;
+	Changed = true;
 }
 
 void GameControl::SetScrolling(bool scroll) {

commit ec71dc5bbd95bf8c4c234678004011c8a55723c2

Progressbar: fix drawing regression from 16c81f7

gemrb/core/GUI/Progressbar.cpp
@@ -96,7 +96,7 @@ void Progressbar::DrawInternal(Region& rgn)
 	Count=Value*KnobStepsCount/100;
 	for(unsigned int i=0; i<Count ;i++ ) {
 		Sprite2D *Knob = PBarAnim->GetFrame(i);
-		core->GetVideoDriver()->BlitSprite( Knob, 0, 0, true );
+		core->GetVideoDriver()->BlitSprite( Knob, Owner->XPos, Owner->YPos, true );
 	}
 }
 