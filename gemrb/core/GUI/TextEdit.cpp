1f645e7a73d163fdd1935f57300e59f7b3228cbb
Brad Allred <bradallred@me.com>
2014-01-02 17:53:54 -0700

    TextEdit: attempt to bix incorrect drawing
    text was being drawn way below its background (see BG2 area map). we still have artifact issues because we dont clip the text to the background.

--- gemrb/core/GUI/TextEdit.cpp
+++ gemrb/core/GUI/TextEdit.cpp
@@ -23,6 +23,7 @@
 
 #include "GameData.h"
 #include "Interface.h"
+#include "Sprite2D.h"
 #include "Video.h"
 #include "GUI/EventMgr.h"
 #include "GUI/Window.h"
@@ -69,20 +70,22 @@ void TextEdit::SetAlignment(unsigned char Alignment)
 /** Draws the Control on the Output Display */
 void TextEdit::DrawInternal(Region& rgn)
 {
+	ieWord yOff = FontPosY;
 	if (Back) {
 		core->GetVideoDriver()->BlitSprite( Back, rgn.x, rgn.y, true );
-
+		if (yOff) yOff += Back->Height;
 	}
 	if (!font)
 		return;
 
+	// FIXME: we should clip text to the background right?
 	//The aligning of textedit fields is done by absolute positioning (FontPosX, FontPosY)
 	if (hasFocus) {
-		font->Print( Region( rgn.x + FontPosX, rgn.y + FontPosY, rgn.w, rgn.h ), Buffer,
+		font->Print( Region( rgn.x + FontPosX, rgn.y - yOff, rgn.w, rgn.h ), Buffer,
 				palette, Alignment,
 				true, NULL, Cursor, CurPos );
 	} else {
-		font->Print( Region( rgn.x + FontPosX, rgn.y + FontPosY, rgn.w, rgn.h ), Buffer,
+		font->Print( Region( rgn.x + FontPosX, rgn.y - yOff, rgn.w, rgn.h ), Buffer,
 				palette, Alignment, true );
 	}
 }
