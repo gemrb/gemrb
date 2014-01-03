3d88c1ceaf9128d8c747e5688d4b148745d2279a
Laurie Chilvers <chilvence@gmail.com>
2014-01-03 18:44:48 +0000

    Make sure to append _DEAD to the actor name when checking kaputz for deadness

--- gemrb/core/GameScript/Triggers.cpp
+++ gemrb/core/GameScript/Triggers.cpp
@@ -1712,12 +1712,12 @@ int GameScript::Dead(Scriptable* Sender, Trigger* parameters)
 {
 	if (parameters->string0Parameter[0]) {
 		ieDword value;
+		ieVariable Variable;
 
 		if (core->HasFeature( GF_HAS_KAPUTZ )) {
-			value = CheckVariable( Sender, parameters->string0Parameter, "KAPUTZ");
+			snprintf(Variable,sizeof(ieVariable),"%s_DEAD",parameters->string0Parameter);
+			value = CheckVariable( Sender, Variable, "KAPUTZ");
 		} else {
-			ieVariable Variable;
-
 			snprintf( Variable, 32, core->GetDeathVarFormat(), parameters->string0Parameter );
 			value = CheckVariable( Sender, Variable, "GLOBAL" );
 		}
