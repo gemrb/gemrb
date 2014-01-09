gemrb/gemrb

commit d49201fe721d12153d931388262dc1f4c44ea608

take the anim zpos into account when building the sprite cover
This faithfully reproduces original rendering errors with the water
spouts in BG2 AR2300 and therefore looks like the right thing to do.

gemrb/core/Map.cpp
@@ -3879,9 +3879,7 @@ void AreaAnimation::Draw(const Region &screen, Map *area)
 		if(covers) {
 			if(!covers[ac] || !covers[ac]->Covers(Pos.x, Pos.y, frame->XPos, frame->YPos, frame->Width, frame->Height)) {
 				delete covers[ac];
-				// TODO: Should this build a sprite cover with base point
-				// Pos.x,Pos.y, or with Pos.y,Pos.y+height ?
-				covers[ac] = area->BuildSpriteCover(Pos.x, Pos.y, -anim->animArea.x,
+				covers[ac] = area->BuildSpriteCover(Pos.x, Pos.y + height, -anim->animArea.x,
 					-anim->animArea.y, anim->animArea.w, anim->animArea.h, 0, true);
 			}
 		}

commit 3bb9de445b53c0e4fe09a7ea07247999ebc55b9c

fix cover check for AreaAnimations (broken in d49201fe7)

gemrb/core/Map.cpp
@@ -3901,7 +3901,7 @@ void AreaAnimation::Draw(const Region &screen, Map *area)
 		Animation *anim = animation[ac];
 		Sprite2D *frame = anim->NextFrame();
 		if(covers) {
-			if(!covers[ac] || !covers[ac]->Covers(Pos.x, Pos.y, frame->XPos, frame->YPos, frame->Width, frame->Height)) {
+			if(!covers[ac] || !covers[ac]->Covers(Pos.x, Pos.y + height, frame->XPos, frame->YPos, frame->Width, frame->Height)) {
 				delete covers[ac];
 				covers[ac] = area->BuildSpriteCover(Pos.x, Pos.y + height, -anim->animArea.x,
 					-anim->animArea.y, anim->animArea.w, anim->animArea.h, 0, true);