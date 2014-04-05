gemrb/gemrb

commit 833056db6f0ed650c2d5e4fefa2ce6b6898c6dda

iwd2: respect actor DifficultyMargin vs. area difficulty level
fixes hordes of orcs/... being spawned in ch1 and more problematic
multiple spawns of npcs like Illium and Odea in ar4000

gemrb/plugins/AREImporter/AREImporter.cpp
@@ -1045,6 +1045,19 @@ Map* AREImporter::GetMap(const char *ResRef, bool day_or_night)
 				if (Flags&AF_INVULNERABLE) {
 					ab->SetMCFlag(MC_INVULNERABLE,BM_OR);
 				}
+				if (!(Flags&AF_ENABLED)) {
+					// DifficultyMargin - only enable actors that are difficult enough vs the area difficulty
+					// 1 - area difficulty 1
+					// 2 - area difficulty 2
+					// 4 - area difficulty 3
+					if (!DifficultyMargin || (DifficultyMargin & map->AreaDifficulty)) {
+						//TODO: save AF_ENABLED instead, so if someone implements Imprisonment for iwd2, this won't interfere
+						ab->SetBase(IE_AVATARREMOVAL, 0);
+					} else {
+						// iwd2 has GF_START_ACTIVE off, but that only touches IF_IDLE
+						ab->SetBase(IE_AVATARREMOVAL, 1);
+					}
+				}
 			}
 			ab->DifficultyMargin = DifficultyMargin;
 

commit c5f9086da0a21a75f1e3c9c780fd3ad2d4f18459

AREImporter: fixed regression from 833056d and bugs from 31d4d4b1
iwd1 also uses IWD2ScriptName, so too much code was run, breaking
avatar removal in particular, making both iwd/how unfinishable

thanks go to merbie for bisecting

gemrb/plugins/AREImporter/AREImporter.cpp
@@ -1038,7 +1038,7 @@ Map* AREImporter::GetMap(const char *ResRef, bool day_or_night)
 				ab->SetScriptName(DefaultName);
 			}
 			//IWD2 specific hacks
-			if (core->HasFeature(GF_IWD2_SCRIPTNAME)) {
+			if (core->HasFeature(GF_3ED_RULES)) {
 				//This flag is used for something else in IWD2
 				if (Flags&AF_NAME_OVERRIDE) {
 					ab->BaseStats[IE_EA]=EA_EVILCUTOFF;