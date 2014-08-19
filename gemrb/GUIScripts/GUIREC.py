7589a6a4a2fb4b7cebea2f356b2b033b7f9ccfbc
Brad Allred <bradallred@me.com>
2014-08-19 10:53:52 -0600

    GUIREC: fix incorrect use of "text" variable

--- gemrb/GUIScripts/GUIREC.py
+++ gemrb/GUIScripts/GUIREC.py
@@ -873,7 +873,7 @@ def OpenKitInfoWindow ():
 	ClassName = GUICommon.GetClassRowName (pc)
 	Multi = GUICommon.HasMultiClassBits (pc)
 	Dual = GUICommon.IsDualClassed (pc, 1)
-
+	text = ""
 	if Multi and Dual[0] == 0: # true multi class
 		text = CommonTables.Classes.GetValue (ClassName, "DESC_REF")
 	else:
@@ -884,12 +884,10 @@ def OpenKitInfoWindow ():
 			if Dual[0] == 1:
 				text = CommonTables.KitList.GetValue (Dual[1], 3)
 			elif Dual[0] == 2:
-				text = GUICommon.GetClassRowName(Dual[1], "index")
-				text += CommonTables.Classes.GetValue (text, "DESC_REF")
+				text = CommonTables.Classes.GetValue (GUICommon.GetClassRowName(Dual[1], "index"), "DESC_REF")
 	
 			text += "\n"
-			text += GUICommon.GetClassRowName(Dual[2], "index")
-			text += CommonTables.Classes.GetValue (text, "DESC_REF")
+			text += CommonTables.Classes.GetValue (GUICommon.GetClassRowName(Dual[2], "index"), "DESC_REF")
 	
 		else: # ordinary class or kit
 			if KitIndex:
