gemrb/gemrb

commit 5fbd7b13bfefc24b2752b4e7b6096c82ad3bf711

TextArea: implement markup parsing

gemrb/GUIScripts/iwd2/GUIREC.py
@@ -311,9 +311,7 @@ def DisplayCommon (pc):
 	return
 
 def DisplaySavingThrows (pc):
-	RecordsTextArea.Append ("\n\n[color=ffff00]")
-	RecordsTextArea.Append (17379)
-	RecordsTextArea.Append ("[/color]\n")
+	RecordsTextArea.Append ("\n\n[color=ffff00]" + GemRB.GetString(17379) + "[/color]\n")
 
 	tmp = GemRB.GetPlayerStat (pc, IE_SAVEFORTITUDE)
 	RecordsTextArea.Append (delimited_txt(17380, ":", PlusMinusStat(tmp)))
@@ -336,15 +334,11 @@ def DisplayGeneral (pc):
 	Window = RecordsWindow
 
 	#levels
-	RecordsTextArea.Append ("[color=ffff00]")
-	RecordsTextArea.Append (40308)
-	RecordsTextArea.Append (" - ")
-	RecordsTextArea.Append (40309)
-	levelsum = GemRB.GetPlayerStat (pc, IE_CLASSLEVELSUM)
 	# get special level penalty for subrace
 	adj = GetECL (pc)
-	RecordsTextArea.Append (": "+str(levelsum) )
-	RecordsTextArea.Append ("[/color]")
+	RecordsTextArea.Append ("[color=ffff00]" + GemRB.GetString(40308) + " - " + GemRB.GetString(40309) + ": " + str(levelsum) + "[/color]")
+	levelsum = GemRB.GetPlayerStat (pc, IE_CLASSLEVELSUM)
+
 	#the class name for highest
 	highest = None
 	tmp = 0
@@ -383,9 +377,7 @@ def DisplayGeneral (pc):
 	RecordsTextArea.Append (tmp)
 
 	#experience
-	RecordsTextArea.Append ("\n\n[color=ffff00]")
-	RecordsTextArea.Append (17089)
-	RecordsTextArea.Append ("[/color]")
+	RecordsTextArea.Append ("\n\n[color=ffff00]" + GemRB.GetString(17089) + "[/color]")
 
 	RecordsTextArea.Append (36928,-1)
 	xp = GemRB.GetPlayerStat (pc, IE_XP)
@@ -397,28 +389,22 @@ def DisplayGeneral (pc):
 	#current effects
 	effects = GemRB.GetPlayerStates (pc)
 	if len(effects):
-		RecordsTextArea.Append ("\n\n[color=ffff00]")
-		RecordsTextArea.Append (32052)
-		RecordsTextArea.Append ("[/color]")
+		RecordsTextArea.Append ("\n\n[color=ffff00]" + GemRB.GetString(32052) + "[/color]")
 		StateTable = GemRB.LoadTable ("statdesc")
 		for c in effects:
 			tmp = StateTable.GetValue (str(ord(c)-66), "DESCRIPTION")
-			RecordsTextArea.Append ("[capital=2]"+c+" ", -1)
+			RecordsTextArea.Append ("[cap]"+c+"[/cap] ", -1)
 			RecordsTextArea.Append (tmp)
 
 	# TODO: Active Feats (eg. Power attack 4)
 
 	#race
-	RecordsTextArea.Append ("\n\n[capital=0][color=ffff00]")
-	RecordsTextArea.Append (1048)
-	RecordsTextArea.Append ("[/color]")
+	RecordsTextArea.Append ("\n\n[color=ffff00]" + GemRB.GetString(1048) + "[/color]")
 
 	RecordsTextArea.Append (Race,-1)
 
 	#alignment
-	RecordsTextArea.Append ("\n\n[color=ffff00]")
-	RecordsTextArea.Append (1049)
-	RecordsTextArea.Append ("[/color]")
+	RecordsTextArea.Append ("\n\n[color=ffff00]" + GemRB.GetString(1049) + "[/color]")
 	tmp = CommonTables.Aligns.FindValue (3, GemRB.GetPlayerStat (pc, IE_ALIGNMENT))
 	Align = CommonTables.Aligns.GetValue (tmp, 2)
 	RecordsTextArea.Append (Align,-1)
@@ -428,9 +414,7 @@ def DisplayGeneral (pc):
 
 	#class features
 	if HasClassFeatures(pc):
-		RecordsTextArea.Append ("\n\n[color=ffff00]")
-		RecordsTextArea.Append (40314)
-		RecordsTextArea.Append ("[/color]")
+		RecordsTextArea.Append ("\n\n[color=ffff00]" + GemRB.GetString(40314) + "[/color]")
 		tmp = GemRB.GetPlayerStat (pc, IE_TURNUNDEADLEVEL)
 		if tmp:
 			RecordsTextArea.Append (12126,-1)
@@ -457,12 +441,12 @@ def DisplayGeneral (pc):
 	# favoured enemies; eg Goblins: +2 & Harpies: +1
 	RangerLevel = GemRB.GetPlayerStat (pc, IE_LEVELRANGER)
 	if RangerLevel:
-		RecordsTextArea.Append ("\n\n[color=ffff00]")
+		RangerString = "\n\n[color=ffff00]"
 		if RangerLevel > 5:
-			RecordsTextArea.Append (15982)
+			RangerString += GemRB.GetString (15982)
 		else:
-			RecordsTextArea.Append (15897)
-		RecordsTextArea.Append ("[/color]\n")
+			RangerString += GemRB.GetString (15897)
+		RecordsTextArea.Append (RangerString + "[/color]\n")
 		DisplayFavouredEnemy (pc, RangerLevel)
 		for i in range (7):
 			DisplayFavouredEnemy (pc, RangerLevel, i)
@@ -470,9 +454,7 @@ def DisplayGeneral (pc):
 	#bonus spells
 	bonusSpells, classes = GetBonusSpells(pc)
 	if len(bonusSpells):
-		RecordsTextArea.Append ("\n\n[color=ffff00]")
-		RecordsTextArea.Append (10344)
-		RecordsTextArea.Append ("[/color]")
+		RecordsTextArea.Append ("\n\n[color=ffff00]" + GemRB.GetString(10344) + "[/color]")
 		for c in classes:
 			if not len(bonusSpells[c]):
 				continue
@@ -485,9 +467,7 @@ def DisplayGeneral (pc):
 				RecordsTextArea.Append (delimited_txt(7192, " " + str(level+1)+":", "+" + str(bonusSpells[c][level]), 0))
 
 	#ability statistics
-	RecordsTextArea.Append ("\n\n[color=ffff00]")
-	RecordsTextArea.Append (40315)
-	RecordsTextArea.Append ("[/color]\n")
+	RecordsTextArea.Append ("\n\n[color=ffff00]" + GemRB.GetString(40315) + "[/color]\n")
 
 	# Weight Allowance
 	tmp = GemRB.GetAbilityBonus( IE_STR, 3, GemRB.GetPlayerStat(pc, IE_STR) )
@@ -544,9 +524,7 @@ def DisplayGeneral (pc):
 			if damage == -1 or damage-mod <= 0:
 				continue
 			if not DisplayedHeader:
-				RecordsTextArea.Append ("\n\n[capital=0][color=ffff00]")
-				RecordsTextArea.Append (39325)
-				RecordsTextArea.Append ("[/color]\n")
+				RecordsTextArea.Append ("\n\n[color=ffff00]" + GemRB.GetString(39325) + "[/color]\n")
 				DisplayedHeader = 1
 
 			enchantment += 1 # since we were checking what is allowable, not what bypasses it
@@ -731,9 +709,7 @@ def DisplayWeapons (pc):
 
 	###################
 	# Attack Roll Modifiers
-	RecordsTextArea.Append ("[color=ffff00]")
-	RecordsTextArea.Append (9457)
-	RecordsTextArea.Append ("[/color]\n")
+	RecordsTextArea.Append ("[color=ffff00]" + GemRB.GetString(9457) + "[/color]\n")
 
 	combatdet = GemRB.GetCombatDetails(pc, 0)
 	combatdet2 = combatdet # placeholder for the potential offhand numbers
@@ -762,9 +738,7 @@ def DisplayWeapons (pc):
 
 	###################
 	# Armor Class
-	RecordsTextArea.Append ("[color=ffff00]")
-	RecordsTextArea.Append (33553)
-	RecordsTextArea.Append ("[/color]\n")
+	RecordsTextArea.Append ("[color=ffff00]" + GemRB.GetString(33553) + "[/color]\n")
 	RecordsTextArea.Append (delimited_txt (33553, ":", str (GS(IE_ARMORCLASS)), 0)) # same as ac["Total"]
 
 	# Base
@@ -801,9 +775,7 @@ def DisplayWeapons (pc):
 	# Armor Class Modifiers
 	stat = GS (IE_ACMISSILEMOD) + GS (IE_ACSLASHINGMOD) + GS (IE_ACPIERCINGMOD) + GS (IE_ACCRUSHINGMOD)
 	if stat:
-		RecordsTextArea.Append ("[color=ffff00]")
-		RecordsTextArea.Append (11766)
-		RecordsTextArea.Append ("[/color]")
+		RecordsTextArea.Append ("[color=ffff00]" + GemRB.GetString(11766) + "[/color]")
 
 		# Missile
 		if GS (IE_ACMISSILEMOD):
@@ -827,9 +799,7 @@ def DisplayWeapons (pc):
 	###################
 	# Arcane spell failure
 	if GS(IE_LEVELBARD) + GS(IE_LEVELSORCERER) + GS(IE_LEVELMAGE):
-		RecordsTextArea.Append ("[color=ffff00]")
-		RecordsTextArea.Append (41391)
-		RecordsTextArea.Append ("[/color]\n")
+		RecordsTextArea.Append ("[color=ffff00]" + GemRB.GetString(41391) + "[/color]\n")
 
 		# Casting Failure
 		failure = GemRB.GetSpellFailure (pc)
@@ -866,9 +836,7 @@ def DisplayWeapons (pc):
 
 	###################
 	# Weapon Statistics
-	RecordsTextArea.Append ("[color=ffff00]")
-	RecordsTextArea.Append (41119)
-	RecordsTextArea.Append ("[/color]\n")
+	RecordsTextArea.Append ("[color=ffff00]" + GemRB.GetString(41119) + "[/color]\n")
 
 	# Main hand
 	WeaponOfHand(pc, combatdet, dualwielding)
@@ -890,9 +858,7 @@ def DisplaySkills (pc):
 	rows = SkillTable.GetRowCount ()
 
 	#skills
-	RecordsTextArea.Append ("[color=ffff00]")
-	RecordsTextArea.Append (11983)
-	RecordsTextArea.Append ("[/color]\n")
+	RecordsTextArea.Append ("[color=ffff00]" + GemRB.GetString(11983) + "[/color]\n")
 
 	skills = []
 	for i in range(rows):
@@ -914,9 +880,7 @@ def DisplaySkills (pc):
 	FeatName = GemRB.LoadTable ("feats")
 	rows = FeatTable.GetRowCount ()
 	#feats
-	RecordsTextArea.Append ("\n\n[color=ffff00]")
-	RecordsTextArea.Append (36361)
-	RecordsTextArea.Append ("[/color]\n")
+	RecordsTextArea.Append ("\n\n[color=ffff00]" + GemRB.GetString(36361) + "[/color]\n")
 
 	feats = []
 	for i in range(rows):
@@ -966,18 +930,14 @@ def DisplayMisc (pc):
 	stat = GemRB.GetPCStats (pc)
 
 	#favourites
-	RecordsTextArea.Append ("[color=ffff00]")
-	RecordsTextArea.Append (40320)
-	RecordsTextArea.Append ("[/color]\n")
+	RecordsTextArea.Append ("[color=ffff00]" + GemRB.GetString(40320) + "[/color]\n")
 
 	#favourite spell and weapon
 	RecordsTextArea.Append (delimited_str (11949, ":", stat['FavouriteSpell']))
 	RecordsTextArea.Append (delimited_str (11950, ":", stat['FavouriteWeapon']))
 
 	# combat details
-	RecordsTextArea.Append ("\n[color=ffff00]")
-	RecordsTextArea.Append (40322)
-	RecordsTextArea.Append ("[/color]\n")
+	RecordsTextArea.Append ("\n[color=ffff00]" + GemRB.GetString(40322) + "[/color]\n")
 
 	#most powerful vanquished, time spent, xp and kills
 	RecordsTextArea.Append (delimited_str (11947, ":", stat['BestKilledName']))
@@ -1128,7 +1088,6 @@ def UpdateRecordsWindow ():
 
 	RecordsTextArea = Window.GetControl (45)
 	RecordsTextArea.SetText ("")
-	RecordsTextArea.Append ("[capital=0]")
 
 	DisplayCommon (pc)
 

gemrb/GUIScripts/GUIREC.py
@@ -679,43 +679,43 @@ def GetStatOverview (pc, LevelDiff=[0,0,0]):
 			if val == 0 and type != '0':
 				continue
 			if type == '+': #pluses
-				res.append ("[capital=0]"+GemRB.GetString (strref) + ' '+ '+' * val)
+				res.append (GemRB.GetString (strref) + ' '+ '+' * val)
 			elif type == 'p': #a plus prefix if positive
 				if val > 0:
-					res.append ("[capital=0]" + GemRB.GetString (strref) + ' +' + str (val) )
+					res.append (GemRB.GetString (strref) + ' +' + str (val) )
 				else:
-					res.append ("[capital=0]" + GemRB.GetString (strref) + ' ' + str (val) )
+					res.append (GemRB.GetString (strref) + ' ' + str (val) )
 			elif type == 'r': #a plus prefix if positive, strref is an already resolved string
 				if val > 0:
-					res.append ("[capital=0]" + strref + ' +' + str (val) )
+					res.append (strref + ' +' + str (val) )
 				else:
-					res.append ("[capital=0]" + strref + ' ' + str (val) )
+					res.append (strref + ' ' + str (val) )
 			elif type == 's': #both base and (modified) stat, but only if they differ
 				base = GB (val)
 				stat = GS (val)
-				base_str = "[capital=0]" + GemRB.GetString (strref) + ': ' + str(stat)
+				base_str = GemRB.GetString (strref) + ': ' + str(stat)
 				if base == stat:
 					res.append (base_str)
 				else:
 					res.append (base_str + " (" + str(stat-base) + ")")
 			elif type == 'x': #x character before value
-				res.append ("[capital=0]"+GemRB.GetString (strref) +': x' + str (val) )
+				res.append (GemRB.GetString (strref) +': x' + str (val) )
 			elif type == 'a': #value (portrait icon) + string
-				res.append ("[capital=2]"+val+" "+GemRB.GetString (strref))
+				res.append ("[cap]"+val+"[/cap] "+GemRB.GetString (strref))
 			elif type == 'b': #strref is an already resolved string
-				res.append ("[capital=0]"+strref+": "+str (val))
+				res.append (strref+": "+str (val))
 			elif type == 'c': #normal string
-				res.append ("[capital=0]"+GemRB.GetString (strref))
+				res.append (GemRB.GetString (strref))
 			elif type == 'd': #strref is an already resolved string
-				res.append ("[capital=0]"+strref)
+				res.append (strref)
 			elif type == '0': #normal value
 				res.append (GemRB.GetString (strref) + ': ' + str (val))
 			else: #normal value + type character, for example percent sign
-				res.append ("[capital=0]"+GemRB.GetString (strref) + ': ' + str (val) + type)
+				res.append (GemRB.GetString (strref) + ': ' + str (val) + type)
 			lines = 1
 		except:
 			if s != None:
-				res.append ("[capital=0]"+ GemRB.GetString (s) )
+				res.append (GemRB.GetString (s) )
 				lines = 0
 			else:
 				if not lines and str_None != -1:

gemrb/GUIScripts/iwd2/CharOverview.py
@@ -155,9 +155,7 @@ def UpdateOverview(CurrentStep):
 		AddText(Tables[2].GetValue(GemRB.GetVar('Alignment') - 1, 0))
 	
 	if GemRB.GetVar('Ability 0') > 0:
-		AddText('\n[color=FFFF00]', -1)
-		AddText(17088)
-		AddText('[/color]')
+		AddText('\n[color=FFFF00]' + GemRB.GetString(17088) + '[/color]')
 		for i in range(0, 6):
 			strref = Tables[3].GetValue(i, 2)
 			AddText(strref, -1)
@@ -165,9 +163,7 @@ def UpdateOverview(CurrentStep):
 			AddText(': %d (%+d)' % (abl, abl / 2 - 5))
 	
 	if CurrentStep > 6:
-		AddText('\n[color=FFFF00]', -1)
-		AddText(11983)
-		AddText('[/color]')
+		AddText('\n[color=FFFF00]' + GemRB.GetString(11983) + '[/color]')
 		
 		ClassColumn = Tables[1].GetValue(GemRB.GetVar('Class') - 1, 3, 1) # Finds base class row id
 		if ClassColumn < 1: ClassColumn = GemRB.GetVar('Class') - 1 # If 0 then already a base class so need actual row
@@ -205,9 +201,7 @@ def UpdateOverview(CurrentStep):
 				if value != Ranks: strn += ' (' + str(Ranks) + ')'
 				AddText(strn)
 
-		AddText('\n[color=FFFF00]', -1)
-		AddText(36310)
-		AddText('[/color]')
+		AddText('\n[color=FFFF00]' + GemRB.GetString(36310) + '[/color]')
 		
 		for i in range(Tables[6].GetRowCount()):
 			value = GemRB.GetVar('Feat ' + str(i))

gemrb/core/GUI/TextArea.cpp
@@ -172,33 +172,145 @@ void TextArea::AppendText(const char* text)
 
 void TextArea::AppendText(String* text)
 {
-	// TODO: parse the string tags ([color],[p],etc) into spans
-	//TextSpan* span = new TextSpan(string, ftext, palette, const Size& frame, 0);
-	if (finit) {
-		// FIXME: this assumes that finit is always for drop caps (not just a diffrent looking font)
-		// append drop cap spans
-		size_t whitespace = text->find_first_not_of(L"\n\t\r ");
-		if (whitespace != String::npos) {
-			// ensure we actually have text!
-			TextSpan* dc = new TextSpan(text->substr(0, 1), finit, NULL);
-			textContainer->AppendSpan(dc);
-			text->erase(0, 1);
-			// FIXME: the instances of the hard coded numbers are arbitrary padding values
-			Size s = Size(Width - dc->SpanFrame().w, GetRowHeight());
-			TextSpan* span = new TextSpan(*text, ftext, palette, s, IE_FONT_ALIGN_LEFT);
+	assert(text && text->length());
+
+	size_t tagPos = text->find_first_of('[');
+	if (tagPos != String::npos) {
+		// parse the text looking for accepted tags ([cap], [color], [p])
+		if (tagPos != 0) {
+			// handle any text before the markup
+			TextSpan* span = new TextSpan(text->substr(0, tagPos), ftext, palette);
 			textContainer->AppendSpan(span);
-			s.w -= 8; // FIXME: arbitrary padding
-			s.h = dc->SpanFrame().h - dc->SpanDescent() - GetRowHeight() + span->SpanDescent() + 5;
-			size_t textLen = span->RenderedString().length() - 1;
-			span = new TextSpan(text->substr(textLen), ftext, palette, s, IE_FONT_ALIGN_LEFT);
-			textContainer->AppendSpan(span);
-			textContainer->SetSpanPadding(span, Size(8, 0)); // FIXME: this is arbitrary
-			textLen += span->RenderedString().length() - 1;
-			// erase what has been handled
-			text->erase(0, textLen);
 		}
+
+		// span properties
+		Color palCol;
+		Palette* pal = NULL;
+		Font* fnt = ftext;
+		Size frame;
+
+		enum ParseState {
+			TEXT = 0,
+			OPEN_TAG,
+			CLOSE_TAG,
+			COLOR
+		};
+
+		TextSpan* lastSpan = NULL;
+		String token;
+		ParseState state = TEXT;
+		String::iterator it = text->begin() + tagPos;
+		for (; it != text->end(); it++) {
+			switch (state) {
+				case OPEN_TAG:
+					switch (*it) {
+						case '=':
+							if (token == L"color") {
+								state = COLOR;
+								token.clear();
+							}
+							// else is a parse error...
+							continue;
+						case ']':
+							if (token == L"cap") {
+								fnt = finit;
+							} else if (token == L"p") {
+								int w = Width;
+								if (lastSpan) {
+									w -= lastSpan->SpanFrame().w;
+								}
+								frame.w = w;
+							}
+							state = TEXT;
+							token.clear();
+							continue;
+					}
+					break;
+				case CLOSE_TAG:
+					switch (*it) {
+						case ']':
+							if (token == L"color") {
+								gamedata->FreePalette(pal);
+							} else if (token == L"cap") {
+								fnt = ftext;
+							} else if (token == L"p") {
+								frame.w = 0;
+							}
+							state = TEXT;
+							token.clear();
+							continue;
+					}
+					break;
+				case TEXT:
+					switch (*it) {
+						case '[':
+							if (token.length()) {
+								// TODO: surely we need some alignment flags?
+								TextSpan* span = new TextSpan(token, fnt, pal, frame, 0);
+								textContainer->AppendSpan(span);
+								lastSpan = span;
+								token.clear();
+							}
+							if (*++it == '/')
+								state = CLOSE_TAG;
+							else {
+								it--;
+								state = OPEN_TAG;
+							}
+							continue;
+					}
+					break;
+				case COLOR:
+					switch (*it) {
+						case L']':
+							swscanf(token.c_str(), L"%02X%02X%02X", &palCol.r, &palCol.g, &palCol.b);
+							pal = core->CreatePalette(palCol, palette->back);
+							state = TEXT;
+							token.clear();
+							continue;
+					}
+					break;
+				default: // parse error, not clearing token
+					state = TEXT;
+					break;
+			}
+			token += *it;
+		}
+		assert(pal == NULL && state == TEXT);
+		if (token.length()) {
+			// there was some text at the end without markup
+			TextSpan* span = new TextSpan(token, ftext, palette);
+			textContainer->AppendSpan(span);
+		}
+	} else {
+		if (finit) {
+			// FIXME: this assumes that finit is always for drop caps (not just a diffrent looking font)
+			// append drop cap spans
+			size_t cappos = text->find_first_not_of(L"\n\t\r ");
+			if (cappos != String::npos) {
+				// ensure we actually have text!
+				TextSpan* dc = new TextSpan(text->substr(cappos, 1), finit, NULL);
+				textContainer->AppendSpan(dc);
+				text->erase(0, cappos + 1);
+				// FIXME: assuming we have more text!
+				// FIXME: the instances of the hard coded numbers are arbitrary padding values
+				Size s = Size(Width - dc->SpanFrame().w, GetRowHeight());
+				TextSpan* span = new TextSpan(*text, ftext, palette, s, IE_FONT_ALIGN_LEFT);
+				textContainer->AppendSpan(span);
+				s.w -= 8; // FIXME: arbitrary padding
+				s.h = dc->SpanFrame().h - dc->SpanDescent() - GetRowHeight() + span->SpanDescent() + 5;
+				size_t textLen = span->RenderedString().length() - 1;
+				span = new TextSpan(text->substr(textLen), ftext, palette, s, IE_FONT_ALIGN_LEFT);
+				textContainer->AppendSpan(span);
+				textContainer->SetSpanPadding(span, Size(8, 0)); // FIXME: this is arbitrary
+				textLen += span->RenderedString().length() - 1;
+				// erase what has been handled
+				text->erase(0, textLen);
+			}
+		}
+		textContainer->AppendText(*text);
 	}
-	textContainer->AppendText(*text);
+
 	// TODO: we will need to keep the string for editable TextAreas
 	delete text; // we own this, but don't need it now
 	UpdateControls();

commit 041bbb2c9bdcbb5acb509ac4b0b75f9205e6f1bf

iwd2: fixed regression from 5fbd7b13bf

gemrb/GUIScripts/iwd2/GUIREC.py
@@ -336,8 +336,9 @@ def DisplayGeneral (pc):
 	#levels
 	# get special level penalty for subrace
 	adj = GetECL (pc)
-	RecordsTextArea.Append ("[color=ffff00]" + GemRB.GetString(40308) + " - " + GemRB.GetString(40309) + ": " + str(levelsum) + "[/color]")
 	levelsum = GemRB.GetPlayerStat (pc, IE_CLASSLEVELSUM)
+	RecordsTextArea.Append ("[color=ffff00]" + GemRB.GetString(40308) + " - " +
+                            GemRB.GetString(40309) + ": " + str(levelsum) + "[/color]")
 
 	#the class name for highest
 	highest = None