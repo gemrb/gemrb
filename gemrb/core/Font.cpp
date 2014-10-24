gemrb/gemrb

commit b4ca537d7c5fd10724d1c00cf02f2a87a127d095

Font: fix some bugs in StringSize
we were only rewinding to the first space when a word didnt fit, need to get all the contiguous whitespace between words
we were alos potentially sizing spaces twice if a string ended in a space.
we could potentially return 1 more cahracter processed than actually fit (we only need to convert i if we bail early)

gemrb/core/Font.cpp
@@ -542,7 +542,7 @@ Size Font::StringSize(const String& string, const Size* stop, size_t* numChars) 
 	ieWord w = 0, lines = 1;
 	ieWord lineW = 0, wordW = 0, spaceW = 0;
 	bool newline = false, eos = false, ws = false;
-	size_t i = 0, wordCharCount = 0;
+	size_t i = 0, wordCharCount = 0, wscount = 0;
 	for (; i < string.length(); i++) {
 		eos = (i == string.length() - 1);
 		ws = std::isspace(string[i]);
@@ -553,6 +553,7 @@ Size Font::StringSize(const String& string, const Size* stop, size_t* numChars) 
 				wordW -= KerningOffset(string[i-1], string[i]);
 			}
 			wordCharCount++;
+			wscount = 0;
 		}
 		if (ws || eos) {
 			if (stop && stop->w
@@ -569,7 +570,7 @@ Size Font::StringSize(const String& string, const Size* stop, size_t* numChars) 
 				lineW += wordW;
 				if (string[i] == L'\n') {
 					newline = true;
-				} else if (string[i] != L'\r') {
+				} else if (string[i] != L'\r' && ws) {
 					spaceW += GetGlyph(string[i]).size.w;
 				}
 				wordCharCount = 0;
@@ -581,9 +582,11 @@ Size Font::StringSize(const String& string, const Size* stop, size_t* numChars) 
 			w = (lineW > w) ? lineW : w;
 			if (stop && stop->h && (LineHeight * (lines + 1)) >= stop->h ) {
 				// adjust i for numChars
-				if (wordCharCount) {
-					i -= wordCharCount + 1; // +1 for the space
+				if (numChars && wordCharCount) {
+					// rewinding to the last word that fit
+					i -= wordCharCount + wscount;
 				}
+				i++; // +1 to convert from index to char count
 				break;
 			}
 			if (newline) {
@@ -594,7 +597,8 @@ Size Font::StringSize(const String& string, const Size* stop, size_t* numChars) 
 		}
 	}
 	if (numChars) {
-		*numChars = i + 1; // +1 to convert from index to char count
+		*numChars = i;
+		assert(*numChars <= string.length());
 	}
 
 	if (string.find_first_not_of(WHITESPACE_STRING) == String::npos) {

commit 3eb5dd25412345aa44bee57ee212c27ad23c1164

Font::SizeString: fix some problems with \n
1. if a \n is encountered, then everything preceeding it goes on the line
2. we shouldnt increment numChars for a line break caused by wrapping
3. implementing a less costly method of handling strings of all whitespace

this should fix a minor regression from b4ca537

gemrb/core/Font.cpp
@@ -580,17 +580,19 @@ Size Font::StringSize(const String& string, const Size* stop, size_t* numChars) 
 			) {
 				newline = true;
 			} else {
-				if (lineW) {
+				assert(newline == false);
+				if (string[i] == L'\n') {
+					newline = true;
+				} else if (ws && string[i] != L'\r') {
+					spaceW += GetGlyph(string[i]).size.w;
+				}
+				if (lineW || newline) {
+					// always append *everything* if there is \n
 					// spaceW is the *cumulative* whitespace between the 2 words
 					lineW += spaceW;
 					spaceW = 0;
 				}
 				lineW += wordW;
-				if (string[i] == L'\n') {
-					newline = true;
-				} else if (string[i] != L'\r' && ws) {
-					spaceW += GetGlyph(string[i]).size.w;
-				}
 				rewindCount = 0;
 				wordW = 0;
 			}
@@ -599,13 +601,7 @@ Size Font::StringSize(const String& string, const Size* stop, size_t* numChars) 
 		if (newline || eos) {
 			w = (lineW > w) ? lineW : w;
 			if (stop && stop->h && (LineHeight * (lines + 1)) >= stop->h ) {
-				// adjust i for numChars
-				if (numChars) {
-					// rewinding to the last word that fit
-					i -= rewindCount;
-					i++; // +1 to convert from index to char count
-				}
-				// TODO: check the rest of the line, if all that remains is WS + \n then set w to max.
+				if (eos || string[i] == L'\n') i++; // +1 to convert from index to char count, not applicable for wrap
 				break;
 			}
 			if (newline) {
@@ -615,14 +611,15 @@ Size Font::StringSize(const String& string, const Size* stop, size_t* numChars) 
 			}
 		}
 	}
+
+	w = (w == 0 && wordW == 0) ? spaceW : w; // if the line is all whitespace
+
 	if (numChars) {
-		*numChars = i;
+		// rewinding to the last word that fit
+		*numChars = i - rewindCount;
 		assert(*numChars <= string.length());
 	}
 
-	if (string.find_first_not_of(WHITESPACE_STRING) == String::npos) {
-		w += spaceW;
-	}
 	// this can only return w > stop->w if there is a singe word wider than stop.
 	return Size(w, (LineHeight * lines));
 }