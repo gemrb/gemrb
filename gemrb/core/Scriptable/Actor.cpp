gemrb/gemrb

commit 01fa3b490c40c1d8b112ba468d6c5bec1443f938

... but not if "Suppress Extra Difficulty Damage" is enabled

gemrb/core/Scriptable/Actor.cpp
@@ -6253,8 +6253,10 @@ void Actor::AddExperience(int exp, int combat)
 		//TODO find out the 3ED variant
 		exp = (exp * (100 + core->GetWisdomBonus(0, Modified[IE_WIS]))) / 100;
 	}
-	if (combat) {
-		exp += exp*xpadjustments[GameDifficulty]/100;
+	int adjustmentPercent = dmgadjustments[GameDifficulty];
+	// the "Suppress Extra Difficulty Damage" also switches off the XP bonus
+	if (combat && (!NoExtraDifficultyDmg || adjustmentPercent < 0)) {
+		exp += exp * adjustmentPercent/100;
 	}
 	SetBase(IE_XP,BaseStats[IE_XP]+exp);
 }

commit 7ea7c70c82f012248d8456d847d5703162257fe7

AddExperience: look up difficulty mod in the right table
regression from 01fa3b490c

gemrb/core/Scriptable/Actor.cpp
@@ -6953,7 +6953,7 @@ void Actor::AddExperience(int exp, int combat)
 	if (core->HasFeature(GF_WISDOM_BONUS)) {
 		exp = (exp * (100 + core->GetWisdomBonus(0, Modified[IE_WIS]))) / 100;
 	}
-	int adjustmentPercent = dmgadjustments[GameDifficulty];
+	int adjustmentPercent = xpadjustments[GameDifficulty];
 	// the "Suppress Extra Difficulty Damage" also switches off the XP bonus
 	if (combat && (!NoExtraDifficultyDmg || adjustmentPercent < 0)) {
 		exp += exp * adjustmentPercent/100;