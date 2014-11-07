45e98b4a1941f667d91d9f788a26206ee9953aea
Jens Granseuer <jensgr@gmx.net>
2014-11-07 21:57:39 +0100

    add support for movie overrides via mvesnd.2da
    as used for the ToB final movie, for example.

--- gemrb/core/Interface.cpp
+++ gemrb/core/Interface.cpp
@@ -3592,11 +3592,7 @@ bool Interface::DelSymbol(unsigned int index)
 /** Plays a Movie */
 int Interface::PlayMovie(const char* ResRef)
 {
-	ResourceHolder<MoviePlayer> mp(ResRef);
-	if (!mp) {
-		return -1;
-	}
-
+	const char *realResRef = ResRef;
 	ieDword subtitles = 0;
 	Font *SubtitleFont = NULL;
 	Palette *palette = NULL;
@@ -3642,6 +3638,28 @@ int Interface::PlayMovie(const char* ResRef)
 			}
 		}
 	}
+	
+	//check whether there is an override for this movie
+	const char *sound_resref = NULL;
+	AutoTable mvesnd;
+	if (mvesnd.load("mvesnd")) {
+		int row = mvesnd->GetRowIndex(ResRef);
+		if (row != -1) {
+			int mvecol = mvesnd->GetColumnIndex("override");
+			if (mvecol != -1) {
+				realResRef = mvesnd->QueryField(row, mvecol);
+			}
+			int sndcol = mvesnd->GetColumnIndex("sound_override");
+			if (sndcol != -1) {
+				sound_resref = mvesnd->QueryField(row, sndcol);
+			}
+		}
+	}
+
+	ResourceHolder<MoviePlayer> mp(realResRef);
+	if (!mp) {
+		return -1;
+	}
 
 	//shutting down music and ambients before movie
 	if (music)
@@ -3650,7 +3668,16 @@ int Interface::PlayMovie(const char* ResRef)
 	if (ambim) ambim->deactivate();
 	video->SetMovieFont(SubtitleFont, palette );
 	mp->CallBackAtFrames(cnt, frames, strrefs);
+
+	Holder<SoundHandle> sound_override;
+	if (sound_resref) {
+		sound_override = AudioDriver->Play(sound_resref);
+	}
 	mp->Play();
+	if (sound_override) {
+		sound_override->Stop();
+		sound_override.release();
+	}
 	gamedata->FreePalette( palette );
 	if (frames)
 		free(frames);
