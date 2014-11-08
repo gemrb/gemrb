gemrb/gemrb

commit feb2600ac73ea142ffc30223a24525a860537293

most of classes/clsskills named access conversion completed
only FindValue remains, but that needs internal changes first

gemrb/GUIScripts/iwd2/Enemy.py
@@ -18,6 +18,7 @@
 #
 #character generation, racial enemy (GUICG15)
 import GemRB
+import GUICommon
 import CommonTables
 from GUIDefines import *
 
@@ -51,9 +52,10 @@ def OnLoad():
 	
 	GemRB.LoadWindowPack("GUICG", 800 ,600)
 	RaceWindow = GemRB.LoadWindow(15)
-
 	Class = GemRB.GetVar("BaseClass")
-	TableName = CommonTables.ClassSkills.GetValue(Class, 0)
+	ClassName = GUICommon.GetClassRowName (Class, "index")
+	print "TODO: check if class %s really has an ID of %d" %(ClassName, Class)
+	TableName = CommonTables.ClassSkills.GetValue(ClassName, "RANGERSKILL")
 	if TableName == "*":
 		GemRB.SetNextScript("Skills")
 		return

gemrb/GUIScripts/Actor.py
@@ -20,6 +20,7 @@
 # Actor.py: Contains the actor class.
 
 import GemRB
+import GUICommon
 from GUIDefines import *
 from ie_stats import *
 #this import is primarily for the tables
@@ -55,12 +56,13 @@ class Actor:
 		dualswap = [0]*classcount
 
 		for i in range(classcount):
-			classid = CommonTables.Classes.GetValue (i, 5)
-			classnames = CommonTables.Classes.GetRowName(i).split("_")
+			rowname = CommonTables.Classes.GetRowName(i)
+			classid = CommonTables.Classes.GetValue (rowname, "ID")
+			classnames = rowname.split("_")
 
 			#set the MC_WAS_ID of the first class
 			if len(classnames) == 2:
-				dualswap[classid-1] = CommonTables.Classes.GetValue (i, 8)
+				dualswap[classid-1] = CommonTables.Classes.GetValue (rowname, "MC_WAS_ID")
 
 	def Classes (self):
 		"""Returns a list with all the class IDs."""
@@ -74,7 +76,7 @@ class Actor:
 		"""Returns a list will all the class names."""
 
 		if self.__classnames == None:
-			self.__classnames = CommonTables.Classes.GetRowName (CommonTables.Classes.FindValue (5, self.classid) ).split("_")
+			self.__classnames = GUICommon.GetClassRowName (self.classid, "class").split("_")
 			if self.IsDualSwap():
 				self.__classnames.reverse()
 		return self.__classnames
@@ -89,24 +91,23 @@ class Actor:
 
 		if self.__classtitle == 0:
 			if self.multiclass and self.isdual == 0:
-				self.__classtitle = CommonTables.Classes.GetValue (self.classindex, 2)
+				self.__classtitle = CommonTables.Classes.GetValue ("_".join(self.__classnames), "CAP_REF")
 				self.__classtitle = GemRB.GetString (self.__classtitle)
 			elif self.isdual:
 				# first (previous) kit or class of the dual class
 				self.Classes()
+				self.ClassNames()
 				if self.KitIndex():
 					self.__classtitle = CommonTables.KitList.GetValue (self.__kitindex, 2)
 				else:
-					self.__classtitle = CommonTables.Classes.GetValue (CommonTables.Classes.FindValue \
-						(5, self.__classes[1]), 2)
+					self.__classtitle = CommonTables.Classes.GetValue (self.__classnames[1], "CAP_REF")
 				self.__classtitle = GemRB.GetString (self.__classtitle) + " / " + \
-					GemRB.GetString (CommonTables.Classes.GetValue (CommonTables.Classes.FindValue \
-						(5, self.__classes[0]), 2) )
+					GemRB.GetString (CommonTables.Classes.GetValue (self.__classnames[0], "CAP_REF") )
 			else: # ordinary class or kit
 				if self.KitIndex():
 					self.__classtitle = CommonTables.KitList.GetValue (self.__kitindex, 2)
 				else:
-					self.__classtitle = CommonTables.Classes.GetValue (self.classindex, 2)
+					self.__classtitle = CommonTables.Classes.GetValue ("_".join(self.__classnames), "CAP_REF")
 				self.__classtitle = GemRB.GetString (self.__classtitle)
 
 		if self.__classtitle == "*":
@@ -210,9 +211,8 @@ class Actor:
 		#accessible variables
 		self.pc = pc
 		self.classid = GemRB.GetPlayerStat (self.pc, IE_CLASS)
-		self.classindex = CommonTables.Classes.FindValue (5, self.classid)
 		self.isdual = GemRB.GetPlayerStat (self.pc, IE_MC_FLAGS) & MC_WAS_ANY_CLASS
-		self.multiclass = CommonTables.Classes.GetValue (self.classindex, 4)
+		self.multiclass = CommonTables.Classes.GetValue (GUICommon.GetClassRowName (pc), "MULTI")
 
 		#internal variables - these are only intialized on the first
 		#call to their respective function, and stored thereafter

gemrb/GUIScripts/DualClass.py
@@ -202,7 +202,8 @@ def DCMainDonePress ():
 	GemRB.SetPlayerStat (pc, IE_TOHIT, ThacoTable.GetValue (NewClassId-1, 0, 1))
 
 	# new saves
-	SavesTable = CommonTables.Classes.GetValue (CommonTables.Classes.FindValue (5, NewClassId), 3, 0)
+	ClassName = GUICommon.GetClassRowName (NewClassId, "class")
+	SavesTable = CommonTables.Classes.GetValue (ClassName, "SAVE", 0)
 	SavesTable = GemRB.LoadTable (SavesTable)
 	for i in range (5):
 		GemRB.SetPlayerStat (pc, IE_SAVEVSDEATH+i, SavesTable.GetValue (i, 0))

gemrb/GUIScripts/GUICommon.py
@@ -409,14 +409,13 @@ def GetActorClassTitle (actor):
 	ClassTitle = GemRB.GetPlayerStat (actor, IE_TITLE1)
 
 	if ClassTitle == 0:
-		Class = GemRB.GetPlayerStat (actor, IE_CLASS)
-		ClassIndex = CommonTables.Classes.FindValue ( 5, Class )
+		ClassName = GetClassRowName (actor)
 		KitIndex = GetKitIndex (actor)
 		Multi = HasMultiClassBits (actor)
 		Dual = IsDualClassed (actor, 1)
 
 		if Multi and Dual[0] == 0: # true multi class
-			ClassTitle = CommonTables.Classes.GetValue (ClassIndex, 2)
+			ClassTitle = CommonTables.Classes.GetValue (ClassName, "CAP_REF")
 			ClassTitle = GemRB.GetString (ClassTitle)
 		else:
 			if Dual[0]: # dual class
@@ -424,14 +423,14 @@ def GetActorClassTitle (actor):
 				if Dual[0] == 1:
 					ClassTitle = CommonTables.KitList.GetValue (Dual[1], 2)
 				elif Dual[0] == 2:
-					ClassTitle = CommonTables.Classes.GetValue (Dual[1], 2)
+					ClassTitle = CommonTables.Classes.GetValue (GetClassRowName(Dual[1], "index"), "CAP_REF")
 				ClassTitle = GemRB.GetString (ClassTitle) + " / "
-				ClassTitle += GemRB.GetString (CommonTables.Classes.GetValue (Dual[2], 2))
+				ClassTitle += GemRB.GetString (CommonTables.Classes.GetValue (GetClassRowName(Dual[2], "index"), "CAP_REF"))
 			else: # ordinary class or kit
 				if KitIndex:
 					ClassTitle = CommonTables.KitList.GetValue (KitIndex, 2)
 				else:
-					ClassTitle = CommonTables.Classes.GetValue (ClassIndex, 2)
+					ClassTitle = CommonTables.Classes.GetValue (ClassName, "CAP_REF")
 				if ClassTitle != "*":
 					ClassTitle = GemRB.GetString (ClassTitle)
 	else:
@@ -449,7 +448,6 @@ def GetKitIndex (actor):
 
 	Returns 0 if the class is not kitted."""
 
-	Class = GemRB.GetPlayerStat (actor, IE_CLASS)
 	Kit = GemRB.GetPlayerStat (actor, IE_KIT)
 	KitIndex = 0
 
@@ -466,9 +464,18 @@ def GetKitIndex (actor):
 	return KitIndex
 
 # fetches the rowname of the passed actor's (base) class from classes.2da
-def GetClassRowName(actor):
-	Class = GemRB.GetPlayerStat (actor, IE_CLASS)
-	ClassIndex = CommonTables.Classes.FindValue (5, Class)
+def GetClassRowName(value, which=-1):
+	if which == "index":
+		ClassIndex = value
+		# if barbarians cause problems, repeat the lookup again here
+	else:
+		if which == -1:
+			Class = GemRB.GetPlayerStat (value, IE_CLASS)
+		elif which == "class":
+			Class = value
+		else:
+			raise RuntimeError, "Bad type parameter for GetClassRowName: ", which
+		ClassIndex = CommonTables.Classes.FindValue (5, Class)
 	ClassRowName = CommonTables.Classes.GetRowName (ClassIndex)
 	return ClassRowName
 
@@ -556,22 +563,18 @@ def IsDualSwap (actor):
 
 	# split the full class name into its individual parts
 	# i.e FIGHTER_MAGE becomes [FIGHTER, MAGE]
-	Class = GemRB.GetPlayerStat (actor, IE_CLASS)
-	Class = CommonTables.Classes.FindValue (5, Class)
-	Class = CommonTables.Classes.GetRowName (Class)
-	Class = Class.split("_")
+	Class = GetClassRowName(actor).split("_")
 
 	# get our old class name
 	if Dual[0] == 2:
-		BaseClass = CommonTables.Classes.GetRowName (Dual[1])
+		BaseClass = GetClassRowName(Dual[1], "index")
 	else:
 		BaseClass = GetKitIndex (actor)
 		BaseClass = CommonTables.KitList.GetValue (BaseClass, 7)
 		if BaseClass == "*":
 			# mod boilerplate
 			return 0
-		BaseClass = CommonTables.Classes.FindValue (5, BaseClass)
-		BaseClass = CommonTables.Classes.GetRowName (BaseClass)
+		BaseClass = GetClassRowName(BaseClass, "index")
 
 	# if our old class is the first class, we need to swap
 	if Class[0] == BaseClass:
@@ -612,7 +615,7 @@ def IsMultiClassed (actor, verbose):
 		if IsMulti&Mask: # it's part of this class
 			#we need to place the classes in the array based on their order in the name,
 			#NOT the order they are detected in
-			CurrentName = CommonTables.Classes.GetRowName (CommonTables.Classes.FindValue (5, i))
+			CurrentName = GetClassRowName (i, "class")
 			if CurrentName == "*":
 				# we read too far, as the upper range limit is greater than the number of "single" classes
 				break

gemrb/GUIScripts/GUICommonWindows.py
@@ -960,11 +960,15 @@ def GetKitIndex (actor, ClassIndex):
 	Kit = GemRB.GetPlayerStat (actor, IE_KIT)
 
 	KitIndex = -1
-	ClassID = CommonTables.Classes.GetValue (ClassIndex, 2)
+	ClassName = CommonTables.Classes.GetRowName (ClassIndex)
+	ClassID = CommonTables.Classes.GetValue (ClassName, "ID")
 	# skip the primary classes
+	# TODO: make more robust, mods could add new primaries
 	for ci in range (10, CommonTables.Classes.GetRowCount ()):
-		BaseClass = CommonTables.Classes.GetValue (ci, 3)
-		if BaseClass == ClassID and Kit & CommonTables.Classes.GetValue (ci, 2):
+		RowName = CommonTables.Classes.GetRowName (ci)
+		BaseClass = CommonTables.Classes.GetValue (RowName, "CLASS")
+		if BaseClass == ClassID and Kit & CommonTables.Classes.GetValue (RowName, "ID"):
+			#FIXME: this will return the last kit only, check if proper multikit return values are needed
 			KitIndex = ci
 
 	if KitIndex == -1:
@@ -979,9 +983,10 @@ def GetActorClassTitle (actor, ClassIndex):
 
 	KitIndex = GetKitIndex (actor, ClassIndex)
 	if KitIndex == 0:
-		ClassTitle = CommonTables.Classes.GetValue (ClassIndex, 0)
+		ClassName = CommonTables.Classes.GetRowName (ClassIndex)
 	else:
-		ClassTitle = CommonTables.Classes.GetValue (KitIndex, 0)
+		ClassName = CommonTables.Classes.GetRowName (KitIndex)
+	ClassTitle = CommonTables.Classes.GetValue (ClassName, "NAME_REF")
 
 	if ClassTitle == "*":
 		return 0

gemrb/GUIScripts/GUIREC.py
@@ -348,10 +348,9 @@ def GetStatOverview (pc, LevelDiff=[0,0,0]):
 		stats.append ( (19721,1,'c') )
 		stats.append (None)
 		for i in range (Multi[0]):
-			ClassIndex = CommonTables.Classes.FindValue (5, Multi[i+1])
-			ClassTitle = GemRB.GetString (CommonTables.Classes.GetValue (ClassIndex, 2))
+			Class = GUICommon.GetClassRowName (Multi[i+1], "class")
+			ClassTitle = GemRB.GetString (CommonTables.Classes.GetValue (Class, "CAP_REF"))
 			GemRB.SetToken ("CLASS", ClassTitle)
-			Class = CommonTables.Classes.GetRowName (ClassIndex)
 			GemRB.SetToken ("LEVEL", str (Levels[i]+LevelDiff[i]-int(LevelDrain/Multi[0])) )
 			GemRB.SetToken ("EXPERIENCE", str (XP/Multi[0]) )
 			if LevelDrain:
@@ -377,10 +376,10 @@ def GetStatOverview (pc, LevelDiff=[0,0,0]):
 
 		Levels[0] += LevelDiff[0]
 
-		ClassTitle = GemRB.GetString (CommonTables.Classes.GetValue (Dual[2], 2))
+		Class = CommonTables.Classes.GetRowName (Dual[2])
+		ClassTitle = GemRB.GetString (CommonTables.Classes.GetValue (Class, "CAP_REF"))
 		GemRB.SetToken ("CLASS", ClassTitle)
 		GemRB.SetToken ("LEVEL", str (Levels[0]-LevelDrain))
-		Class = CommonTables.Classes.GetRowName (Dual[2])
 		XP2 = GemRB.GetPlayerStat (pc, IE_XP)
 		GemRB.SetToken ("EXPERIENCE", str (XP2) )
 		if LevelDrain:
@@ -395,7 +394,8 @@ def GetStatOverview (pc, LevelDiff=[0,0,0]):
 		if Dual[0] == 1:
 			ClassTitle = GemRB.GetString (CommonTables.KitList.GetValue (Dual[1], 2))
 		elif Dual[0] == 2:
-			ClassTitle = GemRB.GetString (CommonTables.Classes.GetValue (Dual[1], 2))
+			ClassTitle = GUICommon.GetClassRowName(Dual[1], "index")
+			ClassTitle = GemRB.GetString (CommonTables.Classes.GetValue (ClassTitle, "CAP_REF"))
 		GemRB.SetToken ("CLASS", ClassTitle)
 		GemRB.SetToken ("LEVEL", str (Levels[1]) )
 
@@ -405,8 +405,7 @@ def GetStatOverview (pc, LevelDiff=[0,0,0]):
 		else:
 			BaseClass = GUICommon.GetKitIndex (pc)
 			BaseClass = CommonTables.KitList.GetValue (BaseClass, 7)
-			BaseClass = CommonTables.Classes.FindValue (5, BaseClass)
-			BaseClass = CommonTables.Classes.GetRowName (BaseClass)
+			BaseClass = GUICommon.GetClassRowName (BaseClass, "class")
 		# the first class' XP is discarded and set to the minimum level
 		# requirement, so if you don't dual class right after a levelup,
 		# the game would eat some of your XP
@@ -863,13 +862,12 @@ def OpenKitInfoWindow ():
 	TextArea = KitInfoWindow.GetControl (0)
 
 	pc = GemRB.GameGetSelectedPCSingle ()
-	Class = GemRB.GetPlayerStat (pc, IE_CLASS)
-	ClassIndex = CommonTables.Classes.FindValue (5, Class)
+	ClassName = GUICommon.GetClassRowName (pc)
 	Multi = GUICommon.HasMultiClassBits (pc)
 	Dual = GUICommon.IsDualClassed (pc, 1)
 
 	if Multi and Dual[0] == 0: # true multi class
-		text = CommonTables.Classes.GetValue (ClassIndex, 1)
+		text = CommonTables.Classes.GetValue (ClassName, "DESC_REF")
 		TextArea.SetText (text)
 		KitInfoWindow.ShowModal (MODAL_SHADOW_GRAY)
 		return
@@ -881,17 +879,19 @@ def OpenKitInfoWindow ():
 		if Dual[0] == 1:
 			text = CommonTables.KitList.GetValue (Dual[1], 3)
 		elif Dual[0] == 2:
-			text = CommonTables.Classes.GetValue (Dual[1], 1)
+			text = GUICommon.GetClassRowName(Dual[1], "index")
+			text = CommonTables.Classes.GetValue (text, "DESC_REF")
 
 		TextArea.SetText (text)
 		TextArea.Append ("\n\n")
-		text = CommonTables.Classes.GetValue (Dual[2], 1)
+		text = GUICommon.GetClassRowName(Dual[2], "index")
+		text = CommonTables.Classes.GetValue (text, "DESC_REF")
 
 	else: # ordinary class or kit
 		if KitIndex:
 			text = CommonTables.KitList.GetValue (KitIndex, 3)
 		else:
-			text = CommonTables.Classes.GetValue (ClassIndex, 1)
+			text = CommonTables.Classes.GetValue (ClassName, "DESC_REF")
 
 	TextArea.Append (text)
 

gemrb/GUIScripts/LUCommon.py
@@ -55,8 +55,8 @@ def CanLevelUp(actor):
 		xp = xp/Multi[0] # divide the xp evenly between the classes
 		for i in range (Multi[0]):
 			# if any class can level, return 1
-			ClassIndex = CommonTables.Classes.FindValue (5, Multi[i+1])
-			tmpNext = int(GetNextLevelExp (Levels[i], CommonTables.Classes.GetRowName (ClassIndex) ) )
+			TmpClassName = GUICommon.GetClassRowName (Multi[i+1], "class")
+			tmpNext = int(GetNextLevelExp (Levels[i], TmpClassName))
 			if tmpNext != 0 and tmpNext <= xp:
 				return 1
 
@@ -100,7 +100,8 @@ def SetupSavingThrows (pc, Level=None):
 		NumClasses = Multi[0]
 		Class = [Multi[1], Multi[2], Multi[3]]
 	elif Dual[0]: #only worry about the newer class
-		Class = [CommonTables.Classes.GetValue (Dual[2], 5)]
+		Class = GUICommon.GetClassRowName(Dual[2], "index")
+		Class = [CommonTables.Classes.GetValue (Class, "ID")]
 		#assume Level is correct if passed
 		if GUICommon.IsDualSwap(pc) and not Level:
 			Levels = [Levels[1], Levels[0], Levels[2]]
@@ -121,8 +122,7 @@ def SetupSavingThrows (pc, Level=None):
 	SaveTables = []
 	ClassBonus = 0
 	for i in range (NumClasses):
-		Row = CommonTables.Classes.FindValue (5, Class[i])
-		RowName = CommonTables.Classes.GetRowName (Row)
+		RowName = GUICommon.GetClassRowName (Class[i], "class")
 		SaveName = CommonTables.Classes.GetValue (RowName, "SAVE", 0)
 		SaveTables.append (GemRB.LoadTable (SaveName) )
 		#use numeric value
@@ -159,8 +159,7 @@ def SetupSavingThrows (pc, Level=None):
 def GetNextLevelFromExp (XP, Class):
 	"""Gets the next level based on current experience."""
 
-	ClassIndex = CommonTables.Classes.FindValue (5, Class)
-	ClassName = CommonTables.Classes.GetRowName (ClassIndex)
+	ClassName = GUICommon.GetClassRowName (Class, "class")
 	Row = CommonTables.NextLevel.GetRowIndex (ClassName)
 	NLNumCols = CommonTables.NextLevel.GetColumnCount()
 	for i in range(1, NLNumCols):
@@ -197,7 +196,8 @@ def SetupThaco (pc, Level=None):
 		NumClasses = Multi[0]
 		Class = [Multi[1], Multi[2], Multi[3]]
 	elif Dual[0]: #only worry about the newer class
-		Class = [CommonTables.Classes.GetValue (Dual[2], 5)]
+		Class = GUICommon.GetClassRowName(Dual[2], "index")
+		Class = [CommonTables.Classes.GetValue (Class, "ID")]
 		#assume Level is correct if passed
 		if GUICommon.IsDualSwap(pc) and not Level:
 			Levels = [Levels[1], Levels[0], Levels[2]]
@@ -215,7 +215,7 @@ def SetupThaco (pc, Level=None):
 	for i in range (NumClasses):
 		#loop through each class and update the save value if we have
 		#a better thac0
-		ClassName = CommonTables.Classes.GetRowName (CommonTables.Classes.FindValue (5, Class[i]))
+		ClassName = GUICommon.GetClassRowName (Class[i], "class")
 		TmpThaco = ThacoTable.GetValue (ClassName, str(Levels[i]+1))
 		if TmpThaco < CurrentThaco:
 			NewThaco = 1
@@ -256,7 +256,8 @@ def SetupLore (pc, LevelDiff=None):
 		NumClasses = Multi[0]
 		Class = [Multi[1], Multi[2], Multi[3]]
 	elif Dual[0]: #only worry about the newer class
-		Class = [CommonTables.Classes.GetValue (Dual[2], 5)]
+		Class = GUICommon.GetClassRowName(Dual[2], "index")
+		Class = [CommonTables.Classes.GetValue (Class, "ID")]
 		#if LevelDiff is passed, we assume it is correct
 		if GUICommon.IsDualSwap(pc) and not LevelDiff:
 			LevelDiffs = [LevelDiffs[1], LevelDiffs[0], LevelDiffs[2]]
@@ -267,7 +268,7 @@ def SetupLore (pc, LevelDiff=None):
 	CurrentLore = GemRB.GetPlayerStat (pc, IE_LORE, 1)
 	for i in range (NumClasses):
 		#correct unlisted progressions
-		ClassName = CommonTables.Classes.GetRowName (CommonTables.Classes.FindValue (5, Class[i]) )
+		ClassName = GUICommon.GetClassRowName (Class[i], "class")
 		if ClassName == "SORCERER":
 			ClassName = "MAGE"
 		elif ClassName == "MONK": #monks have a rate of 1, so this is arbitrary
@@ -324,7 +325,8 @@ def SetupHP (pc, Level=None, LevelDiff=None):
 		#we only get the hp bonus if the old class is reactivated
 		if (Levels[0]<=Levels[1]):
 			return
-		Class = [CommonTables.Classes.GetValue (Dual[2], 5)]
+		Class = GUICommon.GetClassRowName(Dual[2], "index")
+		Class = [CommonTables.Classes.GetValue (Class, "ID")]
 		#if Level and LevelDiff are passed, we assume it is correct
 		if GUICommon.IsDualSwap(pc) and not Level and not LevelDiff:
 			LevelDiffs = [LevelDiffs[1], LevelDiffs[0], LevelDiffs[2]]
@@ -350,7 +352,7 @@ def SetupHP (pc, Level=None, LevelDiff=None):
 	for i in range (NumClasses):
 		#check this classes hp table for any gain
 		if not ClassName or NumClasses > 1:
-			ClassName = CommonTables.Classes.GetRowName (CommonTables.Classes.FindValue (5, Class[i]) )
+			ClassName = GUICommon.GetClassRowName (Class[i], "class")
 		HPTable = CommonTables.Classes.GetValue (ClassName, "HP")
 		HPTable = GemRB.LoadTable (HPTable)
 

gemrb/GUIScripts/LUProfsSelection.py
@@ -198,11 +198,11 @@ def SetupProfsWindow (pc, type, window, callback, level1=[0,0,0], level2=[1,1,1]
 	if classid: #for dual classes when we can't get the class dualing to
 		Class = classid
 	elif IsDual[0]:
-		Class = CommonTables.Classes.GetValue (IsDual[2], 5)
+		Class = GUICommon.GetClassRowName(IsDual[2], "index")
+		Class = CommonTables.Classes.GetValue (Class, "ID")
 	else:
 		Class = GemRB.GetPlayerStat (pc, IE_CLASS)
-	ClassName = CommonTables.Classes.FindValue (5, Class)
-	ClassName = CommonTables.Classes.GetRowName (ClassName)
+	ClassName = GUICommon.GetClassRowName (Class, "class")
 
 	# profs.2da has entries for everyone, so no need to muck around
 	ProfsRate = ProfsTable.GetValue (ClassName, "RATE")

commit 59497c988381bb519ca97cde6707e7498b07edbb

iwd2::cg: fixed determining who has racial enemies
regression from feb2600 (not actually in the future)

gemrb/GUIScripts/iwd2/Enemy.py
@@ -53,9 +53,8 @@ def OnLoad():
 	GemRB.LoadWindowPack("GUICG", 800 ,600)
 	RaceWindow = GemRB.LoadWindow(15)
 	Class = GemRB.GetVar("BaseClass")
-	ClassName = GUICommon.GetClassRowName (Class, "index")
-	print "TODO: check if class %s really has an ID of %d" %(ClassName, Class)
-	TableName = CommonTables.ClassSkills.GetValue(ClassName, "RANGERSKILL")
+	ClassName = GUICommon.GetClassRowName (Class, "class")
+	TableName = CommonTables.ClassSkills.GetValue(ClassName, "HATERACE")
 	if TableName == "*":
 		GemRB.SetNextScript("Skills")
 		return