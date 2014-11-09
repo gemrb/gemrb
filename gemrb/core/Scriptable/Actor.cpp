c5366b9d39a13c145151faa517ed0fb145ba2db1
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2014-11-09 19:00:06 +0100

    Actor::GetBookMask: create a sensible mask
    the important booktypes go from 0-6

--- gemrb/core/Scriptable/Actor.cpp
+++ gemrb/core/Scriptable/Actor.cpp
@@ -9660,7 +9660,7 @@ int Actor::GetBookMask() const
 	int bookmask = 0;
 	for (int i=0; i < ISCLASSES; i++) {
 		if (Modified[levelslotsiwd2[i]] > 0) {
-			bookmask |= 1<<((1<<booksiwd2[i])-1);
+			bookmask |= 1 << booksiwd2[i];
 		}
 	}
 
