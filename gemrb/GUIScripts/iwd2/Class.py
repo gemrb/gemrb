7b001d212d86185736eddcb7485914cf3e6a65cb
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2014-11-22 15:55:54 +0100

    iwd2::cg: fixed syntax error in class/kit resetting

--- gemrb/GUIScripts/iwd2/Class.py
+++ gemrb/GUIScripts/iwd2/Class.py
@@ -176,8 +176,8 @@ def BackPress():
 	GemRB.SetNextScript("CharGen3")
 	GemRB.SetVar("Class",0)  #scrapping the class value
 	MyChar = GemRB.GetVar("Slot")
-	GemRB.SetPlayerStat (IE_CLASS, 0)
-	GemRB.SetPlayerStat (IE_KIT, 0)
+	GemRB.SetPlayerStat (MyChar, IE_CLASS, 0)
+	GemRB.SetPlayerStat (MyChar, IE_KIT, 0)
 	return
 
 def NextPress():
