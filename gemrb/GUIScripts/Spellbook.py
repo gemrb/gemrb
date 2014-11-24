25ed0ef6c16cace5227e1a78f2f6713361b2bc6f
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2014-11-24 21:24:15 +0100

    LearnSpell.py: also pass the memo flag to the memo call
    didn't notice, since it takes care of innates on its own; last field
    specifies whether the memorization is immediately usable or depleted

--- gemrb/GUIScripts/Spellbook.py
+++ gemrb/GUIScripts/Spellbook.py
@@ -24,7 +24,7 @@ import GameCheck
 from GUIDefines import *
 from ie_stats import *
 from ie_action import ACT_LEFT, ACT_RIGHT
-from ie_spells import SP_IDENTIFY, SP_SURGE, LSR_KNOWN, LSR_LEVEL, LSR_STAT, LSR_FULL, LSR_OK
+from ie_spells import *
 from ie_restype import RES_2DA
 
 #################################################################
@@ -633,5 +633,5 @@ def LearnSpell(pc, spellref, booktype, level, count, flags=0):
 		return
 
 	for j in range(count):
-		GemRB.MemorizeSpell (pc, booktype, level, SpellIndex)
+		GemRB.MemorizeSpell (pc, booktype, level, SpellIndex, flags&LS_MEMO)
 
