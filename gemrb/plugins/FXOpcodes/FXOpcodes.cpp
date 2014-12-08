gemrb/gemrb

commit 8bf235f442dce924a73e039b3a80a4967ba020b8

some more coverity fixes: ensure null termination

gemrb/plugins/FXOpcodes/FXOpcodes.cpp
@@ -5927,10 +5927,10 @@ int fx_cast_spell_on_condition (Scriptable* Owner, Actor* target, Effect* fx)
 		// TODO: fail remaining spells if an earlier one fails?
 		unsigned int i, dist;
 		ieResRef refs[4];
-		strncpy(refs[0], fx->Resource, sizeof(ieResRef));
-		strncpy(refs[1], fx->Resource2, sizeof(ieResRef));
-		strncpy(refs[2], fx->Resource3, sizeof(ieResRef));
-		strncpy(refs[3], fx->Resource4, sizeof(ieResRef));
+		CopyResRef(refs[0], fx->Resource);
+		CopyResRef(refs[1], fx->Resource);
+		CopyResRef(refs[2], fx->Resource);
+		CopyResRef(refs[3], fx->Resource);
 		// save the current spell ref, so the rest of its effects can be applied afterwards (in case of a surge)
 		ieResRef OldSpellResRef;
 		memcpy(OldSpellResRef, Owner->SpellResRef, sizeof(OldSpellResRef));

gemrb/core/GameData.cpp
@@ -179,7 +179,7 @@ int GameData::LoadTable(const ieResRef ResRef, bool silent)
 	}
 	Table t;
 	t.refcount = 1;
-	strncpy( t.ResRef, ResRef, 8 );
+	CopyResRef(t.ResRef, ResRef);
 	t.tm = tm;
 	ind = -1;
 	for (size_t i = 0; i < tables.size(); i++) {

gemrb/core/Interface.cpp
@@ -1099,7 +1099,7 @@ bool Interface::ReadModalStates()
 	ModalStatesStruct ms;
 	for (unsigned short i = 0; i < table->GetRowCount(); i++) {
 		CopyResRef(ms.spell, table->QueryField(i, 0));
-		strncpy(ms.action, table->QueryField(i, 1), 16);
+		strlcpy(ms.action, table->QueryField(i, 1), 16);
 		ms.entering_str = atoi(table->QueryField(i, 2));
 		ms.leaving_str = atoi(table->QueryField(i, 3));
 		ms.failed_str = atoi(table->QueryField(i, 4));

gemrb/plugins/CREImporter/CREImporter.cpp
@@ -646,7 +646,7 @@ void CREImporter::WriteChrHeader(DataStream *stream, Actor *act)
 	stream->Write( Signature, 8);
 	memset( Signature,0,sizeof(Signature));
 	memset( name,0,sizeof(name));
-	strncpy( name, act->GetName(0), sizeof(name) );
+	strlcpy(name, act->GetName(0), sizeof(name));
 	stream->Write( name, 32);
 
 	stream->WriteDword( &tmpDword); //cre offset (chr header size)

commit cbcec61f51a4a70f2281e55c4acda2b8ced6d2f8

fixed another regression from 8bf235f, thanks wjp

gemrb/plugins/FXOpcodes/FXOpcodes.cpp
@@ -5929,9 +5929,9 @@ int fx_cast_spell_on_condition (Scriptable* Owner, Actor* target, Effect* fx)
 		unsigned int i, dist;
 		ieResRef refs[4];
 		CopyResRef(refs[0], fx->Resource);
-		CopyResRef(refs[1], fx->Resource);
-		CopyResRef(refs[2], fx->Resource);
-		CopyResRef(refs[3], fx->Resource);
+		CopyResRef(refs[1], fx->Resource2);
+		CopyResRef(refs[2], fx->Resource3);
+		CopyResRef(refs[3], fx->Resource4);
 		// save the current spell ref, so the rest of its effects can be applied afterwards (in case of a surge)
 		ieResRef OldSpellResRef;
 		memcpy(OldSpellResRef, Owner->SpellResRef, sizeof(OldSpellResRef));