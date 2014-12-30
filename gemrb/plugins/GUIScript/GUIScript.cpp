gemrb/gemrb

commit 179dd1ed6374938a7bf67fb7fbac4a8b982d966c

some more leak fixes

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -2658,6 +2658,7 @@ static PyObject* GemRB_AddNewArea(PyObject * /*self*/, PyObject* args)
 			WMPAreaEntry *oarea = wmap->GetArea(larea, areaindex);
 			if (!oarea) {
 				//blabla
+				delete link;
 				return RuntimeError("cannot establish area link!");
 			}
 			strnuprcpy(link->DestEntryPoint, ename, 32);
@@ -2682,7 +2683,7 @@ static PyObject* GemRB_AddNewArea(PyObject * /*self*/, PyObject* args)
 				link->AreaIndex = areaindex;
 				wmap->AddAreaLink(link);
 			}
-
+			delete link;
 		}
 	}
 

gemrb/core/Scriptable/Actor.cpp
@@ -3900,6 +3900,7 @@ void Actor::GetHit(int damage, int spellLevel)
 		}
 		Effect *fx = EffectQueue::CreateEffect(fx_cure_sleep_ref, 0, 0, FX_DURATION_INSTANT_PERMANENT);
 		fxqueue.AddEffect(fx);
+		delete fx;
 	}
 	if (CheckSpellDisruption(damage, spellLevel)) {
 		InterruptCasting = true;

gemrb/plugins/FXOpcodes/FXOpcodes.cpp
@@ -4846,6 +4846,7 @@ int fx_apply_effect (Scriptable* Owner, Actor* target, Effect* fx)
 			//hack to entirely replace this effect with the applied effect, this is required for some generic effects
 			//that must be put directly in the effect queue to have any impact (to be counted by BonusAgainstCreature, etc)
 			target->fxqueue.AddEffect(myfx);
+			delete myfx;
 			return FX_NOT_APPLIED;
 		}
 		ret = target->fxqueue.ApplyEffect(target, myfx, fx->FirstApply, !fx->Parameter3);

commit 1e7b55f8061502576a71be324486d7b9f120edac

fixed worldmap regression from 179dd1ed63

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -2679,11 +2679,11 @@ static PyObject* GemRB_AddNewArea(PyObject * /*self*/, PyObject* args)
 				link->AreaIndex = thisarea;
 				//linktodir may need translation
 				wmap->InsertAreaLink(areaindex, linktodir, link);
+				delete link;
 			} else {
 				link->AreaIndex = areaindex;
 				wmap->AddAreaLink(link);
 			}
-			delete link;
 		}
 	}
 