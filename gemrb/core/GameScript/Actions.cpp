a186784bde3c0507abd9752f59f4e6f92d57bf4d
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2014-12-31 21:11:34 +0100

    GameScript::DayNight: don't increase fatigue when advancing time
    fixes travelling to Brynnlaw making everyone fatigued so much, that
    they fell out of fatigmod.2da limits and its luck penalty not nullifiable
    with resting.
    
    happens since cut41d calls us several times

--- gemrb/core/GameScript/Actions.cpp
+++ gemrb/core/GameScript/Actions.cpp
@@ -5152,7 +5152,7 @@ void GameScript::DayNight(Scriptable* /*Sender*/, Action* parameters)
 	// then, calculate the offset (in hours) required to take us to the desired hour.
 	padding = (24 + parameters->int0Parameter - padding) % 24;
 	// then, advance one day (7200), plus the desired number of hours.
-	core->GetGame()->AdvanceTime(AI_UPDATE_TIME*(7200 + padding*300));
+	core->GetGame()->AdvanceTime(AI_UPDATE_TIME*(7200 + padding*300), false);
 }
 
 //implement pst style parameters:
