f4541724957109a073d9660c84d8d3c3bd77bd72
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2015-01-01 12:50:40 +0100

    ProjectileServer::GetExplosionFlags: modified error message to be unique

--- gemrb/core/ProjectileServer.cpp
+++ gemrb/core/ProjectileServer.cpp
@@ -296,7 +296,7 @@ int ProjectileServer::GetExplosionFlags(unsigned int idx)
 {
 	if (explosioncount==-1) {
 		if (InitExplosion()<0) {
-			Log(ERROR, "ProjectileServer", "Problem with explosions");
+			Log(ERROR, "ProjectileServer", "Problem with explosions!");
 			explosioncount=0;
 		}
 	}
