gemrb/gemrb

commit 9f5eb83aaf511a30aaebf261fdd65cdc508e1d58

Game::GetXPFromCR: fixed display for minimum cr and added note

gemrb/core/Game.cpp
@@ -1172,13 +1172,14 @@ int Game::GetXPFromCR(int cr)
 	int level = GetPartyLevel(true) / size;
 	if (cr >= MAX_CRLEVEL) {
 		cr = MAX_CRLEVEL+1;
-	} else if (cr-2 < 0) {
-		cr = 2;
+	} else if (cr-1 < 0) {
+		cr = 1;
 	}
 	Log(MESSAGE, "Game", "Challenge Rating: %d, party level: %d", cr, level);
 	// it also has a column for cr 0.25 and 0.5, so let's treat cr as a 1-based index
 	// but testing shows something else affects it further, so we divide by 2 to match
-	return crtable[level-1][cr-2]/2;
+	// the net is full of claims of halved values, so perhaps just a quick final rebalancing tweak
+	return crtable[level-1][cr-1]/2;
 }
 
 void Game::ShareXP(int xp, int flags)

commit faedca97625bf714b627ad0bb7a373cde8d41f88

don't crash in the rare case of bad cr value
regression from 9f5eb83aa, thanks coverity

gemrb/core/Game.cpp
@@ -1171,7 +1171,7 @@ int Game::GetXPFromCR(int cr)
 	// compute the party average directly
 	int level = GetPartyLevel(true) / size;
 	if (cr >= MAX_CRLEVEL) {
-		cr = MAX_CRLEVEL+1;
+		cr = MAX_CRLEVEL;
 	} else if (cr-1 < 0) {
 		cr = 1;
 	}