699c83fc4fb4e831a3ae868bbd32cd9975270007
Jens Granseuer <jensgr@gmx.net>
2015-05-17 12:42:45 +0200

    fix saving/restoring of the tooltip delay option (bug #273)

--- gemrb/GUIScripts/GUIOPT.py
+++ gemrb/GUIScripts/GUIOPT.py
@@ -357,7 +357,7 @@ def OpenGameplayOptionsWindow ():
 	GUIOPTControls.OptDone (CloseGameplayOptionsWindow, Window, 7)
 	GUIOPTControls.OptCancel (CloseGameplayOptionsWindow, Window, 20)
 
-	GUIOPTControls.OptSlider (18042, 18017, HelpTextArea, Window, 1, 21, 17143, 'Tooltips', DisplayHelpTooltipDelay, TOOLTIP_DELAY_FACTOR)
+	GUIOPTControls.OptSlider (18042, 18017, HelpTextArea, Window, 1, 21, 17143, 'Tooltips', DisplayHelpTooltipDelay, 10)
 	GUIOPTControls.OptSlider (18042, 18018, HelpTextArea, Window, 2, 22, 17144, 'Mouse Scroll Speed', DisplayHelpMouseScrollingSpeed, 5)
 	GUIOPTControls.OptSlider (18042, 18019, HelpTextArea, Window, 3, 23, 17145, 'Keyboard Scroll Speed', None, 5)
 	GUIOPTControls.OptSlider (18042, 18020, HelpTextArea, Window, 12, 24, 13911, 'Difficulty Level', None, 0)
@@ -382,7 +382,7 @@ def OpenGameplayOptionsWindow ():
 
 def DisplayHelpTooltipDelay ():
 	HelpTextArea.SetText (18017)
-	GemRB.SetTooltipDelay (GemRB.GetVar ("Tooltips") )
+	GemRB.SetTooltipDelay (GemRB.GetVar ("Tooltips") * TOOLTIP_DELAY_FACTOR/10)
 
 def DisplayHelpMouseScrollingSpeed ():
 	HelpTextArea.SetText (18018)
