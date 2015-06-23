gemrb/gemrb

commit 79b79b31557834069417c813e88808dd88261df0

Reimplement listboxes
adding GUIScript method TextArea_SetOptions

gemrb/GUIScripts/GUIMG.py
@@ -525,19 +525,17 @@ def OpenSequencerWindow ():
 		sb = Window.GetControl (7)
 		sb.SetPos (-1,-1)
 	else:
-		CondSelect.SetEvent (IE_GUI_TEXTAREA_ON_CHANGE, ContingencyHelpCondition)
+		CondSelect.SetEvent (IE_GUI_TEXTAREA_ON_SELECT, ContingencyHelpCondition)
 		CondSelect.SetVarAssoc ("ContCond", 0)
-		for elem in ContCond:
-			CondSelect.Append (elem[0], -1)
+		CondSelect.SetOptions ([elem[0] for elem in ContCond])
 
-		TargSelect.SetEvent (IE_GUI_TEXTAREA_ON_CHANGE, ContingencyHelpTarget)
-		TargSelect.SetVarAssoc ("ContTarg", 0)
-		for elem in ContTarg:
-			TargSelect.Append (elem[0], -1)
-			#check if target is only self
-			if Target:
-				TargSelect.SetVarAssoc ("ContTarg", 1)
-				break
+		TargSelect.SetEvent (IE_GUI_TEXTAREA_ON_SELECT, ContingencyHelpTarget)
+		if Target:
+			TargSelect.SetVarAssoc ("ContTarg", 1)
+			TargSelect.SetOptions (ContTarg[0][0])
+		else:
+			TargSelect.SetVarAssoc ("ContTarg", 0)
+			TargSelect.SetOptions ([elem[0] for elem in ContTarg])
 
 	GemRB.SetVar ("SpellType", 0)
 	TypeButton.SetVarAssoc ("SpellType", 1)

gemrb/GUIScripts/GUIClasses.py
@@ -142,6 +142,7 @@ class GTextArea(GControl):
     'Scroll': _GemRB.TextArea_Scroll,
     'SelectText': _GemRB.TextArea_SelectText,
     'SetFlags': _GemRB.Control_TextArea_SetFlags,
+    'SetOptions': _GemRB.TextArea_SetOptions,
     'GetCharSounds': _GemRB.TextArea_GetCharSounds,
     'GetCharacters': _GemRB.TextArea_GetCharacters,
     'GetPortraits': _GemRB.TextArea_GetPortraits

gemrb/GUIScripts/GUIOPT.py
@@ -515,13 +515,7 @@ def OpenMovieWindow ():
 	CreditsButton = Window.GetControl(3)
 	DoneButton = Window.GetControl(4)
 	MoviesTable = GemRB.LoadTable("MOVIDESC")
-	for i in range(0, MoviesTable.GetRowCount() ):
-		t = MoviesTable.GetRowName(i)
-		#temporarily out too (see above)
-		#if GemRB.GetVar(t)==1:
-		if 1==1:
-			s = MoviesTable.GetValue(i, 0)
-			TextAreaControl.Append(s,-1)
+	TextAreaControl.SetOptions([MoviesTable.GetValue(i, 0) for i in range(0, MoviesTable.GetRowCount())])
 	TextAreaControl.SetVarAssoc("MovieIndex",0)
 	PlayButton.SetText(17318)
 	CreditsButton.SetText(15591)

gemrb/GUIScripts/GUIRECCommon.py
@@ -385,7 +385,22 @@ def OpenScriptWindow ():
 	SubCustomizeWindow = GemRB.LoadWindow (11)
 
 	ScriptTextArea = SubCustomizeWindow.GetControl (2)
-	FillScriptList ()
+	row = ScriptsTable.GetRowCount ()
+	
+	options = []
+	for i in range (row):
+		GemRB.SetToken ("script", ScriptsTable.GetRowName (i) )
+		title = ScriptsTable.GetValue (i,0)
+		if title!=-1:
+			desc = ScriptsTable.GetValue (i,1)
+			txt = GemRB.GetString (title)
+			if (desc!=-1):
+				txt += GemRB.GetString (desc) + "\n"
+			options.append(txt) 
+		else:
+			options.append (ScriptsTable.GetRowName (i) + "\n")
+	ScriptTextArea.SetOptions (options)
+	
 	pc = GemRB.GameGetSelectedPCSingle ()
 	script = GemRB.GetPlayerScript (pc)
 	scriptindex = ScriptsTable.GetRowIndex (script)
@@ -405,27 +420,9 @@ def OpenScriptWindow ():
 
 	DoneButton.SetEvent (IE_GUI_BUTTON_ON_PRESS, DoneScriptWindow)
 	CancelButton.SetEvent (IE_GUI_BUTTON_ON_PRESS, CloseSubCustomizeWindow)
-	ScriptTextArea.SetEvent (IE_GUI_TEXTAREA_ON_CHANGE, UpdateScriptSelection)
+	ScriptTextArea.SetEvent (IE_GUI_TEXTAREA_ON_SELECT, UpdateScriptSelection)
 
 	SubCustomizeWindow.ShowModal (MODAL_SHADOW_GRAY)
-	return
-
-def FillScriptList ():
-	global ScriptTextArea
-
-	ScriptTextArea.Clear ()
-	row = ScriptsTable.GetRowCount ()
-	for i in range (row):
-		GemRB.SetToken ("script", ScriptsTable.GetRowName (i) )
-		title = ScriptsTable.GetValue (i,0)
-		if title!=-1:
-			desc = ScriptsTable.GetValue (i,1)
-			txt = GemRB.GetString (title)
-			if (desc!=-1):
-				txt += GemRB.GetString (desc)
-			ScriptTextArea.Append (txt+"\n", -1)
-		else:
-			ScriptTextArea.Append (ScriptsTable.GetRowName (i)+"\n" ,-1)
 	return
 
 def DoneScriptWindow ():

gemrb/GUIScripts/bg1/GUIMOVIE.py
@@ -34,13 +34,10 @@ def OnLoad():
 	CreditsButton = MovieWindow.GetControl (3)
 	DoneButton = MovieWindow.GetControl (4)
 	MoviesTable = GemRB.LoadTable ("MOVIDESC")
-	for i in range( MoviesTable.GetRowCount () ):
-		t = MoviesTable.GetRowName (i)
-		if GemRB.GetVar (t)==1:
-			s = MoviesTable.GetValue (i, 0)
-			TextAreaControl.Append (s,-1)
+	opts = [MoviesTable.GetValue (i, 0) for i in range( MoviesTable.GetRowCount()) if GemRB.GetVar(MoviesTable.GetRowName (i) == 1 ]
+	TextAreaControl.SetOptions(opts)
 	TextAreaControl.SetVarAssoc ("MovieIndex",0)
-	TextAreaControl.SetEvent (IE_GUI_TEXTAREA_ON_CHANGE, MoviePress)
+	TextAreaControl.SetEvent (IE_GUI_TEXTAREA_ON_SELECT, MoviePress)
 	PlayButton.SetText (17318)
 	CreditsButton.SetText (15591)
 	DoneButton.SetText (11973)

gemrb/GUIScripts/bg1/GUISONGS.py
@@ -35,9 +35,7 @@ def OnLoad():
 	CreditsButton = MovieWindow.GetControl(3)
 	DoneButton = MovieWindow.GetControl(4)
 	MoviesTable = GemRB.LoadTable("MUSIC")
-	for i in range(1,MoviesTable.GetRowCount() ):
-			s = MoviesTable.GetValue(i, 0)
-			TextAreaControl.Append(s,-1)
+	TextAreaControl.SetOptions([MoviesTable.GetValue(i, 0) for i in range(1,MoviesTable.GetRowCount() )])
 	TextAreaControl.SetVarAssoc("MovieIndex",0)
 	PlayButton.SetText(17318)
 	CreditsButton.SetText(15591)

gemrb/GUIScripts/bg2/GUIMOVIE.py
@@ -35,13 +35,10 @@ def OnLoad():
 	CreditsButton = MovieWindow.GetControl (3)
 	DoneButton = MovieWindow.GetControl (4)
 	MoviesTable = GemRB.LoadTable ("MOVIDESC")
-	for i in range( MoviesTable.GetRowCount () ):
-		t = MoviesTable.GetRowName (i)
-		if GemRB.GetVar (t)==1:
-			s = MoviesTable.GetValue (i, 0)
-			TextAreaControl.Append (s,-1)
+	opts = [MoviesTable.GetValue (i, 0) for i in range( MoviesTable.GetRowCount () ) if GemRB.GetVar (MoviesTable.GetRowName (i))==1]
+	TextAreaControl.SetOptions(opts)
 	TextAreaControl.SetVarAssoc ("MovieIndex",0)
-	TextAreaControl.SetEvent (IE_GUI_TEXTAREA_ON_CHANGE, MoviePress)
+	TextAreaControl.SetEvent (IE_GUI_TEXTAREA_ON_SELECT, MoviePress)
 	PlayButton.SetText (17318)
 	CreditsButton.SetText (15591)
 	DoneButton.SetText (11973)

gemrb/GUIScripts/bg2/GUISONGS.py
@@ -35,9 +35,7 @@ def OnLoad():
 	CreditsButton = MovieWindow.GetControl(3)
 	DoneButton = MovieWindow.GetControl(4)
 	MoviesTable = GemRB.LoadTable("SONGLIST")
-	for i in range(0, MoviesTable.GetRowCount() ):
-			s = MoviesTable.GetValue(i, 0)
-			TextAreaControl.Append(s,-1)
+	TextAreaControl.SetOptions([MoviesTable.GetValue(i, 0) for i in range(0, MoviesTable.GetRowCount() )])
 	TextAreaControl.SetVarAssoc("MovieIndex",0)
 	PlayButton.SetText(17318)
 	CreditsButton.SetText(15591)

commit bb51b4a0311981153b275c879147e5aeea305470

fixed regression from 79b79b3 preventing use of contingency window
(for normal contingency, where there's no target choice)

gemrb/GUIScripts/GUIMG.py
@@ -531,7 +531,7 @@ def OpenSequencerWindow ():
 		TargSelect.SetEvent (IE_GUI_TEXTAREA_ON_SELECT, ContingencyHelpTarget)
 		if Target:
 			TargSelect.SetVarAssoc ("ContTarg", 1)
-			TargSelect.SetOptions (ContTarg[0][0])
+			TargSelect.SetOptions ([ContTarg[0][0]])
 		else:
 			TargSelect.SetVarAssoc ("ContTarg", 0)
 			TargSelect.SetOptions ([elem[0] for elem in ContTarg])