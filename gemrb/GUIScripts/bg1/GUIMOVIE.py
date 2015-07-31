257d32cebae10c8814b2030b91dd6f694844ae27
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2015-07-31 09:36:30 +0200

    bg1: fixed movie regression from 79b79b31

--- gemrb/GUIScripts/bg1/GUIMOVIE.py
+++ gemrb/GUIScripts/bg1/GUIMOVIE.py
@@ -34,7 +34,7 @@ def OnLoad():
 	CreditsButton = MovieWindow.GetControl (3)
 	DoneButton = MovieWindow.GetControl (4)
 	MoviesTable = GemRB.LoadTable ("MOVIDESC")
-	opts = [MoviesTable.GetValue (i, 0) for i in range( MoviesTable.GetRowCount()) if GemRB.GetVar(MoviesTable.GetRowName (i) == 1 ]
+	opts = [MoviesTable.GetValue (i, 0) for i in range( MoviesTable.GetRowCount()) if GemRB.GetVar(MoviesTable.GetRowName (i)) == 1 ]
 	TextAreaControl.SetOptions(opts)
 	TextAreaControl.SetVarAssoc ("MovieIndex",0)
 	TextAreaControl.SetEvent (IE_GUI_TEXTAREA_ON_SELECT, MoviePress)
