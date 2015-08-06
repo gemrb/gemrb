52b93f7d77ca52fe68c07b131f1185b21b83412a
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2015-08-06 13:16:41 +0200

    iwd2: play the selected char sound in cg and cycle them

--- gemrb/GUIScripts/iwd2/CSound.py
+++ gemrb/GUIScripts/iwd2/CSound.py
@@ -23,15 +23,18 @@ from GUIDefines import *
 SoundWindow = 0
 TextAreaControl = 0
 DoneButton = 0
-TopIndex = 0
+SoundIndex = 0
+VerbalConstants = None
 
 def OnLoad():
-	global SoundWindow, TextAreaControl, DoneButton, TopIndex
+	global SoundWindow, TextAreaControl, DoneButton, VerbalConstants
 
 	GemRB.LoadWindowPack("GUICG", 800,  600)
 	#this hack will redraw the base CG window
 	SoundWindow = GemRB.LoadWindow(19)
 	GemRB.SetVar("Sound",0)  #scrapping the sound value
+	CharSoundTable = GemRB.LoadTable ("CHARSND")
+	VerbalConstants =  [CharSoundTable.GetRowName(i) for i in range(CharSoundTable.GetRowCount())]
 
 	BackButton = SoundWindow.GetControl(10)
 	BackButton.SetText(15416)
@@ -46,6 +49,7 @@ def OnLoad():
 
 	TextAreaControl = SoundWindow.GetControl(45)
 	TextAreaControl.SetVarAssoc("Sound", 0)
+	TextAreaControl.SetEvent(IE_GUI_TEXTAREA_ON_SELECT, SelectSound)
 	RowCount=TextAreaControl.ListResources(CHR_SOUNDS)
 
 	DefaultButton = SoundWindow.GetControl(47)
@@ -74,4 +78,28 @@ def NextPress():
 	if SoundWindow:
 		SoundWindow.Unload()
 	GemRB.SetNextScript("CharGen8") #name
+	return
+
+def SelectSound():
+	PlayPress ()
+	return
+
+def PlayPress():
+	global SoundIndex
+
+	CharSound = TextAreaControl.QueryText()
+	MyChar = GemRB.GetVar ("Slot")
+
+	GemRB.SetPlayerSound (MyChar, CharSound)
+	# play sound as sound slot
+	GemRB.VerbalConstant (MyChar, int(VerbalConstants[SoundIndex]))
+
+	NextSound()
+	return
+
+def NextSound():
+	global SoundIndex
+	SoundIndex += 1
+	if SoundIndex >= len(VerbalConstants):
+		SoundIndex = 0
 	return
