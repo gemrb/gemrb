f52968d7cd9b7f10ec7037b4c15e42904b301f89
Brad Allred <bradallred@me.com>
2015-08-10 13:03:03 -0600

    Font: StringSize should allow another line as long as it doesnt *exceed* stop->h

--- gemrb/core/GUI/TextSystem/Font.cpp
+++ gemrb/core/GUI/TextSystem/Font.cpp
@@ -630,7 +630,7 @@ Size Font::StringSize(const String& string, StringSizeMetrics* metrics) const
 
 		if (newline || eos) {
 			w = (lineW > w) ? lineW : w;
-			if (stop && stop->h && (LineHeight * (lines + 1)) >= stop->h ) {
+			if (stop && stop->h && (LineHeight * (lines + 1)) > stop->h ) {
 				break;
 			}
 			if (newline) {
