62b249ba7a64c4a4c439d12e9f4c9a0ec6b77840
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2015-09-17 15:47:05 +0200

    ApplyClab_internal: handle the iwd2 FA_ and FS_ like we're going to use them
    all these innates are applicable abilities, only a few are actual spells/abs
    those correspond to feats and can be detected also that way

--- gemrb/core/Scriptable/Actor.cpp
+++ gemrb/core/Scriptable/Actor.cpp
@@ -786,18 +786,17 @@ static void ApplyClab_internal(Actor *actor, const char *clab, int level, bool r
 					}
 				}
 				else if (!memcmp(res,"FA_",3)) {//iwd2 only: innate name strref
-					//memorize these
-					int x=atoi(res+3);
-					ieResRef resref;
-					ResolveSpellName(resref, x);
-					actor->LearnSpell(resref, LS_MEMO, IE_IWD2_SPELL_INNATE);
+					//memorize these?
+					// we now learn them just to get the feedback string out
+					actor->LearnSpell(res+3, LS_MEMO|LS_LEARN, IE_IWD2_SPELL_INNATE);
+					actor->spellbook.RemoveSpell(res+3);
+					core->ApplySpell(res+3, actor, actor, 0);
 				}
 				else if (!memcmp(res,"FS_",3)) {//iwd2 only: song name strref (used by unused kits)
-					//don't memorize these
-					int x=atoi(res+3);
-					ieResRef resref;
-					ResolveSpellName(resref, x);
-					actor->LearnSpell(resref, 0, IE_IWD2_SPELL_SONG);
+					//don't memorize these?
+					actor->LearnSpell(res+3, LS_LEARN, IE_IWD2_SPELL_SONG);
+					actor->spellbook.RemoveSpell(res+3);
+					core->ApplySpell(res+3, actor, actor, 0);
 				}
 				else if (!memcmp(res,"RA_",3)) {//iwd2 only
 					//remove ability
