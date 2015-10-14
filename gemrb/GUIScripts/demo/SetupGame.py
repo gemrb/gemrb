f58fcc35be9f4a4b508a8ca3f7d463ca929e633b
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2015-10-14 21:00:43 +0200

    demo: set up character color choice
    we don't have the palettes though, so there's no visible change

--- gemrb/GUIScripts/demo/SetupGame.py
+++ gemrb/GUIScripts/demo/SetupGame.py
@@ -4,4 +4,9 @@ def OnLoad():
 	# skipping chargen
 	GemRB.CreatePlayer ("protagon", 1|0x8000) # exportable
 
+	# setup character colors (IE_MAJOR_COLOR and so on)
+	statcolors = [ (211, 202116108), (214, 0), (210, 791621423), (209, 960051513), (208, 454761243), (212, 370546198),(213, 387389207) ]
+	for (stat, color) in statcolors:
+		GemRB.SetPlayerStat (1, stat, color)
+
 	GemRB.EnterGame()
