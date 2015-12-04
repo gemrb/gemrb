gemrb/gemrb

commit c3a52d2c29dd952cc740e02b1c4791b83cb80739

Fix invalid C++
clang (and presumably other modern compilers) will strip out code where 'this' is assumed to be NULL because in C++ this is defined to not be null.

gemrb/core/Scriptable/Actor.cpp
@@ -10027,7 +10027,6 @@ bool Actor::InvalidSpellTarget(int spellnum, Actor *caster, int range) const
 
 bool Actor::PCInDark() const
 {
-	if (!this) return false;
 	unsigned int level = area->GetLightLevel(Pos);
 	if (level<ref_lightness) {
 		return true;

gemrb/core/GameScript/GameScript.cpp
@@ -2272,10 +2272,6 @@ bool Condition::Evaluate(Scriptable* Sender)
 /* this may return more than a boolean, in case of Or(x) */
 int Trigger::Evaluate(Scriptable* Sender)
 {
-	if (!this) {
-		Log(ERROR, "GameScript", "Trigger evaluation fails due to NULL trigger.");
-		return 0;
-	}
 	if (triggerID > MAX_TRIGGERS) {
 		Log(ERROR, "GameScript", "Corrupted (too high) trigger code: %d", triggerID);
 		return 0;

gemrb/core/GameScript/GameScript.h
@@ -533,7 +533,7 @@ public:
 	GameScript(const ieResRef ResRef, Scriptable* Myself,
 		int ScriptLevel = 0, bool AIScript = false);
 	~GameScript();
-	const char *GetName() { return this?Name:"NONE\0\0\0\0"; }
+	const char *GetName() { return Name; }
 	static void ExecuteString(Scriptable* Sender, const char* String);
 	static int EvaluateString(Scriptable* Sender, char* String);
 	static void ExecuteAction(Scriptable* Sender, Action* aC);

gemrb/plugins/TLKImporter/TlkOverride.cpp
@@ -313,13 +313,6 @@ char* CTlkOverride::ResolveAuxString(ieStrRef strref, int &Length)
 {
 	char *string;
 
-	if (!this) {
-		Length = 0;
-		string = ( char* ) malloc( 1 );
-		string[0] = 0;
-		return string;
-	}
-
 #ifdef CACHE_TLK_OVERRIDE
 	StringMapType::iterator tmp = stringmap.find(strref);
 	if (tmp!=stringmap.end()) {

commit 3bda2f78b2b0dc00a7832237496fbfbb95d280b8

fixed another crash regression from c3a52d2c29
the premise doesn't hold universally; in these two cases "this" is still
just the access pointer and since no membery stuff was done, quite safe

gemrb/core/Scriptable/Actor.cpp
@@ -5855,7 +5855,11 @@ void Actor::CreateStats()
 
 const char* Actor::GetScript(int ScriptIndex) const
 {
-	return Scripts[ScriptIndex]->GetName();
+	if (Scripts[ScriptIndex]) {
+		return Scripts[ScriptIndex]->GetName();
+	} else {
+		return "NONE\0\0\0\0";
+	}
 }
 
 void Actor::SetModal(ieDword newstate, bool force)