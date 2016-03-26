7d8cb747ad492622c51a49c78a6403b7b3f9afc6
Nikolai Vincent Vaags <nikolai.vaags@gmail.com>
2016-03-26 07:17:13 +0100

    only copy backBuf is cursor is visible

--- gemrb/plugins/SDLVideo/SDL20Video.cpp
+++ gemrb/plugins/SDLVideo/SDL20Video.cpp
@@ -332,11 +332,15 @@ void SDL20VideoDriver::showYUVFrame(unsigned char** buf, unsigned int *strides,
 int SDL20VideoDriver::SwapBuffers(void)
 {
 	//this is not pretty. We make a complete copy of the backBuf into tmpBuf, then copy it back after SDLVideoDriver::SwapBuffers has blitted cursors and tooltips, and SDL_UpdateTexture has copied it to the screentexture.
-	SDL_BlitSurface(backBuf, NULL, tmpBuf, NULL);
+	if (Cursor[CursorIndex] && !(MouseFlags & (MOUSE_DISABLED | MOUSE_HIDDEN))) {
+		SDL_BlitSurface(backBuf, NULL, tmpBuf, NULL);
+	}
 	int ret = SDLVideoDriver::SwapBuffers();
 
 	SDL_UpdateTexture(screenTexture, NULL, backBuf->pixels, backBuf->pitch);
-	SDL_BlitSurface(tmpBuf, NULL, backBuf, NULL);
+	if (Cursor[CursorIndex] && !(MouseFlags & (MOUSE_DISABLED | MOUSE_HIDDEN))) {
+		SDL_BlitSurface(tmpBuf, NULL, backBuf, NULL);
+	}
 	/*
 	 Commenting this out because I get better performance (on iOS) with SDL_UpdateTexture
 	 Don't know how universal it is yet so leaving this in commented out just in case
