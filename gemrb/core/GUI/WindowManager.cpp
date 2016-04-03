77aca02972290acf3aac67fc10de36cc59f23038
Brad Allred <bradallred@me.com>
2016-04-03 13:33:13 -0600

    WindowManager: use a display formatted buffer for the cursor
    we dont need full alpha, just color key (for now)

--- gemrb/core/GUI/WindowManager.cpp
+++ gemrb/core/GUI/WindowManager.cpp
@@ -65,7 +65,8 @@ WindowManager::WindowManager(Video* vid)
 
 	// FIXME: need to recreate these if resolution changes
 	// FIXME: this cursor buffer size is arbitrary. We should measure the size of the tooltip area.
-	cursorBuf = vid->CreateBuffer(Region(0,0,128,64), Video::RGBA8888);
+	cursorBuf = vid->CreateBuffer(Region(0,0,128,64));
+	cursorBuf->SetColorKey(ColorGreen);
 	winFrameBuf = vid->CreateBuffer(screen, Video::RGBA8888);
 
 	// set the buffer that always gets cleared just in case anything
