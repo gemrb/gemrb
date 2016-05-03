866b175987987cce000d59ac1a665db2b6177fe6
MarcelHB <marscel@googlemail.com>
2016-05-03 10:31:50 +0200

    SDLVideo/GUI: changes `MouseUp' bitmask on rk to GEM_MB_ONGOING_ACTION
    This way, we can identify `MouseUp' when unintended by modified
    `rk_flags'.

--- gemrb/plugins/SDLVideo/SDLVideo.cpp
+++ gemrb/plugins/SDLVideo/SDLVideo.cpp
@@ -155,7 +155,7 @@ int SDLVideoDriver::PollEvents()
 		GetMousePos(x, y);
 		lastMouseDownTime=lastTime + EvntManager->GetRKDelay();
 		if (!core->ConsolePopped) {
-			EvntManager->MouseUp( x, y, GEM_MB_ACTION, GetModState() );
+			EvntManager->MouseUp( x, y, GEM_MB_ONGOING_ACTION, GetModState() );
 			Control* ctl = EvntManager->GetMouseFocusedControl();
 			if (ctl && ctl->ControlType == IE_GUI_BUTTON)
 				// these are repeat events so the control should stay pressed
