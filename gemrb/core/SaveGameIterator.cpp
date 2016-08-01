aa318cfc245ae15ced639f46899d229c92767f87
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2016-08-01 20:03:20 +0200

    CanSave: also check if the actors are confused etc. (controllable)

--- gemrb/core/SaveGameIterator.cpp
+++ gemrb/core/SaveGameIterator.cpp
@@ -513,8 +513,9 @@ static int CanSave()
 	int i = game->GetPartySize(true);
 	while(i--) {
 		Actor *actor = game->GetPC(i, true);
-		//TODO: can't save while (party) actors are in helpless states
-		if (actor->GetStat(IE_STATE_ID) & STATE_NOSAVE) {
+		// can't save while (party) actors are in helpless or dead states
+		// STATE_NOSAVE tracks actors not to be stored in GAM, not game saveability
+		if (actor->GetStat(IE_STATE_ID) & (STATE_NOSAVE|STATE_MINDLESS)) {
 			//some actor is in nosave state
 			displaymsg->DisplayConstantString(STR_CANTSAVENOCTRL, DMC_BG2XPGREEN);
 			return 5;
