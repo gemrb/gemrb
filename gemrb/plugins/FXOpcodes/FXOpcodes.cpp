5c71f0224895a38018dabc82c4f9c6aebe391a91
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2016-09-30 19:15:59 +0200

    removed unused version of State:Deaf
    looks like both were merged at one point; this way there is also no double
    application of the miscast malus

--- gemrb/plugins/FXOpcodes/FXOpcodes.cpp
+++ gemrb/plugins/FXOpcodes/FXOpcodes.cpp
@@ -183,7 +183,6 @@ int fx_cure_feebleminded_state (Scriptable* Owner, Actor* target, Effect* fx);//
 int fx_set_diseased_state (Scriptable* Owner, Actor* target, Effect*fx);//4e
 int fx_cure_diseased_state (Scriptable* Owner, Actor* target, Effect* fx);//4f
 int fx_set_deaf_state (Scriptable* Owner, Actor* target, Effect* fx); //50
-int fx_set_deaf_state_iwd2 (Scriptable* Owner, Actor* target, Effect* fx); //50
 int fx_cure_deaf_state (Scriptable* Owner, Actor* target, Effect* fx);//51
 int fx_set_ai_script (Scriptable* Owner, Actor* target, Effect*fx);//52
 int fx_protection_from_projectile (Scriptable* Owner, Actor* target, Effect*fx);//53
@@ -722,7 +721,6 @@ static EffectDesc effectnames[] = {
 	{ "State:Charmed", fx_set_charmed_state, EFFECT_NO_LEVEL_CHECK, -1 }, //0x05
 	{ "State:Confused", fx_set_confused_state, 0, -1 },
 	{ "State:Deafness", fx_set_deaf_state, 0, -1 },
-	{ "State:DeafnessIWD2", fx_set_deaf_state_iwd2, 0, -1 }, //this is a modified version
 	{ "State:Diseased", fx_set_diseased_state, 0, -1 },
 	{ "State:Feeblemind", fx_set_feebleminded_state, 0, -1 },
 	{ "State:Hasted", fx_set_hasted_state, 0, -1 },
@@ -829,7 +827,6 @@ static EffectRef fx_death_ward_ref = { "DeathWard", -1 };  //iwd2 specific
 static EffectRef fx_death_magic_ref = { "Death2", -1 };    //iwd2 specific
 static EffectRef fx_apply_effect_curse_ref = { "ApplyEffectCurse", -1 }; //0x11b
 static EffectRef fx_pst_jumble_curse_ref = { "JumbleCurse", -1 };  //PST specific
-static EffectRef fx_deaf_state_iwd2_ref = { "State:DeafnessIWD2", -1 }; //iwd2
 static EffectRef fx_bane_ref = { "Bane", -1 }; //iwd2
 static EffectRef fx_protection_from_animation_ref = { "Protection:Animation", -1 }; //0x128
 static EffectRef fx_change_bardsong_ref = { "ChangeBardSong", -1 };
@@ -2892,27 +2889,6 @@ int fx_set_deaf_state (Scriptable* /*Owner*/, Actor* target, Effect* fx)
 	return FX_APPLIED;
 }
 
-int fx_set_deaf_state_iwd2 (Scriptable* /*Owner*/, Actor* target, Effect* fx)
-{
-	if(0) print("fx_set_deaf_state_iwd2(%2d): Mod: %d, Type: %d", fx->Opcode, fx->Parameter1, fx->Parameter2);
-
-	//gemrb fix
-	if (target->SetSpellState(SS_DEAF)) return FX_APPLIED;
-
-	if (!fx->Parameter1) {
-		//this is a bad hack
-		fx->Parameter1 = 20;
-	}
-	STAT_ADD(IE_SPELLFAILUREMAGE, fx->Parameter1);
-	if (!fx->Parameter2) {
-		fx->Parameter2 = 20;
-	}
-	STAT_ADD(IE_SPELLFAILUREPRIEST, fx->Parameter2);
-	EXTSTATE_SET(EXTSTATE_DEAF); //iwd1/how needs this
-	target->AddPortraitIcon(PI_DEAFNESS); //iwd2 specific
-	return FX_APPLIED;
-}
-
 // 0x51 Cure:Deafness
 //removes the deafness effect
 int fx_cure_deaf_state (Scriptable* /*Owner*/, Actor* target, Effect* fx)
@@ -2920,7 +2896,6 @@ int fx_cure_deaf_state (Scriptable* /*Owner*/, Actor* target, Effect* fx)
 	if(0) print("fx_cure_deaf_state(%2d): Mod: %d, Type: %d", fx->Opcode, fx->Parameter1, fx->Parameter2);
 
 	target->fxqueue.RemoveAllEffects(fx_deaf_state_ref);
-	target->fxqueue.RemoveAllEffects(fx_deaf_state_iwd2_ref);
 	return FX_NOT_APPLIED;
 }
 
