gemrb/gemrb

commit 4a7a3e10ba1890b3c546f1937170290c19f497de

fix some crash issues from last batch

gemrb/core/Interface.cpp
@@ -3160,7 +3160,7 @@ void Interface::LoadGame(SaveGame *sg, int ver_override)
 		gam_str = sg->GetGame();
 		sav_str = sg->GetSave();
 		wmp_str1 = sg->GetWmap(0);
-		if (WorldMapName[1][0]) {
+		if (!WorldMapName[1].IsEmpty()) {
 			wmp_str2 = sg->GetWmap(1);
 			if (!wmp_str2) {
 				//upgrade an IWD game to HOW

gemrb/plugins/GUIScript/PythonConversions.cpp
@@ -73,7 +73,7 @@ Region RectFromPy(PyObject* obj) {
 }
 
 ResRef ResRefFromPy(PyObject* obj) {
-	if (PyString_Check(obj)) {
+	if (obj && PyString_Check(obj)) {
 		return ResRef(PyString_AsString(obj));
 	}
 	return ResRef();

commit c4cc20caaeb5c0a3cfc70fcea1d135894e0a6f0d

fixed more wm regressions not dealt with in 4a7a3e10b
enables starting chargen again

gemrb/core/Interface.cpp
@@ -3153,7 +3153,7 @@ void Interface::LoadGame(SaveGame *sg, int ver_override)
 		gam_str = gamedata->GetResource( GameNameResRef, IE_GAM_CLASS_ID );
 		sav_str = NULL;
 		wmp_str1 = gamedata->GetResource( WorldMapName[0], IE_WMP_CLASS_ID );
-		if (WorldMapName[1][0]) {
+		if (!WorldMapName[1].IsEmpty()) {
 			wmp_str2 = gamedata->GetResource( WorldMapName[1], IE_WMP_CLASS_ID );
 		}
 	} else {
@@ -4389,7 +4389,7 @@ int Interface::WriteWorldMap(const char *folder)
 		return -1;
 	}
 
-	if (WorldMapName[1][0]) {
+	if (!WorldMapName[1].IsEmpty()) {
 		worldmap->SetSingle(false);
 	}
 