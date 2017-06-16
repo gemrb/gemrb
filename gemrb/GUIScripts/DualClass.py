gemrb/gemrb

commit 9490b35c52de76adba7494a36af4b4f337571931

GUIScript: consolidating methods
for setting button picture, and getting PC portrait

gemrb/GUIScripts/pst/GUISAVE.py
@@ -263,7 +263,7 @@ def OpenSaveDetailWindow ():
 	# PC portraits
 	for j in range (PARTY_SIZE):
 		Button = Window.GetControl (6 + j)
-		Button.SetSprite2D(GemRB.GetPCPortrait(j))
+		Button.SetSprite2D(GemRB.GetPlayerPortrait(j))
 
 
 	CheckSaveName ()

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -2322,96 +2322,48 @@ static PyObject* GemRB_Button_SetPictureClipping(PyObject* self, PyObject* args)
 }
 
 PyDoc_STRVAR( GemRB_Button_SetPicture__doc,
-"SetButtonPicture(WindowIndex, ControlIndex, PictureResRef, DefaultResRef)\n\n"
-"Sets the Picture of a Button Control from a BMP file. You can also supply a default picture." );
+"SetButtonPicture(Button, PictureResRef|Sprite2D, DefaultResRef|Sprite2D)\n\n"
+"Sets the Picture of a Button Control from a MOS/BMP file. You can also supply a default picture." );
 
 static PyObject* GemRB_Button_SetPicture(PyObject* self, PyObject* args)
 {
-	char *ResRef;
-	char *DefResRef = NULL;
-	PARSE_ARGS( args,  "Os|s", &self, &ResRef, &DefResRef );
+	PyObject* pypic, *pydefaultPic;
+	PARSE_ARGS( args,  "OO|O", &self, &pypic, &pydefaultPic );
 
 	Button* btn = GetView<Button>(self);
 	if (!btn) {
 		return RuntimeError("Cannot find the button!\n");
 	}
 
-	if (ResRef[0] == 0) {
-		btn->SetPicture( NULL );
-		Py_RETURN_NONE;
+	Holder<Sprite2D> pic = NULL;
+	if (PyObject_TypeCheck( pypic, &PyString_Type )) {
+		ResourceHolder<ImageMgr> im(PyString_AsString(pypic));
+		if (im) {
+			pic = im->GetSprite2D();
+			pic->release(); // prevent leak
+		} else {
+			return RuntimeError("Picture resource not found!\n");
+		}
+	} else {
+		pic = CObject<Sprite2D>(pypic);
 	}
 
-	ImageFactory* fact = ( ImageFactory* )
-		gamedata->GetFactoryResource(ResRef, IE_BMP_CLASS_ID, IE_NORMAL, true);
-
-	//if the resource doesn't exist, but we have a default resource
-	//use this resource
-	if (!fact && DefResRef) {
-		fact = ( ImageFactory* )
-			gamedata->GetFactoryResource( DefResRef, IE_BMP_CLASS_ID, IE_NORMAL );
+	if (!pic) {
+		// use default pic
+		if (PyObject_TypeCheck( pydefaultPic, &PyString_Type )) {
+			ResourceHolder<ImageMgr> im(PyString_AsString(pydefaultPic));
+			if (im) {
+				pic = im->GetSprite2D();
+				pic->release(); // prevent leak
+			} else {
+				return RuntimeError("Picture resource not found!\n");
+			}
+		} else {
+			pic = CObject<Sprite2D>(pydefaultPic);
+		}
 	}
 
-	if (!fact) {
-		return RuntimeError("Picture resource not found!\n");
-	}
-
-	Sprite2D* Picture = fact->GetSprite2D();
-	if (Picture == NULL) {
-		return RuntimeError("Failed to acquire the picture!\n");
-	}
-
-	btn->SetPicture( Picture );
-
-	Py_RETURN_NONE;
-}
-
-PyDoc_STRVAR( GemRB_Button_SetSprite2D__doc,
-"Button.SetSprite2D(WindowIndex, ControlIndex, Sprite2D)\n\n"
-"Sets a Sprite2D onto a button as picture." );
-
-static PyObject* GemRB_Button_SetSprite2D(PyObject* self, PyObject* args)
-{
-	PyObject *obj;
-	PARSE_ARGS( args,  "OO", &self, &obj );
-	Button* btn = GetView<Button>(self);
-	ABORT_IF_NULL(btn);
-
-	CObject<Sprite2D> spr(obj);
-
-	btn->SetPicture( spr.get() );
-
-	Py_RETURN_NONE;
-}
-
-PyDoc_STRVAR( GemRB_Button_SetMOS__doc,
-"SetButtonMOS(WindowIndex, ControlIndex, MOSResRef)\n\n"
-"Sets the Picture of a Button Control from a MOS file." );
-
-static PyObject* GemRB_Button_SetMOS(PyObject* self, PyObject* args)
-{
-	char *ResRef;
-	PARSE_ARGS( args,  "Os", &self, &ResRef );
-
-	Button* btn = GetView<Button>(self);
-	ABORT_IF_NULL(btn);
-
-	if (ResRef[0] == 0) {
-		btn->SetPicture( NULL );
-		Py_RETURN_NONE;
-	}
-
-	ResourceHolder<ImageMgr> im(ResRef);
-	if (im == NULL) {
-		return RuntimeError("Picture resource not found!\n");
-	}
-
-	Sprite2D* Picture = im->GetSprite2D();
-	if (Picture == NULL) {
-		return RuntimeError("Failed to acquire the picture!\n");
-	}
-
-	btn->SetPicture( Picture );
-
+	btn->SetPicture(pic.get());
 	Py_RETURN_NONE;
 }
 
@@ -3120,27 +3072,6 @@ static PyObject* GemRB_GetGamePreview(PyObject * /*self*/, PyObject* /*args*/)
 {
 	GET_GAMECONTROL();
 	return CObject<Sprite2D>(gc->GetPreview());
-}
-
-PyDoc_STRVAR( GemRB_GetPCPortrait__doc,
-"GemRB_GetPCPortrait(PCSlotCount)\n\n"
-"Gets a current game PC portrait." );
-
-static PyObject* GemRB_GetPCPortrait(PyObject * /*self*/, PyObject* args)
-{
-	int PCSlotCount;
-	PARSE_ARGS( args,  "i", &PCSlotCount );
-
-	GET_GAME();
-	Actor* actor = game->GetPC( PCSlotCount, false );
-	if (actor) {
-		Sprite2D* portrait = actor->CopyPortrait(1);
-		CObject<Sprite2D> obj(portrait);
-		portrait->release();
-		return obj;
-	} else {
-		Py_RETURN_NONE;
-	}
 }
 
 PyDoc_STRVAR( GemRB_Roll__doc,
@@ -4030,17 +3961,19 @@ PyDoc_STRVAR( GemRB_GetPlayerPortrait__doc,
 
 static PyObject* GemRB_GetPlayerPortrait(PyObject * /*self*/, PyObject* args)
 {
-	int PlayerSlot, Which;
+	int slot, which = 0;
+	PARSE_ARGS( args,  "i|i", &slot, &which );
 
-	Which = 0;
-	PARSE_ARGS( args,  "i|i", &PlayerSlot, &Which );
 	GET_GAME();
-
-	Actor* MyActor = game->FindPC( PlayerSlot );
-	if (!MyActor) {
-		return PyString_FromString( "");
+	Actor* actor = game->GetPC( slot, false );
+	if (actor) {
+		Sprite2D* portrait = actor->CopyPortrait(which);
+		CObject<Sprite2D> obj(portrait);
+		portrait->release();
+		return obj;
+	} else {
+		Py_RETURN_NONE;
 	}
-	return PyString_FromString( Which ? MyActor->SmallPortrait : MyActor->LargePortrait );
 }
 
 PyDoc_STRVAR( GemRB_GetPlayerString__doc,
@@ -8997,7 +8930,6 @@ static PyMethodDef GemRBMethods[] = {
 	METHOD(GetDamageReduction, METH_VARARGS),
 	METHOD(GetEquippedAmmunition, METH_VARARGS),
 	METHOD(GetEquippedQuickSlot, METH_VARARGS),
-	METHOD(GetPCPortrait, METH_VARARGS),
 	METHOD(GetGamePreview, METH_VARARGS),
 	METHOD(GetGameString, METH_VARARGS),
 	METHOD(GetGameTime, METH_NOARGS),
@@ -9147,13 +9079,11 @@ static PyMethodDef GemRBInternalMethods[] = {
 	METHOD(Button_SetAnchor, METH_VARARGS),
 	METHOD(Button_SetPushOffset, METH_VARARGS),
 	METHOD(Button_SetItemIcon, METH_VARARGS),
-	METHOD(Button_SetMOS, METH_VARARGS),
 	METHOD(Button_SetOverlay, METH_VARARGS),
 	METHOD(Button_SetPLT, METH_VARARGS),
 	METHOD(Button_SetPicture, METH_VARARGS),
 	METHOD(Button_SetPictureClipping, METH_VARARGS),
 	METHOD(Button_SetSpellIcon, METH_VARARGS),
-	METHOD(Button_SetSprite2D, METH_VARARGS),
 	METHOD(Button_SetSprites, METH_VARARGS),
 	METHOD(Button_SetState, METH_VARARGS),
 	METHOD(Button_SetTextColor, METH_VARARGS),

gemrb/GUIScripts/GUIClasses.py
@@ -173,14 +173,18 @@ class GButton(GControl):
     'SetState': _GemRB.Button_SetState,
     'SetPictureClipping': _GemRB.Button_SetPictureClipping,
     'SetPicture': _GemRB.Button_SetPicture,
-    'SetSprite2D': _GemRB.Button_SetSprite2D,
-    'SetMOS': _GemRB.Button_SetMOS,
     'SetPLT': _GemRB.Button_SetPLT,
     'SetBAM': _GemRB.Button_SetBAM,
     'SetSpellIcon': _GemRB.Button_SetSpellIcon,
     'SetItemIcon': _GemRB.Button_SetItemIcon,
     'SetActionIcon': _GemRB.Button_SetActionIcon
   }
+
+  def SetMOS(self, mos):
+	  self.SetPicture(mos) # backwards compatibility
+  
+  def SetSprite2D(self, spr):
+	  self.SetPicture(spr) # backwards compatibility
 
   def CreateLabel(self, labelid, *args):
     frame = self.GetFrame()

commit b3f258a4698408f6454669453e13a398b0a546b9

reworked GemRB_GetPlayerPortrait
fixes blatant regressions from 9490b35c52
fixes crash on null portrait

gemrb/GUIScripts/DualClass.py
@@ -116,7 +116,7 @@ def DualClassWindow ():
 	DCMainPictureButton = DCMainWindow.GetControl (6)
 	DCMainPictureButton.SetState (IE_GUI_BUTTON_LOCKED)
 	DCMainPictureButton.SetFlags (IE_GUI_BUTTON_NO_IMAGE | IE_GUI_BUTTON_PICTURE, OP_SET)
-	DCMainPictureButton.SetPicture (GemRB.GetPlayerPortrait (pc, 0), "NOPORTMD")
+	DCMainPictureButton.SetPicture (GemRB.GetPlayerPortrait (pc, 0)["Sprite"], "NOPORTMD")
 
 	# text area warning
 	DCTextArea = DCMainWindow.GetControl (7)

gemrb/GUIScripts/GUICommonWindows.py
@@ -1607,7 +1607,7 @@ def UpdatePortraitWindow ():
 			UpdateAnimatedPortrait(Window, i)
 			continue
 
-		pic = GemRB.GetPlayerPortrait (pcID, 1)
+		pic = GemRB.GetPlayerPortrait (pcID, 1)["Sprite"]
 		if Inventory and pc != pcID:
 			pic = None
 
@@ -1689,7 +1689,7 @@ def UpdateAnimatedPortrait (Window,i):
 	# note: there are actually two portraits per chr, eg PPPANN (static), WMPANN (animated)
 	Button = Window.GetControl (i)
 	ButtonHP = Window.GetControl (6 + i)
-	pic = GemRB.GetPlayerPortrait (i+1, 0)
+	pic = GemRB.GetPlayerPortrait (i+1, 0)["Sprite"]
 	if not pic:
 		Button.SetFlags (IE_GUI_BUTTON_NO_IMAGE, OP_SET)
 		ButtonHP.SetFlags (IE_GUI_BUTTON_NO_IMAGE, OP_SET)

gemrb/GUIScripts/GUIREC.py
@@ -141,10 +141,11 @@ def UpdateRecordsWindow (Window):
 	Button = Window.GetControl (2)
 	Button.SetFlags (IE_GUI_BUTTON_NO_IMAGE | IE_GUI_BUTTON_PICTURE, OP_SET)
 	Button.SetState (IE_GUI_BUTTON_LOCKED)
+	pic = GemRB.GetPlayerPortrait (pc, 0)["Sprite"]
 	if (GameCheck.IsBG2() or GameCheck.IsIWD1()) and not GameCheck.IsBG2Demo():
-		Button.SetPicture (GemRB.GetPlayerPortrait (pc,0), "NOPORTMD")
+		Button.SetPicture (pic, "NOPORTMD")
 	else:
-		Button.SetPicture (GemRB.GetPlayerPortrait (pc,0), "NOPORTLG")
+		Button.SetPicture (pic, "NOPORTLG")
 
 	# armorclass
 	Label = Window.GetControl (0x10000028)

gemrb/GUIScripts/GUIRECCommon.py
@@ -178,7 +178,7 @@ def OpenPortraitSelectWindow ():
 
 	# get players gender and portrait
 	Pc = GemRB.GameGetSelectedPCSingle ()
-	PcPortrait = GemRB.GetPlayerPortrait(Pc,0)
+	PcPortrait = GemRB.GetPlayerPortrait (Pc, 0)["ResRef"]
 
 	# initialize and set portrait
 	Portrait.Init (Gender)

gemrb/GUIScripts/GUIWORLD.py
@@ -146,7 +146,7 @@ def UpdateReformWindow ():
 			continue # for saved games with higher party count than the current setup supports
 		Button = PortraitButtons[index]
 		Button.EnableBorder (FRAME_PC_SELECTED, select == i)
-		pic = GemRB.GetPlayerPortrait (i, 1)
+		pic = GemRB.GetPlayerPortrait (i, 1)["Sprite"]
 		if not pic:
 			Button.SetFlags (IE_GUI_BUTTON_NO_IMAGE, OP_SET)
 			Button.SetState (IE_GUI_BUTTON_LOCKED)

gemrb/GUIScripts/bg1/GUIMP.py
@@ -48,7 +48,7 @@ def OnLoad():
 		#removing this label, it just disturbs us
 		Label.SetSize(0, 0)
 		Button = PartyFormationWindow.GetControl(i-12)
-		ResRef = GemRB.GetPlayerPortrait(i-17, 1)
+		ResRef = GemRB.GetPlayerPortrait (i-17, 1)["ResRef"]
 		if ResRef == "":
 			Button.SetFlags(IE_GUI_BUTTON_NORMAL,OP_SET)
 		else:
@@ -117,7 +117,7 @@ def ExitCancelPress():
 def GeneratePress():
 	global PartyFormationWindow
 	slot = GemRB.GetVar("Slot")
-	ResRef = GemRB.GetPlayerPortrait(slot, 0)
+	ResRef = GemRB.GetPlayerPortrait (slot, 0)["ResRef"]
 	if ResRef:
 		print "Already existing slot, we should drop it"
 	if PartyFormationWindow:

gemrb/GUIScripts/bg1/ImportFile.py
@@ -62,8 +62,8 @@ def DonePress():
 	FileName = TextAreaControl.QueryText()
 	Slot = GemRB.GetVar("Slot")
 	GemRB.CreatePlayer(FileName, Slot| 0x8000, 1)
-	GemRB.SetToken("SmallPortrait", GemRB.GetPlayerPortrait (Slot, 1) )
-	GemRB.SetToken("LargePortrait", GemRB.GetPlayerPortrait (Slot, 0) )
+	GemRB.SetToken ("SmallPortrait", GemRB.GetPlayerPortrait (Slot, 1)["ResRef"])
+	GemRB.SetToken ("LargePortrait", GemRB.GetPlayerPortrait (Slot, 0)["ResRef"])
 	CharGenCommon.jumpTo("appearance")
 	return
 

gemrb/GUIScripts/bg2/ImportFile.py
@@ -61,9 +61,10 @@ def DonePress():
 	if ImportWindow:
 		ImportWindow.Unload()
 	# the medium portrait isn't available, so we copy the original hack
-	MediumPortrait = GemRB.GetPlayerPortrait (Slot, 1)[0:-1] + "M"
-	GemRB.SetToken("SmallPortrait", GemRB.GetPlayerPortrait (Slot, 1) )
-	GemRB.SetToken("LargePortrait", MediumPortrait )
+	SmallPortrait = GemRB.GetPlayerPortrait (Slot, 1)["ResRef"]
+	MediumPortrait = SmallPortrait[0:-1] + "M"
+	GemRB.SetToken ("SmallPortrait", SmallPortrait)
+	GemRB.SetToken ("LargePortrait", MediumPortrait)
 	GemRB.SetNextScript("CharGen7")
 	return
 	

gemrb/GUIScripts/iwd/CharGen.py
@@ -2704,10 +2704,10 @@ def ImportDonePress():
 	GemRB.CreatePlayer (CharImportList.QueryText(), MyChar|0x8000, 1)
 
 	GemRB.SetToken ("CHARNAME", GemRB.GetPlayerName (MyChar) )
-	GemRB.SetToken ("SmallPortrait", GemRB.GetPlayerPortrait (MyChar, 1) )
-	PortraitName = GemRB.GetPlayerPortrait (MyChar, 0)
-	GemRB.SetToken ("LargePortrait", PortraitName )
-	PortraitButton.SetPicture (PortraitName)
+	GemRB.SetToken ("SmallPortrait", GemRB.GetPlayerPortrait (MyChar, 1)["ResRef"])
+	Portrait = GemRB.GetPlayerPortrait (MyChar, 0)
+	GemRB.SetToken ("LargePortrait", Portrait["ResRef"])
+	PortraitButton.SetPicture (Portrait["Sprite"])
 	Portrait = -1
 
 	ImportedChar = 1

gemrb/GUIScripts/iwd/PartyFormation.py
@@ -60,7 +60,7 @@ def OnLoad ():
 		#removing this label, it just disturbs us
 		Label.SetSize (0, 0)
 		Button = PartyFormationWindow.GetControl (i-12)
-		ResRef = GemRB.GetPlayerPortrait (i-17, 1)
+		ResRef = GemRB.GetPlayerPortrait (i-17, 1)["ResRef"]
 		if ResRef == "":
 			Button.SetFlags (IE_GUI_BUTTON_NORMAL,OP_SET)
 		else:

gemrb/GUIScripts/iwd2/GUIINV.py
@@ -228,7 +228,7 @@ def RefreshInventoryWindow ():
 
 	# portrait
 	Button = Window.GetControl (84)
-	Button.SetPicture (GemRB.GetPlayerPortrait (pc,0))
+	Button.SetPicture (GemRB.GetPlayerPortrait (pc, 0)["Sprite"])
 
 	# encumbrance
 	GUICommon.SetEncumbranceLabels (Window, 0x10000042, None, pc)

gemrb/GUIScripts/iwd2/GUIREC.py
@@ -941,7 +941,7 @@ def UpdateRecordsWindow ():
 
 	#portrait
 	Button = Window.GetControl (2)
-	Button.SetPicture (GemRB.GetPlayerPortrait (pc,0))
+	Button.SetPicture (GemRB.GetPlayerPortrait (pc, 0)["Sprite"])
 
 	# armorclass
 	Label = Window.GetControl (0x10000028)

gemrb/GUIScripts/iwd2/GUISPL.py
@@ -175,7 +175,7 @@ def UpdateSpellBookWindow ():
 	Label.SetText (Name)
 
 	Button = Window.GetControl (1)
-	Button.SetPicture (GemRB.GetPlayerPortrait (pc,0))
+	Button.SetPicture (GemRB.GetPlayerPortrait (pc, 0)["Sprite"])
 	Button.SetState (IE_GUI_BUTTON_LOCKED)
 
 	mem_cnt = GetMemorizedSpellsCount () # count of unique memorized spells

gemrb/GUIScripts/iwd2/ImportFile.py
@@ -90,7 +90,7 @@ def SelectFile():
 	FileName = TextAreaControl.QueryText()
 	Slot = GemRB.GetVar("Slot")
 	GemRB.CreatePlayer(FileName, Slot| 0x8000, 1)
-	Portrait = GemRB.GetPlayerPortrait (Slot,0)
+	Portrait = GemRB.GetPlayerPortrait (Slot, 0)["Sprite"]
 	PortraitButton.SetPicture (Portrait, "NOPORTLG") 
 	ImportWindow.Focus() #bring it to the front
 	DoneButton.SetState(IE_GUI_BUTTON_ENABLED)

gemrb/GUIScripts/iwd2/MessageWindow.py
@@ -97,7 +97,7 @@ def UpdateControlStatus():
 		#gets PC currently talking
 		pc = GemRB.GameGetSelectedPCSingle (1)
 		if pc:
-			Portrait = GemRB.GetPlayerPortrait(pc,1)
+			Portrait = GemRB.GetPlayerPortrait(pc, 1)["Sprite"]
 		else:
 			Portrait = None
 		Button = TMessageWindow.GetControl(11)

gemrb/GUIScripts/iwd2/SPPartyFormation.py
@@ -51,7 +51,7 @@ def OnLoad ():
 		#removing this label, it just disturbs us
 		Label.SetSize (0, 0)
 		Button = PartyFormationWindow.GetControl (i-12)
-		ResRef = GemRB.GetPlayerPortrait (i-17, 1)
+		ResRef = GemRB.GetPlayerPortrait (i-17, 1)["ResRef"]
 		if ResRef == "":
 			Button.SetFlags (IE_GUI_BUTTON_NORMAL,OP_SET)
 			Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, GeneratePress)

gemrb/GUIScripts/pst/GUISAVE.py
@@ -252,7 +252,7 @@ def OpenSaveDetailWindow ():
 	# PC portraits
 	for j in range (PARTY_SIZE):
 		Button = Window.GetControl (6 + j)
-		Button.SetSprite2D(GemRB.GetPlayerPortrait(j))
+		Button.SetSprite2D (GemRB.GetPlayerPortrait (j)["Sprite"])
 
 
 	CheckSaveName ()

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -6068,7 +6068,9 @@ new character you must use FillPlayerInfo().\n\
   * Slot         - the PC's position in the party\n\
   * SmallOrLarge - boolean, specify 1 if you want to get the large portrait\n\
 \n\
-**Return value:** the player's portrait name (image resref)\n\
+**Return value:** dict\n\
+  * Sprite - the player's portrait (image)\n\
+  * ResRef - the portrait's name (image resref)\n\
 \n\
 **See also:** [[guiscript:FillPlayerInfo]]\n\
 "
@@ -6084,10 +6086,13 @@ static PyObject* GemRB_GetPlayerPortrait(PyObject * /*self*/, PyObject* args)
 	if (actor) {
 		Sprite2D* portrait = actor->CopyPortrait(which);
 		CObject<Sprite2D> obj(portrait);
-		portrait->release();
-		return obj;
+		if (portrait) portrait->release();
+		PyObject* dict = PyDict_New();
+		PyDict_SetItemString(dict, "Sprite", obj);
+		PyDict_SetItemString(dict, "ResRef", PyString_FromString(which ? actor->SmallPortrait : actor->LargePortrait));
+		return dict;
 	} else {
-		Py_RETURN_NONE;
+		return Py_BuildValue("{s:O,s:s}", "Sprite", Py_None, "ResRef", "");
 	}
 }
 