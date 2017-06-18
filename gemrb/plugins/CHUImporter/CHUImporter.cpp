gemrb/gemrb

commit 7d3a4234696b3317222182244d071fe9f6776567

remove redundant Color struct

gemrb/plugins/CHUImporter/CHUImporter.cpp
@@ -393,30 +393,31 @@ Window* CHUImporter::GetWindow(ScriptingId wid) const
 				//Label
 				ieResRef FontResRef;
 				ieStrRef StrRef;
-				RevColor fore, back;
 				ieWord alignment;
 				str->ReadDword( &StrRef );
 				str->ReadResRef( FontResRef );
 				Font* fnt = core->GetFont( FontResRef );
-				str->Read( &fore, 4 );
-				str->Read( &back, 4 );
+
+				// bgra format
+				Color f, b;
+				str->Read( &f.b, 1 );
+				str->Read( &f.g, 1 );
+				str->Read( &f.r, 1 );
+				str->Read( &f.a, 1 ); // FIXME: seek 1 instead? previously alpha was 0
+
+				str->Read( &b.b, 1 );
+				str->Read( &b.g, 1 );
+				str->Read( &b.r, 1 );
+				str->Read( &b.a, 1 ); // FIXME: seek 1 instead? previously alpha was 0
+
 				str->ReadWord( &alignment );
 				String* str = core->GetString( StrRef );
 				Label* lab = new Label( ctrlFrame, fnt, *str );
 				delete str;
+				lab->SetColor( f, b );
 
 				if (alignment & 1) {
 					lab->useRGB = true;
-					Color f, b;
-					f.r = fore.b;
-					f.g = fore.g;
-					f.b = fore.r;
-					f.a = 0;
-					b.r = back.b;
-					b.g = back.g;
-					b.b = back.r;
-					b.a = 0;
-					lab->SetColor( f, b );
 				}
 				int align = IE_FONT_ALIGN_CENTER;
 				if (( alignment & 0x10 ) != 0) {

gemrb/includes/RGBAColor.h
@@ -22,10 +22,6 @@
 
 namespace GemRB {
 
-struct RevColor {
-	unsigned char b,g,r,a;
-};
-
 struct Color {
 	unsigned char r,g,b,a;
 

gemrb/plugins/BAMImporter/BAMImporter.cpp
@@ -114,12 +114,11 @@ bool BAMImporter::Open(DataStream* stream)
 	palette = new Palette();
 	// no need to switch this
 	for (i = 0; i < 256; i++) {
-		RevColor rc;
-		str->Read( &rc, 4 );
-		palette->col[i].r = rc.r;
-		palette->col[i].g = rc.g;
-		palette->col[i].b = rc.b;
-		palette->col[i].a = rc.a;
+		// bgra format
+		str->Read( &palette->col[i].b, 1 );
+		str->Read( &palette->col[i].g, 1 );
+		str->Read( &palette->col[i].r, 1 );
+		str->Read( &palette->col[i].a, 1 );
 	}
 	// old bamworkshop semicorrupted shadow entry: recreate a plausible one instead of pink
 	if (palette->col[1].r == 255 && palette->col[1].g == 101 && palette->col[1].b == 151) {

gemrb/plugins/MOSImporter/MOSImporter.cpp
@@ -75,7 +75,7 @@ bool MOSImporter::Open(DataStream* stream)
 
 Sprite2D* MOSImporter::GetSprite2D()
 {
-	RevColor RevCol[256];
+	Color Col[256];
 	unsigned char * pixels = ( unsigned char * ) malloc( Width * Height * 4 );
 	unsigned char * blockpixels = ( unsigned char * )
 		malloc( BlockSize * BlockSize );
@@ -90,7 +90,7 @@ Sprite2D* MOSImporter::GetSprite2D()
 				64;
 			str->Seek( PalOffset + ( y * Cols * 1024 ) +
 				( x * 1024 ), GEM_STREAM_START );
-			str->Read( &RevCol[0], 1024 );
+			str->Read( &Col[0], 1024 );
 			str->Seek( PalOffset + ( Rows * Cols * 1024 ) +
 				( y * Cols * 4 ) + ( x * 4 ),
 				GEM_STREAM_START );
@@ -105,13 +105,13 @@ Sprite2D* MOSImporter::GetSprite2D()
 				( 4 * x * 64 );
 			for (int h = 0; h < bh; h++) {
 				for (int w = 0; w < bw; w++) {
-					*startpixel = RevCol[*bp].b;
+					*startpixel = Col[*bp].r;
 					startpixel++;
-					*startpixel = RevCol[*bp].g;
+					*startpixel = Col[*bp].g;
 					startpixel++;
-					*startpixel = RevCol[*bp].r;
+					*startpixel = Col[*bp].b;
 					startpixel++;
-					*startpixel = RevCol[*bp].a;
+					*startpixel = Col[*bp].a;
 					startpixel++;
 					bp++;
 				}

gemrb/plugins/TISImporter/TISImporter.cpp
@@ -88,7 +88,7 @@ Tile* TISImporter::GetTile(unsigned short* indexes, int count,
 
 Sprite2D* TISImporter::GetTile(int index)
 {
-	RevColor RevCol[256];
+	Color Col[256];
 	Color Palette[256];
 	void* pixels = malloc( 4096 );
 	unsigned long pos = index *(1024+4096) + headerShift;
@@ -113,14 +113,15 @@ Sprite2D* TISImporter::GetTile(int index)
 		return spr;
 	}
 	str->Seek( pos, GEM_STREAM_START );
-	str->Read( &RevCol, 1024 );
+	str->Read( &Col, 1024 );
 	int transindex = 0;
 	bool transparent = false;
 	for (int i = 0; i < 256; i++) {
-		Palette[i].r = RevCol[i].r;
-		Palette[i].g = RevCol[i].g;
-		Palette[i].b = RevCol[i].b;
-		Palette[i].a = RevCol[i].a;
+		// bgra format
+		Palette[i].r = Col[i].b;
+		Palette[i].g = Col[i].g;
+		Palette[i].b = Col[i].r;
+		Palette[i].a = Col[i].a;
 		if (Palette[i].g==255 && !Palette[i].r && !Palette[i].b) {
 			if (transparent) {
 				Log(ERROR, "TISImporter", "Tile has two green (transparent) palette entries");

commit 70e43044c737e54c573d3ba9a03ba0a85245ec75

CHUImporter::GetWindow: label colors are not stored in BGRA format
fixes some titles having wrong colors (eg. item description)
also makes color setting contingent on a flag again

regressions from 7d3a4234

gemrb/plugins/CHUImporter/CHUImporter.cpp
@@ -398,26 +398,25 @@ Window* CHUImporter::GetWindow(ScriptingId wid) const
 				str->ReadResRef( FontResRef );
 				Font* fnt = core->GetFont( FontResRef );
 
-				// bgra format
 				Color f, b;
-				str->Read( &f.b, 1 );
-				str->Read( &f.g, 1 );
 				str->Read( &f.r, 1 );
+				str->Read( &f.g, 1 );
+				str->Read( &f.b, 1 );
 				str->Read( &f.a, 1 ); // FIXME: seek 1 instead? previously alpha was 0
 
-				str->Read( &b.b, 1 );
-				str->Read( &b.g, 1 );
 				str->Read( &b.r, 1 );
+				str->Read( &b.g, 1 );
+				str->Read( &b.b, 1 );
 				str->Read( &b.a, 1 ); // FIXME: seek 1 instead? previously alpha was 0
 
 				str->ReadWord( &alignment );
 				String* str = core->GetString( StrRef );
 				Label* lab = new Label( ctrlFrame, fnt, *str );
 				delete str;
-				lab->SetColor( f, b );
 
 				if (alignment & 1) {
 					lab->useRGB = true;
+					lab->SetColor(f, b);
 				}
 				int align = IE_FONT_ALIGN_CENTER;
 				if (( alignment & 0x10 ) != 0) {