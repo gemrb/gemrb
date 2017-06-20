a8234b94bc215b77335a3e21100d48400d87a654
Brad Allred <bradallred@me.com>
2017-06-19 19:14:26 -0600

    Interface: create methods for hiding/showing GUI

--- gemrb/core/Interface.h
+++ gemrb/core/Interface.h
@@ -450,6 +450,8 @@ public:
 	/** Creates a Window in the Window Manager */
 #undef CreateWindow // Win32 might define this, so nix it
 	Window* CreateWindow(unsigned short WindowID, const Region&);
+	void HideGUI();
+	void ShowGUI();
 
 	Tooltip CreateTooltip(const String&);
 	/** returns the label which should receive game messages (overrides messagetextarea) */
