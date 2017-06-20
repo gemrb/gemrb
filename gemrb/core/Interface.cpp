049e2329f5cbec605e5504042dad59ecf9f3fbd9
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2017-06-20 23:33:47 +0200

    made EventMgr::TouchInputEnabled externally configurable
    it also uses the new driver methods for a default

--- gemrb/core/Interface.cpp
+++ gemrb/core/Interface.cpp
@@ -93,7 +93,6 @@
 #endif
 
 #include <vector>
-#include <SDL/SDL_version.h> // for SDL_VERSION_ATLEAST
 
 namespace GemRB {
 
@@ -1200,15 +1199,6 @@ int Interface::Init(InterfaceConfig* config)
 		value = NULL;
 
 	CONFIG_INT("MouseFeedback", MouseFeedback = );
-
-// guess a good default
-// TODO: add cfg option for an override (for hybrid devices)
-#if SDL_VERSION_ATLEAST(2,0,0)
-	// note from upstream: on some platforms a device may become seen only after use
-	EventMgr::TouchInputEnabled = SDL_GetNumTouchDevices() > 0;
-#else
-	EventMgr::TouchInputEnabled = MouseFeedback > 0;
-#endif
 	CONFIG_INT("TouchInput", TouchInput =);
 
 	CONFIG_INT("Bpp", Bpp =);
@@ -1415,6 +1405,9 @@ int Interface::Init(InterfaceConfig* config)
 	}
 	ieDword brightness = 10;
 	ieDword contrast = 5;
+
+	// ask the driver if a touch device is in use
+	EventMgr::TouchInputEnabled = TouchInput < 0 ? video->TouchInputEnabled() : TouchInput;
 
 	// SDL2 driver requires the display to be created prior to sprite creation (opengl context)
 	// we also need the display to exist to create sprites using the display format
