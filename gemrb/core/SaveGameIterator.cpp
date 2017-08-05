gemrb/gemrb

commit 28af1312beb603281d016aa729c87187b690dfc2

GameControl: remove GetPreview/GetScreenshot
wrong place for this stuff
preview shouldnt be scaled anyway, save at full size and scale/crop in the button.
temporarily breaks saving the screenshot with saved game

gemrb/core/SaveGameIterator.cpp
@@ -460,10 +460,13 @@ static bool DoSaveGame(const char *Path)
 	}
 
 	// Create area preview
-	Sprite2D* preview = core->GetGameControl()->GetPreview();
+	// FIXME: the preview shoudl be passed in by the caller!
+	/*
+	Sprite2D* preview = NULL;
 	FileStream outfile;
 	outfile.Create( Path, core->GameNameResRef, IE_BMP_CLASS_ID );
 	im->PutImage( &outfile, preview );
+	*/
 
 	return true;
 }

gemrb/core/GUI/GameControl.cpp
@@ -2441,60 +2441,6 @@ void GameControl::SetDialogueFlags(int value, int mode)
 	}
 }
 
-//copies a screenshot into a sprite
-Sprite2D* GameControl::GetScreenshot(const Region& rgn, bool show_gui)
-{
-	Sprite2D* screenshot;
-	if (show_gui) {
-		screenshot = core->GetVideoDriver()->GetScreenshot( rgn );
-	} else {
-		int hf = SetGUIHidden(true);
-		Draw ();
-		screenshot = core->GetVideoDriver()->GetScreenshot( rgn );
-		if (hf) {
-			SetGUIHidden(false);
-		}
-		core->DrawWindows ();
-	}
-
-	return screenshot;
-}
-
-//copies a downscaled screenshot into a sprite for save game preview
-Sprite2D* GameControl::GetPreview()
-{
-	// We get preview by first taking a screenshot of quintuple size of the preview control size (a few pixels bigger only in pst),
-	// centered in the display. This is to get a decent picture for
-	// higher screen resolutions.
-	// FIXME: how do orig games solve that?
-	Video* video = core->GetVideoDriver();
-	int w = video->GetWidth();
-	int h = video->GetHeight();
-	int x = (w - 640) / 2;
-	int y = (h - 405) / 2;
-
-	if (x < 0) {
-		x = 0;
-	} else {
-		w = 515;
-	}
-
-	if (y < 0) {
-		y = 0;
-	} else {
-		h = 385;
-	}
-
-	if (!x)
-		y = 0;
-
-	Sprite2D *screenshot = GetScreenshot( Region(x, y, w, h) );
-
-	Sprite2D* preview = video->SpriteScaleDown ( screenshot, 5 );
-	Sprite2D::FreeSprite( screenshot );
-	return preview;
-}
-
 Actor *GameControl::GetActorByGlobalID(ieDword globalID)
 {
 	if (!globalID)

gemrb/core/GUI/GameControl.h
@@ -253,10 +253,6 @@ public:
 	Actor *GetLastActor();
 	/** changes map to the current PC */
 	void ChangeMap(Actor *pc, bool forced);
-	/** Returns game screenshot, with or without GUI controls */
-	Sprite2D* GetScreenshot(const Region& rgn, bool show_gui = false );
-	/** Returns current area preview for saving a game */
-	Sprite2D* GetPreview();
 	/** Sets up targeting with spells or items */
 	void SetupItemUse(int slot, int header, Actor *actor, int targettype, int cnt);
 	/** Page is the spell type + spell level info */

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -3070,8 +3070,10 @@ PyDoc_STRVAR( GemRB_GetGamePreview__doc,
 
 static PyObject* GemRB_GetGamePreview(PyObject * /*self*/, PyObject* /*args*/)
 {
-	GET_GAMECONTROL();
-	return CObject<Sprite2D>(gc->GetPreview());
+	// FIXME: this method should be removed
+	// A SaveGame object should be created prior to this (not the *actual* save files)
+	// from that SaveGame we have methods for SaveGame_GetPortrait and SaveGame_GetPreview
+	Py_RETURN_NONE;
 }
 
 PyDoc_STRVAR( GemRB_Roll__doc,

commit 2c174f8e34befb99ecc8d72d999a93c48ca7c37f

fixed part of the saving regression from 28af1312be
TODO: the gui hiding doesn't have an effect, so the screenshot is still
of the save window

gemrb/core/SaveGameIterator.cpp
@@ -470,12 +470,16 @@ static bool DoSaveGame(const char *Path)
 
 	// Create area preview
 	// FIXME: the preview shoudl be passed in by the caller!
-	/*
-	Sprite2D* preview = NULL;
+	core->HideGUI();
+	// redraw game window, as it is hidden behind a black overlay
+	core->GetWindowManager()->DrawWindows();
+	Sprite2D* preview = core->GetWindowManager()->GetScreenshot(NULL);
+	// scale down to get more of the screen and reduce the size
+	preview = core->GetVideoDriver()->SpriteScaleDown(preview, 10);
 	FileStream outfile;
 	outfile.Create( Path, core->GameNameResRef, IE_BMP_CLASS_ID );
 	im->PutImage( &outfile, preview );
-	*/
+	core->ShowGUI();
 
 	return true;
 }