4a721ac839d4acad0f30279b6b275ae3dedf26e7
Brad Allred <bradallred@me.com>
2017-08-30 19:56:22 -0600

    Button: HitTest should call super

--- gemrb/core/GUI/Button.cpp
+++ gemrb/core/GUI/Button.cpp
@@ -665,14 +665,18 @@ void Button::StackPicture(Sprite2D* Picture)
 
 bool Button::HitTest(const Point& p) const
 {
-	// some buttons have hollow Image frame filled w/ Picture
-	// some buttons in BG2 are text only (if BAM == 'GUICTRL')
-	Sprite2D* Unpressed = buttonImages[BUTTON_IMAGE_UNPRESSED];
-	if (Picture || PictureList.size() || !Unpressed) return true;
-
-	int xOffs = ( frame.w / 2 ) - ( Unpressed->Width / 2 );
-	int yOffs = ( frame.h / 2 ) - ( Unpressed->Height / 2 );
-	return !Unpressed->IsPixelTransparent(p.x - xOffs, p.y - yOffs);
+	bool hit = View::HitTest(p);
+	if (hit) {
+		// some buttons have hollow Image frame filled w/ Picture
+		// some buttons in BG2 are text only (if BAM == 'GUICTRL')
+		Sprite2D* Unpressed = buttonImages[BUTTON_IMAGE_UNPRESSED];
+		if (Picture || PictureList.size() || !Unpressed) return true;
+		
+		int xOffs = ( frame.w / 2 ) - ( Unpressed->Width / 2 );
+		int yOffs = ( frame.h / 2 ) - ( Unpressed->Height / 2 );
+		hit = !Unpressed->IsPixelTransparent(p.x - xOffs, p.y - yOffs);
+	}
+	return hit;
 }
 
 // Set palette used for drawing button label in normal state
