41f1c607bca6d2bd1e83c5a8de8bec1a8f8dd411
Brad Allred <bradallred@me.com>
2018-03-28 22:36:16 -0600

    PST: update MessageWindow.py

--- gemrb/GUIScripts/pst/MessageWindow.py
+++ gemrb/GUIScripts/pst/MessageWindow.py
@@ -43,32 +43,41 @@ def OnLoad():
 	GemRB.GameSetPartySize(MAX_PARTY_SIZE)
 	GemRB.GameSetProtagonistMode(0)
 	GemRB.SetInfoTextColor({'r' : 0, 'g' : 255, 'b' : 0, 'a' : 255})
+
 	ActionsWindow = GemRB.LoadWindow(0, GUICommon.GetWindowPack())
+	ActionsWindow.AddAlias("ACTWIN")
+	ActionsWindow.AddAlias("HIDE_CUT", 1)
+	ActionsWindow.AddAlias("NOT_DLG", 0)
+
 	OptionsWindow = GemRB.LoadWindow(2)
+	OptionsWindow.AddAlias("OPTWIN")
+	OptionsWindow.AddAlias("HIDE_CUT", 2)
+	OptionsWindow.AddAlias("NOT_DLG", 1)
+
 	MessageWindow = GemRB.LoadWindow(7)
+	MessageWindow.SetFlags(WF_DESTROY_ON_CLOSE, OP_NAND)
+	MessageWindow.AddAlias("MSGWIN")
+	MessageWindow.AddAlias("HIDE_CUT", 0)
+
 	PortraitWindow = GUICommonWindows.OpenPortraitWindow (1)
+	PortraitWindow.AddAlias("PORTWIN")
+	PortraitWindow.AddAlias("HIDE_CUT", 3)
+	PortraitWindow.AddAlias("NOT_DLG", 2)
 
 	MessageTA = MessageWindow.GetControl (1)
 	MessageTA.SetFlags (IE_GUI_TEXTAREA_AUTOSCROLL|IE_GUI_TEXTAREA_HISTORY)
-
-	GemRB.SetVar ("MessageTextArea", MessageTA.ID)
-	GemRB.SetVar ("ActionsWindow", ActionsWindow.ID)
-	GemRB.SetVar ("OptionsWindow", OptionsWindow.ID)
-	GemRB.SetVar ("OtherWindow", -1)
-	GemRB.SetVar ("ActionsPosition", 1) #Bottom
-	GemRB.SetVar ("OptionsPosition", 1) #Bottom
-	GemRB.SetVar ("MessagePosition", 1) #Bottom
-	GemRB.SetVar ("OtherPosition", 0) #Left
+	MessageTA.SetResizeFlags(IE_GUI_VIEW_RESIZE_ALL)
+	MessageTA.AddAlias("MsgSys", 0)
 	
 	GemRB.GameSetScreenFlags (0, OP_SET)
 	
 	CloseButton= MessageWindow.GetControl (0)
 	CloseButton.SetText(28082)
-	CloseButton.SetEvent (IE_GUI_BUTTON_ON_PRESS, CommonWindow.OnDecreaseSize)
+	CloseButton.SetEvent (IE_GUI_BUTTON_ON_PRESS, lambda: MessageWindow.Close())
 	CloseButton.MakeDefault()
 	
 	OpenButton = OptionsWindow.GetControl (10)
-	OpenButton.SetEvent (IE_GUI_BUTTON_ON_PRESS, CommonWindow.OnIncreaseSize)
+	OpenButton.SetEvent (IE_GUI_BUTTON_ON_PRESS, lambda: MessageWindow.Focus())
 
 	# Select all
 	Button = ActionsWindow.GetControl (1)
@@ -87,23 +96,7 @@ def OnLoad():
 	UpdateControlStatus ()
 
 def UpdateControlStatus ():
-	global MessageWindow, PortraitWindow, ActionsWindow, OptionsWindow, MessageTA
-
-	Expand = GemRB.GetGUIFlags() & (GS_DIALOGMASK|GS_DIALOG)
-
-	hideflags = IsGameGUIHidden()
-	if Expand:
-		GemRB.SetVar ("PortraitWindow", -1)
-		GemRB.SetVar ("ActionsWindow", -1)
-		GemRB.SetVar ("OptionsWindow", -1)
-		MessageTA = GUIClasses.GTextArea(MessageWindow.ID, GemRB.GetVar ("MessageTextArea"))
-		MessageTA.Focus()
-		
+	if GemRB.GetGUIFlags() & (GS_DIALOGMASK|GS_DIALOG):
 		Label = MessageWindow.GetControl (0x10000003)
 		Label.SetText (str (GemRB.GameGetPartyGold ()))
-	else:
-		GemRB.SetVar ("PortraitWindow", PortraitWindow.ID)
-		GemRB.SetVar ("ActionsWindow", ActionsWindow.ID)
-		GemRB.SetVar ("OptionsWindow", OptionsWindow.ID)
 
-	SetGameGUIHidden(hideflags)
