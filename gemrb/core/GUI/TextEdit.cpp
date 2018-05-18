7af73fe1d114c5aaedefef840a3758182be72d00
Brad Allred <bradallred@me.com>
2018-05-17 22:29:08 -0600

    TextEdit: allow punctuation when Alpha flag is set

--- gemrb/core/GUI/TextEdit.cpp
+++ gemrb/core/GUI/TextEdit.cpp
@@ -77,7 +77,7 @@ bool TextEdit::OnKeyPress(const KeyboardEvent& key, unsigned short mod)
 
 	if (QueryText().length() < max) {
 
-		if (isalpha(key.character) && (Flags()&Alpha) == 0) {
+		if ((isalpha(key.character) || ispunct(key.character)) && (Flags()&Alpha) == 0) {
 			return false;
 		} else if (isnumber(key.character && (Flags()&Numeric) == 0)) {
 			return false;
