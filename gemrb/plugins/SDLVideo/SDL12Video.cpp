gemrb/gemrb

commit da0012e5279465ddd47e215255ae47120de9af3e

SDL12: fix undefined behavior
shifting a negative int is undefined

gemrb/plugins/SDLVideo/SDL12Video.cpp
@@ -473,8 +473,9 @@ void SDL12VideoDriver::DrawLine(const Point& start, const Point& end, const Colo
 		longLen = swap;
 		yLonger = true;
 	}
+
 	int decInc;
-	if (longLen == 0) {
+	if (longLen == 0 || shortLen < 0) {
 		decInc = 0;
 	} else {
 		decInc = ( shortLen << 16 ) / longLen;

commit 9a0199085020d0a3c8b9e915764307517a5bd1db

SDL1: fix broken ellipse segments
regression from da0012e
fixed UB by multiplying by by 2^16 which is defined for negatives
admittedly, we probably dont care about non 2s compliment machines but it does trigger UBSan

issue #133

gemrb/plugins/SDLVideo/SDL12Video.cpp
@@ -468,17 +468,15 @@ void SDL12VideoDriver::DrawLine(const Point& start, const Point& end, const Colo
 	int shortLen = p2.y - p1.y;
 	int longLen = p2.x - p1.x;
 	if (abs( shortLen ) > abs( longLen )) {
-		int swap = shortLen;
-		shortLen = longLen;
-		longLen = swap;
+		std::swap(shortLen, longLen);
 		yLonger = true;
 	}
 
 	int decInc;
-	if (longLen == 0 || shortLen < 0) {
+	if (longLen == 0) {
 		decInc = 0;
 	} else {
-		decInc = ( shortLen << 16 ) / longLen;
+		decInc = ( shortLen * 65536 ) / longLen;
 	}
 
 	std::vector<SDL_Point> points;