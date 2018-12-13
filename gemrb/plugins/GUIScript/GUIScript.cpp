3e4b59add2b72ae34d5ef6a6835fa40779400e58
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2018-12-13 18:25:08 +0100

    GemRB_SetModalState: only immediately run the modal if prereqs match
    fixes hiding giving a bonus application without a check

--- gemrb/plugins/GUIScript/GUIScript.cpp
+++ gemrb/plugins/GUIScript/GUIScript.cpp
@@ -13580,7 +13580,9 @@ static PyObject* GemRB_SetModalState(PyObject * /*self*/, PyObject* args)
 
 	actor->SetModal( (ieDword) state, 0);
 	actor->SetModalSpell(state, spell);
-	actor->ApplyModal(actor->ModalSpell); // force immediate effect
+	if (actor->ModalSpellSkillCheck()) {
+		actor->ApplyModal(actor->ModalSpell); // force immediate effect
+	}
 
 	Py_RETURN_NONE;
 }
