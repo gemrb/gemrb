7356d2a681b1cbfc749a868375c223201eab80f3
Tomsod M <tomsod-m@ya.ru>
2018-12-28 20:06:41 +0300

    LevelUp: allow leveling from 0 to 1
    Some mods make use of BG2 behavior wherein a level 0 PC can be leveled
    to 1, assigning the same skills and proficiencies as at character
    generation.  GemRB mostly behaves the same way, but one place in the
    code incidentally forbade leveling level 0 actors.

--- gemrb/GUIScripts/LUCommon.py
+++ gemrb/GUIScripts/LUCommon.py
@@ -65,7 +65,7 @@ def CanLevelUp(actor):
 			# if any class can level, return 1
 			TmpClassName = GUICommon.GetClassRowName (Multi[i+1], "class")
 			tmpNext = int(GetNextLevelExp (Levels[i], TmpClassName))
-			if tmpNext != 0 and tmpNext <= xp:
+			if (tmpNext != 0 or Levels[i] == 0) and tmpNext <= xp:
 				return 1
 
 		# didn't find a class that could level
@@ -78,7 +78,7 @@ def CanLevelUp(actor):
 
 	# check the class that can be level (single or dual)
 	tmpNext = int(GetNextLevelExp (Levels[0], Class) )
-	return (tmpNext != 0 and tmpNext <= xp)
+	return ((tmpNext != 0 or Levels[0] == 0) and tmpNext <= xp)
 
 # expects a list of character levels of all classes
 # returns sparse list of class ids (of same length)
