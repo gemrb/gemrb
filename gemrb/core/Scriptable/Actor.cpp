c2c568858c1744deff44d62ad1ec5ce9b4300f94
Tomsod M <tomsod-m@ya.ru>
2019-01-01 01:41:43 +0300

    Actor: fix NPC proficiency penalty
    The code was giving a +4 bonus instead of -4 penalty for non-proficient
    non-player classes.  Seems to be a typo.

--- gemrb/core/Scriptable/Actor.cpp
+++ gemrb/core/Scriptable/Actor.cpp
@@ -6601,7 +6601,7 @@ bool Actor::GetCombatDetails(int &tohit, bool leftorright, WeaponInfo& wi, ITMEx
 			}
 		} else {
 			//it is not clear what is the penalty for non player classes
-			prof += 4;
+			prof -= 4;
 		}
 	}
 
