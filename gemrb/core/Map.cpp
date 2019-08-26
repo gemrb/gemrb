gemrb/gemrb

commit 60e911e9034ea624fa247ce6fea2ea7ea0e45d73

Map::PlayAreaSong: if a subarea has no song entries, use a fallback
other part of #99

gemrb/core/Map.cpp
@@ -2001,6 +2001,13 @@ void Map::PlayAreaSong(int SongType, bool restart, bool hard)
 	//a faulty music list on the fly. I don't want to add a method just for that
 	//crap when we already have that pointer at hand!
 	char* poi = core->GetMusicPlaylist( SongHeader.SongList[SongType] );
+	// for subareas fall back to the main list
+	// needed eg. in bg1 ar2607 (intro candlekeep ambush south)
+	// it's not the correct music, perhaps it needs the one from the master area
+	// it would match for ar2607 and ar2600, but very annoying
+	if (!poi && !MasterArea) {
+		poi = core->GetMusicPlaylist(SongType);
+	}
 	if (!poi) return;
 
 	//check if restart needed (either forced or the current song is different)

gemrb/core/Game.cpp
@@ -792,6 +792,40 @@ bool Game::MasterArea(const char *area)
 	return false;
 }
 
+// guess the master area by comparing the area name to entries in mastarea.2da
+// returns the area numerically closest to the passed one
+// pst has also areas named arNNNNc, bgt araNNNN (which we mishandle)
+// TODO: consider caching with an internal field or even a separate table to map the relation
+/*
+Map* Game::GetMasterArea(const char *area)
+{
+	unsigned int areaNum;
+	unsigned int masterNum;
+	unsigned int prevDiff = 0;
+	ieResRef prevArea;
+	sscanf(area, "%*c%*c%u%*c", &areaNum);
+
+	// mastarea.2da is not sorted, so make sure to check all the rows/areas
+	unsigned int i=(int) mastarea.size();
+	while(i--) {
+		sscanf(mastarea[i], "%*c%*c%u%*c", &masterNum);
+		if (areaNum > masterNum) {
+			continue;
+		} else if (areaNum == masterNum) {
+			return NULL; // optimisation, should never be called with a masterarea already
+		}
+		if (prevDiff == 0 || (prevDiff > masterNum - areaNum && masterNum < areaNum)) {
+			// first master bigger than us or
+			// this area is numerically closer than the last choice, but still smaller
+			CopyResRef(prevArea, mastarea[i+1]);
+			prevDiff = masterNum - areaNum;
+		}
+	}
+	// this could be slow, loading a full map!
+	// luckily when queried from subareas, it should already be cached and fast
+	return GetMap(prevArea, false);
+}*/
+
 void Game::SetMasterArea(const char *area)
 {
 	if (MasterArea(area) ) return;

gemrb/core/Game.h
@@ -367,6 +367,8 @@ public:
 	bool MasterArea(const char *area);
 	/** Dynamically adding an area to master areas*/
 	void SetMasterArea(const char *area);
+	/** Guess the master area of the given area*/
+	//Map* GetMasterArea(const char *area);
 	/** place persistent actors in the fresly loaded area*/
 	void PlacePersistents(Map *map, const char *ResRef);
 	/** Returns slot of the map, if it was already loaded,

commit c625af2479ac1a2f73801062184e451a5ef5f971

Map::PlayAreaSong: only fall back for combat music #183
regression from 60e911e

gemrb/core/Map.cpp
@@ -2004,8 +2004,8 @@ void Map::PlayAreaSong(int SongType, bool restart, bool hard)
 	// for subareas fall back to the main list
 	// needed eg. in bg1 ar2607 (intro candlekeep ambush south)
 	// it's not the correct music, perhaps it needs the one from the master area
-	// it would match for ar2607 and ar2600, but very annoying
-	if (!poi && !MasterArea) {
+	// it would match for ar2607 and ar2600, but very annoying (see GetMasterArea)
+	if (!poi && !MasterArea && SongType == SONG_BATTLE) {
 		poi = core->GetMusicPlaylist(SongType);
 	}
 	if (!poi) return;