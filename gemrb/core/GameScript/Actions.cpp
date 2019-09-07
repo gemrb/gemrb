gemrb/gemrb

commit 22f5d75b48fbb46bfdebc8afb35231ea131c3edc

GameScript::DisplayStringWait: wait for at least 1 round
fixes #208

An example to watch this behavior is the recent Imoen4ever mod.
It modifies the cutscene in the beginning of chapter 2 in the council
building ("These are prisoners from the disturbance at Waukeen's Promenade...").
The first part is voiced / vanilla, the rest not.

gemrb/core/GameScript/Actions.cpp
@@ -1738,7 +1738,9 @@ void GameScript::DisplayStringWait(Scriptable* Sender, Action* parameters)
 	DisplayStringCore( target, parameters->int0Parameter, DS_CONSOLE|DS_WAIT|DS_SPEECH|DS_HEAD);
 	Sender->CurrentActionState = 1;
 	// parameters->int2Parameter is unused here so hijack it to store the wait time
-	parameters->int2Parameter = gt + target->GetWait();
+	// and make sure we wait at least one round, so strings without audio have some time to display
+	unsigned long waitCounter = target->GetWait();
+	parameters->int2Parameter = gt + waitCounter > 0 ? waitCounter : core->Time.round_size;
 }
 
 void GameScript::ForceFacing(Scriptable* Sender, Action* parameters)

commit d5f9c110b9feccffc91252fcced2fea41fb027de

DisplayStringWait: add missing braces
regression from 22f5d75b48

gemrb/core/GameScript/Actions.cpp
@@ -1740,7 +1740,7 @@ void GameScript::DisplayStringWait(Scriptable* Sender, Action* parameters)
 	// parameters->int2Parameter is unused here so hijack it to store the wait time
 	// and make sure we wait at least one round, so strings without audio have some time to display
 	unsigned long waitCounter = target->GetWait();
-	parameters->int2Parameter = gt + waitCounter > 0 ? waitCounter : core->Time.round_size;
+	parameters->int2Parameter = gt + (waitCounter > 0 ? waitCounter : core->Time.round_size);
 }
 
 void GameScript::ForceFacing(Scriptable* Sender, Action* parameters)