85b0c85fbce3a8e382d50e8cebdcee0e169849ca
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2019-09-09 19:05:23 +0200

    CastSpell*End: play the casting completion sound #221

--- gemrb/core/Scriptable/Scriptable.cpp
+++ gemrb/core/Scriptable/Scriptable.cpp
@@ -1018,6 +1018,8 @@ void Scriptable::CastSpellPointEnd(int level, int no_stance)
 		caster->PCStats->RegisterFavourite(SpellResRef, FAV_SPELL);
 	}
 
+	core->GetAudioDrv()->Play(spl->CompletionSound, SFX_CHAN_CASTING, caster->Pos.x, caster->Pos.y);
+
 	CreateProjectile(SpellResRef, 0, level, false);
 	//FIXME: this trigger affects actors whom the caster sees, not just the caster itself
 	// the original engine saves lasttrigger only in case of SpellCast, so we have to differentiate
@@ -1083,6 +1085,8 @@ void Scriptable::CastSpellEnd(int level, int no_stance)
 	if (caster && caster->PCStats && !no_stance) {
 		caster->PCStats->RegisterFavourite(SpellResRef, FAV_SPELL);
 	}
+
+	core->GetAudioDrv()->Play(spl->CompletionSound, SFX_CHAN_CASTING, caster->Pos.x, caster->Pos.y);
 
 	//if the projectile doesn't need to follow the target, then use the target position
 	CreateProjectile(SpellResRef, LastSpellTarget, level, GetSpellDistance(SpellResRef, this)==0xffffffff);
