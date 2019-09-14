9e961e9fbe005bbda288d10e8c1bf916cfcaa4c1
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2019-09-14 21:05:14 +0200

    pst::GUIMA: sync variable name with the analog from other games
    preparation for merge

--- gemrb/GUIScripts/pst/GUIMA.py
+++ gemrb/GUIScripts/pst/GUIMA.py
@@ -29,6 +29,7 @@ from GUIDefines import *
 
 MapWindow = None
 WorldMapWindow = None
+WorldMapControl = None
 PosX = 0
 PosY = 0
 
@@ -150,7 +151,7 @@ def OpenWorldMapWindow ():
 	return
 
 def WorldMapWindowCommon (Travel):
-	global WorldMapWindow
+	global WorldMapWindow, WorldMapControl
 
 	GemRB.HideGUI()
 
@@ -170,15 +171,15 @@ def WorldMapWindowCommon (Travel):
 	GemRB.SetVar ("OtherWindow", WorldMapWindow.ID)
 
 	Window.CreateWorldMapControl (4, 0, 62, 640, 418, Travel, "FONTDLG")
-	WMap = Window.GetControl (4)
-	WMap.SetTextColor (IE_GUI_WMAP_COLOR_BACKGROUND, 0x84, 0x4a, 0x2c, 0x00)
-	WMap.SetTextColor (IE_GUI_WMAP_COLOR_NORMAL, 0x20, 0x20, 0x00, 0xff)
-	WMap.SetTextColor (IE_GUI_WMAP_COLOR_SELECTED, 0x20, 0x20, 0x00, 0xff)
-	WMap.SetTextColor (IE_GUI_WMAP_COLOR_NOTVISITED, 0x20, 0x20, 0x00, 0xa0)
-	WMap.SetAnimation ("WMPTY")
+	WorldMapControl = Window.GetControl (4)
+	WorldMapControl.SetTextColor (IE_GUI_WMAP_COLOR_BACKGROUND, 0x84, 0x4a, 0x2c, 0x00)
+	WorldMapControl.SetTextColor (IE_GUI_WMAP_COLOR_NORMAL, 0x20, 0x20, 0x00, 0xff)
+	WorldMapControl.SetTextColor (IE_GUI_WMAP_COLOR_SELECTED, 0x20, 0x20, 0x00, 0xff)
+	WorldMapControl.SetTextColor (IE_GUI_WMAP_COLOR_NOTVISITED, 0x20, 0x20, 0x00, 0xa0)
+	WorldMapControl.SetAnimation ("WMPTY")
 	#center on current area
-	WMap.AdjustScrolling (0,0)
-	WMap.SetStatus (IE_GUI_CONTROL_FOCUSED)
+	WorldMapControl.AdjustScrolling (0,0)
+	WorldMapControl.SetStatus (IE_GUI_CONTROL_FOCUSED)
 
 	# Done
 	Button = Window.GetControl (0)
