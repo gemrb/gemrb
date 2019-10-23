gemrb/gemrb

commit fdeec6b2b97fa7e6d7182ed069f0d26b6602ae11

Actor, GameScript, IDSImporter: drop IE_SPLSTATE stats and move internal stats in their place
This commit relocates IWD spell states to a dynamically allocated array,
both providing support for more than 192 spell states (like in BG:EE)
and freeing space for internal stats 178-182, which need to be relocated
as they conflict with some BG2 mods.

gemrb/core/Scriptable/Actor.cpp
@@ -247,6 +247,7 @@ static int DeathOnZeroStat = true;
 static int IWDSound = false;
 static ieDword TranslucentShadows = 0;
 static int ProjectileSize = 0; //the size of the projectile immunity bitfield (dwords)
+static unsigned int SpellStatesSize = 0; //and this is for the spellStates bitfield
 
 static const char iwd2gemrb[32] = {
 	0,0,20,2,22,25,0,14,
@@ -535,6 +536,7 @@ Actor::Actor()
 	speed = 0;
 	WeaponType = AttackStance = 0;
 	DifficultyMargin = disarmTrap = 0;
+	spellStates = (ieDword *) calloc(SpellStatesSize, sizeof(ieDword));
 
 	polymorphCache = NULL;
 	memset(&wildSurgeMods, 0, sizeof(wildSurgeMods));
@@ -572,6 +574,7 @@ Actor::~Actor(void)
 	delete polymorphCache;
 
 	free(projectileImmunity);
+	free(spellStates);
 }
 
 void Actor::SetFistStat(ieDword stat)
@@ -2641,6 +2644,17 @@ static void InitActorTables()
 		}
 	}
 
+	// IWD, IWD2 and BG:EE have this
+	int splstatetable = core->LoadSymbol("splstate");
+	if (splstatetable != -1) {
+		Holder<SymbolMgr> splstate = core->GetSymbol(splstatetable);
+		int numstates = splstate->GetHighestValue();
+		if (numstates > 0) {
+			//rounding up
+			SpellStatesSize = (numstates >> 5) + 1;
+		}
+	}
+
 	// modal actions/state data
 	ReadModalStates();
 }
@@ -3057,6 +3071,9 @@ void Actor::RefreshEffects(EffectQueue *fx)
 	if (PCStats) {
 		memset( PCStats->PortraitIcons, -1, sizeof(PCStats->PortraitIcons) );
 	}
+	if (SpellStatesSize) {
+		memset(spellStates, 0, sizeof(ieDword) * SpellStatesSize);
+	}
 	AC.ResetAll();
 	ToHit.ResetAll(); // effects can result in the change of any of the boni, so we need to reset all
 
@@ -3067,6 +3084,9 @@ void Actor::RefreshEffects(EffectQueue *fx)
 		//copy back the original stats, because the effects
 		//will be reapplied in ApplyAllEffects again
 		memcpy( Modified, BaseStats, MAX_STATS * sizeof( ieDword ) );
+		if (SpellStatesSize) {
+			memset(spellStates, 0, sizeof(ieDword) * SpellStatesSize);
+		}
 		//also clear the spell bonuses just given, they will be
 		//recalculated below again
 		spellbook.ClearBonus();
@@ -9520,21 +9540,21 @@ void Actor::SetOverlay(unsigned int overlay)
 //returns true if spell state is already set or illegal
 bool Actor::SetSpellState(unsigned int spellstate)
 {
-	if (spellstate >= SS_MAX) return true;
-	unsigned int pos = IE_SPLSTATE_ID1+(spellstate>>5);
-	unsigned int bit = 1<<(spellstate&31);
-	if (Modified[pos]&bit) return true;
-	Modified[pos]|=bit;
+	if (spellstate >= SpellStatesSize << 5) return true;
+	unsigned int pos = spellstate >> 5;
+	unsigned int bit = 1 << (spellstate & 31);
+	if (spellStates[pos] & bit) return true;
+	spellStates[pos] |= bit;
 	return false;
 }
 
 //returns true if spell state is already set
 bool Actor::HasSpellState(unsigned int spellstate) const
 {
-	if (spellstate >= SS_MAX) return false;
-	unsigned int pos = IE_SPLSTATE_ID1+(spellstate>>5);
-	unsigned int bit = 1<<(spellstate&31);
-	if (Modified[pos]&bit) return true;
+	if (spellstate >= SpellStatesSize << 5) return false;
+	unsigned int pos = spellstate >> 5;
+	unsigned int bit = 1 << (spellstate & 31);
+	if (spellStates[pos] & bit) return true;
 	return false;
 }
 

gemrb/GUIScripts/ie_stats.py
@@ -295,14 +295,17 @@ IE_TITLE1			= 172
 IE_TITLE2			= 173
 IE_DISABLEOVERLAY		= 174
 IE_DISABLEBACKSTAB		= 175
-#176-182 overwritten by us
+#the next two are overwritten by us in PST
 IE_XP_MAGE	 		= 176 # In PST this stores secondary level exp
 IE_XP_THIEF	 		= 177 # In PST this stores tertiary level exp
-IE_DIALOGRANGE                  = 178 # distance for dialogue
-IE_MOVEMENTRATE                 = 179
-IE_MORALE                       = 180
-IE_BOUNCE                       = 181
-IE_MIRRORIMAGES                 = 182
+#but in other games they're still used sometimes
+IE_SCRIPTINGSTATE21  		= 176
+IE_SCRIPTINGSTATE22  		= 177
+IE_SCRIPTINGSTATE23  		= 178
+IE_SCRIPTINGSTATE24  		= 179
+IE_SCRIPTINGSTATE25  		= 180
+IE_SCRIPTINGSTATE26  		= 181
+IE_SCRIPTINGSTATE27  		= 182
 #these are original
 IE_ENABLEOFFSCREENAI            = 183
 IE_EXISTANCEDELAY               = 184
@@ -364,8 +367,12 @@ IE_LEVELRANGER                  = 246
 IE_LEVELSORCERER                = 247
 #248 IE_LEVELCLASS12
 #249 IE_LEVELCLASS13
-#the remaining six stats are spell states
-IE_SPLSTATE_ID1                 = 250
+#the remaining stats are for our internal use now
+IE_DIALOGRANGE                  = 250 # distance for dialogue
+IE_MOVEMENTRATE                 = 251
+IE_MORALE                       = 252
+IE_BOUNCE                       = 253
+IE_MIRRORIMAGES                 = 254
 
 #these stats exist only in PC's (but we access only PCs anyway)
 IE_EXPERTISE                    = 0x1003

gemrb/core/GameScript/Triggers.cpp
@@ -2442,7 +2442,7 @@ int GameScript::CheckSpellState(Scriptable* Sender, Trigger* parameters)
 	}
 	unsigned int position = parameters->int0Parameter>>5;
 	unsigned int bit = 1<<(parameters->int0Parameter&31);
-	if (actor->GetStat(IE_SPLSTATE_ID1+position) & bit) {
+	if (actor->spellStates[position] & bit) {
 		return 1;
 	}
 	return 0;

gemrb/core/Scriptable/Actor.h
@@ -370,6 +370,7 @@ public:
 	PolymorphCache *polymorphCache; // fx_polymorph etc
 	WildSurgeSpellMods wildSurgeMods;
 	ieByte DifficultyMargin;
+	ieDword *spellStates;
 private:
 	//this stuff doesn't get saved
 	CharAnimations* anims;

gemrb/core/SymbolMgr.h
@@ -48,6 +48,7 @@ public:
 	virtual int FindValue(int val) const = 0;
 	virtual int FindString(char *str, int len) const = 0;
 	virtual int GetSize() const = 0;
+	virtual int GetHighestValue() const = 0;
 };
 
 }

gemrb/includes/ie_stats.h
@@ -466,6 +466,16 @@ namespace GemRB {
 //180 IE_HIDE_IN_SHADOWS_BONUS
 //181 DETECT_ILLUSIONS_BONUS
 //182 SET_TRAPS_BONUS
+
+// some bg2 mods use these
+#define IE_SCRIPTINGSTATE21	 176
+#define IE_SCRIPTINGSTATE22	 177
+#define IE_SCRIPTINGSTATE23	 178
+#define IE_SCRIPTINGSTATE24	 179
+#define IE_SCRIPTINGSTATE25	 180
+#define IE_SCRIPTINGSTATE26	 181
+#define IE_SCRIPTINGSTATE27	 182
+
 #define IE_ENABLEOFFSCREENAI    183 // bg2 has this on this spot
 #define IE_EXISTANCEDELAY       184 // affects the displaying of EXISTANCE strings
 #define IE_ATTACKNUMBERDOUBLE   185 // used by haste option 2
@@ -492,12 +502,7 @@ namespace GemRB {
 //these are temporary only
 #define IE_XP_MAGE              176 // XP2 
 #define IE_XP_THIEF             177 // XP3
-#define IE_DIALOGRANGE          178 // iwd2
-#define IE_MOVEMENTRATE         179
-#define IE_MORALE               180 // this has no place
-#define IE_BOUNCE               181 // has projectile bouncing effect
-#define IE_MIRRORIMAGES         182 
-//
+//the rest are relocated at 250+
 
 #define IE_ETHEREALNESS         202
 #define IE_IMMUNITY             203
@@ -554,14 +559,13 @@ namespace GemRB {
 // place for 2 more classes
 #define IE_LEVELCLASS12          248
 #define IE_LEVELCLASS13          249
-// these are iwd2 spell states, iwd2 uses ~180, we have place for 192
-// TODO: consider dropping these (move them to class variable) if unused by guiscript
-#define IE_SPLSTATE_ID1          250
-#define IE_SPLSTATE_ID2          251
-#define IE_SPLSTATE_ID3          252
-#define IE_SPLSTATE_ID4          253
-#define IE_SPLSTATE_ID5          254
-#define IE_SPLSTATE_ID6          255
+
+//more GemRB internal stats
+#define IE_DIALOGRANGE          250 // iwd2
+#define IE_MOVEMENTRATE         251
+#define IE_MORALE               252 // this has no place
+#define IE_BOUNCE               253 // has projectile bouncing effect
+#define IE_MIRRORIMAGES         254
 
 }
 

gemrb/includes/opcode_params.h
@@ -211,8 +211,6 @@ namespace GemRB {
 #define SS_DAYBLINDNESS 178
 #define SS_REBUKED      179
 
-#define SS_MAX          192
-
 }
 
 #endif //IE_OPCODE_PARAMS_H

gemrb/override/shared/stats.ids
@@ -232,11 +232,11 @@ IDS
 175        DISABLEBACKSTAB
 176        XP_MAGE
 177        XP_THIEF
-178        DIALOGRANGE
-179        MOVEMENTRATE
-180        MORALE
-181        BOUNCE
-182        MIRRORIMAGES
+178        SCRIPTINGSTATE23
+179        SCRIPTINGSTATE24
+180        SCRIPTINGSTATE25
+181        SCRIPTINGSTATE26
+182        SCRIPTINGSTATE27
 183        ENABLEOFFSCREENAI
 184        EXISTANCEDELAY
 185        ATTACKNUMBERDOUBLE
@@ -304,9 +304,8 @@ IDS
 247        CLASSLEVELSORCERER
 248        CLASSLEVEL12
 249        CLASSLEVEL13
-250        SPLSTATE1
-251        SPLSTATE2
-252        SPLSTATE3
-253        SPLSTATE4
-254        SPLSTATE5
-255        SPLSTATE6
+250        DIALOGRANGE
+251        MOVEMENTRATE
+252        MORALE
+253        BOUNCE
+254        MIRRORIMAGES

commit d53600da7da1685383328fbd27b5eedeac672e08

Actor: fixed regression from fdeec6b2
pcfs references need to be moved to when stats renumber

fixes #317

gemrb/core/Scriptable/Actor.cpp
@@ -1619,7 +1619,7 @@ NULL,NULL,NULL,NULL, NULL, NULL, NULL, NULL,
 NULL,NULL,NULL,NULL, NULL, NULL, NULL, NULL, //9f
 NULL,NULL,NULL,NULL, NULL, NULL, NULL, NULL,
 NULL,NULL,NULL,NULL, NULL, NULL, NULL, NULL, //af
-NULL,NULL,NULL,NULL, pcf_morale, pcf_bounce, NULL, NULL,
+NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
 NULL,NULL,NULL,NULL, NULL, NULL, NULL, NULL, //bf
 NULL,NULL,NULL,NULL, NULL, NULL, NULL, NULL,
 NULL,pcf_avatarremoval,NULL,NULL, pcf_dbutton, pcf_animid,pcf_state, pcf_extstate, //cf
@@ -1628,7 +1628,7 @@ NULL, pcf_alignment, pcf_dbutton, pcf_armorlevel, NULL, NULL, NULL, NULL, //df
 NULL,NULL,NULL,NULL, NULL, NULL, NULL, NULL,
 pcf_class,NULL,pcf_ea,NULL, NULL, NULL, NULL, NULL, //ef
 pcf_level_barbarian,pcf_level_bard,pcf_level_cleric,pcf_level_druid, pcf_level_monk, pcf_level_paladin, pcf_level_ranger, pcf_level_sorcerer,
-NULL,NULL,NULL,NULL, NULL, NULL, NULL, NULL //ff
+NULL, NULL, NULL, NULL, pcf_morale, pcf_bounce, NULL, NULL //ff
 };
 
 /** call this from ~Interface() */