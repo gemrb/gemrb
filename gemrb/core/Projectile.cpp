gemrb/gemrb

commit 3604435d6e8efbf9480951c533ecf81cb88e17dc

converted Map::GetAllActorsInRadius to a vector

gemrb/core/Projectile.cpp
@@ -1181,10 +1181,6 @@ void Projectile::SecondaryTarget()
 		maxdeg = mindeg + Extension->ConeWidth;
 	}
 
-	int radius = Extension->ExplosionRadius;
-	Actor **actors = area->GetAllActorsInRadius(Pos, CalculateTargetFlag(), radius);
-	Actor **poi=actors;
-
 	if (Extension->DiceCount) {
 		//precalculate the maximum affected target count in case of PAF_AFFECT_ONE 
 		extension_targetcount = core->Roll(Extension->DiceCount, Extension->DiceSize, 0);
@@ -1193,25 +1189,25 @@ void Projectile::SecondaryTarget()
 		extension_targetcount = 1;
 	}
 
-	while(*poi) {
+	int radius = Extension->ExplosionRadius;
+	std::vector<Actor *> actors = area->GetAllActorsInRadius(Pos, CalculateTargetFlag(), radius);
+	std::vector<Actor *>::iterator poi;
+	for (poi = actors.begin(); poi != actors.end(); poi++) {
 		ieDword Target = (*poi)->GetGlobalID();
 
 		//this flag is actually about ignoring the caster (who is at the center)
 		if ((SFlags & PSF_IGNORE_CENTER) && (Caster==Target)) {
-			poi++;
 			continue;
 		}
 
 		//IDS targeting for area projectiles
 		if (FailedIDS(*poi)) {
-			poi++;
 			continue;
 		}
 
 		if (Extension->AFlags&PAF_CONE) {
 			//cone never affects the caster
 			if(Caster==Target) {
-				poi++;
 				continue;
 			}
 			double xdiff = (*poi)->Pos.x-Pos.x;
@@ -1240,7 +1236,6 @@ void Projectile::SecondaryTarget()
 			//Log(DEBUG, "Projectile", "Target %s at angle %d (%d to %d), ignoring? %d %d, %d -> %d, %d", (*poi)->GetName(1), deg, mindeg, maxdeg, mindeg>deg || maxdeg<deg, degOffset, Orientation, saneOrientation, deg0);
 			//not in the right sector of circle
 			if (mindeg>deg || maxdeg<deg) {
-				poi++;
 				continue;
 			}
 		}
@@ -1259,6 +1254,7 @@ void Projectile::SecondaryTarget()
 		area->AddProjectile(pro, Pos, Target, false);
 
 		poi++;
+		if (poi == actors.end()) break;
 		fail=false;
 
 		//we already got one target affected in the AOE, this flag says
@@ -1270,14 +1266,12 @@ void Projectile::SecondaryTarget()
 			//if target counting is per HD and this target is an actor, use the xp level field
 			//otherwise count it as one
 			if ((Extension->APFlags&APF_COUNT_HD) && ((*poi)->Type==ST_ACTOR) ) {
-				Actor *actor = (Actor *) *poi;
-				extension_targetcount-= actor->GetXPLevel(true);
+				extension_targetcount-= (*poi)->GetXPLevel(true);
 			} else {
 				extension_targetcount--;
 			}
 		}
 	}
-	free(actors);
 
 	//In case of utter failure, apply a spell of the same name on the caster
 	//this feature is used by SCHARGE, PRTL_OP and PRTL_CL in the HoW pack

gemrb/core/GUI/GameControl.cpp
@@ -510,16 +510,14 @@ void GameControl::DrawInternal(Region& screen)
 		Actor *actor = area->GetActorByGlobalID(trackerID);
 
 		if (actor) {
-			Actor **monsters = area->GetAllActorsInRadius(actor->Pos, GA_NO_DEAD|GA_NO_LOS|GA_NO_UNSCHEDULED, distance);
-
-			int i = 0;
-			while(monsters[i]) {
-				Actor *target = monsters[i++];
-				if (target->InParty) continue;
-				if (target->GetStat(IE_NOTRACKING)) continue;
-				DrawArrowMarker(screen, target->Pos, viewport, ColorBlack);
+			// FIXME: no EA check? Summons and familiars and neutrals would now be marked too
+			std::vector<Actor *> monsters = area->GetAllActorsInRadius(actor->Pos, GA_NO_DEAD|GA_NO_LOS|GA_NO_UNSCHEDULED, distance);
+			std::vector<Actor *>::iterator monster;
+			for (monster = monsters.begin(); monster != monsters.end(); monster++) {
+				if ((*monster)->InParty) continue;
+				if ((*monster)->GetStat(IE_NOTRACKING)) continue;
+				DrawArrowMarker(screen, (*monster)->Pos, viewport, ColorBlack);
 			}
-			free(monsters);
 		} else {
 			trackerID = 0;
 		}

gemrb/core/Map.cpp
@@ -936,19 +936,18 @@ bool Map::DoStepForActor(Actor *actor, int speed, ieDword time) {
 }
 
 void Map::ClearSearchMapFor( Movable *actor ) {
-	Actor** nearActors = GetAllActorsInRadius(actor->Pos, GA_NO_DEAD|GA_NO_LOS|GA_NO_UNSCHEDULED, MAX_CIRCLE_SIZE*2*16);
+	std::vector<Actor *> nearActors = GetAllActorsInRadius(actor->Pos, GA_NO_DEAD|GA_NO_LOS|GA_NO_UNSCHEDULED, MAX_CIRCLE_SIZE*2*16);
 	BlockSearchMap( actor->Pos, actor->size, PATH_MAP_FREE);
 
 	// Restore the searchmap areas of any nearby actors that could
 	// have been cleared by this BlockSearchMap(..., 0).
 	// (Necessary since blocked areas of actors may overlap.)
-	int i=0;
-	while(nearActors[i]!=NULL) {
-		if(nearActors[i]!=actor && nearActors[i]->BlocksSearchMap())
-			BlockSearchMap( nearActors[i]->Pos, nearActors[i]->size, nearActors[i]->IsPartyMember()?PATH_MAP_PC:PATH_MAP_NPC);
-		++i;
+	std::vector<Actor *>::iterator neighbour;
+	for (neighbour = nearActors.begin(); neighbour != nearActors.end(); neighbour++) {
+		if (*neighbour != actor && (*neighbour)->BlocksSearchMap()) {
+			BlockSearchMap((*neighbour)->Pos, (*neighbour)->size, (*neighbour)->IsPartyMember() ? PATH_MAP_PC : PATH_MAP_NPC);
+		}
 	}
-	free(nearActors);
 }
 
 void Map::DrawHighlightables()
@@ -1687,11 +1686,10 @@ Actor* Map::GetActorInRadius(const Point &p, int flags, unsigned int radius)
 	return NULL;
 }
 
-//maybe consider using a simple list
-Actor **Map::GetAllActorsInRadius(const Point &p, int flags, unsigned int radius, Scriptable *see)
+std::vector<Actor *> Map::GetAllActorsInRadius(const Point &p, int flags, unsigned int radius, Scriptable *see) const
 {
-	ieDword count = 1;
 	size_t i = actors.size();
+	std::vector<Actor *> neighbours;
 	while (i--) {
 		Actor* actor = actors[i];
 
@@ -1706,31 +1704,9 @@ Actor **Map::GetAllActorsInRadius(const Point &p, int flags, unsigned int radius
 				continue;
 			}
 		}
-		count++;
+		neighbours.push_back(actor);
 	}
-
-	Actor **ret = (Actor **) malloc( sizeof(Actor*) * count);
-	i = actors.size();
-	int j = 0;
-	while (i--) {
-		Actor* actor = actors[i];
-
-		if (PersonalDistance( p, actor ) > radius)
-			continue;
-		if (!actor->ValidTarget(flags) ) {
-			continue;
-		}
-		if (!(flags&GA_NO_LOS)) {
-			if (!IsVisibleLOS(actor->Pos, p)) {
-				continue;
-			}
-		}
-
-		ret[j++]=actor;
-	}
-
-	ret[j]=NULL;
-	return ret;
+	return neighbours;
 }
 
 

gemrb/core/Map.h
@@ -437,7 +437,7 @@ public:
 	Actor* GetActorByGlobalID(ieDword objectID);
 	Actor* GetActor(const Point &p, int flags);
 	Actor* GetActorInRadius(const Point &p, int flags, unsigned int radius);
-	Actor **GetAllActorsInRadius(const Point &p, int flags, unsigned int radius, Scriptable *see=NULL);
+	std::vector<Actor *> GetAllActorsInRadius(const Point &p, int flags, unsigned int radius, Scriptable *see = NULL) const;
 	Actor* GetActor(const char* Name, int flags);
 	Actor* GetActor(int i, bool any) const;
 	Scriptable* GetActorByDialog(const char* resref);

gemrb/core/SaveGameIterator.cpp
@@ -537,18 +537,15 @@ static int CanSave()
 	}
 
 	Point pc1 =  game->GetPC(0, true)->Pos;
-	Actor **nearActors = map->GetAllActorsInRadius(pc1, GA_NO_DEAD|GA_NO_UNSCHEDULED, 15*10);
-	i = 0;
-	while (nearActors[i]) {
-		Actor *actor = nearActors[i];
-		if (actor->GetInternalFlag() & IF_NOINT) {
+	std::vector<Actor *> nearActors = map->GetAllActorsInRadius(pc1, GA_NO_DEAD|GA_NO_UNSCHEDULED, 15*10);
+	std::vector<Actor *>::iterator neighbour;
+	for (neighbour = nearActors.begin(); neighbour != nearActors.end(); neighbour++) {
+		if ((*neighbour)->GetInternalFlag() & IF_NOINT) {
 			// dialog about to start or similar
 			displaymsg->DisplayConstantString(STR_CANTSAVEDIALOG2, DMC_BG2XPGREEN);
 			return 8;
 		}
-		i++;
 	}
-	free(nearActors);
 
 	//TODO: can't save while AOE spells are in effect -> CANTSAVE
 	//TODO: can't save  during a rest, chapter information or movie -> CANTSAVEMOVIE

gemrb/core/Scriptable/Actor.cpp
@@ -5410,10 +5410,12 @@ static const char *GetVarName(const char *table, int value)
 void Actor::SendDiedTrigger()
 {
 	if (!area) return;
-	Actor **neighbours = area->GetAllActorsInRadius(Pos, GA_NO_LOS|GA_NO_DEAD|GA_NO_UNSCHEDULED, GetSafeStat(IE_VISUALRANGE));
-	Actor **poi = neighbours;
+	std::vector<Actor *> neighbours = area->GetAllActorsInRadius(Pos, GA_NO_LOS|GA_NO_DEAD|GA_NO_UNSCHEDULED, GetSafeStat(IE_VISUALRANGE));
 	ieDword ea = Modified[IE_EA];
-	while (*poi) {
+
+	std::vector<Actor *>::iterator poi;
+	for (poi = neighbours.begin(); poi != neighbours.end(); poi++) {
+		// NOTE: currently also sending the trigger to ourselves — prevent if it causes problems
 		(*poi)->AddTrigger(TriggerEntry(trigger_died, GetGlobalID()));
 
 		// allies take a hit on morale and nobody cares about neutrals
@@ -5423,10 +5425,7 @@ void Actor::SendDiedTrigger()
 		} else if (ea > EA_EVILCUTOFF && pea > EA_EVILCUTOFF) {
 			(*poi)->NewBase(IE_MORALE, (ieDword) -1, MOD_ADDITIVE);
 		}
-
-		poi++;
 	}
-	free(neighbours);
 }
 
 void Actor::Die(Scriptable *killer, bool grantXP)
@@ -7866,13 +7865,11 @@ void Actor::ApplyModal(ieResRef modalSpell)
 		// target actors around us manually
 		// used for iwd2 songs, as the spells don't use an aoe projectile
 		if (!area) return;
-		Actor **neighbours = area->GetAllActorsInRadius(Pos, GA_NO_LOS|GA_NO_DEAD|GA_NO_UNSCHEDULED, GetSafeStat(IE_VISUALRANGE)*VOODOO_SPL_RANGE_F);
-		Actor **poi = neighbours;
-		while (*poi) {
-			core->ApplySpell(modalSpell, *poi, this, 0);
-			poi++;
+		std::vector<Actor *> neighbours = area->GetAllActorsInRadius(Pos, GA_NO_LOS|GA_NO_DEAD|GA_NO_UNSCHEDULED, GetSafeStat(IE_VISUALRANGE)*VOODOO_SPL_RANGE_F);
+		std::vector<Actor *>::iterator neighbour;
+		for (neighbour = neighbours.begin(); neighbour != neighbours.end(); neighbour++) {
+			core->ApplySpell(modalSpell, *neighbour, this, 0);
 		}
-		free(neighbours);
 	} else {
 		core->ApplySpell(modalSpell, this, this, 0);
 	}
@@ -10640,21 +10637,20 @@ bool Actor::SeeAnyOne(bool enemy, bool seenby)
 			flag|=GA_NO_ALLY|GA_NO_NEUTRAL;
 		} else return false; //neutrals got no enemy
 	}
-	Actor** visActors = area->GetAllActorsInRadius(Pos, flag, seenby?15*10:GetSafeStat(IE_VISUALRANGE)*10, this);
 
-	Actor** poi = visActors;
+	std::vector<Actor *> visActors = area->GetAllActorsInRadius(Pos, flag, seenby?15*10:GetSafeStat(IE_VISUALRANGE)*10, this);
 	bool seeEnemy = false;
 
 	//we need to look harder if we look for seenby anyone
-	while (*poi && !seeEnemy) {
-		Actor *toCheck = *poi++;
+	std::vector<Actor *>::iterator neighbour;
+	for (neighbour = visActors.begin(); neighbour != visActors.end()  && !seeEnemy; neighbour++) {
+		Actor *toCheck = *neighbour;
 		if (toCheck==this) continue;
 		if (seenby) {
 			if(ValidTarget(GA_NO_HIDDEN, toCheck) && (toCheck->Modified[IE_VISUALRANGE]*10<PersonalDistance(toCheck, this) ) ) seeEnemy=true;
 		}
 		else seeEnemy = true;
 	}
-	free(visActors);
 	return seeEnemy;
 }
 
@@ -10726,8 +10722,7 @@ bool Actor::TryToHide()
 // skill check when trying to maintain invisibility: separate move silently and visibility check
 bool Actor::TryToHideIWD2()
 {
-	Actor **neighbours = area->GetAllActorsInRadius(Pos, GA_NO_DEAD|GA_NO_LOS|GA_NO_ALLY|GA_NO_NEUTRAL|GA_NO_SELF|GA_NO_UNSCHEDULED, 60);
-	Actor **poi = neighbours;
+	std::vector<Actor *> neighbours = area->GetAllActorsInRadius(Pos, GA_NO_DEAD|GA_NO_LOS|GA_NO_ALLY|GA_NO_NEUTRAL|GA_NO_SELF|GA_NO_UNSCHEDULED, 60);
 	ieDword roll = LuckyRoll(1, 20, GetArmorSkillPenalty(0));
 	int targetDC = 0;
 	bool checked = false;
@@ -10737,8 +10732,9 @@ bool Actor::TryToHideIWD2()
 	// TODO: use crehidemd.2da as a skill bonus/malus (after refreshing effects, not here)
 	ieDword skill = GetStat(IE_HIDEINSHADOWS);
 	bool seen = false;
-	while (*poi) {
-		Actor *toCheck = *poi++;
+	std::vector<Actor *>::iterator neighbour;
+	for (neighbour = neighbours.begin(); neighbour != neighbours.end(); neighbour++) {
+		Actor *toCheck = *neighbour;
 		if (toCheck->GetStat(IE_STATE_ID)&STATE_BLIND) {
 			continue;
 		}
@@ -10752,7 +10748,6 @@ bool Actor::TryToHideIWD2()
 		seen = skill < (roll + targetDC);
 		if (seen) {
 			HideFailed(this, 1, skill, roll, targetDC);
-			free(neighbours);
 			return false;
 		} else {
 			// ~You were not seen by creature! Hide check %d vs. creature's Level+Wisdom+Race modifier  %d + %d D20 Roll.~
@@ -10762,16 +10757,14 @@ bool Actor::TryToHideIWD2()
 
 	// we're stationary, so no need to check if we're making movement sounds
 	if (!InMove() && !checked) {
-		free(neighbours);
 		return true;
 	}
 
 	// separate move silently check
 	skill = GetStat(IE_STEALTH);
-	poi = neighbours;
 	bool heard = false;
-	while (*poi) {
-		Actor *toCheck = *poi++;
+	for (neighbour = neighbours.begin(); neighbour != neighbours.end(); neighbour++) {
+		Actor *toCheck = *neighbour;
 		if (toCheck->HasSpellState(SS_DEAF)) {
 			continue;
 		}
@@ -10782,7 +10775,6 @@ bool Actor::TryToHideIWD2()
 		heard = skill < (roll + targetDC);
 		if (heard) {
 			HideFailed(this, 2, skill, roll, targetDC);
-			free(neighbours);
 			return false;
 		} else {
 			// ~You were not heard by creature! Move silently check %d vs. creature's Level+Wisdom+Race modifier  %d + %d D20 Roll.~
@@ -10790,7 +10782,6 @@ bool Actor::TryToHideIWD2()
 		}
 	}
 
-	free(neighbours);
 	return true;
 }
 
@@ -11133,18 +11124,15 @@ bool Actor::ConcentrationCheck() const
 	if (Modified[IE_SPECFLAGS]&SPECF_DRIVEN) return true;
 
 	// anyone in a 5' radius?
-	Actor **neighbours = area->GetAllActorsInRadius(Pos, GA_NO_DEAD|GA_NO_ALLY|GA_NO_SELF|GA_NO_UNSCHEDULED|GA_NO_HIDDEN, 5*VOODOO_SPL_RANGE_F);
-	Actor **poi = neighbours;
+	std::vector<Actor *> neighbours = area->GetAllActorsInRadius(Pos, GA_NO_DEAD|GA_NO_ALLY|GA_NO_SELF|GA_NO_UNSCHEDULED|GA_NO_HIDDEN, 5*VOODOO_SPL_RANGE_F);
 	bool enemyFound = false;
-	while (*poi) {
-		Actor *neighbour = *poi;
-		if (neighbour->GetStat(IE_EA) > EA_EVILCUTOFF) {
+	std::vector<Actor *>::iterator neighbour;
+	for (neighbour = neighbours.begin(); neighbour != neighbours.end(); neighbour++) {
+		if ((*neighbour)->GetStat(IE_EA) > EA_EVILCUTOFF) {
 			enemyFound = true;
 			break;
 		}
-		poi++;
 	}
-	free(neighbours);
 	if (!enemyFound) return true;
 
 	// so there is someone out to get us and we should do the real concentration check

gemrb/core/Scriptable/Scriptable.cpp
@@ -943,16 +943,15 @@ void Scriptable::DisplaySpellCastMessage(ieDword tgt, Spell *spl)
 	delete spell;
 }
 
+// NOTE: currently includes the sender
 void Scriptable::SendTriggerToAll(TriggerEntry entry)
 {
-	Actor** nearActors = area->GetAllActorsInRadius(Pos, GA_NO_DEAD|GA_NO_UNSCHEDULED, 15*10);
-	int i=0;
-	while(nearActors[i]!=NULL) {
-		nearActors[i]->AddTrigger(entry);
-		++i;
+	std::vector<Actor *> nearActors = area->GetAllActorsInRadius(Pos, GA_NO_DEAD|GA_NO_UNSCHEDULED, 15*10);
+	std::vector<Actor *>::iterator neighbour;
+	for (neighbour = nearActors.begin(); neighbour != nearActors.end(); neighbour++) {
+		(*neighbour)->AddTrigger(entry);
 	}
 	area->AddTrigger(entry);
-	free(nearActors);
 }
 
 inline void Scriptable::ResetCastingState(Actor *caster) {
@@ -1206,17 +1205,15 @@ void Scriptable::SpellcraftCheck(const Actor *caster, const ieResRef SpellResRef
 	Spell* spl = gamedata->GetSpell(SpellResRef);
 	assert(spl); // only a bad surge could make this fail and we want to catch it
 	int AdjustedSpellLevel = spl->SpellLevel + 15;
-	Actor **neighbours = area->GetAllActorsInRadius(caster->Pos, GA_NO_DEAD|GA_NO_ENEMY|GA_NO_SELF|GA_NO_UNSCHEDULED, 10*caster->GetBase(IE_VISUALRANGE));
-	Actor **poi = neighbours;
-	while (*poi) {
-		Actor *detective = *poi;
+	std::vector<Actor *> neighbours = area->GetAllActorsInRadius(caster->Pos, GA_NO_DEAD|GA_NO_ENEMY|GA_NO_SELF|GA_NO_UNSCHEDULED, 10*caster->GetBase(IE_VISUALRANGE));
+	std::vector<Actor *>::iterator neighbour;
+	for (neighbour = neighbours.begin(); neighbour != neighbours.end(); neighbour++) {
+		Actor *detective = *neighbour;
 		// disallow neutrals from helping the party
 		if (detective->GetStat(IE_EA) > EA_CONTROLLABLE) {
-			poi++;
 			continue;
 		}
 		if ((signed)detective->GetSkill(IE_SPELLCRAFT) <= 0) {
-			poi++;
 			continue;
 		}
 
@@ -1237,10 +1234,8 @@ void Scriptable::SpellcraftCheck(const Actor *caster, const ieResRef SpellResRef
 			displaymsg->DisplayRollStringName(39306, DMC_LIGHTGREY, detective, Spellcraft+IntMod, AdjustedSpellLevel, IntMod);
 			break;
 		}
-		poi++;
 	}
 	gamedata->FreeSpell(spl, SpellResRef, false);
-	free(neighbours);
 }
 
 // shortcut for internal use when there is no wait

commit 5f0ad64d378c9e139eec9e26a95f24b857718258

Projectile: fixed regression from 3604435d
aoe wasn't affecting everyone any more
& removed some commented out cruft

gemrb/core/Projectile.cpp
@@ -1220,11 +1220,9 @@ void Projectile::SecondaryTarget()
 
 			//unsigned int dist = (unsigned int) sqrt(xdiff*xdiff+ydiff*ydiff);
 			//int width = (*poi)->GetAnims()->GetCircleSize();
-			//int deg0=0;
 			if (ydiff) {
 				// ensure [0,360] range: transform [-180,180] from atan2, but also take orientation correction factor into account
 				deg = (int) (std::atan2(ydiff, xdiff) * 180/M_PI);
-				//deg0 = deg;
 				deg = ((deg % 360) + 360 + degOffset) % 360;
 			} else {
 				if (xdiff < 0) {
@@ -1234,7 +1232,6 @@ void Projectile::SecondaryTarget()
 				}
 			}
 
-			//Log(DEBUG, "Projectile", "Target %s at angle %d (%d to %d), ignoring? %d %d, %d -> %d, %d", (*poi)->GetName(1), deg, mindeg, maxdeg, mindeg>deg || maxdeg<deg, degOffset, Orientation, saneOrientation, deg0);
 			//not in the right sector of circle
 			if (mindeg>deg || maxdeg<deg) {
 				continue;
@@ -1253,9 +1250,6 @@ void Projectile::SecondaryTarget()
 		//TODO:actually some of the splash projectiles are a good example of faketarget
 		//projectiles (that don't follow the target, but still hit)
 		area->AddProjectile(pro, Pos, Target, false);
-
-		poi++;
-		if (poi == actors.end()) break;
 		fail=false;
 
 		//we already got one target affected in the AOE, this flag says