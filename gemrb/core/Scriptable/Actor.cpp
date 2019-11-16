gemrb/gemrb

commit 9054cd0915a66f652291161ca15acb8a48390270

pcf_hitpoint: bleaaargh #238

gemrb/core/Scriptable/Actor.cpp
@@ -1284,7 +1284,8 @@ static void pcf_hitpoint(Actor *actor, ieDword oldValue, ieDword hp)
 	if (actor->checkHP == 2) return;
 
 	int maxhp = (signed) actor->GetSafeStat(IE_MAXHITPOINTS);
-	if ((signed) hp>maxhp) {
+	// ERWAN.CRE from Victor's Improvement Pack has a max of 0 and still survives, grrr
+	if ((signed) hp>maxhp && (actor->GetBase(IE_CLASS) > 0 && actor->GetBase(IE_CLASS) < (ieDword)classcount)) {
 		hp=maxhp;
 	}
 

commit 246dce11ba2d4353ebe3213a99f0ce569bfb3dd3

pcf_hitpoint: fixed regression from 9054cd0 #393

gemrb/core/Scriptable/Actor.cpp
@@ -1297,7 +1297,7 @@ static void pcf_hitpoint(Actor *actor, ieDword oldValue, ieDword hp)
 
 	int maxhp = (signed) actor->GetSafeStat(IE_MAXHITPOINTS);
 	// ERWAN.CRE from Victor's Improvement Pack has a max of 0 and still survives, grrr
-	if ((signed) hp>maxhp && actor->HasPlayerClass()) {
+	if (maxhp && (signed) hp > maxhp) {
 		hp=maxhp;
 	}
 