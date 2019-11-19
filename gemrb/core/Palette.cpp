0b797baafb6d9d64a61fdfdd2e888fa134e82b11
Brad Allred <bradallred@me.com>
2019-11-18 17:32:32 -0700

    Fix a bunch of undefined behavior errors
    barfed up by UBSan

--- gemrb/core/Palette.cpp
+++ gemrb/core/Palette.cpp
@@ -92,7 +92,7 @@ Palette* Palette::Copy()
 
 void Palette::SetupPaperdollColours(const ieDword* Colors, unsigned int type)
 {
-	unsigned int s = 8*type;
+	unsigned int s = Clamp<ieDword>(8*type, 0, 8*sizeof(ieDword)-1);
 	//metal
 	core->GetPalette( (Colors[0]>>s)&0xFF, 12, &col[0x04]);
 	//minor
