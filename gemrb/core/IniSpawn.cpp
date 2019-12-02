gemrb/gemrb

commit 9c63e0dfa281780fdc3a3d754fe1f2bd4c5dab4e

fix spawn interval check

gemrb/core/IniSpawn.cpp
@@ -829,7 +829,7 @@ void IniSpawn::SpawnGroup(SpawnEntry &event)
 	}
 	unsigned int interval = event.interval;
 	if (interval) {
-		if (core->GetGame()->GameTime + interval <= last_spawndate) {
+		if (last_spawndate + interval >= core->GetGame()->GameTime) {
 			return;
 		}
 	}

commit 660fca27b55ed79cd7b5f660ae9361422b766383

IniSpawn::SpawnGroup: fixed regression from 9c63e0df
fixes starting pst area lacking most of the zombies

gemrb/core/IniSpawn.cpp
@@ -828,12 +828,14 @@ void IniSpawn::SpawnGroup(SpawnEntry &event)
 		return;
 	}
 	unsigned int interval = event.interval;
-	if (interval) {
-		if (last_spawndate + interval >= core->GetGame()->GameTime) {
+	ieDword gameTime = core->GetGame()->GameTime;
+	// gameTime can be 0 for the first area, so make sure to not exit prematurely
+	if (interval && gameTime) {
+		if (last_spawndate + interval >= gameTime) {
 			return;
 		}
 	}
-	last_spawndate=core->GetGame()->GameTime;
+	last_spawndate = gameTime;
 	
 	for(int i=0;i<event.crittercount;i++) {
 		CritterEntry* critter = event.critters+i;