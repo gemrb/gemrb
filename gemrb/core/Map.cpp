63ab6deadca8c873e373b21b1d057ec37b91b104
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2019-12-16 14:09:12 -0600

    Update gemrb/core/Map.cpp
    Co-Authored-By: Jaka Kranjc <smgl@lynxlynx.info>

--- gemrb/core/Map.cpp
+++ gemrb/core/Map.cpp
@@ -1397,7 +1397,8 @@ void Map::Shout(Actor* actor, int shoutID, bool global)
 
 		if (!global) {
 			/* Audible range was confirmed to be 3x visual range in EEs, accounting for isometric scaling 
-			and more than 1x visual range in others in EE, the '48' (3*16) default can be set by the 'Audible Range'
+			and more than 1x visual range in others;
+			in EE, the '48' (3*16) default can be set by the 'Audible Range' option in baldur.lua
 
 			This is a bit tricky, it has been show to not be very consistent. The game used a double value of visual 
 			range in several places, so we will use '3 * visual_range / 2' */
