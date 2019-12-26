gemrb/gemrb

commit 2abf0bab9272b881730113653d92d37262d7fcc9

shrouds of flame: apply only once per round
tick conversion was missing + still had the non-iwd round size

gemrb/plugins/IWDOpcodes/IWDOpcodes.cpp
@@ -1845,7 +1845,7 @@ int fx_shroud_of_flame (Scriptable* Owner, Actor* target, Effect* fx)
 
 	//timing
 	ieDword time = core->GetGame()->GameTime;
-	if ((fx->Parameter4==time) || (time%6) ) {
+	if ((fx->Parameter4==time) || (time%core->Time.round_size) ) {
 		return FX_APPLIED;
 	}
 	fx->Parameter4=time;
@@ -1898,7 +1898,7 @@ int fx_shroud_of_flame2 (Scriptable* Owner, Actor* target, Effect* fx)
 
 	//timing
 	ieDword time = core->GetGame()->GameTime;
-	if ((fx->Parameter4==time) || (time%6) ) {
+	if ((fx->Parameter4==time) || (time%time%core->Time.round_size) ) {
 		return FX_APPLIED;
 	}
 	fx->Parameter4=time;

commit cb4847cb6bc396eda3c50d76f79568a47c1ab216

fx_shroud_of_flame2: fixed regression from 2abf0bab

gemrb/plugins/IWDOpcodes/IWDOpcodes.cpp
@@ -1912,7 +1912,7 @@ int fx_shroud_of_flame2 (Scriptable* /*Owner*/, Actor* target, Effect* fx)
 
 	//timing
 	ieDword time = core->GetGame()->GameTime;
-	if ((fx->Parameter4==time) || (time%time%core->Time.round_size) ) {
+	if (fx->Parameter4 == time || time % core->Time.round_size) {
 		return FX_APPLIED;
 	}
 	fx->Parameter4=time;