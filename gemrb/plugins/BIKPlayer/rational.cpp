5100b37240d79d84b00b330af26175a22fdb2259
MarcelHB <marscel@googlemail.com>
2020-01-10 21:40:30 +0100

    BIKPlayer: fixes a type error over signedness
    After enabling C++11 and removing some subsidiary definitions, it turns
    out that there is a confusing piece of code in a part of the BIK decoder
    by FFMpeg - that now turns into a compiler error.
    
    We did not spot any obvious reasons against fixing it that way.
    
    fixes #592

--- gemrb/plugins/BIKPlayer/rational.cpp
+++ gemrb/plugins/BIKPlayer/rational.cpp
@@ -52,14 +52,14 @@ int av_reduce(int &dst_num, int &dst_den, int64_t num, int64_t den, int64_t max)
     }
 
     while(den){
-        uint64_t x      = num / den;
+        int64_t x      = num / den;
         int64_t next_den= num - den*x;
         int64_t a2n= x*a1.num + a0.num;
         int64_t a2d= x*a1.den + a0.den;
 
         if(a2n > max || a2d > max){
             if(a1.num) x= (max - a0.num) / a1.num;
-            if(a1.den) x= FFMIN(x, (max - a0.den) / a1.den);
+            if(a1.den) x= std::min(x, (max - a0.den) / a1.den);
 
             if (den*(2*x*a1.den + a0.den) > num*a1.den) {
                 a1.den  =x*a1.den + a0.den;
