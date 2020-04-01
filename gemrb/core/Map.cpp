gemrb/gemrb

commit 5a5b021559baa00e2f6cbcb0992de56e6282e3f4

Core: added WithinAudibleRange to simplify calcs

gemrb/core/Map.cpp
@@ -1378,17 +1378,7 @@ void Map::Shout(Actor* actor, int shoutID, bool global)
 		}
 
 		if (!global) {
-			/* Audible range was confirmed to be 3x visual range in EEs, accounting for isometric scaling 
-			and more than 1x visual range in others;
-			in EE, the '48' (3*16) default can be set by the 'Audible Range' option in baldur.lua
-
-			This is a bit tricky, it has been show to not be very consistent. The game used a double value of visual 
-			range in several places, so we will use '3 * visual_range / 2' */
-			const Point& A = actor->Pos;
-			const Point& B = listener->Pos;
-			double angle = atan2(B.y - A.y, B.x - A.x);
-			int distance = (3 * actor->GetStat(IE_VISUALRANGE)) / 2;
-			if (Distance(A, B) > Feet2Pixels(distance, angle)) {
+			if (WithinAudibleRange(actor, listener->Pos)) {
 				continue;
 			}
 		}

gemrb/core/Core.cpp
@@ -218,6 +218,19 @@ double Feet2Pixels(int feet, double angle)
 	return r * feet;
 }
 
+/* Audible range was confirmed to be 3x visual range in EEs, accounting for isometric scaling
+ a nd more than 1x visual range in others;
+ in EE, the '48' (3*16) default can be set by the 'Audible Range' option in baldur.lua
+
+ This is a bit tricky, it has been show to not be very consistent. The game used a double value of visual
+ range in several places, so we will use '3 * visual_range / 2' */
+bool WithinAudibleRange(const Actor *actor, const Point &dest)
+{
+	double angle = atan2(actor->Pos.y - dest.y, actor->Pos.x - dest.x);
+	int distance = (3 * actor->GetStat(IE_VISUALRANGE)) / 2;
+	return Distance(actor->Pos, dest) <= Feet2Pixels(distance, angle);
+}
+
 // returns EA relation between two scriptables (non actors are always enemies)
 // it is used for protectile targeting/iwd ids targeting too!
 int EARelation(Scriptable* Owner, Actor* target)

gemrb/core/Scriptable/Actor.cpp
@@ -4343,9 +4343,7 @@ void Actor::PlayExistenceSounds()
 	int xpos, ypos;
 	audio->GetListenerPos(xpos, ypos);
 	Point listener(xpos, ypos);
-	double angle = atan2(listener.y - Pos.y, listener.x - Pos.x);
-	int distance = (3 * GetStat(IE_VISUALRANGE)) / 2; // refer to Map::Shout regarding audible range
-	if (nextComment && !Immobile() && Distance(Pos, listener) <= Feet2Pixels(distance, angle)) {
+	if (nextComment && !Immobile() && WithinAudibleRange(this, listener)) {
 		//setup as an ambient
 		ieStrRef strref = GetVerbalConstant(VB_EXISTENCE, 5);
 		if (strref != (ieStrRef) -1) {

gemrb/includes/globals.h
@@ -202,6 +202,7 @@ GEM_EXPORT unsigned int PersonalDistance(Scriptable *a, Scriptable *b);
 GEM_EXPORT unsigned int SquaredPersonalDistance(Scriptable *a, Scriptable *b);
 GEM_EXPORT unsigned int SquaredMapDistance(Scriptable *a, Scriptable *b);
 GEM_EXPORT double Feet2Pixels(int feet, double angle);
+GEM_EXPORT bool WithinAudibleRange(const Actor *actor, const Point &dest);
 GEM_EXPORT int EARelation(Scriptable *a, Actor *b);
 GEM_EXPORT bool Schedule(ieDword schedule, ieDword time);
 GEM_EXPORT void CopyResRef(ieResRef d, const ieResRef s);

commit 706ef152aa4549c270f047ffd5af80eedb7f0da2

Map: fixed regression from 5a5b021559b
fixes #647

gemrb/core/Map.cpp
@@ -1375,7 +1375,7 @@ void Map::Shout(Actor* actor, int shoutID, bool global)
 		}
 
 		if (!global) {
-			if (WithinAudibleRange(actor, listener->Pos)) {
+			if (!WithinAudibleRange(actor, listener->Pos)) {
 				continue;
 			}
 		}