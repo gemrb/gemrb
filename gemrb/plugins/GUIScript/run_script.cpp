0da3b58334eef3628c875fcd14d648cb0585cba6
Ignat Insarov <kindaro@gmail.com>
2020-04-21 21:25:41 +0300

    Initialize the interpreter more comprehensively.
    Example of use:
    
        % ./build/gemrb/plugins/GUIScript/run_script 'admin/extract_docstrings.py' |
            read directory
            for file in "$directory"/*
            do pandoc --from=dokuwiki --to=markdown < "$file" > "$file.md"
            done
            echo "$directory"

--- gemrb/plugins/GUIScript/run_script.cpp
+++ gemrb/plugins/GUIScript/run_script.cpp
@@ -1,11 +1,25 @@
 #include "Python.h"
 #include "GUIScript.h"
+#include "Interface.h"
+#include "InterfaceConfig.h"
 
 static void run_script (const char * script_file_path) {
-	GemRB::GUIScript * interpreter = new GemRB::GUIScript;
-	interpreter -> Init_non_specific ();
+
+	/* There are global variables `core` and `gs` declared in headers. `gs`
+	   is automagically assigned when the object is constructed. */
+	GemRB::InterfaceConfig * config = new GemRB::InterfaceConfig (0, NULL);
+	GemRB::Interface * interface = new GemRB::Interface ( );
+	GemRB::core = interface;
+	interface -> Init (config);
+	GemRB::GUIScript * interpreter = new GemRB::GUIScript ( );
+	/* From now on, `gs` = `interpreter` is well-defined. */
+
+	interpreter -> Init ( );
 	interpreter -> ExecFile (script_file_path);
+
 	delete interpreter;
+	delete interface;
+	delete config;
 }
 
 int main (int argc, char ** argv) {
