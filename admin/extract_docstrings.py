c53c4e773e61df1bbb27dd6f2519d057b7626f55
Ignat Insarov <kindaro@gmail.com>
2020-04-22 21:19:52 +0300

    Adjust style.

--- admin/extract_docstrings.py
+++ admin/extract_docstrings.py
@@ -1,12 +1,13 @@
+import GemRB
+
 import inspect
 import tempfile
 import os
-import GemRB
 
-temporary_directory = tempfile.mkdtemp (prefix="GemRB_GUIScript_docstrings_")
+temporary_directory = tempfile.mkdtemp(prefix="GemRB_GUIScript_docstrings_")
 
-for (name, value) in inspect.getmembers (GemRB, inspect.isbuiltin):
-	with open (os.path.join (temporary_directory, name), "w") as target_file:
-		target_file.write(inspect.getdoc (value))
+for (name, value) in inspect.getmembers(GemRB, inspect.isbuiltin):
+	with open(os.path.join(temporary_directory, name), "w") as target_file:
+		target_file.write(inspect.getdoc(value))
 
 print temporary_directory
--- gemrb/plugins/GUIScript/run_script.cpp
+++ gemrb/plugins/GUIScript/run_script.cpp
@@ -1,35 +1,37 @@
-#include "Python.h"
 #include "GUIScript.h"
 #include "Interface.h"
 #include "InterfaceConfig.h"
 
-static void run_script (const char * script_file_path) {
+#include "Python.h"
+
+static void run_script(const char *script_file_path) {
 
 	/* There are global variables `core` and `gs` declared in headers. `gs`
 	   is automagically assigned when the object is constructed. */
-	GemRB::InterfaceConfig * config = new GemRB::InterfaceConfig (0, NULL);
-	GemRB::Interface * interface = new GemRB::Interface ( );
+	GemRB::InterfaceConfig *config = new GemRB::InterfaceConfig(0, NULL);
+	GemRB::Interface *interface = new GemRB::Interface();
 	GemRB::core = interface;
-	interface -> Init (config);
-	GemRB::GUIScript * interpreter = new GemRB::GUIScript ( );
+	interface->Init(config);
+	GemRB::GUIScript *interpreter = new GemRB::GUIScript();
 	/* From now on, `gs` = `interpreter` is well-defined. */
 
-	interpreter -> Init ( );
-	interpreter -> ExecFile (script_file_path);
+	interpreter->Init();
+	interpreter->ExecFile(script_file_path);
 
 	delete interpreter;
 	delete interface;
 	delete config;
 }
 
-int main (int argc, char ** argv) {
+int main(int argc, char **argv) {
 	if (argc != 2) {
-		exit (1);
+		exit(1);
 	} else {
-		run_script (argv[1]);
-		/* Unfortunately, ExecFile returns silently when the file cannot be read
-		into a memory buffer, so this branch may exit successfully even when the
-		code was not run. Also, it will segfault if there is an error in the script. */
-		exit (0);
+		run_script(argv[1]);
+		/* Unfortunately, ExecFile returns silently when the file cannot
+		be read into a memory buffer, so this branch may exit
+		successfully even when the code was not run. Also, it will
+		segfault if there is an error in the script. */
+		exit(0);
 	}
 }
