06ddb42e09c4d6ca9447ae8cdbf83bb618d84af2
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2020-04-23 23:28:48 +0200

    guidoc_wikifier.sh: prepare for jekyll
    fixes 9 on website repo

--- admin/guidoc_wikifier.sh
+++ admin/guidoc_wikifier.sh
@@ -17,24 +17,39 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 #
-# this script takes the guiscript docs and prepares them to shine on dokuwiki
+# this script takes the guiscript docs and prepares them to shine as markdown
 
 out_dir="${1:-$PWD/guiscript-docs.wikified}"
 plugincpp="${2:-gemrb/plugins/GUIScript/GUIScript.cpp}"
 
 mkdir -p "$out_dir"
-rm -f "$out_dir"/*.txt
+rm -f "$out_dir"/*.md
 
 function dumpDocs() {
   # extract the functions docs from the doc strings into individual files
   sed -n '/PyDoc_STRVAR/,/PyObject/ { /"=/,/"$/ { s,\\n,,; s,\\,,; p} }' "$plugincpp" |
     awk -v RS='"' -v out="$out_dir" \
-    '{ split($0, t); title=tolower(t[2]); if (title) print $0 >  out "/" title ".txt" }' || return 1
+    '{ split($0, t)
+       title=t[2]
+       if (title) {
+         filename = out "/" title ".md"
+         module = index(title, "_") ? "_GemRB" : "GemRB"
+         # remove old title
+         sub("^=====.*=====.", "")
+         # fix See also links
+         $0 = gensub("\\[\\[guiscript:(\\w+)\\]\\]", "[\\1](\\1.md)", "g")
+         # also make some examples nicer until we convert the format
+         sub("\*\*Examples*:..", "&\n")
+         gsub("  [a-zA-Z0-9#]+", "  &")
 
-  # append a link to the function index
-  for txt_file in "$out_dir"/*.txt; do
-    echo -e '\n[[guiscript:index|Function index]]' >> "$txt_file"
-  done
+         # dump frontmatter and contents
+         print "---" > filename
+         print "title: " title >> filename
+         print "module: " module >> filename
+         print "---" >> filename
+         print $0 >> filename
+       }
+     }' || return 1
 
   echo "Done, now manually copy the contents of $out_dir to wiki/data/pages/guiscript"
   echo "Tar it up and move to FRS via the web UI or via SCP, eg.:"
