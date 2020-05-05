gemrb/gemrb

commit 7e8bdca5b75223b6f2d31db9d21b63432a6bddb0

reimplement previous SetMouseEnabled calls
fixes #718

gemrb/core/GameScript/Actions.cpp
@@ -796,8 +796,11 @@ void GameScript::SetCursorState(Scriptable* /*Sender*/, Action* parameters)
 
 	Game *game = core->GetGame();
 	game->SetControlStatus(CS_HIDEGUI, (active) ? OP_OR : OP_NAND );
-	// FIXME: reimplement this
-	// core->GetVideoDriver()->SetMouseEnabled(!active);
+	if (active) {
+		core->GetWindowManager()->SetCursorFeedback(WindowManager::MOUSE_NONE);
+	} else {
+		core->GetWindowManager()->SetCursorFeedback(WindowManager::MOUSE_ALL);
+	}
 }
 
 void GameScript::StartCutSceneMode(Scriptable* /*Sender*/, Action* /*parameters*/)

gemrb/core/Interface.cpp
@@ -555,8 +555,7 @@ void Interface::HandleEvents()
 
 	if (EventFlag&EF_TEXTSCREEN) {
 		EventFlag&=~EF_TEXTSCREEN;
-		// FIXME: need to reimplement this
-		//video->SetMouseEnabled(true);
+		winmgr->SetCursorFeedback(WindowManager::MOUSE_ALL);
 		guiscript->RunFunction( "TextScreen", "StartTextScreen" );
 		return;
 	}

commit f1f6463e7f8b7687ae4e81b271a2bad0610883ed

don't trample over user feedback setting
regression from 7e8bdca5b

gemrb/core/GameScript/Actions.cpp
@@ -799,7 +799,7 @@ void GameScript::SetCursorState(Scriptable* /*Sender*/, Action* parameters)
 	if (active) {
 		core->GetWindowManager()->SetCursorFeedback(WindowManager::MOUSE_NONE);
 	} else {
-		core->GetWindowManager()->SetCursorFeedback(WindowManager::MOUSE_ALL);
+		core->GetWindowManager()->SetCursorFeedback(WindowManager::CursorFeedback(core->MouseFeedback));
 	}
 }
 

gemrb/core/Interface.cpp
@@ -555,7 +555,7 @@ void Interface::HandleEvents()
 
 	if (EventFlag&EF_TEXTSCREEN) {
 		EventFlag&=~EF_TEXTSCREEN;
-		winmgr->SetCursorFeedback(WindowManager::MOUSE_ALL);
+		winmgr->SetCursorFeedback(WindowManager::CursorFeedback(core->MouseFeedback));
 		guiscript->RunFunction( "TextScreen", "StartTextScreen" );
 		return;
 	}