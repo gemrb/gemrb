92a17bfcc7a0853da146891008cbcb45d4011e13
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2020-05-28 09:36:12 +0200

    clean up some compiler checks
    GCC 4.8.1 was the first feature-complete implementation of the 2011 C++ standard

--- gemrb/core/System/Logging.h
+++ gemrb/core/System/Logging.h
@@ -79,7 +79,7 @@ GEM_EXPORT void Log(log_level, const char* owner, StringBuffer const&);
 
 // poison printf
 #if !defined(__MINGW32__)
-#if (__GNUC__ >= 4 && (__GNUC_MINOR__ >= 5 || __GNUC__ > 4))
+#if __GNUC__ >= 4
 extern "C" int printf(const char* message, ...) __attribute__ ((deprecated("GemRB doesn't use printf; use Log instead.")));
 #elif __GNUC__ >= 4
 extern "C" int printf(const char* message, ...) __attribute__ ((deprecated));
