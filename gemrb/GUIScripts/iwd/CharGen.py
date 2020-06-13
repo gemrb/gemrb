gemrb/gemrb

commit 55b65cdf0c967d04748dd39107268ec6fca71bdc

SkillsNullify: also nullify the saved skills if specified
and use it in iwd to avoid #432

gemrb/GUIScripts/iwd/CharGen.py
@@ -1546,6 +1546,7 @@ def SkillsPress():
 	MageSpell = CommonTables.ClassSkills.GetValue (ClassName, "MAGESPELL")
 	IsBard = CommonTables.ClassSkills.GetValue (ClassName, "BARDSKILL")
 	IsThief = CommonTables.ClassSkills.GetValue (ClassName, "THIEFSKILL")
+	LUSkillsSelection.SkillsNullify (MyChar)
 
 	if SkillsState == 0:
 		GemRB.SetVar ("HatedRace", 0)

gemrb/GUIScripts/LUSkillsSelection.py
@@ -393,7 +393,7 @@ def SkillsSave (pc):
 		if SkillValue > 0:
 			GemRB.SetPlayerStat (pc, SkillID, SkillValue)
 
-def SkillsNullify ():
+def SkillsNullify (pc = None):
 	global SkillsTable
 	if not SkillsTable:
 		SkillsTable = GemRB.LoadTable ("skills")
@@ -401,6 +401,10 @@ def SkillsNullify ():
 	for i in range(SkillsTable.GetRowCount()-2):
 		GemRB.SetVar ("Skill "+str(i), 0)
 		GemRB.SetVar ("SkillBase "+str(i), 0)
+		if pc:
+			SkillName = SkillsTable.GetRowName (i+2)
+			SkillID = SkillsTable.GetValue (SkillName, "ID")
+			GemRB.SetPlayerStat (pc, SkillID, 0)
 
 def SkillsHide (i):
 	Label = SkillsWindow.GetControl (0x10000000+SkillsOffsetName+i)

commit 21473e05aa1f025f66220bba96232c7f2d969b89

iwd::cg: fixed regression from 55b65cd
this function gets run several times

gemrb/GUIScripts/iwd/CharGen.py
@@ -1521,9 +1521,9 @@ def SkillsPress():
 	MageSpell = CommonTables.ClassSkills.GetValue (ClassName, "MAGESPELL")
 	IsBard = CommonTables.ClassSkills.GetValue (ClassName, "BARDSKILL")
 	IsThief = CommonTables.ClassSkills.GetValue (ClassName, "THIEFSKILL")
-	LUSkillsSelection.SkillsNullify (MyChar)
 
 	if SkillsState == 0:
+		LUSkillsSelection.SkillsNullify (MyChar)
 		HateRace = CommonTables.ClassSkills.GetValue(ClassName, "HATERACE")
 		if DruidSpell != "*" and HateRace != "*":
 			Skill = GemRB.LoadTable("SKILLRNG").GetValue(str(Level), "STEALTH")