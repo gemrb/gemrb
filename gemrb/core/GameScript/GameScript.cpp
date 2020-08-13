gemrb/gemrb

commit 855365e40ca3d0bc488c0c024f78e813cc35871a

implemented NextTriggerObject
example test script (make sure to have ai enabled):
IF
	HotKey(K)
	Race(Myself,ELF) // some way to differentiate between party members
THEN
	RESPONSE #100
		ActionOverride(Player1,DisplayStringHead(Player1,23959))
		DisplayStringHead(Myself,324)
END

// press L while a non-ELF pc is selected, which should still display
// the messages like if an ELF was selected
IF
	HotKey(L)
	NextTriggerObject(Player2) // the ELF pc number (InParty)
	Race(Myself,ELF)
THEN
	RESPONSE #100
		ActionOverride(Player1,DisplayStringHead(Player1,23959))
		DisplayStringHead(Myself,324)
END

gemrb/core/GameScript/GameScript.cpp
@@ -279,7 +279,7 @@ static const TriggerLink triggernames[] = {
 	{"nearbydialogue", GameScript::NearbyDialog, 0},
 	{"nearlocation", GameScript::NearLocation, 0},
 	{"nearsavedlocation", GameScript::NearSavedLocation, 0},
-	{"nexttriggerobject", GameScript::NextTriggerObject, 0},
+	{"nexttriggerobject", NULL, 0}, // handled inline
 	{"nightmaremodeon", GameScript::NightmareModeOn, 0},
 	{"notstatecheck", GameScript::NotStateCheck, 0},
 	{"nulldialog", GameScript::NullDialog, 0},
@@ -1976,6 +1976,9 @@ static Trigger* ReadTrigger(DataStream* stream)
 	return tR;
 }
 
+// NOTE: keep these in sync with gemtrig.ids!
+#define NEXT_TRIGGER_OBJECT_EX 0x4100
+#define NEXT_TRIGGER_OBJECT_EE 0x40e0
 static Condition* ReadCondition(DataStream* stream)
 {
 	char line[10];
@@ -1985,10 +1988,31 @@ static Condition* ReadCondition(DataStream* stream)
 		return NULL;
 	}
 	Condition* cO = new Condition();
+	Object *triggerer = NULL;
 	while (true) {
 		Trigger* tR = ReadTrigger( stream );
-		if (!tR)
+		if (!tR) {
+			if (triggerer) delete triggerer;
 			break;
+		}
+
+		// handle NextTriggerObject
+		/* Defines the object that the next trigger will be evaluated in reference to. This trigger
+		 * does not evaluate and does not count as a trigger in an OR() block. This trigger ignores
+		 * the Eval() trigger when finding the next trigger to evaluate the object for. If the object
+		 * cannot be found, the next trigger will evaluate to false.
+		 */
+		if (triggerer) {
+			delete tR->objectParameter; // not using Release, so we don't have to check if it's null
+			tR->objectParameter = triggerer;
+			triggerer = NULL;
+		} else if (tR->triggerID == (0x3fff&NEXT_TRIGGER_OBJECT_EX) || tR->triggerID == (0x3fff&NEXT_TRIGGER_OBJECT_EE)) {
+			triggerer = tR->objectParameter;
+			tR->objectParameter = NULL;
+			delete tR;
+			continue;
+		}
+
 		cO->triggers.push_back( tR );
 	}
 	return cO;

gemrb/core/GameScript/GameScript.h
@@ -792,7 +792,6 @@ public: //Script Functions
 	static int NearbyDialog(Scriptable* Sender, Trigger* parameters);
 	static int NearLocation(Scriptable* Sender, Trigger* parameters);
 	static int NearSavedLocation(Scriptable* Sender, Trigger* parameters);
-	static int NextTriggerObject(Scriptable* /*Sender*/, Trigger* /*parameters*/);
 	static int NightmareModeOn(Scriptable* Sender, Trigger* parameters);
 	static int NotStateCheck(Scriptable* Sender, Trigger* parameters);
 	static int NullDialog(Scriptable* Sender, Trigger* parameters);

gemrb/core/GameScript/Triggers.cpp
@@ -4204,17 +4204,6 @@ int GameScript::BeenInParty(Scriptable* Sender, Trigger* /*parameters*/)
  * TobEx triggers
  */
 
-/* Defines the object that the next trigger will be evaluated in reference to. This trigger
- * does not evaluate and does not count as a trigger in an OR() block. This trigger ignores
- * the Eval() trigger when finding the next trigger to evaluate the object for. If the object
- * cannot be found, the next trigger will evaluate to false.
- */
-int GameScript::NextTriggerObject(Scriptable* /*Sender*/, Trigger* /*parameters*/)
-{
-	// TODO: implement
-	return 0;
-}
-
 /* Compares the animation movement rate of the target creature specified by Object to Value.
  * This is not affected by slow or haste, but is affected if the Object is entangled, webbed, etc.
  */

gemrb/unhardcoded/shared/gemtrig.ids
@@ -1,4 +1,5 @@
 IDS
+0x40e0 NextTriggerObject(O:Object*)
 0x40ff UsedExit(S:ResRef*,O:Actor*)
 0x4100 NextTriggerObject(O:Object*)
 0x4101 MovementRate(O:Object*,I:Value*)

commit d5e75d6af5cdf21d9a58cf9ab03388cc4de5599e

GameScript: fixed regression from 855365e40c
cleaner too; since we were checking both IDs, it didn't matter
that iwd2 didn't have it at a bad ID

gemrb/core/GameScript/GameScript.cpp
@@ -1178,6 +1178,8 @@ static const IDSLink idsnames[] = {
 	{ NULL,NULL}
 };
 
+static int NextTriggerObjectID = 0;
+
 static const TriggerLink* FindTrigger(const char* triggername)
 {
 	if (!triggername) {
@@ -1676,12 +1678,15 @@ void InitializeIEScript()
 			int i = overrideTriggersTable->GetValueIndex( j );
 			bool was_condition = (i & 0x4000);
 			i &= 0x3fff;
+			const char *trName = overrideTriggersTable->GetStringIndex(j);
 			if (i >= MAX_TRIGGERS) {
-				Log(ERROR, "GameScript", "trigger %d (%s) is too high, ignoring",
-					i, overrideTriggersTable->GetStringIndex( j ) );
+				Log(ERROR, "GameScript", "Trigger %d (%s) is too high, ignoring", i, trName);
 				continue;
 			}
-			const TriggerLink *poi = FindTrigger( overrideTriggersTable->GetStringIndex( j ));
+			if (!NextTriggerObjectID && !stricmp(trName, "NextTriggerObject(O:Object*)")) {
+				NextTriggerObjectID = i;
+			}
+			const TriggerLink *poi = FindTrigger(trName);
 			if (!poi) {
 				StringBuffer buffer;
 
@@ -1693,8 +1698,7 @@ void InitializeIEScript()
 			if (triggers[i] && ( (triggers[i]!=poi->Function) || (triggerflags[i]!=tf) ) ) {
 				StringBuffer buffer;
 
-				buffer.appendFormatted("%s overrides existing trigger ",
-					overrideTriggersTable->GetStringIndex( j ) );
+				buffer.appendFormatted("%s overrides existing trigger ", trName);
 				int x = triggersTable->FindValue(i);
 				if (x<0) x = triggersTable->FindValue(i|0x4000);
 				if (x>=0) {
@@ -2020,9 +2024,6 @@ static Trigger* ReadTrigger(DataStream* stream)
 	return tR;
 }
 
-// NOTE: keep these in sync with gemtrig.ids!
-#define NEXT_TRIGGER_OBJECT_EX 0x4100
-#define NEXT_TRIGGER_OBJECT_EE 0x40e0
 static Condition* ReadCondition(DataStream* stream)
 {
 	char line[10];
@@ -2050,7 +2051,7 @@ static Condition* ReadCondition(DataStream* stream)
 			delete tR->objectParameter; // not using Release, so we don't have to check if it's null
 			tR->objectParameter = triggerer;
 			triggerer = NULL;
-		} else if (tR->triggerID == (0x3fff&NEXT_TRIGGER_OBJECT_EX) || tR->triggerID == (0x3fff&NEXT_TRIGGER_OBJECT_EE)) {
+		} else if (tR->triggerID == NextTriggerObjectID) {
 			triggerer = tR->objectParameter;
 			tR->objectParameter = NULL;
 			delete tR;