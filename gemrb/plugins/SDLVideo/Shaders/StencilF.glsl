f2de5e303d709fc3cedc07a4f50e925ee3faa3aa
Brad Allred <bradallred@me.com>
2020-08-17 10:07:05 -0600

    OpenGL: dithering is an alternative to half trans

--- gemrb/plugins/SDLVideo/Shaders/StencilF.glsl
+++ gemrb/plugins/SDLVideo/Shaders/StencilF.glsl
@@ -10,7 +10,8 @@ void main()
 	vec4 color = texture2D(s_texture, v_texCoord);
 	gl_FragColor = vec4(0, 0, 0, color[u_channel - 1]);
 	
-	if (u_dither == 1 && gl_FragColor.a > 0.0) {
+	if (u_dither == 1 && gl_FragColor.a == 0.5) {
+		// dithering is an alternative to BLIT_HALFTRANS
 		vec2 p = vec2(floor(gl_FragCoord.x), floor(gl_FragCoord.y));
 		if (mod(p.y, 2.0) == 0.0) {
 			gl_FragColor.a = float(mod(p.x, 2.0) == 0.0);
