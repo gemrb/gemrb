gemrb/gemrb

commit 2509a3c2fc454c5290e456639ee61cb4e38ddbd1

GUICG7: cleaned up

gemrb/GUIScripts/bg1/GUICG7.py
@@ -19,19 +19,17 @@
 # character generation, mage spells (GUICG7)
 
 import GemRB
-from GUIDefines import *
-from ie_stats import *
 import GUICommon
-import Spellbook
 import CommonTables
 import LUSpellSelection
+from GUIDefines import *
+from ie_stats import *
 
 def OnLoad():
 	Slot = GemRB.GetVar ("Slot")
 	Class = GemRB.GetPlayerStat (Slot, IE_CLASS)
 	ClassName = GUICommon.GetClassRowName (Class, "class")
 	TableName = CommonTables.ClassSkills.GetValue(ClassName, "MAGESPELL")
-	BookType = CommonTables.ClassSkills.GetValue (ClassName, "BOOKTYPE")
 
 	# get our kit
 	KitValue = GemRB.GetPlayerStat (Slot, IE_KIT)
@@ -46,7 +44,7 @@ def OnLoad():
 			if CommonTables.ClassSkills.GetValue (ClassName, "MAGESPELL", GTV_STR) != "*":
 				Level = GemRB.GetPlayerStat (Slot, IE_LEVEL2+i-1)
 			break
-	Spellbook.SetupSpellLevels(Slot, TableName, IE_SPELL_TYPE_WIZARD, 1)
+
 	LUSpellSelection.OpenSpellsWindow (Slot, TableName, Level, Level, KitValue, 1,False)
 
 	return

gemrb/GUIScripts/bg2/GUICG7.py
@@ -20,19 +20,16 @@
 
 import GemRB
 import GUICommon
-import Spellbook
 import CommonTables
-from ie_stats import *
-from GUIDefines import *
 import LUSpellSelection
+from GUIDefines import *
+from ie_stats import *
 
 def OnLoad():
-	KitTable = GemRB.LoadTable("magesch")
 	Slot = GemRB.GetVar ("Slot")
 	Class = GemRB.GetPlayerStat (Slot, IE_CLASS)
 	ClassName = GUICommon.GetClassRowName (Class, "class")
 	TableName = CommonTables.ClassSkills.GetValue(ClassName, "MAGESPELL")
-	BookType = CommonTables.ClassSkills.GetValue (ClassName, "BOOKTYPE")
 
 	# make sure we have a correct table
 	if TableName == "*":
@@ -40,6 +37,7 @@ def OnLoad():
 		return
 
 	# get our kit index
+	KitTable = GemRB.LoadTable ("magesch")
 	KitIndex = GUICommon.GetKitIndex (Slot)
 	if KitIndex:
 		KitValue = KitTable.GetValue(KitIndex - 21, 3)

commit db13c4fef64a69e5063fd7d1f85015baa7017bf8

guicg7: fixed regression from 2509a3c2

gemrb/GUIScripts/bg1/GUICG7.py
@@ -22,6 +22,7 @@ import GemRB
 import GUICommon
 import CommonTables
 import LUSpellSelection
+import Spellbook
 from GUIDefines import *
 from ie_stats import *
 
@@ -44,6 +45,7 @@ def OnLoad():
 			if CommonTables.ClassSkills.GetValue (ClassName, "MAGESPELL", GTV_STR) != "*":
 				Level = GemRB.GetPlayerStat (Slot, IE_LEVEL2+i-1)
 			break
+	Spellbook.SetupSpellLevels(Slot, TableName, IE_SPELL_TYPE_WIZARD, 1)
 
 	LUSpellSelection.OpenSpellsWindow (Slot, TableName, Level, Level, KitValue, 1,False)
 