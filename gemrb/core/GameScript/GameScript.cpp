gemrb/gemrb

commit 67c1d53a29797d0e5a3534ff36d60d1c2ba6d9e0

Condition::Evaluate: warn if we ever get an empty condition again

gemrb/core/GameScript/GameScript.cpp
@@ -2309,6 +2309,11 @@ bool Condition::Evaluate(Scriptable* Sender)
 	unsigned int result = 0;
 	bool subresult = true;
 
+	if (triggers.empty()) {
+		Log(ERROR, "GameScript", "Trigger block without triggers encountered!");
+		return false;
+	}
+
 	for (size_t i = 0; i < triggers.size(); i++) {
 		Trigger* tR = triggers[i];
 		//do not evaluate triggers in an Or() block if one of them

commit 2584ca8e922c6e9ed1120771f8e8ab3b1899d5c4

Condition::Evaluate: fixed regression from 67c1d53a
fixes #989

gemrb/core/GameScript/GameScript.cpp
@@ -2318,8 +2318,7 @@ bool Condition::Evaluate(Scriptable *Sender) const
 	bool subresult = true;
 
 	if (triggers.empty()) {
-		Log(ERROR, "GameScript", "Trigger block without triggers encountered!");
-		return false;
+		return true;
 	}
 
 	for (const Trigger *tR : triggers) {