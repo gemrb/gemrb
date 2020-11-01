gemrb/gemrb

commit e1b7b90a9bd1f13c23a505bec21a7f75fe3cc737

Game: constified the rest of the functions

gemrb/core/Game.h
@@ -330,7 +330,7 @@ public:
 	/** Finds an actor in store by scripting name*/
 	Actor* FindNPC(const char *deathvar) const;
 	/** Sets the area and position of the actor to the starting position */
-	void InitActorPos(Actor *actor);
+	void InitActorPos(Actor *actor) const;
 	/** Joins party */
 	int JoinParty(Actor* pc, int join=JP_JOIN);
 	/** Return current party size */
@@ -338,19 +338,19 @@ public:
 	/** Returns the npcs count */
 	int GetNPCCount() const { return (int)NPCs.size(); }
 	/** Sends the hotkey trigger to all selected pcs */
-	void SetHotKey(unsigned long Key);
+	void SetHotKey(unsigned long Key) const;
 	/** Select PC for non-walking environment (shops, inventory, ...) */
 	bool SelectPCSingle(int index);
 	/** Get index of selected PC for non-walking env (shops, inventory, ...) */
 	int GetSelectedPCSingle() const;
-	Actor* GetSelectedPCSingle(bool onlyalive);
+	Actor* GetSelectedPCSingle(bool onlyalive) const;
 	/** (De)selects actor. */
 	bool SelectActor( Actor* actor, bool select, unsigned flags );
 
 	/** Return current party level count for xp calculations */
 	int GetTotalPartyLevel(bool onlyalive) const;
 	/** Reassigns inparty numbers, call it after party creation */
-	void ConsolidateParty();
+	void ConsolidateParty() const;
 	/** Removes actor from party (if in there) */
 	int LeaveParty(Actor* pc);
 	/** Returns slot*/
@@ -362,10 +362,10 @@ public:
 	 * use it for the biggest safety, change = true will change the current map */
 	Map* GetMap(const char *areaname, bool change);
 	/** Returns slot of the map if found */
-	int FindMap(const char *ResRef);
+	int FindMap(const char *ResRef) const;
 	int AddMap(Map* map);
 	/** Determine if area is master area*/
-	bool MasterArea(const char *area);
+	bool MasterArea(const char *area) const;
 	/** Dynamically adding an area to master areas*/
 	void SetMasterArea(const char *area);
 	/** Guess the master area of the given area*/
@@ -378,9 +378,9 @@ public:
 	int LoadMap(const char* ResRef, bool loadscreen);
 	int DelMap(unsigned int index, int forced = 0);
 	int AddNPC(Actor* npc);
-	Actor* GetNPC(unsigned int Index);
-	void SwapPCs(unsigned int Index1, unsigned int Index2);
-	bool IsDay();
+	Actor* GetNPC(unsigned int Index) const;
+	void SwapPCs(unsigned int Index1, unsigned int Index2) const;
+	bool IsDay() const;
 	/** checks if the actor should be replaced via npclevel.2da and then does it */
 	bool CheckForReplacementActor(int i);
 
@@ -396,8 +396,8 @@ public:
 	/** Adds a journal entry while loading the .gam structure */
 	void AddJournalEntry(GAMJournalEntry* entry);
 	unsigned int GetJournalCount() const;
-	GAMJournalEntry* FindJournalEntry(ieStrRef strref);
-	GAMJournalEntry* GetJournalEntry(unsigned int Index);
+	GAMJournalEntry* FindJournalEntry(ieStrRef strref) const;
+	GAMJournalEntry* GetJournalEntry(unsigned int Index) const;
 
 	//saved locations
 	unsigned int GetSavedLocationCount() const;
@@ -437,9 +437,9 @@ public:
 	}
 
 	/** converts challenge rating to xp */
-	int GetXPFromCR(int cr);
+	int GetXPFromCR(int cr) const;
 	/** shares XP among all party members */
-	void ShareXP(int XP, int flags);
+	void ShareXP(int XP, int flags) const;
 	/** returns true if we should start the party overflow window */
 	bool PartyOverflow() const;
 	/** returns true if any pc is attacker or being attacked */
@@ -448,11 +448,11 @@ public:
 	bool EveryoneDead() const;
 	/** returns true if no one moves */
 	bool EveryoneStopped() const;
-	bool EveryoneNearPoint(Map *map, const Point &p, int flags) const;
+	bool EveryoneNearPoint(const Map *map, const Point &p, int flags) const;
 	/** a party member just died now */
-	void PartyMemberDied(Actor *);
+	void PartyMemberDied(const Actor *);
 	/** Increments chapter variable and refreshes kill stats */
-	void IncrementChapter();
+	void IncrementChapter() const;
 	/** Sets party reputation */
 	void SetReputation(ieDword r);
 	/** Sets the gamescreen control status (pane states, dialog textarea size) */
@@ -479,7 +479,7 @@ public:
 	/** timestop effect initiated by actor */
 	void TimeStop(Actor *actor, ieDword end);
 	/** check if the passed actor is a victim of timestop */
-	bool TimeStoppedFor(const Actor* target=NULL);
+	bool TimeStoppedFor(const Actor* target = nullptr) const;
 	/** updates the infravision info */
 	void Infravision();
 	/** applies the global tint if it is needed */
@@ -498,7 +498,7 @@ public:
 	/** Dumps information about the object */
 	void dump() const;
 	/** Finds an actor by global ID */
-	Actor *GetActorByGlobalID(ieDword objectID);
+	Actor *GetActorByGlobalID(ieDword objectID) const;
 	/** Allocates maze data */
 	ieByte *AllocateMazeData();
 	/** Checks if any timestop effects are active */
@@ -509,13 +509,13 @@ public:
 	/** Checks the bounty encounters (used in bg1) */
 	bool RandomEncounter(ieResRef &BaseArea);
 	/** Resets the area and bored comment timers of the whole party */
-	void ResetPartyCommentTimes();
-	void ReversePCs();
+	void ResetPartyCommentTimes() const;
+	void ReversePCs() const;
 	bool OnlyNPCsSelected() const;
 private:
-	bool DetermineStartPosType(const TableMgr *strta);
+	bool DetermineStartPosType(const TableMgr *strta) const;
 	ieResRef *GetDream(Map *area);
-	void CastOnRest();
+	void CastOnRest() const;
 	void PlayerDream();
 	void TextDream();
 };

gemrb/core/GUI/GameControl.cpp
@@ -330,7 +330,7 @@ void GameControl::DrawArrowMarker(const Region &screen, Point p, const Region &v
 	spr->release();
 }
 
-void GameControl::DrawTargetReticle(Point p, int size, bool animate, bool flash, bool actorSelected)
+void GameControl::DrawTargetReticle(Point p, int size, bool animate, bool flash, bool actorSelected) const
 {
 	// reticles are never drawn in cutscenes
 	if (GetScreenFlags()&SF_CUTSCENE)
@@ -1829,7 +1829,7 @@ bool GameControl::HandleActiveRegion(InfoPoint *trap, Actor * actor, Point &p)
 			//there. Here we have to check on the
 			//reset trap and deactivated flags
 			if (trap->Scripts[0]) {
-				GameControl *gc = core->GetGameControl();
+				const GameControl *gc = core->GetGameControl();
 				if (!(trap->Flags & TRAP_DEACTIVATED) && !(gc->GetDialogueFlags() & DF_FREEZE_SCRIPTS)) {
 					trap->AddTrigger(TriggerEntry(trigger_clicked, actor->GetGlobalID()));
 					actor->LastMarked = trap->GetGlobalID();
@@ -2078,7 +2078,7 @@ formation_handling:
 
 void GameControl::ExecuteMovement(Actor *actor, unsigned short x, unsigned short y, bool createWaypoint)
 {
-	Game *game = core->GetGame();
+	const Game *game = core->GetGame();
 	Point p(x,y);
 	core->GetVideoDriver()->ConvertToGame(p.x, p.y);
 

gemrb/core/GUI/GameControl.h
@@ -102,7 +102,7 @@ public:
 	// GameControl always needs to redraw
 	bool NeedsDraw() const { return true; };
 	/** Draws the target reticle for Actor movement. */
-	void DrawTargetReticle(Point p, int size, bool animate, bool flash=false, bool actorSelected=false);
+	void DrawTargetReticle(Point p, int size, bool animate, bool flash = false, bool actorSelected = false) const;
 	/** Sets multiple quicksaves flag*/
 	//static void MultipleQuickSaves(int arg);
 	void SetTracker(Actor *actor, ieDword dist);
@@ -169,8 +169,8 @@ public: //Events
 	int GetTargetMode() { return target_mode; }
 	void SetScreenFlags(unsigned int value, int mode);
 	void SetDialogueFlags(unsigned int value, int mode);
-	int GetScreenFlags() { return ScreenFlags; }
-	int GetDialogueFlags() { return DialogueFlags; }
+	int GetScreenFlags() const { return ScreenFlags; }
+	int GetDialogueFlags() const { return DialogueFlags; }
 	void SetDisplayText(String* text, unsigned int time);
 	void SetDisplayText(ieStrRef text, unsigned int time);
 	/* centers viewport to the points specified */

gemrb/core/Game.cpp
@@ -105,7 +105,6 @@ Game::Game(void) : Scriptable( ST_GLOBAL )
 	BanterBlockTime = 0;
 	BanterBlockFlag = 0;
 	WeatherBits = 0;
-	crtable = NULL;
 	kaputz = NULL;
 	beasts = NULL;
 	mazedata = NULL;
@@ -170,6 +169,8 @@ Game::Game(void) : Scriptable( ST_GLOBAL )
 		}
 	}
 
+	LoadCRTable();
+
 	interval = 1000/AI_UPDATE_TIME;
 	hasInfra = false;
 	familiarBlock = false;
@@ -227,7 +228,7 @@ Game::~Game(void)
 	}
 }
 
-static bool IsAlive(Actor *pc)
+static bool IsAlive(const Actor *pc)
 {
 	if (pc->GetStat(IE_STATE_ID)&STATE_DEAD) {
 		return false;
@@ -235,7 +236,7 @@ static bool IsAlive(Actor *pc)
 	return true;
 }
 
-void Game::ReversePCs()
+void Game::ReversePCs() const
 {
 	for (auto pc : PCs) {
 		pc->InParty = PCs.size() - pc->InParty + 1;
@@ -374,7 +375,7 @@ int Game::DelNPC(unsigned int slot, bool autoFree)
 }
 
 //i'm sure this could be faster
-void Game::ConsolidateParty()
+void Game::ConsolidateParty() const
 {
 	int max = (int) PCs.size();
 	for (int i=1;i<=max;) {
@@ -434,7 +435,7 @@ int Game::LeaveParty (Actor* actor)
 }
 
 //determines if startpos.2da has rotation rows (it cannot have tutorial line)
-bool Game::DetermineStartPosType(const TableMgr *strta)
+bool Game::DetermineStartPosType(const TableMgr *strta) const
 {
 	if ((strta->GetRowCount()>=6) && !stricmp(strta->GetRowName(4),"START_ROT" ) )
 	{
@@ -445,7 +446,7 @@ bool Game::DetermineStartPosType(const TableMgr *strta)
 
 #define PMODE_COUNT 3
 
-void Game::InitActorPos(Actor *actor)
+void Game::InitActorPos(Actor *actor) const
 {
 	//start.2da row labels
 	const char *mode[PMODE_COUNT] = { "NORMAL", "TUTORIAL", "EXPANSION" };
@@ -569,7 +570,7 @@ int Game::GetPartySize(bool onlyalive) const
 }
 
 /* sends the hotkey trigger to all selected actors */
-void Game::SetHotKey(unsigned long Key)
+void Game::SetHotKey(unsigned long Key) const
 {
 	for (auto actor : selected) {
 		if (actor->IsSelected()) {
@@ -580,7 +581,7 @@ void Game::SetHotKey(unsigned long Key)
 
 bool Game::SelectPCSingle(int index)
 {
-	Actor* actor = FindPC( index );
+	const Actor* actor = FindPC( index );
 	if (!actor)
 		return false;
 
@@ -595,7 +596,7 @@ int Game::GetSelectedPCSingle() const
 	return SelectedSingle;
 }
 
-Actor* Game::GetSelectedPCSingle(bool onlyalive)
+Actor* Game::GetSelectedPCSingle(bool onlyalive) const
 {
 	Actor *pc = FindPC(SelectedSingle);
 	if (!pc) return NULL;
@@ -716,11 +717,11 @@ int Game::GetTotalPartyLevel(bool onlyalive) const
 }
 
 // Returns map structure (ARE) if it is already loaded in memory
-int Game::FindMap(const char *ResRef)
+int Game::FindMap(const char *ResRef) const
 {
 	int index = (int) Maps.size();
 	while (index--) {
-		Map *map=Maps[index];
+		const Map *map = Maps[index];
 		if (strnicmp(ResRef, map->GetScriptName(), 8) == 0) {
 			return index;
 		}
@@ -767,7 +768,7 @@ Map *Game::GetMap(const char *areaname, bool change)
 	return area;
 }
 
-bool Game::MasterArea(const char *area)
+bool Game::MasterArea(const char *area) const
 {
 	for (auto ma : mastarea) {
 		if (!strnicmp(ma, area, 8)) {
@@ -1053,7 +1054,7 @@ int Game::AddNPC(Actor* npc)
 	return (int) NPCs.size() - 1;
 }
 
-Actor* Game::GetNPC(unsigned int Index)
+Actor* Game::GetNPC(unsigned int Index) const
 {
 	if (Index >= NPCs.size()) {
 		return NULL;
@@ -1061,7 +1062,7 @@ Actor* Game::GetNPC(unsigned int Index)
 	return NPCs[Index];
 }
 
-void Game::SwapPCs(unsigned int Index1, unsigned int Index2)
+void Game::SwapPCs(unsigned int Index1, unsigned int Index2) const
 {
 	if (Index1 >= PCs.size()) {
 		return;
@@ -1149,7 +1150,7 @@ unsigned int Game::GetJournalCount() const
 	return (unsigned int) Journals.size();
 }
 
-GAMJournalEntry* Game::FindJournalEntry(ieStrRef strref)
+GAMJournalEntry* Game::FindJournalEntry(ieStrRef strref) const
 {
 	for (auto entry : Journals) {
 		if (entry->Text == strref) {
@@ -1160,7 +1161,7 @@ GAMJournalEntry* Game::FindJournalEntry(ieStrRef strref)
 	return NULL;
 }
 
-GAMJournalEntry* Game::GetJournalEntry(unsigned int Index)
+GAMJournalEntry* Game::GetJournalEntry(unsigned int Index) const
 {
 	if (Index >= Journals.size()) {
 		return NULL;
@@ -1250,9 +1251,8 @@ void Game::LoadCRTable()
 }
 
 // FIXME: figure out the real mechanism
-int Game::GetXPFromCR(int cr)
+int Game::GetXPFromCR(int cr) const
 {
-	if (!crtable) LoadCRTable();
 	if (!crtable) {
 		Log(ERROR, "Game", "Cannot find moncrate.2da!");
 		return 0;
@@ -1275,7 +1275,7 @@ int Game::GetXPFromCR(int cr)
 	return crtable[level-1][cr-1]/2;
 }
 
-void Game::ShareXP(int xp, int flags)
+void Game::ShareXP(int xp, int flags) const
 {
 	int individual;
 
@@ -1322,7 +1322,7 @@ bool Game::EveryoneStopped() const
 }
 
 //canmove=true: if some PC can't move (or hostile), then this returns false
-bool Game::EveryoneNearPoint(Map *area, const Point &p, int flags) const
+bool Game::EveryoneNearPoint(const Map *area, const Point &p, int flags) const
 {
 	for (auto pc : PCs) {
 		if (flags&ENP_ONLYSELECT) {
@@ -1355,10 +1355,10 @@ bool Game::EveryoneNearPoint(Map *area, const Point &p, int flags) const
 }
 
 //called when someone died
-void Game::PartyMemberDied(Actor *actor)
+void Game::PartyMemberDied(const Actor *actor)
 {
 	//this could be null, in some extreme cases...
-	Map *area = actor->GetCurrentArea();
+	const Map *area = actor->GetCurrentArea();
 
 	unsigned int size = PCs.size();
 	Actor *react = NULL;
@@ -1389,7 +1389,7 @@ void Game::PartyMemberDied(Actor *actor)
 	}
 }
 
-void Game::IncrementChapter()
+void Game::IncrementChapter() const
 {
 	//chapter first set to 0 (prologue)
 	ieDword chapter = (ieDword) -1;
@@ -1510,7 +1510,7 @@ void Game::AdvanceTime(ieDword add, bool fatigue)
 //returns true if there are excess players in the team
 bool Game::PartyOverflow() const
 {
-	GameControl *gc = core->GetGameControl();
+	const GameControl *gc = core->GetGameControl();
 	if (!gc) {
 		return false;
 	}
@@ -1541,7 +1541,7 @@ bool Game::EveryoneDead() const
 		return true;
 	}
 	if (protagonist==PM_NO) {
-		Actor *nameless = PCs[0];
+		const Actor *nameless = PCs[0];
 		// don't trigger this outside pst, our game loop depends on it
 		if (nameless->GetStat(IE_STATE_ID)&STATE_NOSAVE && core->HasFeature(GF_PST_STATE_FLAGS)) {
 			if (area->INISpawn) {
@@ -1726,9 +1726,9 @@ int Game::CanPartyRest(int checks) const
 		}
 	}
 
-	Actor *leader = GetPC(0, true);
+	const Actor *leader = GetPC(0, true);
 	assert(leader);
-	Map *area = leader->GetCurrentArea();
+	const Map *area = leader->GetCurrentArea();
 	//we let them rest if someone is paralyzed, but the others gather around
 	if (checks & REST_SCATTER) {
 		if (!EveryoneNearPoint(area, leader->Pos, 0)) {
@@ -1792,7 +1792,7 @@ bool Game::RestParty(int checks, int dream, int hp)
 		return false;
 	}
 
-	Actor *leader = GetPC(0, true);
+	const Actor *leader = GetPC(0, true);
 	assert(leader);
 	// TODO: implement "rest until healed", it's an option in some games
 	int hours = 8;
@@ -1923,7 +1923,7 @@ inline static int CastOnRestHealingAmount(Actor *caster, SpecialSpellType &speci
 }
 
 // heal on rest and similar
-void Game::CastOnRest()
+void Game::CastOnRest() const
 {
 	typedef std::vector<HealingResource> RestSpells;
 	typedef std::vector<Injured> RestTargets;
@@ -2037,7 +2037,7 @@ void Game::TimeStop(Actor* owner, ieDword end)
 }
 
 // check if the passed actor is a victim of timestop
-bool Game::TimeStoppedFor(const Actor* target)
+bool Game::TimeStoppedFor(const Actor* target) const
 {
 	if (!timestop_owner) {
 		return false;
@@ -2052,7 +2052,7 @@ bool Game::TimeStoppedFor(const Actor* target)
 void Game::Infravision()
 {
 	hasInfra = false;
-	Map *map = GetCurrentArea();
+	const Map *map = GetCurrentArea();
 	if (!map) return;
 
 	ieDword tmp = 0;
@@ -2092,7 +2092,7 @@ static const Color DarkTint={0x80,0x80,0xe0,0x10};     //slightly dark bluish
 
 const Color *Game::GetGlobalTint() const
 {
-	Map *map = GetCurrentArea();
+	const Map *map = GetCurrentArea();
 	if (!map) return NULL;
 	if (map->AreaFlags&AF_DREAM) {
 		return &DreamTint;
@@ -2133,7 +2133,7 @@ void Game::ApplyGlobalTint(Color &tint, ieDword &flags) const
 	}
 }
 
-bool Game::IsDay()
+bool Game::IsDay() const
 {
 	ieDword daynight = core->Time.GetHour(GameTime);
 	if(daynight<4 || daynight>20) {
@@ -2294,7 +2294,7 @@ void Game::dump() const
 	Log(DEBUG, "Game", buffer);
 }
 
-Actor *Game::GetActorByGlobalID(ieDword globalID)
+Actor *Game::GetActorByGlobalID(ieDword globalID) const
 {
 	for (auto map : Maps) {
 		Actor *actor = map->GetActorByGlobalID(globalID);
@@ -2347,7 +2347,7 @@ bool Game::RandomEncounter(ieResRef &BaseArea)
 	return gamedata->Exists(BaseArea, IE_ARE_CLASS_ID);
 }
 
-void Game::ResetPartyCommentTimes()
+void Game::ResetPartyCommentTimes() const
 {
 	for (auto pc : PCs) {
 		pc->ResetCommentTime();

gemrb/core/GameScript/Actions.cpp
@@ -567,7 +567,7 @@ void GameScript::JumpToObject(Scriptable* Sender, Action* parameters)
 
 void GameScript::TeleportParty(Scriptable* /*Sender*/, Action* parameters)
 {
-	Game *game = core->GetGame();
+	const Game *game = core->GetGame();
 	int i = game->GetPartySize(false);
 	while (i--) {
 		Actor *tar = game->GetPC(i, false);
@@ -636,7 +636,7 @@ void GameScript::ExitPocketPlane(Scriptable* /*Sender*/, Action* /*parameters*/)
 //moves pcs and npcs from an area to another area
 void GameScript::MoveGlobalsTo(Scriptable* /*Sender*/, Action* parameters)
 {
-	Game *game = core->GetGame();
+	const Game *game = core->GetGame();
 	int i = game->GetPartySize(false);
 	while (i--) {
 		Actor *tar = game->GetPC(i, false);

gemrb/core/GameScript/GSUtils.cpp
@@ -2208,7 +2208,7 @@ ieDword CheckVariable(const Scriptable *Sender, const char *VarName, bool *valid
 		ScriptDebugLog(ID_VARIABLES, "CheckVariable %s: %d", VarName, value);
 		return value;
 	}
-	Game *game = core->GetGame();
+	const Game *game = core->GetGame();
 	if (HasKaputz && !stricmp(newVarName,"KAPUTZ") ) {
 		game->kaputz->Lookup( poi, value );
 		ScriptDebugLog(ID_VARIABLES, "CheckVariable %s: %d", VarName, value);
@@ -2251,7 +2251,7 @@ ieDword CheckVariable(const Scriptable *Sender, const char *VarName, const char 
 		ScriptDebugLog(ID_VARIABLES, "CheckVariable %s%s: %d", Context, VarName, value);
 		return value;
 	}
-	Game *game = core->GetGame();
+	const Game *game = core->GetGame();
 	if (HasKaputz && !stricmp(newVarName,"KAPUTZ") ) {
 		game->kaputz->Lookup( VarName, value );
 		ScriptDebugLog(ID_VARIABLES, "CheckVariable %s%s: %d", Context, VarName, value);
@@ -2280,7 +2280,7 @@ bool VariableExists(Scriptable *Sender, const char *VarName, const char *Context
 	ieDword value = 0;
 	char newVarName[8];
 	strlcpy(newVarName, Context, 7);
-	Game *game = core->GetGame();
+	const Game *game = core->GetGame();
 
 	if (Sender->GetCurrentArea()->locals->Lookup(VarName, value)) {
 		return true;

gemrb/core/GameScript/Matching.cpp
@@ -333,7 +333,7 @@ Scriptable *GetActorFromObject(Scriptable *Sender, const Object *oC, int ga_flag
 {
 	Scriptable *aC = NULL;
 
-	Game *game = core->GetGame();
+	const Game *game = core->GetGame();
 	Targets *tgts = GetAllObjects(Sender->GetCurrentArea(), Sender, oC, ga_flags);
 	if (tgts) {
 		//now this could return other than actor objects

gemrb/core/Interface.cpp
@@ -3808,7 +3808,7 @@ void Interface::SetCutSceneMode(bool active)
 /** returns true if in dialogue or cutscene */
 bool Interface::InCutSceneMode() const
 {
-	GameControl *gc = GetGameControl();
+	const GameControl *gc = GetGameControl();
 	if (!gc || (gc->GetDialogueFlags()&DF_IN_DIALOG) || (gc->GetScreenFlags()&(SF_DISABLEMOUSE|SF_CUTSCENE))) {
 		return true;
 	}
@@ -5461,7 +5461,7 @@ int Interface::GetReputationMod(int column) const
 
 PauseSetting Interface::TogglePause()
 {
-	GameControl *gc = GetGameControl();
+	const GameControl *gc = GetGameControl();
 	if (!gc) return PAUSE_OFF;
 	PauseSetting pause = (PauseSetting)(~gc->GetDialogueFlags()&DF_FREEZE_SCRIPTS);
 	if (SetPause(pause)) return pause;

commit e8bbca70d20a578b07f9d401c1d9f87fefb6970c

Game: fixed regression from e1b7b90a
fixes #1029

gemrb/core/Game.h
@@ -257,7 +257,7 @@ private:
 	std::vector<std::vector<char *> > npclevels;
 	int *bntchnc;
 	int bntrows;
-	CRRow *crtable;
+	CRRow *crtable = nullptr;
 	ieResRef restmovies[8];
 	ieResRef daymovies[8];
 	ieResRef nightmovies[8];