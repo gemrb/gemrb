f8b2f03f1058bc3956edd5f9705cf30c98b9dd65
m7600 <56371872+m7600@users.noreply.github.com>
2020-11-13 16:55:36 -0300

    Add files via upload

--- gemrb/GUIScripts/demo/Start.py
+++ gemrb/GUIScripts/demo/Start.py
@@ -1,9 +1,42 @@
 import GemRB
 
+StartWindow = 0
+
 def OnLoad():
+	global StartWindow
+
 	GemRB.LoadGame(None)
 
-	# this is needed, so the game loop runs and the load happens
-	# before other code (eg. CreatePlayer) depending on it is run
-	GemRB.SetNextScript("SetupGame")
+	GemRB.LoadWindowPack("GUICONN", 800, 600)
 
+	StartWindow = GemRB.LoadWindow(0)
+
+	NewGameButton = StartWindow.GetControl(0x00)
+
+	QuitGameButton = StartWindow.GetControl(0x01)
+
+	NewGameButton.SetStatus(IE_GUI_BUTTON_ENABLED)
+
+	QuitGameButton.SetStatus(IE_GUI_BUTTON_ENABLED)
+
+	NewGameButton.SetEvent(IE_GUI_BUTTON_ON_PRESS, NewGamePress)
+	QuitGameButton.SetEvent(IE_GUI_BUTTON_ON_PRESS, QuitPress)
+
+	StartWindow.SetVisible(WINDOW_VISIBLE)
+
+	return
+
+def QuitPress():
+	global StartWindow
+	if StartWindow:
+		StartWindow.Unload()
+	GemRB.QuitGame()
+	GemRB.Quit()
+	return
+
+def NewGamePress():
+	global StartWindow
+	if StartWindow:
+		StartWindow.Unload()
+	GemRB.SetNextScript("SetupGame")
+	return
