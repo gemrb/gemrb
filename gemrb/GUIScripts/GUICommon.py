430c30561e3c6fd04eaefb4874f02ed69ae8ae85
Laurie Chilvers <chilvence@gmail.com>
2020-11-17 03:22:35 +0000

    GUICommon.IsDualSwap: Use Dual[1] to lookup kit parent class
    The call to GetKitIndex is already made in IsDualClassed, so this is redundant

--- gemrb/GUIScripts/GUICommon.py
+++ gemrb/GUIScripts/GUICommon.py
@@ -515,8 +515,7 @@ def IsDualSwap (actor, override=None):
 	if Dual[0] > 1:
 		BaseClass = GetClassRowName(Dual[1], "index")
 	else:
-		BaseClass = GetKitIndex (actor)
-		BaseClass = CommonTables.KitList.GetValue (BaseClass, 7)
+		BaseClass = CommonTables.KitList.GetValue (Dual[1], 7)
 		if BaseClass == "*":
 			# mod boilerplate
 			return 0
