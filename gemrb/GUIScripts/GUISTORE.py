e4083230b90ffe678153971066357dd8ae284872
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2020-12-06 18:03:49 +0100

    GUISTORE: let the donation coin drop animation play
    should stick with the last frame too, but I haven't managed to achieve it
    just with flag bits

--- gemrb/GUIScripts/GUISTORE.py
+++ gemrb/GUIScripts/GUISTORE.py
@@ -1824,6 +1824,7 @@ def DonateGold ():
 	TextArea.SetFlags (IE_GUI_TEXTAREA_AUTOSCROLL)
 
 	Button = Window.GetControl (10)
+	Button.SetFlags (IE_GUI_BUTTON_PLAYALWAYS, OP_OR)
 	if Button:
 		Button.SetAnimation ("DONATE")
 
