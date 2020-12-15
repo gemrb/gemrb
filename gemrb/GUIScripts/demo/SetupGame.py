70ed14320f875e71f57b0b0c95e3da90bae018d3
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2020-12-15 11:29:29 +0100

    demo: CHARNAME doesn't use a token

--- gemrb/GUIScripts/demo/SetupGame.py
+++ gemrb/GUIScripts/demo/SetupGame.py
@@ -4,12 +4,12 @@ from GUIDefines import GS_PARTYAI
 def OnLoad():
 	# skipping chargen
 	GemRB.CreatePlayer ("protagon", 1|0x8000) # exportable
+	GemRB.SetPlayerName (1, "Seeker", 0)
 
 	# setup character colors (IE_MAJOR_COLOR and so on)
 	statcolors = [ (211, 202116108), (214, 0), (210, 791621423), (209, 960051513), (208, 454761243), (212, 370546198),(213, 387389207) ]
 	for (stat, color) in statcolors:
 		GemRB.SetPlayerStat (1, stat, color)
 
-	GemRB.SetToken ("CHARNAME", "Seeker")
 	GemRB.GameSetScreenFlags (GS_PARTYAI, OP_OR)
 	GemRB.EnterGame()
