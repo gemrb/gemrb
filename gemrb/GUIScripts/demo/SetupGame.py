90b24a9082b5a4db087f0584e0120b4a72879c90
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2020-12-17 22:34:26 +0100

    demo: added a few gradients to fix clown colors
    the way zhmw uses major and minor is not ideal, though
    
    fixes #1045

--- gemrb/GUIScripts/demo/SetupGame.py
+++ gemrb/GUIScripts/demo/SetupGame.py
@@ -6,10 +6,5 @@ def OnLoad():
 	GemRB.CreatePlayer ("protagon", 1|0x8000) # exportable
 	GemRB.SetPlayerName (1, "Seeker", 0)
 
-	# setup character colors (IE_MAJOR_COLOR and so on)
-	statcolors = [ (211, 202116108), (214, 0), (210, 791621423), (209, 960051513), (208, 454761243), (212, 370546198),(213, 387389207) ]
-	for (stat, color) in statcolors:
-		GemRB.SetPlayerStat (1, stat, color)
-
 	GemRB.GameSetScreenFlags (GS_PARTYAI, OP_OR)
 	GemRB.EnterGame()
