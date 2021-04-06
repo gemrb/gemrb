gemrb/gemrb

commit a8de141f50e9106910f35a87f2554625de12d33b

WorldMapControl: use SetVarAssoc + ValueChange
we can resolve #1145 simply by using the existing control infrastructure
and adding a window close handler to set the Travel var back to -1

gemrb/GUIScripts/pst/GUIMA.py
@@ -37,7 +37,7 @@ def InitMapWindow (Window):
 	# World Map
 	Button = Window.GetControl (0)
 	Button.SetText (20429)
-	Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, lambda: OpenTravelWindow (False))
+	Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, lambda: OpenTravelWindow())
 
 	# Add Note
 	Button = Window.GetControl (1)
@@ -82,7 +82,7 @@ def InitMapWindow (Window):
 
 	return
 
-def OpenTravelWindow (Travel = True):
+def OpenTravelWindow ():
 	global WorldMapControl
 
 	GUICommonWindows.DisableAnimatedWindows ()
@@ -90,13 +90,15 @@ def OpenTravelWindow (Travel = True):
 	Travel = GemRB.GetVar ("Travel")
 
 	Window = GemRB.LoadWindow (0, "GUIWMAP")
+	Window.SetAction(lambda: GemRB.SetVar("Travel", -1), ACTION_WINDOW_CLOSED)
 
-	WorldMapControl = WMap = Window.ReplaceSubview (4, IE_GUI_WORLDMAP, Travel, "FONTDLG")
+	WorldMapControl = WMap = Window.ReplaceSubview (4, IE_GUI_WORLDMAP, "FONTDLG")
 	WMap.SetTextColor (IE_GUI_WMAP_COLOR_BACKGROUND, {'r' : 0x84, 'g' : 0x4a, 'b' : 0x2c, 'a' : 0x00})
 	WMap.SetTextColor (IE_GUI_WMAP_COLOR_NORMAL, {'r' : 0x20, 'g' : 0x20, 'b' : 0x00, 'a' : 0xff})
 	WMap.SetTextColor (IE_GUI_WMAP_COLOR_SELECTED, {'r' : 0x20, 'g' : 0x20, 'b' : 0x00, 'a' : 0xff})
 	WMap.SetTextColor (IE_GUI_WMAP_COLOR_NOTVISITED, {'r' : 0x20, 'g' : 0x20, 'b' : 0x00, 'a' : 0xa0})
 	WMap.SetAnimation ("WMPTY")
+	WMap.SetVarAssoc("Travel", GemRB.GetVar("Travel"))
 	#center on current area
 	WMap.Scroll (0,0)
 	WMap.Focus()

gemrb/GUIScripts/GUIMA.py
@@ -78,8 +78,12 @@ def InitMapWindow (Window):
 	MapWindow = Window
 
 	# World Map
+	def OpenWorldMap():
+		GemRB.SetVar("Travel", -1)
+		OpenTravelWindow()
+
 	Button = Window.GetControl (1)
-	Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, lambda: OpenWorldMapWindow())
+	Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, OpenWorldMap)
 
 	# Hide or Show mapnotes
 	if HasMapNotes ():
@@ -109,14 +113,85 @@ def InitMapWindow (Window):
 	return
 	
 def InitWorldMapWindow (Window):
-	WorldMapWindowCommon (Window, -1)
+	global WorldMapControl
+
+	Window.SetFlags(WF_ALPHA_CHANNEL, OP_NAND)
+	Window.SetAction(lambda: GemRB.SetVar("Travel", -1), ACTION_WINDOW_CLOSED)
+
+	if GameCheck.IsBG2():
+		WorldMapControl = Window.ReplaceSubview (4, IE_GUI_WORLDMAP, "floattxt")
+	elif GameCheck.IsBG1():
+		WorldMapControl = Window.ReplaceSubview (4, IE_GUI_WORLDMAP, "toolfont", 1, True)
+		WorldMapControl.SetTextColor (IE_GUI_WMAP_COLOR_BACKGROUND, {'r' : 0xa4, 'g' : 0x6a, 'b' : 0x4c})
+	else:
+		WorldMapControl = Window.ReplaceSubview (4, IE_GUI_WORLDMAP, "infofont")
+
+	WorldMapControl.SetVarAssoc("Travel", GemRB.GetVar("Travel"))
+	WorldMapControl.SetAnimation ("WMDAG")
+	WorldMapControl.SetEvent (IE_GUI_WORLDMAP_ON_PRESS, GUIMACommon.MoveToNewArea)
+	WorldMapControl.SetAction(ChangeTooltip, IE_ACT_MOUSE_ENTER)
+	# center on current area
+	MapC()
+
+	if not GameCheck.IsIWD2():
+		#north
+		Button = Window.GetControl (1)
+		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapN)
+		Button.SetActionInterval (200)
+
+		#south
+		Button = Window.GetControl (2)
+		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapS)
+		Button.SetActionInterval (200)
+
+	if GameCheck.IsBG2():
+		#northwest
+		Button = Window.GetControl (8)
+		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapNW)
+		Button.SetActionInterval (200)
+
+		#northeast
+		Button = Window.GetControl (9)
+		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapNE)
+		Button.SetActionInterval (200)
+
+		#west
+		Button = Window.GetControl (10)
+		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapW)
+		Button.SetActionInterval (200)
+
+		#center
+		Button = Window.GetControl (11)
+		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapC)
+		Button.SetActionInterval (200)
+
+		#east
+		Button = Window.GetControl (12)
+		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapE)
+		Button.SetActionInterval (200)
+
+		#southwest
+		Button = Window.GetControl (13)
+		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapSW)
+		Button.SetActionInterval (200)
+
+		#southeast
+		Button = Window.GetControl (14)
+		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapSE)
+		Button.SetActionInterval (200)
+
+	# Done
+	Button = Window.GetControl (0)
+	Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, lambda: OpenMapWindow ())
+	Button.SetHotKey('m')
+
 	return
 
 ToggleMapWindow = GUICommonWindows.CreateTopWinLoader(2, "GUIMAP", GUICommonWindows.ToggleWindow, InitMapWindow)
 OpenMapWindow = GUICommonWindows.CreateTopWinLoader(2, "GUIMAP", GUICommonWindows.OpenWindowOnce, InitMapWindow)
 
 WMWID = 2 if GameCheck.IsIWD2 () else 0
-OpenWorldMapWindow = GUICommonWindows.CreateTopWinLoader(WMWID, "GUIWMAP", GUICommonWindows.OpenWindowOnce, InitWorldMapWindow)
+OpenTravelWindow = GUICommonWindows.CreateTopWinLoader(WMWID, "GUIWMAP", GUICommonWindows.OpenWindowOnce, InitWorldMapWindow)
 
 def HasMapNotes ():
 	return GameCheck.IsBG2() or GameCheck.IsIWD2() or GameCheck.IsPST()
@@ -185,11 +260,6 @@ def AddNoteWindow ():
 	NoteWindow.ShowModal (MODAL_SHADOW_GRAY)
 	NoteLabel.Focus()
 	return
-	
-def OpenTravelWindow ():
-	Window = OpenWorldMapWindow()
-	WorldMapWindowCommon (Window, GemRB.GetVar ("Travel"))
-	return
 
 def ChangeTooltip ():
 	global WorldMapControl
@@ -202,79 +272,6 @@ def ChangeTooltip ():
 			tt = "%s: %d"%(str,area["Distance"])
 
 	WorldMapControl.SetTooltip (tt)
-	return
-
-def WorldMapWindowCommon (Window, Travel):
-	global WorldMapControl
-
-	Window.SetFlags(WF_ALPHA_CHANNEL, OP_NAND)
-
-	if GameCheck.IsBG2():
-		WorldMapControl = Window.ReplaceSubview (4, IE_GUI_WORLDMAP, Travel, "floattxt")
-	elif GameCheck.IsBG1():
-		WorldMapControl = Window.ReplaceSubview (4, IE_GUI_WORLDMAP, Travel, "toolfont", 1, True)
-		WorldMapControl.SetTextColor (IE_GUI_WMAP_COLOR_BACKGROUND, {'r' : 0xa4, 'g' : 0x6a, 'b' : 0x4c})
-	else:
-		WorldMapControl = Window.ReplaceSubview (4, IE_GUI_WORLDMAP, Travel, "infofont")
-
-	WorldMapControl.SetAnimation ("WMDAG")
-	WorldMapControl.SetEvent (IE_GUI_WORLDMAP_ON_PRESS, GUIMACommon.MoveToNewArea)
-	WorldMapControl.SetAction(ChangeTooltip, IE_ACT_MOUSE_ENTER)
-	# center on current area
-	MapC()
-
-	if not GameCheck.IsIWD2():
-		#north
-		Button = Window.GetControl (1)
-		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapN)
-		Button.SetActionInterval (200)
-
-		#south
-		Button = Window.GetControl (2)
-		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapS)
-		Button.SetActionInterval (200)
-
-	if GameCheck.IsBG2():
-		#northwest
-		Button = Window.GetControl (8)
-		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapNW)
-		Button.SetActionInterval (200)
-
-		#northeast
-		Button = Window.GetControl (9)
-		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapNE)
-		Button.SetActionInterval (200)
-
-		#west
-		Button = Window.GetControl (10)
-		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapW)
-		Button.SetActionInterval (200)
-
-		#center
-		Button = Window.GetControl (11)
-		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapC)
-		Button.SetActionInterval (200)
-
-		#east
-		Button = Window.GetControl (12)
-		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapE)
-		Button.SetActionInterval (200)
-
-		#southwest
-		Button = Window.GetControl (13)
-		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapSW)
-		Button.SetActionInterval (200)
-
-		#southeast
-		Button = Window.GetControl (14)
-		Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, MapSE)
-		Button.SetActionInterval (200)
-
-	# Done
-	Button = Window.GetControl (0)
-	Button.SetEvent (IE_GUI_BUTTON_ON_PRESS, lambda: OpenMapWindow ())
-	Button.SetHotKey('m')
-
 	return
 
 def MapN():

gemrb/GUIScripts/demo/GUIMA.py
@@ -29,7 +29,7 @@ from GUIDefines import *
 WorldMapControl = None
 AreaMapControl = None
 
-def InitMapWindow (Window, WorldMap = False, Travel = -1):
+def InitMapWindow (Window, WorldMap = False):
 	global WorldMapControl, AreaMapControl
 
 	Label = Window.GetControl (0)
@@ -48,6 +48,8 @@ def InitMapWindow (Window, WorldMap = False, Travel = -1):
 
 	# Map or World Map control
 	if WorldMap:
+		Window.SetAction(lambda: GemRB.SetVar("Travel", -1), ACTION_WINDOW_CLOSED)
+
 		if AreaMapControl:
 			AreaMapControl.SetVisible (False)
 			AreaMapControl.SetDisabled (True)
@@ -60,6 +62,7 @@ def InitMapWindow (Window, WorldMap = False, Travel = -1):
 			WorldMapControl.SetAnimation ("WMDAG")
 			WorldMapControl.SetEvent (IE_GUI_WORLDMAP_ON_PRESS, GUIMACommon.MoveToNewArea)
 
+		WorldMapControl.SetVarAssoc("Travel", GemRB.GetVar("Travel"))
 		# center on current area
 		WorldMapControl.Scroll (0, 0, False)
 		WorldMapControl.Focus ()
@@ -102,5 +105,5 @@ OpenMapWindow = GUICommonWindows.CreateTopWinLoader (0, "GUIMAP", GUICommonWindo
 
 def OpenTravelWindow ():
 	Window = OpenMapWindow ()
-	InitMapWindow (Window, True, GemRB.GetVar ("Travel"))
+	InitMapWindow (Window, True)
 	return

gemrb/core/GUI/WorldMapControl.cpp
@@ -33,12 +33,10 @@
 
 namespace GemRB {
 
-WorldMapControl::WorldMapControl(const Region& frame, Font *font, int direction)
+WorldMapControl::WorldMapControl(const Region& frame, Font *font)
 	: Control(frame), ftext(font)
 {
 	ControlType = IE_GUI_WORLDMAP;
-	Area = NULL;
-	SetValue(direction);
 	SetCursor(core->Cursors[IE_CURSOR_GRAB]);
 	OverrideIconPalette = false;
 	Game* game = core->GetGame();
@@ -58,11 +56,16 @@ WorldMapControl::WorldMapControl(const Region& frame, Font *font, int direction)
 			CopyResRef(currentArea, m->AreaResRef);
 		}
 	}
-
-	//this also updates visible locations
-	worldmap->CalculateDistances(currentArea, direction);
 	
 	SetColor(IE_GUI_WMAP_COLOR_BACKGROUND, ColorBlack);
+	
+	ControlEventHandler handler = [this](Control* /*this*/) {
+		//this also updates visible locations
+		WorldMap* worldmap = core->GetWorldMap();
+		worldmap->CalculateDistances(currentArea, GetValue());
+	};
+	
+	SetAction(handler, Control::ValueChange);
 }
 
 /** Draws the Control on the Output Display */

gemrb/core/GUI/WorldMapControl.h
@@ -58,7 +58,7 @@ private:
 	void DrawSelf(Region drawFrame, const Region&) override;
 
 public:
-	WorldMapControl(const Region& frame, Font *font, int direction);
+	WorldMapControl(const Region& frame, Font *font);
 
 	/** Allows modification of the scrolling factor from outside */
 	void ScrollDelta(const Point& delta) override;
@@ -70,7 +70,7 @@ public:
 	void SetOverrideIconPalette(bool ipOverride) { OverrideIconPalette = ipOverride; };
 	Point Pos;
 	/** pointer to last pointed area */
-	WMPAreaEntry *Area;
+	WMPAreaEntry *Area = nullptr;
 
 protected:
 	/** Mouse Over Event */

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -2124,13 +2124,13 @@ static PyObject* GemRB_CreateView(PyObject * /*self*/, PyObject* args)
 			break;
 		case IE_GUI_WORLDMAP:
 		{
-			int direction, recolor = 0;
+			int recolor = 0;
 			char *fontname = nullptr;
 			int paletteOverride = false;
-			PARSE_ARGS( constructArgs, "i|sii", &direction, &fontname, &recolor, &paletteOverride);
+			PARSE_ARGS(constructArgs, "|sii", &fontname, &recolor, &paletteOverride);
 			
 			Font* font = (fontname) ? core->GetFont(fontname) : nullptr;
-			WorldMapControl* wmap = new WorldMapControl(rgn, font, direction );
+			WorldMapControl* wmap = new WorldMapControl(rgn, font);
 			wmap->SetOverrideIconPalette(paletteOverride);
 			view = wmap;
 		}

commit c58de5d620a4aac3cfac914a5cc2e8f816eb5353

pst: fixed regressions from a8de141 and remainder of #1071
with a start value of 0, values never changed

gemrb/GUIScripts/GUIMACommon.py
@@ -32,6 +32,10 @@ def MoveToNewArea ():
 
 	hours = travel["Distance"]
 	GUICommonWindows.CloseTopWindow ()
+	if GemRB.GetView ("WIN_PSTWMAP"):
+		GemRB.GetView ("WIN_PSTWMAP").Close ()
+		GemRB.SetVar ("Travel", -1)
+		GemRB.GamePause (0, 0)
 
 	if travel["Destination"].lower() == GemRB.GetGameString(STR_AREANAME).lower():
 		return

gemrb/GUIScripts/pst/GUIMA.py
@@ -90,17 +90,18 @@ def OpenTravelWindow ():
 	Travel = GemRB.GetVar ("Travel")
 
 	Window = GemRB.LoadWindow (0, "GUIWMAP")
+	Window.AddAlias ("WIN_PSTWMAP")
 
 	WorldMapControl = WMap = Window.ReplaceSubview (4, IE_GUI_WORLDMAP, "FONTDLG")
 	WMap.SetTextColor (IE_GUI_WMAP_COLOR_NORMAL, {'r' : 0x20, 'g' : 0x20, 'b' : 0x00, 'a' : 0xff})
 	WMap.SetTextColor (IE_GUI_WMAP_COLOR_SELECTED, {'r' : 0x20, 'g' : 0x20, 'b' : 0x00, 'a' : 0xff})
 	WMap.SetTextColor (IE_GUI_WMAP_COLOR_NOTVISITED, {'r' : 0x20, 'g' : 0x20, 'b' : 0x00, 'a' : 0xa0})
 	WMap.SetAnimation ("WMPTY")
-	WMap.SetVarAssoc("Travel", GemRB.GetVar("Travel"))
+	WMap.SetVarAssoc ("Travel", Travel)
 	#center on current area
 	WMap.Scroll (0,0)
 	WMap.Focus()
-	if Travel:
+	if Travel != -1:
 		WMap.SetEvent (IE_GUI_WORLDMAP_ON_PRESS, GUIMACommon.MoveToNewArea)
 
 	# Done

gemrb/core/GUI/GameControl.cpp
@@ -2269,6 +2269,7 @@ void GameControl::PerformSelectedAction(const Point& p)
 					game->selected[i]->UseExit(exitID);
 				}
 			}
+			CommandSelectedMovement(p);
 		}
 		if (HandleActiveRegion(overInfoPoint, selectedActor, p)) {
 			core->SetEventFlag(EF_RESETTARGET);

gemrb/core/GameScript/Actions.cpp
@@ -2298,7 +2298,7 @@ void GameScript::NIDSpecial2(Scriptable* Sender, Action* /*parameters*/)
 		// FIXME: not ideal, pst uses the infopoint links (ip->EntranceName), so direction doesn't matter
 		// but we're not travelling through them (the whole point of the world map), so how to pick a good entrance?
 		// DestEntryPoint is all zeroes, pst just didn't use it
-		direction = 0;
+		direction = 1;
 	}
 	core->GetDictionary()->SetAt("Travel", (ieDword) direction);
 	core->GetGUIScriptEngine()->RunFunction( "GUIMA", "OpenTravelWindow" );