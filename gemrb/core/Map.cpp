gemrb/gemrb

commit 91eb0610d2b115879421e0ec597b74fc74c5a2af

Map: don't bother attempting to draw shrouded actors

gemrb/core/Map.cpp
@@ -1388,19 +1388,31 @@ void Map::DrawMap(const Region& viewport, uint32_t dFlags)
 	while (actor || a || sca || spark || pro || pile) {
 		switch(SelectObject(actor,q,a,sca,spark,pro,pile)) {
 		case AOT_ACTOR:
-			assert(actor != NULL);
-			if (actor->UpdateDrawingState()) {
-				uint32_t flags = SetDrawingStencilForScriptable(actor, viewport);
-				// when time stops, almost everything turns dull grey, the caster and immune actors being the most notable exceptions
-				if (game->TimeStoppedFor(actor)) {
-					flags |= BLIT_GREY;
+			{
+				bool visible = false;
+				// always update the animations even if we arent visible
+				if (actor->UpdateDrawingState() && IsExplored(actor->Pos)) {
+					// apparently birds and the dead are always visible?
+					visible = IsVisible(actor->Pos) || (actor->Modified[IE_DONOTJUMP] & DNJ_BIRD) || (actor->GetInternalFlag() & IF_REALLYDIED);
+					if (visible) {
+						uint32_t flags = SetDrawingStencilForScriptable(actor, viewport);
+						if (game->TimeStoppedFor(actor)) {
+							// when time stops, almost everything turns dull grey,
+							// the caster and immune actors being the most notable exceptions
+							flags |= BLIT_GREY;
+						}
+						actor->Draw(viewport, flags|BLIT_BLENDED);
+					}
 				}
-				actor->Draw(viewport, flags|BLIT_BLENDED);
-			} else {
-				actor->SetInternalFlag(IF_TRIGGER_AP, OP_NAND);
-			}
+				
+				if (!visible) {
+					actor->SetInternalFlag(IF_TRIGGER_AP, OP_NAND);
+					//turning actor inactive if there is no action next turn
+					actor->HibernateIfAble();
+				}
 
-			actor = GetNextActor(q, index);
+				actor = GetNextActor(q, index);
+			}
 			break;
 		case AOT_PILE:
 			// draw piles

gemrb/core/Scriptable/Actor.cpp
@@ -8593,30 +8593,11 @@ uint8_t Actor::GetElevation() const
 
 bool Actor::UpdateDrawingState()
 {
-	const Map* area = GetCurrentArea();
-	if (!area) {
-		return false;
-	}
-	
 	if (!AdvanceAnimations()) {
 		return false;
 	}
 	
 	UpdateDrawingRegion();
-
-	int explored = Modified[IE_DONOTJUMP]&DNJ_UNHINDERED;
-	bool visible = (explored) ? area->IsExplored(Pos) : area->IsVisible(Pos);
-	//check the deactivation condition only if needed
-	//this fixes dead actors disappearing from fog of war (they should be permanently visible)
-	if ((!visible || (InternalFlags & IF_REALLYDIED)) && (InternalFlags & IF_ACTIVE) ) {
-		//turning actor inactive if there is no action next turn
-		HibernateIfAble();
-		if (!(InternalFlags&IF_REALLYDIED)) {
-			// for a while this didn't return (disable drawing) if about to hibernate;
-			// Avenger said (aa10aaed) "we draw the actor now for the last time".
-			return false;
-		}
-	}
 
 	// if an actor isn't visible, should we still draw video cells?
 	// let us assume not, for now..

commit 2ac2dbf0ff7d1ddd2b291b5d5d30934210b61df5

Map::DrawMap: fixed regression from 91eb061 #1222
deaders didn't get a chance to idle

gemrb/core/Map.cpp
@@ -1432,8 +1432,8 @@ void Map::DrawMap(const Region& viewport, uint32_t dFlags)
 						actor->Draw(viewport, baseTint, tint, flags|BLIT_BLENDED);
 					}
 				}
-				
-				if (!visible) {
+
+				if (!visible || (actor->GetInternalFlag() & (IF_REALLYDIED|IF_ACTIVE)) == (IF_REALLYDIED|IF_ACTIVE)) {
 					actor->SetInternalFlag(IF_TRIGGER_AP, OP_NAND);
 					//turning actor inactive if there is no action next turn
 					actor->HibernateIfAble();