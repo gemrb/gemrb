71bd17faea046535f184c3b224746067f2c6ba23
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2021-05-24 21:37:58 +0200

    use named constants for the TextArea colors in python as well

--- gemrb/GUIScripts/InventoryCommon.py
+++ gemrb/GUIScripts/InventoryCommon.py
@@ -268,7 +268,7 @@ def DisplayItem (slotItem, type):
 	#textarea
 	Text = Window.GetControl (5)
 	if GameCheck.IsBG2(): # I believe only BG2 has special initials
-		Text.SetColor({'r' : 255, 'g' : 255, 'b' : 255, 'a' : 255}, 1)
+		Text.SetColor ({'r' : 255, 'g' : 255, 'b' : 255, 'a' : 255}, TA_COLOR_INITIALS)
 	if (type&2):
 		text = item["ItemDesc"]
 	else:
--- gemrb/GUIScripts/iwd2/GUIREC.py
+++ gemrb/GUIScripts/iwd2/GUIREC.py
@@ -1003,7 +1003,7 @@ def UpdateRecordsWindow (Window):
 
 	RecordsTextArea = Window.GetControl (45)
 	RecordsTextArea.SetText ("")
-	RecordsTextArea.SetColor({'r' : 255, 'g' : 255, 'b' : 255, 'a' : 255}, 1)
+	RecordsTextArea.SetColor ({'r' : 255, 'g' : 255, 'b' : 255, 'a' : 255}, TA_COLOR_INITIALS)
 
 	DisplayCommon (pc)
 
