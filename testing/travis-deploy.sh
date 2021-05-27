09812b2276368842f754d72b934f93f154aa74a1
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2021-05-27 10:12:54 +0200

    travis-deploy.sh: removed obsolete stuff
    mac builds have been done on github actions for a while now

--- testing/travis-deploy.sh
+++ testing/travis-deploy.sh
@@ -4,12 +4,7 @@
 # uploads builds versioned by "git describe"
 
 sshid=../id_travissfbot
-if [[ $TRAVIS_OS_NAME == osx ]]; then
-  filepath=Apple/OSX
-  tarpath=/Applications/GemRB.app
-  # this can be removed in 2016 (the bug was fixed and just needs to be deployed)
-  ssh-keyscan -t rsa,dsa -H frs.sourceforge.net 2>&1 | tee -a $HOME/.ssh/known_hosts
-elif [[ $TRAVIS_OS_NAME == linux ]]; then
+if [[ $TRAVIS_OS_NAME == linux ]]; then
   filepath=Linux
   tarpath=iprefix
 else
@@ -25,10 +20,3 @@ tar cjf $file $tarpath || exit 15
 filepath="$filepath/$file"
 scp -i $sshid $file \
  gemrb-travisbot@frs.sourceforge.net:/home/frs/project/gemrb/Buildbot\\\ Binaries/$filepath
-
-# No need to make anything a default download
-# candidates: "default=windows&default=mac&default=linux&default=bsd&default=solaris&default=others"
-#curl -H "Accept: application/json" -X PUT -d "default=linux" \
-# -d "api_key=bot sf api key as per profile" \
-# https://sourceforge.net/projects/gemrb/files/Buildbot\ Binaries/$filepath
-
