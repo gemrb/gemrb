gemrb/gemrb

commit 32442ac0147a04f62009362fc274340d4fb0bc07

Projectile: fix line projectiles after recent lightning bolt changes

gemrb/core/Projectile.cpp
@@ -965,7 +965,8 @@ void Projectile::NextTarget(const Point &p)
 	}
 
 	int flags = (ExtFlags&PEF_BOUNCE) ? GL_REBOUND : GL_PASS;
-	path = area->GetLine(Pos, Destination, 1, Orientation, flags);
+	int stepping = (ExtFlags & PEF_BOUNCE) ? 1 : Speed;
+	path = area->GetLine(Pos, Destination, stepping, Orientation, flags);
 }
 
 void Projectile::SetTarget(const Point &p)

commit 530b31c10d02e23899399813a96953c0af705c8d

Projectile::NextTarget: fixed regression from 32442ac0
made eg. magic missile almost instant
scorcher and lightning still look fine after this

gemrb/core/Projectile.cpp
@@ -963,7 +963,7 @@ void Projectile::NextTarget(const Point &p)
 	}
 
 	int flags = (ExtFlags&PEF_BOUNCE) ? GL_REBOUND : GL_PASS;
-	int stepping = (ExtFlags & PEF_BOUNCE) ? 1 : Speed;
+	int stepping = (ExtFlags & PEF_LINE) ? Speed : 1;
 	path = area->GetLine(Pos, Destination, stepping, Orientation, flags);
 }
 