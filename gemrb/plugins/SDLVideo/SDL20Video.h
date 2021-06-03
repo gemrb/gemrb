c2483b94e8382e39a0b66183459fc7f121e4b862
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2021-06-03 23:33:03 +0200

    SDLPixelFormatFromBufferFormat: fix fallthrough comment triping checkers

--- gemrb/plugins/SDLVideo/SDL20Video.h
+++ gemrb/plugins/SDLVideo/SDL20Video.h
@@ -48,7 +48,7 @@ Uint32 SDLPixelFormatFromBufferFormat(Video::BufferFormat fmt, SDL_Renderer* ren
 			// the renderer will throw an error for such a format
 			// fall-through
 		case Video::DISPLAY:
-			// fall-though
+			// fall-through
 		case Video::DISPLAY_ALPHA:
 			if (renderer) {
 				// I looked at the SDL source code to determine that the format at index 0 is the default
