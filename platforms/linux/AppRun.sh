941c2260085d32e8f61840b75aa9c52a0f92049f
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2021-06-11 19:12:49 +0200

    cmake: use a custom apprun to fix python paths

---
+++ platforms/linux/AppRun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f -- "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+APPDIR="${APPDIR:-$SCRIPTPATH}"
+
+export PYTHONPATH="$APPDIR/usr/share/pyshared/"
+export PYTHONHOME="$APPDIR/usr/"
+
+"$APPDIR"/usr/bin/gemrb "$@"
