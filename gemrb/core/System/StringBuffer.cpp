gemrb/gemrb

commit 8cf878b033ac5bc4fe559ef8c9578d5899a74069

StringBuffer: dont use VLA extension
we dont need performance that badly

gemrb/core/System/StringBuffer.cpp
@@ -36,12 +36,9 @@ void StringBuffer::appendFormatted(const char* message, ...)
 	const size_t len = vsnprintf(NULL, 0, message, ap);
 	va_end(ap);
 
-#if defined(__GNUC__)
-	__extension__ // Variable-length arrays
-#endif
-	char buf[len+1];
+	std::string buf(len + 1, '\0');
 	va_start(ap, message);
-	vsnprintf(buf, len + 1, message, ap);
+	vsnprintf(&buf[0], len + 1, message, ap);
 	va_end(ap);
 
 	// TODO: If we manage the string ourselves, we can avoid this extra copy.

commit 6f6a70025e469127b1ed6707cad6e5a5fc8fa29b

StringBuffer::appendFormatted: not sure what's going on, but this fixes it
debug dumps were broken since 8cf878b0

gemrb/core/System/StringBuffer.cpp
@@ -43,7 +43,7 @@ void StringBuffer::appendFormatted(const char* message, ...)
 	va_end(ap);
 
 	// TODO: If we manage the string ourselves, we can avoid this extra copy.
-	buffer += buf;
+	buffer += buf.c_str();
 }
 
 void StringBuffer::append(const char* message)