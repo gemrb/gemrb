gemrb/gemrb

commit 362d55af3c1981de0fb842baf451354080035e3e

add some global templates to perform string -> numeric conversions
to fix a bunch of implicit conversion warnings

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -791,10 +791,10 @@ static PyObject* GemRB_Table_GetValue(PyObject* self, PyObject* args)
 		case 2:
 			return PyInt_FromLong( core->TranslateStat(ret) );
 		default:
-			long val;
-			bool valid = valid_number(ret, val);
+			ieStrRef val;
+			bool valid = valid_unsignednumber(ret, val);
 			if (type == 3) {
-				char* cstr = core->GetCString(ieStrRef(val));
+				char* cstr = core->GetCString(val);
 				PyObject* pystr = PyString_FromString(cstr);
 				free(cstr);
 				return pystr;

gemrb/core/CharAnimations.cpp
@@ -504,7 +504,7 @@ void CharAnimations::InitAvatarsTable() const
 	AvatarTable = (AvatarStruct *) calloc(AvatarsCount, sizeof(AvatarStruct));
 	const DataFileMgr *resdata = core->GetResDataINI();
 	for (int i = AvatarsCount - 1; i >= 0; i--) {
-		AvatarTable[i].AnimID=(unsigned int) strtol(Avatars->GetRowName(i),NULL,0 );
+		AvatarTable[i].AnimID = strtounsigned<unsigned int>(Avatars->GetRowName(i));
 		AvatarTable[i].Prefixes[0] = Avatars->QueryField(i, AV_PREFIX1);
 		AvatarTable[i].Prefixes[1] = Avatars->QueryField(i, AV_PREFIX2);
 		AvatarTable[i].Prefixes[2] = Avatars->QueryField(i, AV_PREFIX3);
@@ -555,18 +555,17 @@ void CharAnimations::InitAvatarsTable() const
 	if (blood) {
 		int rows = blood->GetRowCount();
 		for(int i=0;i<rows;i++) {
-			unsigned long value = 0;
-			unsigned long flags = 0;
-			unsigned long rmin = 0;
-			unsigned long rmax = 0xffff;
+			char value = 0;
+			unsigned int flags = 0;
+			unsigned int rmin = 0;
+			unsigned int rmax = 0xffff;
 
-			valid_number(blood->QueryField(i,0), (long &)value);
-			valid_number(blood->QueryField(i,1), (long &)rmin);
-			valid_number(blood->QueryField(i,2), (long &)rmax);
-			valid_number(blood->QueryField(i,3), (long &)flags);
-			if (value>255 || rmin>rmax || rmax>0xffff) {
-				Log(ERROR, "CharAnimations", "Invalid bloodclr entry: %02x %04x-%04x ",
-						(unsigned int) value, (unsigned int) rmin, (unsigned int) rmax);
+			valid_signednumber(blood->QueryField(i,0), value);
+			valid_unsignednumber(blood->QueryField(i,1), rmin);
+			valid_unsignednumber(blood->QueryField(i,2), rmax);
+			valid_unsignednumber(blood->QueryField(i,3), flags);
+			if (rmin > rmax || rmax > 0xffff) {
+				Log(ERROR, "CharAnimations", "Invalid bloodclr entry: %02x %04x-%04x ", value, rmin, rmax);
 				continue;
 			}
 			for(int j=0;j<AvatarsCount;j++) {
@@ -583,28 +582,27 @@ void CharAnimations::InitAvatarsTable() const
 		int rows = walk->GetRowCount();
 		for(int i=0;i<rows;i++) {
 			ResRef value;
-			unsigned long rmin = 0;
-			unsigned long rmax = 0xffff;
-			unsigned long range = 0;
+			unsigned int rmin = 0;
+			unsigned int rmax = 0xffff;
+			ieByte range = 0;
 
 			value = walk->QueryField(i, 0);
-			valid_number(walk->QueryField(i,1), (long &)rmin);
-			valid_number(walk->QueryField(i,2), (long &)rmax);
-			valid_number(walk->QueryField(i,3), (long &)range);
+			valid_unsignednumber(walk->QueryField(i,1), rmin);
+			valid_unsignednumber(walk->QueryField(i,2), rmax);
+			valid_unsignednumber(walk->QueryField(i,3), range);
 			if (value.IsStar()) {
 				value.Reset();
 				range = 0;
 			}
-			if (range>255 || rmin>rmax || rmax>0xffff) {
-				Log(ERROR, "CharAnimations", "Invalid walksnd entry: %02x %04x-%04x ",
-						(unsigned int) range, (unsigned int) rmin, (unsigned int) rmax);
+			if (rmin > rmax || rmax > 0xffff) {
+				Log(ERROR, "CharAnimations", "Invalid walksnd entry: %02x %04x-%04x ", range, rmin, rmax);
 				continue;
 			}
 			for(int j=0;j<AvatarsCount;j++) {
 				if (rmax<AvatarTable[j].AnimID) break;
 				if (rmin>AvatarTable[j].AnimID) continue;
 				AvatarTable[j].WalkSound = value;
-				AvatarTable[j].WalkSoundCount = (unsigned int) range;
+				AvatarTable[j].WalkSoundCount = range;
 			}
 		}
 	}
@@ -613,14 +611,13 @@ void CharAnimations::InitAvatarsTable() const
 	if (stances) {
 		int rows = stances->GetRowCount();
 		for (int i = 0; i < rows; i++) {
-			unsigned long id = 0, s1 = 0, s2 = 0;
-			valid_number(stances->GetRowName(i), (long &)id);
-			valid_number(stances->QueryField(i, 0), (long &)s1);
-			valid_number(stances->QueryField(i, 1), (long &)s2);
+			unsigned int id = 0, s1 = 0, s2 = 0;
+			valid_unsignednumber(stances->GetRowName(i), id);
+			valid_unsignednumber(stances->QueryField(i, 0), s1);
+			valid_unsignednumber(stances->QueryField(i, 1), s2);
 
 			if (s1 >= MAX_ANIMS || s2 >= MAX_ANIMS) {
-				Log(ERROR, "CharAnimations", "Invalid stances entry: %04x %d %d",
-						(unsigned int) id, (unsigned int) s1, (unsigned int) s2);
+				Log(ERROR, "CharAnimations", "Invalid stances entry: %04x %d %d", id, s1, s2);
 				continue;
 			}
 
@@ -638,8 +635,8 @@ void CharAnimations::InitAvatarsTable() const
 	if (avatarShadows) {
 		int rows = avatarShadows->GetRowCount();
 		for (int i = 0; i < rows; ++i) {
-			unsigned long id = 0;
-			valid_number(avatarShadows->GetRowName(i), (long &)id);
+			unsigned int id = 0;
+			valid_unsignednumber(avatarShadows->GetRowName(i), id);
 
 			for (int j = 0; j < AvatarsCount; j++) {
 				if (id < AvatarTable[j].AnimID) {

gemrb/core/EffectQueue.cpp
@@ -274,7 +274,7 @@ bool Init_EffectQueue()
 		return false;
 	}
 
-	for (long i = 0; i < MAX_EFFECTS; i++) {
+	for (int i = 0; i < MAX_EFFECTS; i++) {
 		const char* effectname = effectsTable->GetValue( i );
 
 		EffectDesc* poi = FindEffect( effectname );
@@ -284,7 +284,7 @@ bool Init_EffectQueue()
 			//reverse linking opcode number
 			//using this unused field
 			if( (poi->opcode!=-1) && effectname[0]!='*') {
-				error("EffectQueue", "Clashing Opcodes FN: %ld vs. %d, %s\n", i, poi->opcode, effectname);
+				error("EffectQueue", "Clashing Opcodes FN: %d vs. %d, %s\n", i, poi->opcode, effectname);
 			}
 			poi->opcode = i;
 		}
@@ -293,8 +293,8 @@ bool Init_EffectQueue()
 			int row = efftextTable->GetRowCount();
 			while (row--) {
 				const char* ret = efftextTable->GetRowName( row );
-				long val;
-				if( valid_number( ret, val ) && (i == val) ) {
+				int val;
+				if(valid_signednumber(ret, val) && (i == val)) {
 					Opcodes[i].Strref = atoi( efftextTable->QueryField( row, 1 ) );
 				} else {
 					Opcodes[i].Strref = -1;

gemrb/core/GameData.cpp
@@ -709,7 +709,7 @@ const Color& GameData::GetColor(const char *row)
 	if (colors.empty()) {
 		AutoTable colorTable("colors", true);
 		for (size_t r = 0; r < colorTable->GetRowCount(); r++) {
-			ieDword c = strtol(colorTable->QueryField(r, 0), nullptr, 0);
+			ieDword c = strtounsigned<ieDword>(colorTable->QueryField(r, 0));
 			colors[strdup(colorTable->GetRowName(r))] = Color(c);
 		}
 	}

gemrb/core/GameScript/GSUtils.cpp
@@ -96,7 +96,7 @@ void InitScriptTables()
 	if (tab) {
 		for (int alignment=0;alignment<3;alignment++) {
 			for (int reputation=0;reputation<MAX_REP_COLUMN;reputation++) {
-				happiness[alignment][reputation]=strtol(tab->QueryField(reputation,alignment), NULL, 0);
+				happiness[alignment][reputation] = strtosigned<int>(tab->QueryField(reputation,alignment));
 			}
 		}
 	}
@@ -106,7 +106,7 @@ void InitScriptTables()
 	AutoTable rmr("rmodrep");
 	if (rmr) {
 		for (int reputation=0; reputation<MAX_REP_COLUMN; reputation++) {
-			rmodrep[reputation] = strtol(rmr->QueryField(0, reputation), NULL, 0);
+			rmodrep[reputation] = strtosigned<int>(rmr->QueryField(0, reputation));
 		}
 	}
 
@@ -114,7 +114,7 @@ void InitScriptTables()
 	AutoTable rmc("rmodchr");
 	if (rmc) {
 		for (int charisma=0; charisma<MAX_CHR_COLUMN; charisma++) {
-			rmodchr[charisma] = strtol(rmc->QueryField(0, charisma), NULL, 0);
+			rmodchr[charisma] = strtosigned<int>(rmc->QueryField(0, charisma));
 		}
 	}
 
@@ -226,11 +226,11 @@ ieDword ResolveSpellNumber(const ResRef& spellRef)
 	for (int i = 0; i < 5; i++) {
 		if (spellRef == spell_suffices[i]) {
 			tmp = spellRef + 4;
-			long n = strtol(tmp.CString(), nullptr, 10);
+			ieDword n = strtounsigned<ieDword>(tmp.CString());
 			if (!n) {
 				return 0xffffffff;
 			}
-			return i * 1000 + static_cast<ieDword>(n);
+			return i * 1000 + n;
 		}
 	}
 	return 0xffffffff;
@@ -1550,7 +1550,7 @@ inline bool ismysymbol(const char letter)
 static int GetIdsValue(const char *&symbol, const char *idsname)
 {
 	char *newsymbol;
-	int value=strtol(symbol, &newsymbol, 0);
+	int value = strtosigned<int>(symbol, &newsymbol);
 	if (symbol!=newsymbol) {
 		symbol=newsymbol;
 		return value;
@@ -1599,7 +1599,7 @@ static int ParseIntParam(const char *&src, const char *&str)
 		}
 	}
 	//no IDS table
-	return strtol(src, (char **) &src, 0);
+	return strtosigned<int>(src, (char **) &src);
 }
 
 static void ParseIdsTarget(const char *&src, Object *&object)
@@ -1705,9 +1705,9 @@ Action* GenerateActionCore(const char *src, const char *str, unsigned short acti
 			case 'p': //Point
 				SKIP_ARGUMENT();
 				src++; //Skip [
-				newAction->pointParameter.x = (int) strtol( src, (char **) &src, 10 );
+				newAction->pointParameter.x = strtosigned<int>( src, (char **) &src, 10);
 				src++; //Skip .
-				newAction->pointParameter.y = (int) strtol( src, (char **) &src, 10 );
+				newAction->pointParameter.y = strtosigned<int>( src, (char **) &src, 10);
 				src++; //Skip ]
 				break;
 
@@ -2053,9 +2053,9 @@ Trigger *GenerateTriggerCore(const char *src, const char *str, int trIndex, int 
 			case 'p': //Point
 				SKIP_ARGUMENT();
 				src++; //Skip [
-				newTrigger->pointParameter.x = (int) strtol( src, (char **) &src, 10 );
+				newTrigger->pointParameter.x = strtosigned<int>(src, (char **) &src, 10);
 				src++; //Skip .
-				newTrigger->pointParameter.y = (int) strtol( src, (char **) &src, 10 );
+				newTrigger->pointParameter.y = strtosigned<int>(src, (char **) &src, 10);
 				src++; //Skip ]
 				break;
 

gemrb/core/GameScript/GameScript.cpp
@@ -2221,7 +2221,7 @@ Response* GameScript::ReadResponse(DataStream* stream)
 	rE->weight = 0;
 	stream->ReadLine( line, 1024 );
 	char *poi;
-	rE->weight = (unsigned char)strtoul(line,&poi,10);
+	rE->weight = strtounsigned<uint8_t>(line,&poi,10);
 	if (strncmp(poi, "AC", 2) != 0) {
 		free(line);
 		return rE;
@@ -2231,7 +2231,7 @@ Response* GameScript::ReadResponse(DataStream* stream)
 		//not autofreed, because it is referenced by the Script
 		Action* aC = new Action(false);
 		stream->ReadLine( line, 1024 );
-		aC->actionID = (unsigned short)strtoul(line, NULL,10);
+		aC->actionID = strtounsigned<uint16_t>(line, nullptr, 10);
 		for (int i = 0; i < 3; i++) {
 			stream->ReadLine( line, 1024 );
 			Object* oB = DecodeObject( line );

gemrb/core/Interface.cpp
@@ -565,13 +565,13 @@ bool Interface::ReadAbilityTable(const ResRef& tablename, ieWordSigned *mem, int
 		fix = atoi(tmp);
 		for (int i=0;i<fix;i++) {
 			for (int j=0;j<columns;j++) {
-				mem[rows*j+i]=(ieWordSigned) strtol(tab->QueryField(0,j),NULL,0 );
+				mem[rows*j+i] = strtosigned<ieWordSigned>(tab->QueryField(0,j));
 			}
 		}
 	}
 	for (int j=0;j<columns;j++) {
 		for( int i=0;i<rows-fix;i++) {
-			mem[rows*j+i+fix] = (ieWordSigned) strtol(tab->QueryField(i,j),NULL,0 );
+			mem[rows*j+i+fix] = strtosigned<ieWordSigned>(tab->QueryField(i,j));
 		}
 	}
 	return true;
@@ -659,9 +659,9 @@ bool Interface::ReadSpecialSpells()
 	table.load("wildmag");
 	if (table) {
 		SurgeSpell ss;
-		for (int i = 0; (unsigned) i < table->GetRowCount(); i++) {
+		for (ieDword i = 0; i < table->GetRowCount(); i++) {
 			ss.spell = table->QueryField(i, 0);
-			ss.message = strtol(table->QueryField(i, 1), NULL, 0);
+			ss.message = strtounsigned<ieStrRef>(table->QueryField(i, 1));
 			// comment ignored
 			SurgeSpells.push_back(ss);
 		}
@@ -768,7 +768,7 @@ bool Interface::ReadDamageTypeTable() {
 	for (ieDword i = 0; i < tm->GetRowCount(); i++) {
 		di.strref = displaymsg->GetStringReference(atoi(tm->QueryField(i, 0)));
 		di.resist_stat = TranslateStat(tm->QueryField(i, 1));
-		di.value = strtol(tm->QueryField(i, 2), (char **) NULL, 16);
+		di.value = strtounsigned<unsigned int>(tm->QueryField(i, 2), nullptr, 16);
 		di.iwd_mod_type = atoi(tm->QueryField(i, 3));
 		di.reduction = atoi(tm->QueryField(i, 4));
 		DamageInfoMap.insert(std::make_pair(di.value, di));
@@ -3273,7 +3273,7 @@ bool Interface::InitItemTypes()
 			unsigned int value = 0;
 			unsigned int k = 1;
 			for (int j=0;j<InvSlotTypes;j++) {
-				if (strtol(it->QueryField(i,j),NULL,0) ) {
+				if (strtosigned<long>(it->QueryField(i,j))) {
 					value |= k;
 				}
 				k <<= 1;
@@ -3320,7 +3320,7 @@ bool Interface::InitItemTypes()
 		slotTypes.resize(SlotTypes);
 		for (unsigned int row = 0; row < SlotTypes; row++) {
 			bool alias;
-			unsigned int i = (ieDword) strtol(st->GetRowName(row),NULL,0 );
+			ieDword i = strtounsigned<ieDword>(st->GetRowName(row));
 			if (i>=SlotTypes) continue;
 			if (slotTypes[i].slotEffects != 100) { // SLOT_EFFECT_ALIAS
 				slotTypes[row].slot = i;
@@ -3330,16 +3330,16 @@ bool Interface::InitItemTypes()
 				slotTypes[row].slot = i;
 				alias = false;
 			}
-			slotTypes[i].slotType = (ieDword) strtol(st->QueryField(row, 0), nullptr, 0);
-			slotTypes[i].slotID = (ieDword) strtol(st->QueryField(row, 1), nullptr, 0);
+			slotTypes[i].slotType = strtounsigned<ieDword>(st->QueryField(row, 0));
+			slotTypes[i].slotID = strtounsigned<ieDword>(st->QueryField(row, 1));
 			slotTypes[i].slotResRef = st->QueryField(row, 2);
-			slotTypes[i].slotTip = (ieDword) strtol(st->QueryField(row, 3), nullptr, 0);
-			slotTypes[i].slotFlags = (ieDword) strtol(st->QueryField(row, 5), nullptr, 0);
+			slotTypes[i].slotTip = strtounsigned<ieDword>(st->QueryField(row, 3));
+			slotTypes[i].slotFlags = strtounsigned<ieDword>(st->QueryField(row, 5));
 			//don't fill sloteffects for aliased slots (pst)
 			if (alias) {
 				continue;
 			}
-			slotTypes[i].slotEffects = (ieDword) strtol(st->QueryField(row, 4), nullptr, 0);
+			slotTypes[i].slotEffects = strtounsigned<ieDword>(st->QueryField(row, 4));
 			//setting special slots
 			if (slotTypes[i].slotType & SLOT_ITEM) {
 				if (slotTypes[i].slotType & SLOT_INVENTORY) {
@@ -3947,11 +3947,11 @@ bool Interface::ResolveRandomItem(CREItem *itm) const
 		int diceSides;
 		if (p) {
 			*p=0; //doing this so endptr is ok
-			diceSides = strtol(p + 1, nullptr, 10);
+			diceSides = strtosigned<int>(p + 1, nullptr, 10);
 		} else {
 			diceSides = 1;
 		}
-		int diceThrows = strtol(NewItem, &endptr, 10);
+		int diceThrows = strtosigned<int>(NewItem, &endptr, 10);
 		if (diceThrows <1) {
 			diceThrows = 1;
 		}
@@ -4672,7 +4672,7 @@ ieDword *Interface::GetListFrom2DAInternal(const ResRef& resref)
 		ret = (ieDword *) malloc((1+cnt)*sizeof(ieDword));
 		ret[0]=cnt;
 		while(cnt) {
-			ret[cnt]=strtol(tab->QueryField(cnt-1, 0),NULL, 0);
+			ret[cnt] = strtounsigned<ieDword>(tab->QueryField(cnt-1, 0));
 			cnt--;
 		}
 		return ret;
@@ -4697,10 +4697,9 @@ ieDword* Interface::GetListFrom2DA(const ResRef& tablename)
 //returns a numeric value associated with a stat name (symbol) from stats.ids
 ieDword Interface::TranslateStat(const char *stat_name)
 {
-	long tmp;
-
-	if (valid_number(stat_name, tmp)) {
-		return (ieDword) tmp;
+	ieDword tmp;
+	if (valid_unsignednumber(stat_name, tmp)) {
+		return tmp;
 	}
 
 	int symbol = LoadSymbol( "stats" );
@@ -4736,8 +4735,8 @@ int Interface::ResolveStatBonus(Actor *actor, const char *tablename, ieDword fla
 	// tables for additive modifiers of bonus type
 	for (int i = 0; i < count; i++) {
 		tablename = mtm->GetRowName(i);
-		int checkcol = strtol(mtm->QueryField(i,1), nullptr, 0);
-		unsigned int readcol = strtol(mtm->QueryField(i,2), nullptr, 0);
+		int checkcol = strtosigned<int>(mtm->QueryField(i,1));
+		unsigned int readcol = strtounsigned<unsigned int>(mtm->QueryField(i,2));
 		int stat = TranslateStat(mtm->QueryField(i,0) );
 		if (!(flags&1)) {
 			value = actor->GetSafeStat(stat);
@@ -4758,7 +4757,7 @@ int Interface::ResolveStatBonus(Actor *actor, const char *tablename, ieDword fla
 			row = tm->FindTableValue(checkcol, value, 0);
 		}
 		if (row>=0) {
-			ret += strtol(tm->QueryField(row, readcol), NULL, 0);
+			ret += strtosigned<int>(tm->QueryField(row, readcol));
 		}
 	}
 	return ret;

gemrb/core/MapReverb.cpp
@@ -84,10 +84,10 @@ unsigned char MapReverb::obtainProfile () {
 	for (int i = 0; i < rows; ++i) {
 		ResRef mapName = reverbMapping->GetRowName(i);
 		if (mapName == map.WEDResRef) {
-			long profile = strtoul(reverbMapping->QueryField(i, 0), NULL, 0);
+			uint8_t profile = strtounsigned<uint8_t>(reverbMapping->QueryField(i, 0));
 
-			if (profile >= 0 && profile < EFX_MAX_REVERB_PROFILE_INDEX) {
-				configValue = static_cast<unsigned char>(profile);
+			if (profile < EFX_MAX_REVERB_PROFILE_INDEX) {
+				configValue = profile;
 			}
 			break;
 		}

commit 765fe48a6fe2694e700568316758d0819b70003a

GemRB_Table_GetValue: fixed regression from 362d55af3c
only one branch is content with a mere dword

broke all sorts of things, but noticed with AC display for chars
with weapon specialization bonuses

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -791,10 +791,10 @@ static PyObject* GemRB_Table_GetValue(PyObject* self, PyObject* args)
 		case 2:
 			return PyInt_FromLong( core->TranslateStat(ret) );
 		default:
-			ieStrRef val;
-			bool valid = valid_unsignednumber(ret, val);
+			long val;
+			bool valid = valid_signednumber(ret, val);
 			if (type == 3) {
-				char* cstr = core->GetCString(val);
+				char* cstr = core->GetCString(ieStrRef(val));
 				PyObject* pystr = PyString_FromString(cstr);
 				free(cstr);
 				return pystr;