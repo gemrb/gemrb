gemrb/gemrb

commit d62d0cf285db2a7aea694a7a387f80555bf7654a

voodoo: removed a constant, since we understand it already

gemrb/core/GameScript/GSUtils.cpp
@@ -556,7 +556,7 @@ int CanSee(const Scriptable *Sender, const Scriptable *target, bool range, int s
 			los = false;
 		}
 
-		if (Distance(target->Pos, Sender->Pos) > dist * VOODOO_CANSEE_F) {
+		if (WithinRange(target, Sender->Pos, dist)) {
 			return 0;
 		}
 		if (!los) {

gemrb/includes/voodooconst.h
@@ -75,7 +75,6 @@ static const unsigned int VOODOO_EXISTENCE_DELAY_DEFAULT = 300;
 static const int VOODOO_NEARLOC_F = 15; // sqrt(8*8+12+12)
 
 // visual range stuff
-static const int VOODOO_CANSEE_F = 16;
 // these two are well understood for actors, but could be different for other scriptables
 // eg. visual range is supposedly 15 (see note in DoObjectChecks)
 static const int VOODOO_VISUAL_RANGE = 28;

commit c39b00f49171b3d741cfaa9c44e4ffc7af43d9c0

CanSee: fixed regression from d62d0cf2, the return values are different

gemrb/core/GameScript/GSUtils.cpp
@@ -556,7 +556,7 @@ int CanSee(const Scriptable *Sender, const Scriptable *target, bool range, int s
 			los = false;
 		}
 
-		if (WithinRange(target, Sender->Pos, dist)) {
+		if (!WithinRange(target, Sender->Pos, dist)) {
 			return 0;
 		}
 		if (!los) {