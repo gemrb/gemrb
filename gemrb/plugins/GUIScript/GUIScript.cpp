15daf7fdeac5ebafa29f3329e3adcfb06e54c792
Brad Allred <bradallred@me.com>
2021-08-07 19:10:18 -0600

    explicitly invoke the const char* conversion for PyStringWrapper
    no idea why its needed since neither the constructor for FixedSizeString nor the conversion operator on PyStringWrapper are explicit
    i would have thought implicit conversion to const char* then implicit conversion to FixedSizeString would be seen by the compiler
    
    fixes #1405

--- gemrb/plugins/GUIScript/GUIScript.cpp
+++ gemrb/plugins/GUIScript/GUIScript.cpp
@@ -186,7 +186,7 @@ static const ScriptingRefBase* GetScriptingRef(PyObject* obj) {
 		RuntimeError("Invalid Scripting reference, must have SCRIPT_GROUP attribute.");
 		return NULL;
 	}
-	ScriptingGroup_t group = PyString_AsString(attr);
+	ScriptingGroup_t group = static_cast<const char*>(PyString_AsString(attr));
 	Py_DecRef(attr);
 
 	return gs->GetScripingRef(group, id);
