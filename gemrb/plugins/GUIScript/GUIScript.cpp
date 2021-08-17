gemrb/gemrb

commit d377b78f53a3751a72c37e98781d88799fe42ece

GemRB_Control_SetText: fixed #1432

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -1503,6 +1503,15 @@ static PyObject* GemRB_Control_SetText(PyObject* self, PyObject* args)
 	} else if (str == Py_None) {
 		// clear the text
 		ctrl->SetText(NULL);
+	} else if (PyObject_TypeCheck(str, &PyByteArray_Type)) { // state font
+#if PY_MAJOR_VERSION >= 3
+		char *tmp = PyString_AsString(str);
+#else // python2 has inbuilt PyString_AsString that can't handle bytearrays
+		char *tmp = PyByteArray_AS_STRING(str);
+#endif
+		const String* string = StringFromCString(tmp);
+		ctrl->SetText(string);
+		delete string;
 	} else { // string value of the object
 		const String* string = StringFromCString( PyString_AsString( str ) );
 		ctrl->SetText(string);

commit a869a7f395cb2585c806bd743bc26336e2e7de96

fixed py3 regression from d377b78, a different return type

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -1505,9 +1505,9 @@ static PyObject* GemRB_Control_SetText(PyObject* self, PyObject* args)
 		ctrl->SetText(NULL);
 	} else if (PyObject_TypeCheck(str, &PyByteArray_Type)) { // state font
 #if PY_MAJOR_VERSION >= 3
-		char *tmp = PyString_AsString(str);
+		const char *tmp = PyString_AsString(str);
 #else // python2 has inbuilt PyString_AsString that can't handle bytearrays
-		char *tmp = PyByteArray_AS_STRING(str);
+		const char *tmp = PyByteArray_AS_STRING(str);
 #endif
 		const String* string = StringFromCString(tmp);
 		ctrl->SetText(string);