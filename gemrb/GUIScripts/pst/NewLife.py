9ed98285302b71efc9a6d6a8d09e4a9d2e1e81fa
TimHyers <hyerst@gmail.com>
2021-09-03 21:24:19 -0400

    Added hpinit.2da support to NewLife.py. Also fixed issue where the player would not start at max hp when con >14

--- gemrb/GUIScripts/pst/NewLife.py
+++ gemrb/GUIScripts/pst/NewLife.py
@@ -250,17 +250,15 @@ def AcceptPress():
 
 	#don't add con bonus, it will be calculated by the game
 	#interestingly enough, the game adds only one level's con bonus
-	Con = Stats[4]
-	if Con > 14:
-		x = 30
-	else:
-		x = 20 + (Con-9)*2
+	Table = GemRB.LoadTable ("hpinit")
+	x = Table.GetValue (Stats[4]-1, 0)
 
 	print("Setting max hp to: ", x)
 	GemRB.SetPlayerStat(1, IE_MAXHITPOINTS, x)
 	#adding the remaining constitution bonus to the current hp
 	#if Con>14:
 	#	x = x+(Con-14)*3
+	x = x + 100 #ensures the player starts at max hp when con > 14
 	print("Setting current hp to: ", x)
 	GemRB.SetPlayerStat(1, IE_HITPOINTS, x)
 
