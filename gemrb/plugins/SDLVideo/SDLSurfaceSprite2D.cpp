3a7284432b4c9584f9eb7866a96a986897c17a91
Brad Allred <bradallred@me.com>
2021-09-08 13:44:54 -0600

    SDLSurfaceSprite::RenderWithFlags fix version check for paletted sprites
    fixes issues like item highlights being permanent

--- gemrb/plugins/SDLVideo/SDLSurfaceSprite2D.cpp
+++ gemrb/plugins/SDLVideo/SDLSurfaceSprite2D.cpp
@@ -228,10 +228,7 @@ BlitFlags SDLSurfaceSprite2D::RenderWithFlags(BlitFlags renderflags, const Color
 			newVersion |= tintv << 32;
 		}
 		
-		if (oldVersion != newVersion) {
-			if (IsPaletteStale()) {
-				Invalidate();
-			}
+		if (IsPaletteStale()) {
 			SDL_Palette* pal = static_cast<SDL_Palette*>(NewVersion(newVersion));
 
 			for (size_t i = 0; i < 256; ++i) {
