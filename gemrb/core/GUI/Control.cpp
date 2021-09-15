c1ae4d4f56538d7ced9d2b77e46a5f9bafd5e3fa
Brad Allred <bradallred@me.com>
2021-09-15 08:34:32 -0600

    Window: add a per-window variable context
    there are GUIScript methods for loading/storing from the global variables
    
    1. dont needlessly pollute globals. Removes the need to "unset" variables that are only used while the window is active
    2. prevent 2 windows controls from conflicting with each other by using the same variable name
    3. easily allows implementing of "canceling" something instead of committing it (eg game settings)
    
    unfortunately, this  breaks things anywhere that is expecting to read a global bound to a control
    the callers should be updated to read from the window context or the variable needs to be synchronized with the globals

--- gemrb/core/GUI/Control.cpp
+++ gemrb/core/GUI/Control.cpp
@@ -171,26 +171,27 @@ void Control::UpdateDictValue() noexcept
 		return;
 	}
 	
+	Window* win = GetWindow();
+	assert(win);
+	
 	// set this even when the value doesn't change
 	// if a radio is clicked, then one of its siblings, the siblings value wont change
 	// but we expect the dictionary to reflect the selected value
 	BitOp op = GetDictOp();
-	value_t curVal = op == BitOp::SET ? INVALID_VALUE : 0;
-	core->GetDictionary()->Lookup(VarName, curVal);
-	value_t newVal = curVal;
-	SetBits(newVal, Value, op);
-	core->GetDictionary()->SetAt(VarName, newVal);
-	
-	const Window* win = GetWindow();
-	if (win) {
-		win->RedrawControls(VarName);
-	} else {
-		UpdateState(VarName, newVal);
+	value_t curVal = win->GetDictVariable(VarName);
+	if (curVal == INVALID_VALUE && op != BitOp::SET) {
+		curVal = 0;
 	}
+	SetBits(curVal, Value, op);
+	win->SetDictVariable(VarName, curVal);
 }
 
-void Control::BindDictVariable(const varname_t& var, value_t val, ValueRange range) noexcept
+bool Control::BindDictVariable(const varname_t& var, value_t val, ValueRange range) noexcept
 {
+	const Window* win = GetWindow();
+	if (win == nullptr) {
+		return false;
+	}
 	// blank out any old varname so we can set the control value without setting the old variable
 	VarName[0] = '\0';
 	if (range.first != Control::INVALID_VALUE) {
@@ -203,12 +204,11 @@ void Control::BindDictVariable(const varname_t& var, value_t val, ValueRange ran
 	if (GetDictOp() == BitOp::SET) {
 		// SET implies the dictionary value should always mirror Value
 		UpdateDictValue();
-	} else {
-		value_t dictVal = INVALID_VALUE;
-		if (core->GetDictionary()->Lookup(VarName, dictVal)) {
-			UpdateState(VarName, dictVal);
-		}
+	} else if ((val = win->GetDictVariable(VarName)) != INVALID_VALUE) {
+		UpdateState(VarName, val);
 	}
+	
+	return true;
 }
 
 bool Control::IsDictBound() const noexcept
