gemrb/gemrb

commit dbd9ce4ec5f8c75c3c556bdf8e1c94fc75a7552f

implemented a few "bugprone" clang suggestions and one more ResRef conversion

gemrb/core/Scriptable/Actor.cpp
@@ -3280,7 +3280,7 @@ void Actor::RefreshEffects(EffectQueue *fx)
 
 	// check if any new portrait icon was removed or added
 	if (PCStats) {
-		if (memcmp(PCStats->PreviousPortraitIcons, PCStats->PortraitIcons, sizeof(PCStats->PreviousPortraitIcons))) {
+		if (memcmp(PCStats->PreviousPortraitIcons, PCStats->PortraitIcons, sizeof(PCStats->PreviousPortraitIcons)) != 0) {
 			core->SetEventFlag(EF_PORTRAIT);
 			memcpy( PCStats->PreviousPortraitIcons, PCStats->PortraitIcons, sizeof(PCStats->PreviousPortraitIcons) );
 		}
@@ -3689,7 +3689,7 @@ bool Actor::GetSavingThrow(ieDword type, int modifier, const Effect *fx)
 
 	if (savingthrows[type] == IE_SAVEWILL) {
 		// aura of courage
-		if (Modified[IE_EA] < EA_GOODCUTOFF && stricmp(fx->Source, "SPWI420")) {
+		if (Modified[IE_EA] < EA_GOODCUTOFF && stricmp(fx->Source, "SPWI420") != 0) {
 			// look if an ally paladin of at least level 2 is near
 			std::vector<Actor *> neighbours = area->GetAllActorsInRadius(Pos, GA_NO_LOS|GA_NO_DEAD|GA_NO_UNSCHEDULED|GA_NO_ENEMY|GA_NO_NEUTRAL|GA_NO_SELF, 10);
 			for (const Actor *ally : neighbours) {
@@ -3726,7 +3726,7 @@ bool Actor::GetSavingThrow(ieDword type, int modifier, const Effect *fx)
 
 	// handle animal taming last
 	// must roll a Will Save of 5 + player's total skill or higher to save
-	if (stricmp(fx->Source, "SPIN108") && fx->Opcode == 5) {
+	if (stricmp(fx->Source, "SPIN108") != 0 && fx->Opcode == 5) {
 		saveDC = 5;
 		const Actor *caster = core->GetGame()->GetActorByGlobalID(fx->CasterID);
 		if (caster) {
@@ -4198,7 +4198,7 @@ bool Actor::PlayWarCry(int range) const
 //call this when a PC receives a command from GUI
 void Actor::CommandActor(Action* action, bool clearPath)
 {
-	Scriptable::Stop(); // stop what you were doing
+	Movable::Stop(); // stop what you were doing
 	if (clearPath) ClearPath(true);
 	AddAction(action); // now do this new thing
 
@@ -8973,8 +8973,11 @@ void Actor::ResolveStringConstant(ieResRef& Sound, unsigned int index) const
 	}
 
 	//Empty resrefs
-	if (Sound[0]=='*') Sound[0]=0;
-	else if(!strncmp(Sound,"nosound",8) ) Sound[0]=0;
+	if (Sound[0] == '*') {
+		Sound[0] = 0;
+	} else if (!strncmp(Sound, "nosound", 7)) {
+		Sound[0] = 0;
+	}
 }
 
 void Actor::SetActionButtonRow(ActionButtonRow &ar) const
@@ -9446,7 +9449,7 @@ int Actor::GetSneakAttackDamage(Actor *target, WeaponInfo &wi, int &multiplier, 
 				// special effects on hit for arterial strike (-1d6) and hamstring (-2d6)
 				// both are available at level 10+ (5d6), so it's safe to decrease multiplier without checking
 				if (BackstabResRef[0]!='*') {
-					if (stricmp(BackstabResRef, resref_arterial)) {
+					if (stricmp(BackstabResRef, resref_arterial) != 0) {
 						// ~Sneak attack for %d inflicts hamstring damage (Slowed)~
 						multiplier -= 2;
 						sneakAttackDamage = LuckyRoll(multiplier, 6, 0, 0, target);
@@ -9819,7 +9822,7 @@ void Actor::SetupFist()
 		}
 	}
 	CREItem *currentFist = inventory.GetSlotItem(slot);
-	if (!currentFist || stricmp(currentFist->ItemResRef, ItemResRef)) {
+	if (!currentFist || stricmp(currentFist->ItemResRef, ItemResRef) != 0) {
 		inventory.SetSlotItemRes(ItemResRef, slot);
 	}
 }

gemrb/core/EffectQueue.cpp
@@ -795,7 +795,7 @@ static inline bool check_level(const Actor *target, Effect *fx)
 		fx->Parameter1 = DICE_ROLL((signed)fx->Parameter1);
 		//this is a hack for PST style diced effects
 		if( core->HasFeature(GF_SAVE_FOR_HALF) ) {
-			if( memcmp(fx->Resource,"NEG",4) ) {
+			if (strnicmp(fx->Resource, "NEG", 4) != 0) {
 				fx->IsSaveForHalfDamage=1;
 			}
 		} else {
@@ -881,7 +881,7 @@ static int check_type(const Actor *actor, const Effect *fx)
 			return 0;
 		}
 		if( actor->fxqueue.HasEffectWithResource(fx_spell_immunity2_ref, fx->Source) ) {
-			if (strnicmp(fx->Source, "detect", 6)) { // our secret door pervasive effect
+			if (strnicmp(fx->Source, "detect", 6) != 0) { // our secret door pervasive effect
 				Log(DEBUG, "EffectQueue", "Resisted by spell immunity2 (%s)", fx->Source);
 			}
 			return 0;

gemrb/core/Game.cpp
@@ -980,7 +980,7 @@ bool Game::CheckForReplacementActor(int i)
 		return false;
 	}
 
-	ieResRef newcre = "****"; // default table value
+	ResRef newcre = "****"; // default table value
 	for (auto nl : npclevels) {
 		if (!stricmp(nl[0], act->GetScriptName()) && (level > 2)) {
 			// the tables have entries only up to level 24
@@ -988,12 +988,12 @@ bool Game::CheckForReplacementActor(int i)
 			if (level < safeLevel) {
 				safeLevel = level;
 			}
-			CopyResRef(newcre, nl[safeLevel-2]);
+			newcre = nl[safeLevel - 2];
 			break;
 		}
 	}
 
-	if (stricmp(newcre, "****")) {
+	if (newcre == "****") {
 		int pos = gamedata->LoadCreature(newcre, 0, false, act->version);
 		if (pos < 0) {
 			error("Game::CheckForReplacementActor", "LoadCreature failed: pos is negative!\n");

gemrb/core/GameScript/Actions.cpp
@@ -642,7 +642,7 @@ void GameScript::MoveGlobalsTo(Scriptable* /*Sender*/, Action* parameters)
 	while (i--) {
 		Actor *tar = game->GetPC(i, false);
 		//if the actor isn't in the source area, we don't care
-		if (strnicmp(tar->Area, parameters->string0Parameter,8) ) {
+		if (strnicmp(tar->Area, parameters->string0Parameter, 8) != 0) {
 			continue;
 		}
 		// no need of CreateMovementEffect, party members are always moved immediately
@@ -653,7 +653,7 @@ void GameScript::MoveGlobalsTo(Scriptable* /*Sender*/, Action* parameters)
 	while (i--) {
 		Actor *tar = game->GetNPC(i);
 		//if the actor isn't in the source area, we don't care
-		if (strnicmp(tar->Area, parameters->string0Parameter, 8) ) {
+		if (strnicmp(tar->Area, parameters->string0Parameter, 8) != 0) {
 			continue;
 		}
 		//if the actor is currently in a loaded area, remove it from there
@@ -6109,7 +6109,7 @@ void GameScript::ChangeStoreMarkup(Scriptable* /*Sender*/, Action* parameters)
 	if (!store) {
 		store = core->SetCurrentStore(parameters->string0Parameter, 0);
 	} else {
-		if (strnicmp(store->Name, parameters->string0Parameter, sizeof(ieResRef)-1) ) {
+		if (strnicmp(store->Name, parameters->string0Parameter, 8) != 0) {
 			//not the current store, we need some dirty hack
 			has_current = true;
 			strnlwrcpy(current, store->Name, sizeof(ieResRef)-1);

gemrb/core/GameScript/GSUtils.cpp
@@ -362,7 +362,7 @@ void TransformItemCore(Actor *actor, Action *parameters, bool onlyone)
 		if (!item) {
 			continue;
 		}
-		if (strnicmp(item->ItemResRef, parameters->string0Parameter, 8) ) {
+		if (strnicmp(item->ItemResRef, parameters->string0Parameter, 8) != 0) {
 			continue;
 		}
 		actor->inventory.SetSlotItemRes(parameters->string1Parameter,i,parameters->int0Parameter,parameters->int1Parameter,parameters->int2Parameter);
@@ -1307,7 +1307,7 @@ void MoveBetweenAreasCore(Actor* actor, const char *area, const Point &position,
 	Map* map2;
 	Game* game = core->GetGame();
 	bool newSong = false;
-	if (area[0] && (!map1 || stricmp(area, map1->GetScriptName()))) { //do we need to switch area?
+	if (area[0] && (!map1 || stricmp(area, map1->GetScriptName()) != 0)) { //do we need to switch area?
 		//we have to change the pathfinder
 		//to the target area if adjust==true
 		map2 = game->GetMap(area, false);
@@ -2194,7 +2194,7 @@ void SetVariable(Scriptable* Sender, const char* VarName, ieDword value, const c
 		game->kaputz->SetAt( poi, value, NoCreate );
 		return;
 	}
-	if (strnicmp(newVarName, "GLOBAL", 6) ) {
+	if (strnicmp(newVarName, "GLOBAL", 6) != 0) {
 		Map *map=game->GetMap(game->FindMap(newVarName));
 		if (map) {
 			map->locals->SetAt( poi, value, NoCreate);
@@ -2250,8 +2250,8 @@ ieDword CheckVariable(const Scriptable *Sender, const char *VarName, const char 
 		return value;
 	}
 	
-	if (stricmp(newVarName,"GLOBAL") ) {
-		Map *map=game->GetMap(game->FindMap(newVarName));
+	if (stricmp(newVarName, "GLOBAL") != 0) {
+		const Map *map = game->GetMap(game->FindMap(newVarName));
 		if (map) {
 			map->locals->Lookup(poi, value);
 		} else {

gemrb/core/GameScript/Triggers.cpp
@@ -3213,7 +3213,7 @@ int GameScript::CalledByName(Scriptable *Sender, const Trigger *parameters)
 		return 0;
 	}
 	const Actor *actor = (const Actor *) tar;
-	if (stricmp(actor->GetScriptName(), parameters->string0Parameter) ) {
+	if (stricmp(actor->GetScriptName(), parameters->string0Parameter) != 0) {
 		return 0;
 	}
 	return 1;
@@ -4260,11 +4260,11 @@ int GameScript::UsedExit(Scriptable *Sender, const Trigger *parameters)
 	int count = tm->GetRowCount();
 	for (int i=0;i<count;i++) {
 		const char *area = tm->QueryField( i, 0 );
-		if (strnicmp(actor->LastArea, area, 8) ) {
+		if (strnicmp(actor->LastArea, area, 8) != 0) {
 			continue;
 		}
 		const char *exit = tm->QueryField( i, 1 );
-		if (strnicmp(actor->UsedExit, exit, 32) ) {
+		if (strnicmp(actor->UsedExit, exit, 32) != 0) {
 			continue;
 		}
 		return 1;

gemrb/core/Interface.cpp
@@ -1503,7 +1503,7 @@ int Interface::Init(InterfaceConfig* config)
 
 	// if unset, manually populate GameName (window title)
 	if (!stricmp(GameName, GEMRB_STRING)) {
-		if (stricmp(DefaultWindowTitle.c_str(), GEMRB_STRING)) {
+		if (stricmp(DefaultWindowTitle.c_str(), GEMRB_STRING) != 0) {
 			std::string title = GEMRB_STRING" running " + DefaultWindowTitle;
 			strlcpy(GameName, title.c_str(), sizeof(GameName));
 		} else {
@@ -3032,7 +3032,7 @@ bool Interface::SaveConfig()
 {
 	char ini_path[_MAX_PATH] = { '\0' };
 	char gemrbINI[_MAX_PATH+4] = { '\0' };
-	if (strncmp(INIConfig, "gem-", 4)) {
+	if (strncmp(INIConfig, "gem-", 4) != 0) {
 		snprintf(gemrbINI, sizeof(gemrbINI), "gem-%s", INIConfig);
 	}
 	PathJoin(ini_path, GamePath, gemrbINI, nullptr);
@@ -4244,7 +4244,7 @@ int Interface::CanMoveItem(const CREItem *item) const
 	}
 	//not gold, we allow only one single coin ResRef, this is good
 	//for all of the original games
-	if (strnicmp(item->ItemResRef, GoldResRef, 8 ) )
+	if (strnicmp(item->ItemResRef, GoldResRef, 8) != 0)
 		return -1;
 	//gold, returns the gold value (stack size)
 	return item->Usages[0];
@@ -4893,7 +4893,7 @@ void Interface::SetNextScript(const char *script)
 }
 
 void Interface::SanityCheck(const char *ver) {
-	if (strcmp(ver, VERSION_GEMRB)) {
+	if (strcmp(ver, VERSION_GEMRB) != 0) {
 		error("Core", "version check failed: core version %s doesn't match caller's version %s\n", VERSION_GEMRB, ver);
 	}
 }

gemrb/core/Inventory.cpp
@@ -294,7 +294,7 @@ int Inventory::CountItems(const char *resref, bool stacks) const
 			continue;
 		}
 		if (resref && resref[0]) {
-			if (strnicmp(resref, item->ItemResRef, 8) )
+			if (strnicmp(resref, item->ItemResRef, 8) != 0)
 				continue;
 		}
 		if (stacks && (item->Flags&IE_INV_ITEM_STACKED) ) {
@@ -322,7 +322,7 @@ bool Inventory::HasItem(const char *resref, ieDword flags) const
 		if ( (flags&item->Flags)!=flags) {
 				continue;
 		}
-		if (resref[0] && strnicmp(item->ItemResRef, resref,8) ) {
+		if (resref[0] && strnicmp(item->ItemResRef, resref, 8) != 0) {
 			continue;
 		}
 		return true;
@@ -456,7 +456,7 @@ unsigned int Inventory::DestroyItem(const char *resref, ieDword flags, ieDword c
 		if ( (flags&item->Flags)!=flags) {
 			continue;
 		}
-		if (resref[0] && strnicmp(item->ItemResRef, resref, 8) ) {
+		if (resref[0] && strnicmp(item->ItemResRef, resref, 8) != 0) {
 			continue;
 		}
 		//we need to acknowledge that the item was destroyed
@@ -533,7 +533,7 @@ int Inventory::RemoveItem(const char *resref, unsigned int flags, CREItem **res_
 		if (!flags && (mask&item->Flags)!=0) {
 			continue;
 		}
-		if (resref[0] && strnicmp(item->ItemResRef, resref, 8) ) {
+		if (resref[0] && strnicmp(item->ItemResRef, resref, 8) != 0) {
 			continue;
 		}
 		*res_item=RemoveItem( (unsigned int) slot, count);
@@ -736,7 +736,7 @@ int Inventory::FindItem(const char *resref, unsigned int flags, unsigned int ski
 		if ( mask & item->Flags ) {
 			continue;
 		}
-		if (resref[0] && strnicmp(item->ItemResRef, resref, 8) ) {
+		if (resref[0] && strnicmp(item->ItemResRef, resref, 8) != 0) {
 			continue;
 		}
 		if (skip) {
@@ -798,7 +798,7 @@ bool Inventory::DropItemAtLocation(const char *resref, unsigned int flags, Map *
 		if ( ((flags^IE_INV_ITEM_UNDROPPABLE)&item->Flags)!=flags) {
 				continue;
 		}
-		if (resref[0] && strnicmp(item->ItemResRef, resref, 8) ) {
+		if (resref[0] && strnicmp(item->ItemResRef, resref, 8) != 0) {
 			continue;
 		}
 		// mark it as unequipped, so it doesn't cause problems in stores

commit 204a066cc10a08ce60609e36fd71987e76a91035

Actor::CommandActor: fixed too zealous path resetting
waypoints work again, regression from dbd9ce4e

gemrb/core/Scriptable/Actor.cpp
@@ -4074,7 +4074,7 @@ bool Actor::PlayWarCry(int range) const
 //call this when a PC receives a command from GUI
 void Actor::CommandActor(Action* action, bool clearPath)
 {
-	Movable::Stop(); // stop what you were doing
+	ClearActions(); // stop what you were doing
 	if (clearPath) ClearPath(true);
 	AddAction(action); // now do this new thing
 