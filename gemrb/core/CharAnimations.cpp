gemrb/gemrb

commit 9d1ef2ea636ad3a9dad5beca3615ba0c05cd78f4

abandon AutoTable
shared_ptr is already a reference counted handle so AutoTable was just a useless layer of indirection
this also fixes the awkward index based interface for looking up a table handle

fixes #1403

gemrb/core/CharAnimations.cpp
@@ -73,7 +73,7 @@ struct EquipResRefData {
 };
 
 CharAnimations::AvatarTableLoader::AvatarTableLoader() noexcept {
-	AutoTable Avatars("avatars");
+	AutoTable Avatars = gamedata->LoadTable("avatars");
 	if (!Avatars) {
 		error("CharAnimations", "A critical animation file is missing!\n");
 	}
@@ -130,7 +130,7 @@ CharAnimations::AvatarTableLoader::AvatarTableLoader() noexcept {
 		return a.AnimID < b.AnimID;
 	});
 
-	AutoTable blood("bloodclr");
+	AutoTable blood = gamedata->LoadTable("bloodclr");
 	if (blood) {
 		int rows = blood->GetRowCount();
 		for(int i=0;i<rows;i++) {
@@ -156,7 +156,7 @@ CharAnimations::AvatarTableLoader::AvatarTableLoader() noexcept {
 		}
 	}
 
-	AutoTable walk("walksnd");
+	AutoTable walk = gamedata->LoadTable("walksnd");
 	if (walk) {
 		int rows = walk->GetRowCount();
 		for(int i=0;i<rows;i++) {
@@ -186,7 +186,7 @@ CharAnimations::AvatarTableLoader::AvatarTableLoader() noexcept {
 		}
 	}
 
-	AutoTable stances("stances", true);
+	AutoTable stances = gamedata->LoadTable("stances", true);
 	if (stances) {
 		int rows = stances->GetRowCount();
 		for (int i = 0; i < rows; i++) {
@@ -210,7 +210,7 @@ CharAnimations::AvatarTableLoader::AvatarTableLoader() noexcept {
 		}
 	}
 
-	AutoTable avatarShadows("avatar_shadows");
+	AutoTable avatarShadows = gamedata->LoadTable("avatar_shadows");
 	if (avatarShadows) {
 		int rows = avatarShadows->GetRowCount();
 		for (int i = 0; i < rows; ++i) {

gemrb/core/MapReverb.cpp
@@ -4,8 +4,8 @@
 namespace GemRB {
 
 MapReverb::MapReverb (Map& _map) :
-		reverbMapping(AutoTable("area_reverbs"))
-	, reverbs(AutoTable("reverb"))
+		reverbMapping(gamedata->LoadTable("area_reverbs"))
+	, reverbs(gamedata->LoadTable("reverb"))
 	, map(_map)
 	, reverbProfile(EFX_PROFILE_REVERB_INVALID) {
 	MapReverbProperties _properties = {EFX_REVERB_GENERIC, true};

gemrb/core/CMakeLists.txt
@@ -56,7 +56,6 @@ FILE(GLOB gemrb_core_LIB_SRCS
 	Sprite2D.cpp
 	SpriteCover.cpp
 	Store.cpp
-	TableMgr.cpp
 	TileMap.cpp
 	TileOverlay.cpp
 	Variables.cpp

gemrb/core/Calendar.cpp
@@ -29,7 +29,7 @@ namespace GemRB {
 Calendar::Calendar(void)
 {
 	daysinyear = 0;
-	AutoTable tab("months");
+	AutoTable tab = gamedata->LoadTable("months");
 	if (!tab) {
 		monthnamecount=-1;
 		monthnames = NULL;

gemrb/core/DialogHandler.cpp
@@ -232,7 +232,7 @@ void DialogHandler::EndDialog(bool try_to_break)
 // (we could cache the entries, for example)
 static Actor* FindBanter(const Scriptable* target, const ResRef& dialog)
 {
-	AutoTable pdtable("interdia");
+	AutoTable pdtable = gamedata->LoadTable("interdia");
 	if (!pdtable) return nullptr;
 
 	int col;

gemrb/core/DisplayMessage.cpp
@@ -48,7 +48,7 @@ DisplayMessage::StrRefs::StrRefs()
 
 bool DisplayMessage::StrRefs::LoadTable(const std::string& name)
 {
-	AutoTable tab(name.c_str());
+	AutoTable tab = gamedata->LoadTable(name.c_str());
 	if (tab) {
 		for(int i=0;i<STRREF_COUNT;i++) {
 			table[i]=atoi(tab->QueryField(i,0));

gemrb/core/EffectQueue.cpp
@@ -255,7 +255,7 @@ bool Init_EffectQueue()
 	iwd2fx = !!core->HasFeature(GF_ENHANCED_EFFECTS);
 	initialized = 1;
 
-	AutoTable efftextTable("efftext");
+	AutoTable efftextTable = gamedata->LoadTable("efftext");
 
 	int eT = core->LoadSymbol( "effects" );
 	if (eT < 0) {

gemrb/core/GUI/GameControl.cpp
@@ -145,7 +145,7 @@ GameControl::~GameControl()
 //so it doesn't cause problems with the original engine
 void GameControl::ReadFormations() const
 {
-	AutoTable tab("formatio");
+	AutoTable tab = gamedata->LoadTable("formatio");
 	if (!tab) {
 		// fallback
 		formationcount = 1;

commit f7e0f6b4091ae57316f8fba71a6d34316ed9a9f7

fixed regression from 9d1ef2ea
lame restriction, but at least the code is cleaner

gemrb/core/CharAnimations.cpp
@@ -213,7 +213,7 @@ CharAnimations::AvatarTableLoader::AvatarTableLoader() noexcept {
 		}
 	}
 
-	AutoTable avatarShadows = gamedata->LoadTable("avatar_shadows");
+	AutoTable avatarShadows = gamedata->LoadTable("shadows");
 	if (avatarShadows) {
 		int rows = avatarShadows->GetRowCount();
 		for (int i = 0; i < rows; ++i) {

gemrb/core/MapReverb.cpp
@@ -4,7 +4,7 @@
 namespace GemRB {
 
 MapReverb::MapReverb (Map& _map) :
-		reverbMapping(gamedata->LoadTable("area_reverbs"))
+		reverbMapping(gamedata->LoadTable("reverbs"))
 	, reverbs(gamedata->LoadTable("reverb"))
 	, map(_map)
 	, reverbProfile(EFX_PROFILE_REVERB_INVALID) {