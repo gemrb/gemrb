gemrb/gemrb

commit 24d59155c3737bf9021fb5bda58ce2911916e2f9

convert more users to orient_t

gemrb/core/VEFObject.cpp
@@ -152,7 +152,11 @@ bool VEFObject::UpdateDrawingState(int orientation)
 		switch(entry.type) {
 		case VEFTypes::BAM:
 		case VEFTypes::VVC:
-			ended = ((ScriptedAnimation *) entry.ptr)->UpdateDrawingState(orientation);
+			{
+				ScriptedAnimation* anim = static_cast<ScriptedAnimation*>(entry.ptr);
+				orient_t orient = orientation == -1 ? anim->Orientation : ClampToOrientation(orientation);
+				anim->UpdateDrawingState(orient);
+			}
 			break;
 		case VEFTypes::_2DA:
 		case VEFTypes::VEF:

gemrb/core/Orientation.h
@@ -84,6 +84,11 @@ inline orient_t ReduceToHalf(orient_t orient)
 	return ClampToOrientation(orient & ~1);
 }
 
+inline orient_t ReflectOrientation(orient_t orient)
+{
+	return orient_t(orient ^ 8);
+}
+
 // clockwise
 inline orient_t NextOrientation(orient_t orient, int step = 1)
 {

gemrb/core/Projectile.h
@@ -344,7 +344,7 @@ public:
 		return effects;
 	}
 
-	inline unsigned char GetOrientation() const {
+	inline orient_t GetOrientation() const {
 		return Orientation;
 	}
 	//no idea if projectiles got height, using y

gemrb/core/Scriptable/Actor.cpp
@@ -1105,9 +1105,9 @@ static void pcf_state(Actor *actor, ieDword /*oldValue*/, ieDword State)
 static void pcf_extstate(Actor *actor, ieDword oldValue, ieDword State)
 {
 	if ((oldValue^State)&EXTSTATE_SEVEN_EYES) {
-		int eyeCount = 7;
+		orient_t eyeCount = NNW;
 		for (ieDword mask = EXTSTATE_EYE_MIND; mask <= EXTSTATE_EYE_STONE; mask <<= 1) {
-			if (State & mask) --eyeCount;
+			if (State & mask) eyeCount = PrevOrientation(eyeCount);
 		}
 		ScriptedAnimation *sca = actor->FindOverlay(OV_SEVENEYES);
 		if (sca) {
@@ -8211,7 +8211,7 @@ void Actor::UpdateDrawingRegion()
 	}
 	
 	if (Modified[IE_STATE_ID] & STATE_BLUR) {
-		int face = GetOrientation();
+		orient_t face = GetOrientation();
 		int blurx = (OrientdX[face] * (int)Modified[IE_MOVEMENTRATE])/20;
 		int blury = (OrientdY[face] * (int)Modified[IE_MOVEMENTRATE])/20;
 		
@@ -8287,7 +8287,7 @@ void Actor::Draw(const Region& vp, Color baseTint, Color tint, BlitFlags flags) 
 	}
 
 	if (!currentStance.anim.empty()) {
-		unsigned char face = GetOrientation();
+		orient_t face = GetOrientation();
 		// Drawing the actor:
 		// * mirror images:
 		//     Drawn without transparency, unless fully invisible.
@@ -10349,9 +10349,9 @@ void Actor::ResetState()
 // orientation match with a +/-2 allowed difference
 bool Actor::IsBehind(const Actor* target) const
 {
-	unsigned char tar_orient = target->GetOrientation();
+	orient_t tar_orient = target->GetOrientation();
 	// computed, since we don't care where we face
-	unsigned char my_orient = GetOrient(target->Pos, Pos);
+	orient_t my_orient = GetOrient(target->Pos, Pos);
 
 	signed char diff;
 	for (int i=-2; i <= 2; i++) {

gemrb/core/ScriptedAnimation.cpp
@@ -231,7 +231,8 @@ ScriptedAnimation::ScriptedAnimation(DataStream* stream)
 	else if (FrameRate > 30) FrameRate = 30;
 
 	stream->ReadDword(NumOrientations);
-	stream->ReadDword(Orientation);
+	stream->ReadDword(tmp);
+	Orientation = ClampToOrientation(tmp);
 	stream->ReadDword(OrientationFlags);
 	stream->Seek( 8, GEM_CURRENT_POS ); // CResRef m_cNewPaletteRef in the original
 
@@ -433,15 +434,12 @@ void ScriptedAnimation::SetDefaultDuration(ieDword duration)
 	}
 }
 
-void ScriptedAnimation::SetOrientation(int orientation)
+void ScriptedAnimation::SetOrientation(orient_t orientation)
 {
-	if (orientation == -1) {
-		return;
-	}
 	if (NumOrientations > 1) {
-		Orientation = (ieByte) orientation;
+		Orientation = orientation;
 	} else {
-		Orientation = 0;
+		Orientation = S;
 	}
 	if (twin) {
 		twin->Orientation = Orientation;
@@ -571,7 +569,7 @@ void ScriptedAnimation::IncrementPhase()
 	Phase++;
 }
 
-bool ScriptedAnimation::UpdateDrawingState(int orientation)
+bool ScriptedAnimation::UpdateDrawingState(orient_t orientation)
 {
 	if (!(OrientationFlags & IE_VVC_FACE_FIXED)) {
 		SetOrientation(orientation);

gemrb/core/ScriptedAnimation.h
@@ -122,7 +122,7 @@ public:
 	Holder<Sprite2D> light = nullptr; // this is just a round/halftrans sprite, has no animation
 	ieDword FrameRate = ANI_DEFAULT_FRAMERATE;
 	ieDword NumOrientations = 0;
-	ieDword Orientation = 0;
+	orient_t Orientation = S;
 	ieDword OrientationFlags = 0;
 	ieDword Duration = 0xffffffff;
 	ieDword Delay = 0;
@@ -137,7 +137,7 @@ public:
 	tick_t starttime = 0;
 public:
 	//draws the next frame of the videocell
-	bool UpdateDrawingState(int orientation);
+	bool UpdateDrawingState(orient_t orientation);
 	void Draw(const Region &vp, Color tint, int height, BlitFlags flags) const;
 	Region DrawingRegion() const;
 	//sets phase (0-2)
@@ -159,7 +159,7 @@ public:
 	/* sets default duration if it wasn't set yet */
 	void SetDefaultDuration(ieDword duration);
 	/* sets up the direction of the vvc */
-	void SetOrientation(int orientation);
+	void SetOrientation(orient_t orientation);
 	/* transforms vvc to blended */
 	void SetBlend();
 	/* sets the effect owned flag */

gemrb/core/VEFObject.h
@@ -22,6 +22,8 @@
 
 #include "exports.h"
 #include "ie_types.h"
+
+#include "Orientation.h"
 #include "Region.h"
 #include "Resource.h"
 #include "RGBAColor.h"

gemrb/plugins/FXOpcodes/FXOpcodes.cpp
@@ -6438,7 +6438,7 @@ int fx_wing_buffet (Scriptable* /*Owner*/, Actor* target, Effect* fx)
 		return FX_APPLIED;
 
 	//create movement in actor
-	ieDword dir;
+	orient_t dir;
 	switch(fx->Parameter2) {
 		case WB_AWAY:
 		default:
@@ -6448,13 +6448,13 @@ int fx_wing_buffet (Scriptable* /*Owner*/, Actor* target, Effect* fx)
 			dir = GetOrient(fx->Source, target->Pos);
 			break;
 		case WB_FIXDIR:
-			dir = fx->Parameter3;
+			dir = ClampToOrientation(fx->Parameter3);
 			break;
 		case WB_OWNDIR:
 			dir = target->GetOrientation();
 			break;
 		case WB_AWAYOWNDIR:
-			dir = target->GetOrientation()^8;
+			dir = ReflectOrientation(target->GetOrientation());
 			break;
 	}
 	Point newpos=target->Pos;

commit ba5f6c52fe09619804374bddd104b8e4e5f90b50

VEFObject::UpdateDrawingState: fixed regression from 24d5915
it caused immediate deletion; an easy test is the summoning pop-in animation

it's still drawn upside down since sometime after the last release

gemrb/core/VEFObject.cpp
@@ -155,7 +155,7 @@ bool VEFObject::UpdateDrawingState(int orientation)
 			{
 				ScriptedAnimation* anim = static_cast<ScriptedAnimation*>(entry.ptr);
 				orient_t orient = orientation == -1 ? anim->Orientation : ClampToOrientation(orientation);
-				anim->UpdateDrawingState(orient);
+				ended = anim->UpdateDrawingState(orient);
 			}
 			break;
 		case VEFTypes::_2DA: