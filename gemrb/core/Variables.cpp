215fb412d42a03c764c4de4ef7544c45324951cb
Brad Allred <bradallred@me.com>
2022-03-02 21:31:03 -0700

    MakeVariable should take StringView

--- gemrb/core/Variables.cpp
+++ gemrb/core/Variables.cpp
@@ -499,7 +499,7 @@ void Variables::LoadInitialValues(const char* name)
 		if (strnicmp(buffer, name, 6) != 0) continue;
 		// copy variable (types got 2 extra spaces, and the name is padded too)
 		// (true = uppercase, needed for original engine save compat, see 315b8f2e)
-		varname = MakeVariable(buffer + 8);
+		varname = MakeVariable(StringView(buffer + 8));
 		StringToUpper(varname);
 		SetAt(varname, value);
 	}  
