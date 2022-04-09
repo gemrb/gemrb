gemrb/gemrb

commit bce82dd8bb8794b286f934c6163d5dd8542f6fe2

GameScript: GenerateTrigger should take std::string

gemrb/core/GameScript/GameScript.cpp
@@ -2489,27 +2489,28 @@ void GameScript::ExecuteAction(Scriptable* Sender, Action* aC)
 	}
 }
 
-Trigger* GenerateTrigger(char* String)
+Trigger* GenerateTrigger(std::string string)
 {
-	StringToLower(String, String + strlen(String), String);
-	ScriptDebugLog(ID_TRIGGERS, "Compiling: {}", String);
+	StringToLower(string);
+	ScriptDebugLog(ID_TRIGGERS, "Compiling: {}", string);
 
 	int negate = 0;
-	if (*String == '!') {
-		String++;
+	strpos_t start = 0;
+	if (string[start] == '!') {
+		++start;
 		negate = TF_NEGATE;
 	}
-	int len = strlench(String,'(')+1; //including (
-	int i = triggersTable->FindString(String, len);
+	strpos_t len = string.find_first_of('(', start) + 1; //including (
+	int i = triggersTable->FindString(string.c_str() + start, int(len));
 	if (i<0) {
-		Log(ERROR, "GameScript", "Invalid scripting trigger: {}", String);
+		Log(ERROR, "GameScript", "Invalid scripting trigger: {}", string);
 		return NULL;
 	}
-	const char *src = String + len;
+	const char *src = string.c_str() + len;
 	const char *str = triggersTable->GetStringIndex(i) + len;
 	Trigger *trigger = GenerateTriggerCore(src, str, i, negate);
 	if (!trigger) {
-		Log(ERROR, "GameScript", "Malformed scripting trigger: {}", String);
+		Log(ERROR, "GameScript", "Malformed scripting trigger: {}", string);
 		return NULL;
 	}
 	return trigger;

gemrb/core/GameScript/GameScript.h
@@ -1519,7 +1519,7 @@ public: //Script Functions
 
 GEM_EXPORT Action* GenerateAction(std::string String);
 Action *GenerateActionDirect(std::string string, const Scriptable *object);
-GEM_EXPORT Trigger* GenerateTrigger(char* String);
+GEM_EXPORT Trigger* GenerateTrigger(std::string string);
 
 void InitializeIEScript();
 

commit 337911ad99ebc39a8cdae631528fc7c8fbcd2f20

GenerateTrigger: fixed regression from bce82dd8, fixes #1602

gemrb/core/GameScript/GameScript.cpp
@@ -2500,13 +2500,13 @@ Trigger* GenerateTrigger(std::string string)
 		++start;
 		negate = TF_NEGATE;
 	}
-	strpos_t len = string.find_first_of('(', start) + 1; //including (
+	strpos_t len = string.find_first_of('(', start) + 1 - start; // including (
 	int i = triggersTable->FindString(string.c_str() + start, int(len));
 	if (i<0) {
 		Log(ERROR, "GameScript", "Invalid scripting trigger: {}", string);
 		return NULL;
 	}
-	const char *src = string.c_str() + len;
+	const char *src = string.c_str() + start + len;
 	const char *str = triggersTable->GetStringIndex(i) + len;
 	Trigger *trigger = GenerateTriggerCore(src, str, i, negate);
 	if (!trigger) {