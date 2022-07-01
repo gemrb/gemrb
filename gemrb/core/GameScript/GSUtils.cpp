gemrb/gemrb

commit 289c9fbe87a84934c02656429661bd3d1f3f9c10

MoveToObjectCore: remove internal wait
fixes #549, #1271
but not really correct in the CurrentActionInterruptable bit, since it makes
MoveToObject and MoveToObjectNoInterrupt almost identical.
While the actions are blocking, a subsequent Update several ticks later
will call Stop (see MoveToObjectFollow comment), clearing the action

let's see if it fixes more things than it breaks

gemrb/core/GameScript/GSUtils.cpp
@@ -1361,7 +1361,7 @@ void MoveBetweenAreasCore(Actor* actor, const ResRef &area, const Point &positio
 
 //repeat movement, until goal isn't reached
 //if int0parameter is !=0, then it will try only x times
-void MoveToObjectCore(Scriptable *Sender, const Action *parameters, ieDword flags, bool untilsee)
+void MoveToObjectCore(Scriptable *Sender, Action *parameters, ieDword flags, bool untilsee)
 {
 	Actor* actor = Scriptable::As<Actor>(Sender);
 	if (!actor) {
@@ -1392,6 +1392,9 @@ void MoveToObjectCore(Scriptable *Sender, const Action *parameters, ieDword flag
 	if (!actor->InMove() || actor->Destination != dest) {
 		actor->WalkTo(dest, flags);
 	}
+
+	Sender->CurrentActionInterruptable = false;
+
 	//hopefully this hack will prevent lockups
 	if (!actor->InMove()) {
 		if (flags&IF_NOINT) {
@@ -1401,20 +1404,14 @@ void MoveToObjectCore(Scriptable *Sender, const Action *parameters, ieDword flag
 		return;
 	}
 
-	//repeat movement...
-	Action *newaction = ParamCopyNoOverride(parameters);
-	if (newaction->int0Parameter!=1) {
-		if (newaction->int0Parameter) {
-			newaction->int0Parameter--;
+	// try just a few times (unused in original data)
+	if (parameters->int0Parameter) {
+		parameters->int0Parameter--;
+		if (!parameters->int0Parameter) {
+			actor->Interrupt();
+			Sender->ReleaseCurrentAction();
 		}
-		actor->AddActionInFront(newaction);
-		actor->SetWait(1);
-	} else {
-		delete newaction;
-		actor->Interrupt();
 	}
-
-	Sender->ReleaseCurrentAction();
 }
 
 bool CreateItemCore(CREItem *item, const ResRef &resref, int a, int b, int c)

gemrb/core/GameScript/GSUtils.h
@@ -79,7 +79,7 @@ void ChangeAnimationCore(Actor *src, const char *resref, bool effect);
 void PolymorphCopyCore(const Actor *src, Actor *tar);
 void CreateCreatureCore(Scriptable* Sender, Action* parameters, int flags);
 int MoveItemCore(Scriptable *Sender, Scriptable *target, const char *resref, int flags, int setflag, int count = 0);
-void MoveToObjectCore(Scriptable *Sender, const Action *parameters, ieDword flags, bool untilsee);
+void MoveToObjectCore(Scriptable *Sender, Action *parameters, ieDword flags, bool untilsee);
 GEM_EXPORT bool CreateItemCore(CREItem *item, const ResRef &resref, int a, int b, int c);
 void AttackCore(Scriptable *Sender, Scriptable *target, int flags);
 void InitScriptTables();

commit d869d930307aa66fed392565df5bc65829b466f5

MoveToObjectCore: don't force non-interruptability and clean up in one more case
regression from 289c9fbe, together with parent commit mostly fixes #1557

gemrb/core/GameScript/GSUtils.cpp
@@ -1395,6 +1395,9 @@ void MoveToObjectCore(Scriptable *Sender, Action *parameters, ieDword flags, boo
 		return;
 	} else {
 		if (PersonalDistance(actor, target)<MAX_OPERATING_DISTANCE) {
+			if (flags & IF_NOINT) {
+				actor->Interrupt();
+			}
 			Sender->ReleaseCurrentAction();
 			return;
 		}
@@ -1402,8 +1405,6 @@ void MoveToObjectCore(Scriptable *Sender, Action *parameters, ieDword flags, boo
 	if (!actor->InMove() || actor->Destination != dest) {
 		actor->WalkTo(dest, flags);
 	}
-
-	Sender->CurrentActionInterruptable = false;
 
 	//hopefully this hack will prevent lockups
 	if (!actor->InMove()) {