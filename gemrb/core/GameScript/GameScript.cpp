gemrb/gemrb

commit b344079ac5979962a9b6b02fc6207a73549cdce2

replace strlench with FindFirstOf

gemrb/core/GameScript/GameScript.cpp
@@ -1183,7 +1183,7 @@ static const TriggerLink* FindTrigger(StringView triggername)
 		return nullptr;
 	}
 
-	int len = strlench(triggername.c_str(), '(');
+	auto len = std::min(FindFirstOf(triggername, "("), triggername.length() - 1);
 	for (int i = 0; triggernames[i].Name; i++) {
 		if (!strnicmp(triggernames[i].Name, triggername.c_str(), len)) {
 			return triggernames + i;
@@ -1198,7 +1198,7 @@ static const ActionLink* FindAction(StringView actionname)
 		return nullptr;
 	}
 
-	int len = strlench(actionname.c_str(), '(');
+	auto len = std::min(FindFirstOf(actionname, "("), actionname.length() - 1);
 	for (int i = 0; actionnames[i].Name; i++) {
 		if (!strnicmp(actionnames[i].Name, actionname.c_str(), len)) {
 			return actionnames + i;
@@ -1213,7 +1213,7 @@ static const ObjectLink* FindObject(StringView objectname)
 		return nullptr;
 	}
 
-	int len = strlench(objectname.c_str(), '(');
+	auto len = std::min(FindFirstOf(objectname, "("), objectname.length() - 1);
 	for (int i = 0; objectnames[i].Name; i++) {
 		if (!strnicmp(objectnames[i].Name, objectname.c_str(), len)) {
 			return objectnames + i;
@@ -2526,7 +2526,8 @@ Action* GenerateAction(std::string actionString)
 	StringToLower(actionString);
 	ScriptDebugLog(ID_ACTIONS, "Compiling: {}", actionString);
 
-	int len = strlench(actionString.c_str(),'(')+1; //including (
+	auto len = actionString.find_first_of('(') + 1; //including (
+	assert(len != std::string::npos);
 	const char *src = &actionString[len];
 	int i = -1;
 	const char *str;

gemrb/core/Strings/CString.cpp
@@ -20,15 +20,6 @@
 
 namespace GemRB {
 
-/** Returns the length of string (up to a delimiter) */
-GEM_EXPORT int strlench(const char* string, char ch)
-{
-	int i;
-	for (i = 0; string[i] && string[i] != ch; i++)
-		;
-	return i;
-}
-
 #ifndef HAVE_STRNLEN
 int strnlen(const char* string, int maxlen)
 {

gemrb/core/Strings/CString.h
@@ -47,8 +47,6 @@ GEM_EXPORT int strnlen(const char* string, int maxlen);
 GEM_EXPORT size_t strlcpy(char *d, const char *s, size_t l);
 #endif
 
-GEM_EXPORT int strlench(const char* string, char ch);
-
 constexpr int NoTransform(int c) { return c; }
 
 template <typename STR_T, int(*TRANS)(int) = NoTransform>

gemrb/core/Strings/String.h
@@ -50,6 +50,16 @@ STR StringFromView(StringViewT<STR> sv)
 	return STR(sv.c_str(), sv.length());
 }
 
+template <typename STR>
+typename STR::size_type FindFirstOf(const STR& s, StringViewT<STR> sv, typename STR::size_type pos = 0) noexcept
+{
+	if (pos >= s.length() || s.empty()) {
+		return STR::npos;
+	}
+	auto iter = std::find_first_of(s.begin() + pos, s.end(), sv.begin(), sv.end());
+	return iter == s.end() ? STR::npos : std::distance(s.begin(), iter);
+}
+
 template <typename STR, typename IT>
 IT FindNotOf(IT first, IT last, StringViewT<STR> s) noexcept
 {

commit e4b0eb669c18850eda7a051d8657653de3debef6

GameScript: fixed regression from b344079
strlench return the length for strings without a delimiter
we were now checking one char too few, so Player2 to Player10 also used Player1

gemrb/core/GameScript/GameScript.cpp
@@ -1183,7 +1183,7 @@ static const TriggerLink* FindTrigger(StringView triggername)
 		return nullptr;
 	}
 
-	auto len = std::min(FindFirstOf(triggername, "("), triggername.length() - 1);
+	auto len = std::min(FindFirstOf(triggername, "("), triggername.length());
 	for (int i = 0; triggernames[i].Name; i++) {
 		if (!strnicmp(triggernames[i].Name, triggername.c_str(), len)) {
 			return triggernames + i;
@@ -1198,7 +1198,7 @@ static const ActionLink* FindAction(StringView actionname)
 		return nullptr;
 	}
 
-	auto len = std::min(FindFirstOf(actionname, "("), actionname.length() - 1);
+	auto len = std::min(FindFirstOf(actionname, "("), actionname.length());
 	for (int i = 0; actionnames[i].Name; i++) {
 		if (!strnicmp(actionnames[i].Name, actionname.c_str(), len)) {
 			return actionnames + i;
@@ -1213,7 +1213,7 @@ static const ObjectLink* FindObject(StringView objectname)
 		return nullptr;
 	}
 
-	auto len = std::min(FindFirstOf(objectname, "("), objectname.length() - 1);
+	auto len = std::min(FindFirstOf(objectname, "("), objectname.length());
 	for (int i = 0; objectnames[i].Name; i++) {
 		if (!strnicmp(objectnames[i].Name, objectname.c_str(), len)) {
 			return objectnames + i;