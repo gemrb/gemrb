gemrb/gemrb

commit 3e362c486e2df4d3d4a50f4657f4cb0585f51cf1

GUIScripts: update table index checks
None is returned for invalid values

gemrb/GUIScripts/Spellbook.py
@@ -534,7 +534,7 @@ def HasSorcererBook (pc, cls=-1):
 	import GUICommon
 
 	ClassName = GUICommon.GetClassRowName (pc)
-	if cls != -1:
+	if cls is not None:
 		ClassName = GUICommon.GetClassRowName (cls, "class")
 	SorcererBook = CommonTables.ClassSkills.GetValue (ClassName, "BOOKTYPE")
 	if SorcererBook == "*" or ClassName == "":

gemrb/GUIScripts/GUIREC.py
@@ -498,7 +498,7 @@ def GetSkills(pc):
 	if HatedRace:
 		HateTable = GemRB.LoadTable ("haterace")
 		Racist = HateTable.FindValue (1, HatedRace)
-		if Racist != -1:
+		if Racist is not None:
 			HatedRace = HateTable.GetValue (Racist, 0, GTV_REF)
 			stats.append ( (15982, HatedRace, '') )
 

gemrb/GUIScripts/LUCommon.py
@@ -385,7 +385,7 @@ def SetupHP (pc, Level=None, LevelDiff=None):
 	ClassName = None
 	if Kit and not Dual[0] and Multi[0]<2:
 		KitName = CommonTables.KitList.GetValue (Kit, 0, GTV_STR)
-		if CommonTables.Classes.GetRowIndex (KitName) >= 0:
+		if CommonTables.Classes.GetRowIndex (KitName) is not None:
 			ClassName = KitName
 
 	# determine the minimum hp roll

gemrb/GUIScripts/LUSkillsSelection.py
@@ -203,7 +203,7 @@ def SetupSkillsWindow (pc, skilltype, window, callback, level1=[0,0,0], level2=[
 		for i in range(SkillsTable.GetRowCount()-2):
 			# -2/+2 to compensate for the special first_level and rate rows
 			SkillName = SkillsTable.GetRowName (i+2)
-			if SkillsTable.GetValue (SkillName, SkillsKitName) != -1:
+			if SkillsTable.GetValue (SkillName, SkillsKitName) is not None:
 				SkillsIndices.append(i)
 
 		LevelDiff = []

gemrb/GUIScripts/TextScreen.py
@@ -77,7 +77,7 @@ def StartTextScreen ():
 		TextTable = GemRB.LoadTable ("textscrn", 1)
 		if TextTable != None:
 			TxtRow = TextTable.GetRowIndex (TableName)
-			if TxtRow >= 0:
+			if TxtRow is not None:
 				ID = TextTable.GetValue (TxtRow, 0)
 				MusicName = TextTable.GetValue (TxtRow, 1)
 				Message = TextTable.GetValue (TxtRow, 2)

gemrb/GUIScripts/bg1/CharGenGui.py
@@ -206,13 +206,13 @@ def getSkills(TextAreaControl):
 	else:
 		KitName = CommonTables.KitList.GetValue (KitName, 0)
 		
-	if SkillTable.GetValue ("RATE", KitName) != -1 or BardSkills != "*" or RangerSkills != "*":
+	if SkillTable.GetValue ("RATE", KitName) is not None or BardSkills != "*" or RangerSkills != "*":
 		for skill in range(SkillTable.GetRowCount () - 2):
 			name = SkillTable.GetValue (skill+2, 1, GTV_REF)
 			stat = SkillTable.GetValue (skill + 2, 2)
 			available = SkillTable.GetValue (SkillTable.GetRowName (skill+2), KitName)
 			value = GemRB.GetPlayerStat (MyChar, stat)
-			if value >= 0 and available != -1:
+			if value >= 0 and available is not None:
 				info += name + ": " + str(value) + "\n"
 				
 	if info != "":

gemrb/GUIScripts/bg2/CharGenCommon.py
@@ -190,13 +190,13 @@ def DisplayOverview(step):
 			else:
 				KitName = CommonTables.KitList.GetValue (KitName, 0)
 
-			if SkillTable.GetValue ("RATE", KitName) != -1:
+			if SkillTable.GetValue ("RATE", KitName) is not None:
 				for skill in range(SkillTable.GetRowCount () - 2):
 					name = SkillTable.GetValue (skill+2, 1, GTV_REF)
 					available = SkillTable.GetValue (SkillTable.GetRowName (skill+2), KitName)
 					statID = SkillTable.GetValue (skill+2, 2)
 					value = GemRB.GetPlayerStat (MyChar, statID, 1)
-					if value >= 0 and available != -1:
+					if value >= 0 and available is not None:
 						info += name + ": " + str(value) + "\n"
 			elif BardSkills != "*" or RangerSkills != "*":
 				for skill in range(SkillTable.GetRowCount () - 2):

gemrb/GUIScripts/iwd2/Feats.py
@@ -248,7 +248,7 @@ def OpenFeatsWindow(chargen=0):
 	ClassName = KitName = GUICommon.GetClassRowName (ClassIndex, "index")
 	# classcolumn is base class or 0 if it is not a kit
 	ClassColumn = CommonTables.Classes.GetValue(ClassName, "CLASS") - 1
-	if ClassColumn < 0:  #it was already a base class
+	if ClassColumn is None:  #it was already a base class
 		ClassColumn = ClassIndex
 		# feats.2da is transposed, but has the same ordering
 		FeatsClassColumn = ClassIndex + 3 # CommonTables.Classes.GetValue (ClassName, "ID") + 2

commit e79d2cb63063d76fbac233fd5836e917abe76a4e

Spellbook: fixed another regression from 3e362c48

gemrb/GUIScripts/Spellbook.py
@@ -534,7 +534,7 @@ def HasSorcererBook (pc, cls=-1):
 	import GUICommon
 
 	ClassName = GUICommon.GetClassRowName (pc)
-	if cls is not None:
+	if cls != -1:
 		ClassName = GUICommon.GetClassRowName (cls, "class")
 	SorcererBook = CommonTables.ClassSkills.GetValue (ClassName, "BOOKTYPE")
 	if SorcererBook == "*" or ClassName == "":