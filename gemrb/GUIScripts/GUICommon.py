gemrb/gemrb

commit 4139830ea67f5632a1192c1d0cecca5edc71ea6f

IsDualClassed: simplify
KittedClassIndex is only ever properly set if KitIndex is not 0,
while FirstClassIndex can't be 0

gemrb/GUIScripts/GUICommon.py
@@ -401,12 +401,8 @@ def IsDualClassed(actor, verbose):
 	Return[2] contains the class index of the new class.
 	If verbose is false, only Return[0] contains useable data."""
 
-	if GameCheck.IsIWD2():
-		return (0,-1,-1)
-
 	Multi = HasMultiClassBits (actor)
-
-	if Multi == 0:
+	if Multi == 0: # also catches iwd2
 		return (0, -1, -1)
 
 	DualedFrom = GemRB.GetPlayerStat (actor, IE_MC_FLAGS) & MC_WAS_ANY_CLASS
@@ -440,7 +436,7 @@ def IsDualClassed(actor, verbose):
 		print(DualedFrom, Multi, KitIndex, FirstClassIndex)
 		return (0, -1, -1)
 
-	if KittedClassIndex == FirstClassIndex and KitIndex:
+	if KittedClassIndex == FirstClassIndex:
 		return (1, KitIndex, SecondClassIndex)
 	elif KittedClassIndex == SecondClassIndex:
 		return (3, FirstClassIndex, KitIndex)

commit 5792057e29ccb9f92dbce876753c0b11b7e3a2d7

IsDualClassed: fixed regression from 4139830e
it's an index, so it can indeed be 0

gemrb/GUIScripts/GUICommon.py
@@ -440,7 +440,7 @@ def IsDualClassed(actor, verbose):
 		print(DualedFrom, Multi, KitIndex, FirstClassIndex)
 		return (0, -1, -1)
 
-	if KittedClassIndex == FirstClassIndex:
+	if KittedClassIndex == FirstClassIndex and KitIndex:
 		return (1, KitIndex, SecondClassIndex)
 	elif KittedClassIndex == SecondClassIndex:
 		return (3, FirstClassIndex, KitIndex)