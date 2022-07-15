gemrb/gemrb

commit 090cb6d5229441eac823490c09f3fd30d77d5635

use ieVariable for music playlists
adds DisableMusicPlaylist to core for const correctness

gemrb/plugins/MUSImporter/MUSImporter.cpp
@@ -66,16 +66,16 @@ bool MUSImporter::Init()
 }
 
 /** Loads a PlayList for playing */
-bool MUSImporter::OpenPlaylist(const char* name)
+bool MUSImporter::OpenPlaylist(const ieVariable& name)
 {
-	if (Playing || CurrentPlayList(name)) {
+	if (Playing || IsCurrentPlayList(name)) {
 		return true;
 	}
 	core->GetAudioDrv()->ResetMusics();
 	playlist.clear();
 	PLpos = 0;
-	PLName[0] = '\0';
-	if (name[0] == '*') {
+	PLName.Reset();
+	if (IsStar(name)) {
 		return false;
 	}
 	char path[_MAX_PATH];
@@ -85,7 +85,7 @@ bool MUSImporter::OpenPlaylist(const char* name)
 		Log(ERROR, "MUSImporter", "Didn't find playlist '{}'.", path);
 		return false;
 	}
-	strret_t c = str->ReadLine( PLName, 32 );
+	strret_t c = str->ReadLine(PLName.begin(), 32);
 	while (c > 0) {
 		if (isblank(PLName[c - 1]))
 			PLName[c - 1] = 0;
@@ -196,12 +196,12 @@ void MUSImporter::HardEnd()
 }
 
 /** Switches the current PlayList while playing the current one, return nonzero on error */
-int MUSImporter::SwitchPlayList(const char* name, bool Hard)
+int MUSImporter::SwitchPlayList(const ieVariable& name, bool Hard)
 {
 	if (Playing) {
 		//don't do anything if the requested song is already playing
 		//this fixed PST's infinite song start
-		if (CurrentPlayList(name))
+		if (IsCurrentPlayList(name))
 			return 0;
 		if (Hard) {
 			HardEnd();
@@ -211,7 +211,7 @@ int MUSImporter::SwitchPlayList(const char* name, bool Hard)
 		//if still playing, then don't insist on trying to open it now
 		//either HardEnd stopped it for us, or End marked it for early ending
 		if (Playing) {
-			strlcpy(PLNameNew, name, sizeof(PLNameNew) );
+			PLNameNew = name;
 			return 0;
 		}
 	}
@@ -266,7 +266,7 @@ void MUSImporter::PlayMusic(int pos)
 	PlayMusic( playlist[pos].PLFile );
 }
 
-void MUSImporter::PlayMusic(char* name)
+void MUSImporter::PlayMusic(const ieVariable& name)
 {
 	char FName[_MAX_PATH];
 	if (strnicmp( name, "mx9000", 6 ) == 0) { //iwd2
@@ -275,7 +275,7 @@ void MUSImporter::PlayMusic(char* name)
 		PathJoin(FName, "mx0000", name, nullptr);
 	} else if (strnicmp( name, "SPC", 3 ) != 0) { //bg2
 		char File[_MAX_PATH];
-		snprintf(File, _MAX_PATH, "%s%s", PLName, name);
+		fmt::format_to(File, "{}{}", PLName, name);
 		PathJoin(FName, PLName, File, nullptr);
 	} else {
 		strlcpy(FName, name, _MAX_PATH);
@@ -293,8 +293,8 @@ void MUSImporter::PlayMusic(char* name)
 	Log(MESSAGE, "MUSImporter", "Playing {}...", FName);
 }
 
-bool MUSImporter::CurrentPlayList(const char* name) {
-	return stricmp(name, PLName) == 0;
+bool MUSImporter::IsCurrentPlayList(const ieVariable& name) {
+	return name == PLName;
 }
 
 #include "plugindef.h"

gemrb/core/GameScript/Actions.cpp
@@ -1738,8 +1738,8 @@ void GameScript::StartSong(Scriptable* /*Sender*/, Action* parameters)
 {
 	//the force play logic should be handled by SwitchPlayList
 	bool force;
-	char* poi = core->GetMusicPlaylist( parameters->int0Parameter );
-	if (!poi) return;
+	const ieVariable& poi = core->GetMusicPlaylist(parameters->int0Parameter);
+	if (IsStar(poi)) return;
 
 	//if parameter is force, force the music, otherwise just schedule it for next
 	if (parameters->int1Parameter==1) {
@@ -1749,7 +1749,7 @@ void GameScript::StartSong(Scriptable* /*Sender*/, Action* parameters)
 	}
 	int ret = core->GetMusicMgr()->SwitchPlayList( poi, force );
 	if (ret) {
-		*poi = '*';
+		core->DisableMusicPlaylist(parameters->int0Parameter);
 	}
 }
 

gemrb/core/Interface.cpp
@@ -283,10 +283,6 @@ Interface::~Interface() noexcept
 
 	delete sgiterator;
 
-	for (auto& track : musiclist) {
-		free((void *) track);
-	}
-
 	DamageInfoMap.clear();
 
 	delete plugin_flags;
@@ -541,7 +537,7 @@ bool Interface::ReadMusicTable(const ResRef& tablename, int col) {
 		return false;
 
 	for (TableMgr::index_t i = 0; i < tm->GetRowCount(); i++) {
-		musiclist.push_back(strdup(tm->QueryField(i, col).c_str()));
+		musiclist.push_back(tm->QueryField(i, col));
 	}
 
 	return true;
@@ -587,12 +583,17 @@ bool Interface::ReadSoundChannelsTable() const
 	return true;
 }
 
-//Not a constant anymore, we let the caller set the entry to zero
-char *Interface::GetMusicPlaylist(int SongType) const {
-	if (SongType < 0 || (unsigned int)SongType >= musiclist.size())
-		return NULL;
-
+const static ieVariable star("*");
+const ieVariable& Interface::GetMusicPlaylist(size_t SongType) const {
+	if (SongType >= musiclist.size()) return star;
 	return musiclist[SongType];
+}
+
+void Interface::DisableMusicPlaylist(size_t SongType)
+{
+	if (SongType < musiclist.size()) {
+		musiclist[SongType] = star;
+	}
 }
 
 /** this is the main loop */

gemrb/core/Interface.h
@@ -444,7 +444,7 @@ public:
 	std::vector<Holder<Sprite2D> > Cursors;
 	Holder<Sprite2D> FogSprites[16] {};
 	Holder<Sprite2D> GroundCircles[MAX_CIRCLE_SIZE][6] {};
-	std::vector<char *> musiclist;
+	std::vector<ieVariable> musiclist;
 	std::multimap<ieDword, DamageInfoStruct> DamageInfoMap;
 	TimeStruct Time{};
 public:
@@ -703,8 +703,8 @@ public:
 	/** Opens CD prompt window and waits for the specified disc */
 	void WaitForDisc(int disc_number, const char* path) const;
 	/** Returns the music playlist corresponding to the provided type */
-	/** it allows scrapping the entry, hence it isn't const */
-	char *GetMusicPlaylist(int SongType) const;
+	const ieVariable& GetMusicPlaylist(size_t SongType) const;
+	void DisableMusicPlaylist(size_t SongType);
 	/** Returns the DeathVarFormat of the day */
 	static const char *GetDeathVarFormat();
 	/** Saves config variables to a file */

gemrb/core/Map.cpp
@@ -2503,27 +2503,26 @@ bool Map::SpawnsAlive() const
 
 void Map::PlayAreaSong(int SongType, bool restart, bool hard) const
 {
-	//Ok, we use a non constant pointer here, so it is easy to disable
-	//a faulty music list on the fly. I don't want to add a method just for that
-	//crap when we already have that pointer at hand!
-	char* poi = core->GetMusicPlaylist(SongList[SongType]);
+	size_t pl = SongList[SongType];
+	const ieVariable* poi = &core->GetMusicPlaylist(pl);
 	// for subareas fall back to the main list
 	// needed eg. in bg1 ar2607 (intro candlekeep ambush south)
 	// it's not the correct music, perhaps it needs the one from the master area
 	// it would match for ar2607 and ar2600, but very annoying (see GetMasterArea)
 	// ... but this is also definitely wrong for iwd
-	if (!poi && !MasterArea && SongType == SONG_BATTLE && core->HasFeature(GF_BREAKABLE_WEAPONS)) {
-		poi = core->GetMusicPlaylist(SongType);
+	if (IsStar(*poi) && !MasterArea && SongType == SONG_BATTLE && core->HasFeature(GF_BREAKABLE_WEAPONS)) {
+		poi = &core->GetMusicPlaylist(SongType);
+		pl = SongType;
 	}
-	if (!poi) return;
+
+	if (IsStar(*poi)) return;
 
 	//check if restart needed (either forced or the current song is different)
-	if (!restart && core->GetMusicMgr()->CurrentPlayList(poi)) return;
-	int ret = core->GetMusicMgr()->SwitchPlayList( poi, hard );
-	//Here we disable the faulty musiclist entry
+	if (!restart && core->GetMusicMgr()->IsCurrentPlayList(*poi)) return;
+	int ret = core->GetMusicMgr()->SwitchPlayList(*poi, hard);
 	if (ret) {
-		//Apparently, the playlist manager prefers a *
-		*poi='*';
+		//Here we disable the faulty musiclist entry
+		core->DisableMusicPlaylist(pl);
 		return;
 	}
 	if (SongType == SONG_BATTLE) {

gemrb/core/MusicMgr.h
@@ -35,15 +35,15 @@ public:
 	/** Initializes the PlayList Manager */
 	virtual bool Init() = 0;
 	/** Loads a PlayList for playing */
-	virtual bool OpenPlaylist(const char* name) = 0;
+	virtual bool OpenPlaylist(const ieVariable& name) = 0;
 	/** Switches the current PlayList while playing the current one, return nonzero on error */
-	virtual int SwitchPlayList(const char* name, bool Hard) = 0;
+	virtual int SwitchPlayList(const ieVariable& name, bool Hard) = 0;
 	/** Plays the Next Entry */
 	virtual void PlayNext() = 0;
 	/** Returns whether music is currently playing */
 	virtual bool IsPlaying() = 0;
 	/** Returns whether given playlist is currently loaded */
-	virtual bool CurrentPlayList(const char* name) = 0;
+	virtual bool IsCurrentPlayList(const ieVariable& name) = 0;
 };
 
 }

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -3999,11 +3999,11 @@ PyDoc_STRVAR( GemRB_LoadMusicPL__doc,
 
 static PyObject* GemRB_LoadMusicPL(PyObject * /*self*/, PyObject* args)
 {
-	char *ResRef;
+	const char* pl = nullptr;
 	int HardEnd = 0;
-	PARSE_ARGS( args,  "s|i", &ResRef, &HardEnd );
+	PARSE_ARGS(args,  "s|i", &pl, &HardEnd);
 
-	core->GetMusicMgr()->SwitchPlayList( ResRef, (bool) HardEnd );
+	core->GetMusicMgr()->SwitchPlayList(ieVariable(pl), HardEnd);
 
 	Py_RETURN_NONE;
 }

gemrb/plugins/MUSImporter/MUSImporter.h
@@ -46,8 +46,8 @@ class MUSImporter : public MusicMgr {
 private:
 	bool Initialized = false;
 	bool Playing = false;
-	char PLName[32]{};
-	char PLNameNew[32]{};
+	ieVariable PLName;
+	ieVariable PLNameNew;
 	int PLpos = 0;
 	int PLnext = -1;
 	FileStream* str;
@@ -56,18 +56,18 @@ private:
 	ResourceManager manager;
 private:
 	void PlayMusic(int pos);
-	void PlayMusic(char* name);
+	void PlayMusic(const ieVariable& name);
 public:
 	MUSImporter();
 	MUSImporter(const MUSImporter&) = delete;
 	~MUSImporter() override;
 	MUSImporter& operator=(const MUSImporter&) = delete;
 	/** Loads a PlayList for playing */
-	bool OpenPlaylist(const char* name) override;
+	bool OpenPlaylist(const ieVariable& name) override;
 	/** Initializes the PlayList Manager */
 	bool Init() override;
 	/** Switches the current PlayList while playing the current one */
-	int SwitchPlayList(const char* name, bool Hard) override;
+	int SwitchPlayList(const ieVariable& name, bool Hard) override;
 	/** Ends the Current PlayList Execution */
 	void End() override;
 	void HardEnd() override;
@@ -78,7 +78,7 @@ public:
 	/** Returns whether music is currently playing */
 	bool IsPlaying() override { return Playing; }
 	/** Returns whether given playlist is currently loaded */
-	bool CurrentPlayList(const char* name) override;
+	bool IsCurrentPlayList(const ieVariable& name) override;
 };
 
 }

commit df2d75f2e34962106ce72bd228db4614294c6284

MUSImporter: fixed regression from introducing ReadVariable in 090cb6d
fixes the rest of #1640

gemrb/core/Streams/DataStream.h
@@ -140,7 +140,9 @@ public:
 	}
 	
 	strret_t WriteFilling(strpos_t len);
-	
+
+	// NOTE: RTrim doesn't cut it when reading text files, since we may accintally read more than one line
+	// so in those situations rather use ReadLine and then convert to desired string type
 	template <typename STR>
 	strret_t ReadRTrimString(STR& dest, size_t len) {
 		strret_t read = Read(dest.begin(), len);

gemrb/plugins/MUSImporter/MUSImporter.cpp
@@ -85,7 +85,9 @@ bool MUSImporter::OpenPlaylist(const ieVariable& name)
 		Log(ERROR, "MUSImporter", "Didn't find playlist '{}'.", path);
 		return false;
 	}
-	strret_t c = str->ReadVariable(PLName);
+	std::string line;
+	strret_t c = str->ReadLine(line);
+	PLName = line;
 	while (c > 0) {
 		if (isblank(PLName[c - 1]))
 			PLName[c - 1] = 0;
@@ -93,7 +95,7 @@ bool MUSImporter::OpenPlaylist(const ieVariable& name)
 			break;
 		c--;
 	}
-	std::string line;
+
 	str->ReadLine(line, 5);
 	int count = atoi(line.c_str());
 	while (count != 0) {