gemrb/gemrb

commit fd233a1f8b59c7db6cec5a40df59466b5a749908

cleanup several more .c_str() calls

gemrb/core/CharAnimations.cpp
@@ -1396,7 +1396,7 @@ void CharAnimations::GetAnimResRef(unsigned char StanceID,
 		case IE_ANI_PST_STAND:
 			NewResRef = ResRefBase[0];
 			NewResRef += "STD";
-			NewResRef += *(ResRefBase.CString() + 1);
+			NewResRef += ResRefBase[1];
 			Cycle = SixteenToFive[Orient];
 			break;
 		case IE_ANI_PST_GHOST: // pst static animations

gemrb/core/DisplayMessage.cpp
@@ -58,7 +58,7 @@ DisplayMessage::StrRefs::StrRefs()
 
 bool DisplayMessage::StrRefs::LoadTable(const std::string& name)
 {
-	AutoTable tab = gamedata->LoadTable(ResRef(name.c_str()));
+	AutoTable tab = gamedata->LoadTable(name);
 	if (tab) {
 		for(int i=0;i<STRREF_COUNT;i++) {
 			table[i] = tab->QueryFieldAsStrRef(i,0);

gemrb/core/GUI/View.cpp
@@ -294,7 +294,7 @@ void View::Draw()
 				ScriptingId id = ref->Id;
 				id &= 0x00000000ffffffff; // control id is lower 32bits
 
-				std::string formatted = fmt::format("id: {}  grp: {}  \nflgs: {}\ntype:{}", id, ref->ScriptingGroup().CString(), flags, typeid(*this).name());
+				std::string formatted = fmt::format("id: {}  grp: {}  \nflgs: {}\ntype:{}", id, ref->ScriptingGroup(), flags, typeid(*this).name());
 				const String* string = StringFromCString(formatted.c_str());
 				assert(string);
 				

gemrb/core/GameScript/Triggers.cpp
@@ -4197,12 +4197,12 @@ int GameScript::UsedExit(Scriptable *Sender, const Trigger *parameters)
 
 	TableMgr::index_t count = tm->GetRowCount();
 	for (TableMgr::index_t i = 0; i < count; ++i) {
-		const char *area = tm->QueryField( i, 0 ).c_str();
+		const ResRef area = tm->QueryField(i, 0);
 		if (actor->LastArea != area) {
 			continue;
 		}
-		const char *exit = tm->QueryField( i, 1 ).c_str();
-		if (strnicmp(actor->UsedExit.CString(), exit, 32) != 0) {
+		const ieVariable exit = tm->QueryField(i, 1);
+		if (actor->UsedExit != exit) {
 			continue;
 		}
 		return 1;

gemrb/core/Interface.cpp
@@ -2423,9 +2423,9 @@ int Interface::PlayMovie(const ResRef& movieRef)
 				const char* rowName = sttable->GetRowName(i).c_str();
 				if (!std::isdigit(rowName[0])) continue; // this skips the initial palette rows (red, blue, green)
 
-				const char* frameField = sttable->QueryField(i, 0).c_str();
+				int frameField = sttable->QueryFieldSigned<int>(i, 0);
 				ieStrRef strField = sttable->QueryFieldAsStrRef(i, 1);
-				subs[atoi(frameField)] = strField;
+				subs[frameField] = strField;
 			}
 		}
 
@@ -4320,7 +4320,7 @@ std::vector<ieDword>* Interface::GetListFrom2DAInternal(const ResRef& resref)
 	if (tab) {
 		ret->resize(tab->GetRowCount());
 		for (size_t i = 0; i < ret->size(); ++i) {
-			ret->at(i) = strtounsigned<ieDword>(tab->QueryField(i, 0).c_str());
+			ret->at(i) = tab->QueryFieldUnsigned<ieDword>(i, 0);
 		}
 	}
 	return ret;

gemrb/core/KeyMap.cpp
@@ -104,19 +104,19 @@ bool KeyMap::InitializeKeyMap(const char* inifile, const ResRef& tablefile)
 
 		ieVariable moduleName;
 		ieVariable function;
-		const char *group;
+		int group;
 
 		if (kmtable->GetRowIndex(name) != TableMgr::npos) {
 			moduleName = kmtable->QueryField(name, "MODULE");
 			function = kmtable->QueryField(name, "FUNCTION");
-			group = kmtable->QueryField(name, "GROUP").c_str();
+			group = kmtable->QueryFieldSigned<int>(name, "GROUP");
 		} else {
 			moduleName = kmtable->QueryField("Default","MODULE");
 			function = kmtable->QueryField("Default","FUNCTION");
-			group = kmtable->QueryField("Default","GROUP").c_str();
+			group = kmtable->QueryFieldSigned<int>("Default","GROUP");
 			Log(MESSAGE, "KeyMap", "Adding key {} with function {}::{}", value, moduleName, function);
 		}
-		Function *fun = new Function(moduleName, function, atoi(group), tolower(value[0]));
+		Function *fun = new Function(moduleName, function, group, tolower(value[0]));
 
 		// lookup by either key or name
 		keymap.SetAt(value, fun);

gemrb/core/Scriptable/Actor.cpp
@@ -1812,8 +1812,7 @@ static void InitActorTables()
 			if (tm->QueryFieldSigned<int>( i, 1)) {
 				hc_locations|=mask;
 			}
-			const char *flags = tm->QueryField(i, 2).c_str();
-			hc_flags[i] = atoi(flags);
+			hc_flags[i] = tm->QueryFieldSigned<int>(i, 2);
 			mask<<=1;
 		}
 	}
@@ -1824,7 +1823,7 @@ static void InitActorTables()
 		tm = gamedata->LoadTable("csound");
 		if (tm) {
 			for (int i = 0; i < VCONST_COUNT; i++) {
-				const char *suffix = tm->QueryField(i, 0).c_str();
+				const auto& suffix = tm->QueryField(i, 0);
 				switch(suffix[0]) {
 					case '*': break;
 					//I have no idea what this ! mean
@@ -2302,8 +2301,8 @@ static void InitActorTables()
 		if (count> 0 && count<8) {
 			avPrefix.resize(count - 1);
 			avBase = tm->QueryFieldSigned<int>(0, 0);
-			const char *poi = tm->QueryField(0,1).c_str();
-			if (*poi!='*') {
+			const auto& poi = tm->QueryField(0,1);
+			if (poi[0] != '*') {
 				avStance = tm->QueryFieldSigned<int>(0,1);
 			} else {
 				avStance = -1;
@@ -2328,7 +2327,7 @@ static void InitActorTables()
 		for (TableMgr::index_t i = 0; i < racesNRows; i++) {
 			int raceID = tm->QueryFieldSigned<int>(i, 3);
 			int favClass = tm->QueryFieldSigned<int>(i, 8);
-			const char *raceName = tm->GetRowName(i).c_str();
+			const auto& raceName = tm->GetRowName(i);
 			favoredMap.emplace(raceID, favClass);
 			raceID2Name.emplace(raceID, raceName);
 		}
@@ -3571,8 +3570,8 @@ bool Actor::HasSpecialDeathReaction(const ieVariable& deadname) const
 {
 	AutoTable tm = gamedata->LoadTable("death");
 	if (!tm) return false;
-	const char *value = tm->QueryField(scriptName.CString(), deadname).c_str();
-	return value && value[0] != '0';
+	const auto& value = tm->QueryField(scriptName.CString(), deadname);
+	return value[0] != '0';
 }
 
 void Actor::ReactToDeath(const ieVariable& deadname)
@@ -10831,8 +10830,7 @@ int Actor::UpdateAnimationID(bool derived)
 		StatID = av.stat;
 		StatID = derived?GetSafeStat(StatID):GetBase( StatID );
 
-		const char *poi = tm->QueryField( StatID ).c_str();
-		AnimID += strtosigned<int>(poi);
+		AnimID += tm->QueryFieldSigned<int>(StatID, 0);
 	}
 	if (BaseStats[IE_ANIMATION_ID]!=(unsigned int) AnimID) {
 		SetBase(IE_ANIMATION_ID, (unsigned int) AnimID);

commit 6d58215e5e3324bf9b44837036229cc756683a28

CharAnimations::GetAnimResRef: fixed regression from fd233a1f
fixes qui-sai not being drawn in pst ar0601

gemrb/core/CharAnimations.cpp
@@ -1403,7 +1403,7 @@ void CharAnimations::GetAnimResRef(unsigned char StanceID,
 			break;
 
 		case IE_ANI_PST_STAND:
-			NewResRef.Format("{}{}{}", ResRefBase[0], "STD", ResRefBase[1]);
+			NewResRef.Format("{}STD{}{}{}{}", ResRefBase[0], ResRefBase[1], ResRefBase[2], ResRefBase[3], ResRefBase[4]);
 			Cycle = SixteenToFive[Orient];
 			break;
 		case IE_ANI_PST_GHOST: // pst static animations