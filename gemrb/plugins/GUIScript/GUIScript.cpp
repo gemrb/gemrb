3b4af44b3d4a589f277f8250fff6f0f19ac8ac92
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2022-08-29 21:07:31 +0200

    GemRB_Table_GetValue: add python compat for <3.10

--- gemrb/plugins/GUIScript/GUIScript.cpp
+++ gemrb/plugins/GUIScript/GUIScript.cpp
@@ -682,7 +682,7 @@ static PyObject* GemRB_Table_GetValue(PyObject* self, PyObject* args)
 	AutoTable tm = CObject<TableMgr, std::shared_ptr>(self);
 	ABORT_IF_NULL(tm);
 	
-	if (!Py_IsNone(row) && !Py_IsNone(col) && !PyObject_TypeCheck(row, Py_TYPE(col))) {
+	if (row != Py_None && col != Py_None && !PyObject_TypeCheck(row, Py_TYPE(col))) {
 		return AttributeError("RowIndex/RowString and ColIndex/ColString must be the same type.");
 	}
 	
