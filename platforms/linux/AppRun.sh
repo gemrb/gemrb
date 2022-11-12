addadd50775e78a485ba79d2abfa2cc200202d4f
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2022-11-12 13:27:29 +0100

    appimage: try nesting python one level less

--- platforms/linux/AppRun.sh
+++ platforms/linux/AppRun.sh
@@ -5,9 +5,9 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 APPDIR="${APPDIR:-$SCRIPTPATH}"
 
 export PYTHONPATH="$APPDIR/usr/share/pyshared/"
-echo ":$GITHUB_ACTIONS:"
-if [[ -z $GITHUB_ACTIONS ]]; then
+#echo ":$GITHUB_ACTIONS:"
+#if [[ -z $GITHUB_ACTIONS ]]; then
   export PYTHONHOME="$APPDIR/usr/"
-fi
+#fi
 
 "$APPDIR"/usr/bin/gemrb "$@"
