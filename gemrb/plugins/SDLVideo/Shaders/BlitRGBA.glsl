22e76dd48bbb1c4799b68a44e1b8fa29eb6640ac
MarcelHB <marscel@googlemail.com>
2022-11-14 03:01:48 +0100

    SDL: fixes alpha channel in shader for grey mode
    fixes #1712

--- gemrb/plugins/SDLVideo/Shaders/BlitRGBA.glsl
+++ gemrb/plugins/SDLVideo/Shaders/BlitRGBA.glsl
@@ -21,7 +21,7 @@ void main() {
 
 	if (u_greyMode == 1) {
 		float grey = (color.r + color.g + color.b)*0.333333;
-		gl_FragColor = vec4(grey, grey, grey, color.a);
+		gl_FragColor = vec4(grey, grey, grey, gl_FragColor.a);
 	} else if (u_greyMode == 2) {
 		float grey = (color.r + color.g + color.b)*0.333333;
 		gl_FragColor = vec4(grey + (21.0/256.0), grey, max(0.0, grey - (32.0/256.0)), gl_FragColor.a);
