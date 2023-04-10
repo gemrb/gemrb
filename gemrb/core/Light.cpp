gemrb/gemrb

commit c365af4d97f0faaa87e7b576f800c1b06b6ff4c8

fixed some recent sonar warnings

gemrb/core/Light.cpp
@@ -48,7 +48,7 @@ Holder<Sprite2D> CreateLight(const Size& size, uint8_t intensity) noexcept
 	uint8_t* px = (uint8_t*)calloc(size.w, size.h);
 	
 	const Point& radii = size.Center();
-	const float maxr = std::max(radii.x, radii.y);
+	const float maxr = std::max<float>(radii.x, radii.y);
 	Region r(Point() - radii, size);
 	
 	const auto points = PlotEllipse(r);
@@ -62,10 +62,10 @@ Holder<Sprite2D> CreateLight(const Size& size, uint8_t intensity) noexcept
 		assert(q3.y == q4.y);
 		
 		for (int x = q1.x; x >= 0; --x) {
-			// by the power of Pythagurus
-			int hyp = std::hypot(x, q1.y);
-			int dist = (hyp / maxr) * intensity;
-			assert(dist >= 0 && dist <= intensity);
+			// by the power of Pythagoras
+			float hyp = std::hypot(x, q1.y);
+			uint8_t dist = static_cast<uint8_t>((hyp / maxr) * intensity);
+			assert(dist <= intensity);
 			uint8_t light = Clamp<uint8_t>(intensity - dist, 0, intensity);
 			px[(radii.y + q1.y) * size.w + radii.x + x] = light; // quad 1
 			px[(radii.y + q2.y) * size.w + radii.x - x] = light; // quad 2

gemrb/core/EnumIndex.h
@@ -104,7 +104,7 @@ class EnumBitset {
 public:
 	static constexpr auto size = UnderType(ENUM::count);
 		
-	constexpr EnumBitset(under_t<ENUM> value) : bits(value) {}
+	explicit constexpr EnumBitset(under_t<ENUM> value) : bits(value) {}
 	constexpr EnumBitset() = default;
 	
 	constexpr

gemrb/plugins/PSTOpcodes/PSTOpcodes.cpp
@@ -545,7 +545,7 @@ int fx_tint_screen (Scriptable* /*Owner*/, Actor* /*target*/, Effect* fx)
 			// extend the duration for some time of the fade-in
 			if (fx->FirstApply) {
 				core->timer.SetFadeToColor(toTime, 2);
-				fx->Parameter6 = toTime + 1;
+				fx->Parameter6 = static_cast<ieDword>(toTime) + 1;
 				fx->Duration += toTime;
 			} else if (fx->Parameter6 != 0) {
 				fx->Parameter6--;

gemrb/plugins/SDLVideo/SDLVideo.cpp
@@ -339,7 +339,7 @@ void SDLVideoDriver::BlitSpriteClipped(const Holder<Sprite2D>& spr, Region src, 
 		flags ^= BlitFlags::MIRRORY;
 	}
 
-	flags |= spr->renderFlags & (BlitFlags::BLEND_MASK);
+	flags |= spr->renderFlags & BlitFlags::BLEND_MASK;
 
 	if (!spr->HasTransparency()) {
 		flags &= ~BlitFlags::BLENDED;

commit 3d063a56a85ddf3571011d2456e341d6aad38716

CreateLight: fixed regression from c365af4
could cause an off-by-1 and then hitting the assert

gemrb/core/Light.cpp
@@ -63,7 +63,7 @@ Holder<Sprite2D> CreateLight(const Size& size, uint8_t intensity) noexcept
 		
 		for (int x = q1.x; x >= 0; --x) {
 			// by the power of Pythagoras
-			float hyp = std::hypot(x, q1.y);
+			uint8_t hyp = std::hypot<uint8_t>(x, q1.y);
 			uint8_t dist = static_cast<uint8_t>((hyp / maxr) * intensity);
 			assert(dist <= intensity);
 			uint8_t light = Clamp<uint8_t>(intensity - dist, 0, intensity);