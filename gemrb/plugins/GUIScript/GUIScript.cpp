gemrb/gemrb

commit 35bdc660cd8b46b3d14f27ad1e0492571e6abd09

refactor how we give feedback when resting isn't possible
paves the way to fixing #658

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -14248,7 +14248,10 @@ scattered). It is possible to play a movie or dream too.\n\
   * movie - a number, see restmov.2da\n\
   * hp    - hit points healed, 0 means full healing\n\
 \n\
-**Return value:** N/A\n\
+**Return value:** dict\n\
+  * Error: True if resting was prevented by one of the checks\n\
+  * ErrorMsg: a strref with a reason for the error\n\
+  * Cutscene: True if a cutscene needs to run\n\
 \n\
 **See also:** StartStore(gamescript)"
 );
@@ -14263,7 +14266,24 @@ static PyObject* GemRB_RestParty(PyObject * /*self*/, PyObject* args)
 	}
 	GET_GAME();
 
-	return PyInt_FromLong(game->RestParty(noareacheck, dream, hp));
+	// check if resting is possible and if not, return the reason, otherwise rest
+	// Error feedback is eventually handled this way:
+	// - resting outside: popup an error window with the reason in pst, print it to message window elsewhere
+	// - resting in inns: popup a GUISTORE error window with the reason
+	// - if the particular reason strref does not exist, don't print anything
+	PyObject* dict = PyDict_New();
+	int cannotRest = game->CanPartyRest(noareacheck);
+	PyDict_SetItemString(dict, "Error", PyBool_FromLong(cannotRest != 0));
+	if (cannotRest) {
+		PyDict_SetItemString(dict, "ErrorMsg", PyInt_FromLong(cannotRest));
+		PyDict_SetItemString(dict, "Cutscene", PyBool_FromLong(0));
+	} else {
+		PyDict_SetItemString(dict, "ErrorMsg", PyInt_FromLong(-1));
+		// all is well, so do the actual resting
+		PyDict_SetItemString(dict, "Cutscene", PyBool_FromLong(game->RestParty(0, dream, hp)));
+	}
+
+	return dict;
 }
 
 PyDoc_STRVAR( GemRB_ChargeSpells__doc,

gemrb/GUIScripts/GUICommonWindows.py
@@ -2090,7 +2090,14 @@ def RealRestPress ():
 	# only bg2 has area-based rest movies
 	# outside movies start at 2, 1 is for inns
 	# 15 means run all checks to see if resting is possible
-	GemRB.RestParty(15, 0 if GameCheck.IsBG2() else 2, 1)
+	info = GemRB.RestParty(15, 0 if GameCheck.IsBG2() else 2, 1)
+	if info["Error"] and info["ErrorMsg"] > 0:
+		if GameCheck.IsPST ():
+			# TODO: open error window
+			pass
+		else:
+			GemRB.DisplayString (info["ErrorMsg"], 0xff0000)
+
 	return
 
 def SwitchPCByKey (wIdx, key, mod):

gemrb/GUIScripts/GUISTORE.py
@@ -1987,13 +1987,18 @@ def RentConfirm ():
 	Gold = GemRB.GameGetPartyGold ()
 	GemRB.GameSetPartyGold (Gold-price)
 	# TODO: run GemRB.RunRestScripts ()
-	cutscene = GemRB.RestParty (2, 1, RentIndex+1) # 2 = REST_SCATTER, check that everyone is close by
+	info = GemRB.RestParty (2, 1, RentIndex+1) # 2 = REST_SCATTER, check that everyone is close by
+	cutscene = info["Cutscene"]
+	if info["Error"] and info["ErrorMsg"] > 0:
+		# TODO: open error modal with reason
+		pass
+
 	if RentConfirmWindow:
 		RentConfirmWindow.Unload ()
 	Window = StoreRentWindow
 	if cutscene:
 		CloseStoreWindow ()
-	else:
+	elif not info["Error"]:
 		TextArea = Window.GetControl (12)
 		#is there any way to change this???
 		GemRB.SetToken ("HP", "%d"%(RentIndex+1))

gemrb/core/Game.cpp
@@ -1710,13 +1710,16 @@ void Game::TextDream()
 	}
 }
 
-bool Game::CanPartyRest(int checks) const
+// returns 0 if it can
+// returns strref or -1 if it can't
+int Game::CanPartyRest(int checks) const
 {
-	if (checks == REST_NOCHECKS) return true;
+	if (checks == REST_NOCHECKS) return 0;
 
 	if (checks & REST_MOVE) {
 		if (!EveryoneStopped()) {
-			return false;
+			// no string in any game — not sure anyone checked for this at all
+			return displaymsg->GetStringReference(STR_SCATTERED);
 		}
 	}
 
@@ -1727,21 +1730,18 @@ bool Game::CanPartyRest(int checks) const
 	if (checks & REST_SCATTER) {
 		if (!EveryoneNearPoint(area, leader->Pos, 0)) {
 			//party too scattered
-			displaymsg->DisplayConstantString(STR_SCATTERED, DMC_RED);
-			return false;
+			return displaymsg->GetStringReference(STR_SCATTERED);
 		}
 	}
 
 	if (checks & REST_CRITTER) {
 		//don't allow resting while in combat
 		if (AnyPCInCombat()) {
-			displaymsg->DisplayConstantString(STR_CANTRESTMONS, DMC_RED);
-			return false;
+			return displaymsg->GetStringReference(STR_CANTRESTMONS);
 		}
 		//don't allow resting if hostiles are nearby
 		if (area->AnyEnemyNearPoint(leader->Pos)) {
-			displaymsg->DisplayConstantString(STR_CANTRESTMONS, DMC_RED);
-			return false;
+			return displaymsg->GetStringReference(STR_CANTRESTMONS);
 		}
 	}
 
@@ -1749,16 +1749,15 @@ bool Game::CanPartyRest(int checks) const
 	if (checks & REST_AREA) {
 		//you cannot rest here
 		if (area->AreaFlags & AF_NOSAVE) {
-			displaymsg->DisplayConstantString(STR_MAYNOTREST, DMC_RED);
-			return false;
+			return displaymsg->GetStringReference(STR_MAYNOTREST);
 		}
 
 		if (core->HasFeature(GF_AREA_OVERRIDE)) {
 			// pst doesn't care about area types (see comments near AF_NOSAVE definition)
 			// and repurposes these area flags!
 			if (area->AreaFlags & (AF_TUTORIAL|AF_DEADMAGIC)/* == (AF_TUTORIAL|AF_DEADMAGIC)*/) {
-				displaymsg->DisplayConstantString(STR_MAYNOTREST, DMC_RED); // get permission 38587
-				return false;
+				// get permission 38587
+				return displaymsg->GetStringReference(STR_MAYNOTREST);
 			/* TODO: add all the other strings
 			} else if (area->AreaFlags&AF_TUTORIAL) {
 				displaymsg->DisplayConstantString(STR_MAYNOTREST, DMC_RED); // here 34601
@@ -1770,13 +1769,12 @@ bool Game::CanPartyRest(int checks) const
 		} else {
 			// you may not rest here, find an inn
 			if (!(area->AreaType & (AT_OUTDOOR|AT_FOREST|AT_DUNGEON|AT_CAN_REST_INDOORS))) {
-				displaymsg->DisplayConstantString(STR_MAYNOTREST, DMC_RED);
-				return false;
+				return displaymsg->GetStringReference(STR_MAYNOTREST);
 			}
 		}
 	}
 
-	return true;
+	return 0;
 }
 
 // checks: can anything prevent us from resting?
@@ -1788,7 +1786,7 @@ bool Game::CanPartyRest(int checks) const
 // returns true if a cutscene dream is about to be played
 bool Game::RestParty(int checks, int dream, int hp)
 {
-	if (!CanPartyRest(checks)) {
+	if (CanPartyRest(checks)) {
 		return false;
 	}
 

gemrb/core/Game.h
@@ -473,7 +473,7 @@ public:
 	areas run scripts on door, infopoint, container, actors too */
 	void UpdateScripts();
 	/** checks if resting is possible */
-	bool CanPartyRest(int checks) const;
+	int CanPartyRest(int checks) const;
 	/** runs area functionality, sets partyrested trigger */
 	bool RestParty(int checks, int dream, int hp);
 	/** timestop effect initiated by actor */

commit e06d530078e78f83e9b2784e6943f09073b02fa7

GemRB_RestParty: reenable area checks, fixes #1836
regression from 35bdc660

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -11939,9 +11939,9 @@ scattered). It is possible to play a movie or dream too.\n\
 
 static PyObject* GemRB_RestParty(PyObject * /*self*/, PyObject* args)
 {
-	int noareacheck;
+	int flags;
 	int dream, hp;
-	PARSE_ARGS( args,  "iii", &noareacheck, &dream, &hp);
+	PARSE_ARGS(args, "iii", &flags, &dream, &hp);
 	GET_GAME();
 
 	// check if resting is possible and if not, return the reason, otherwise rest
@@ -11950,7 +11950,7 @@ static PyObject* GemRB_RestParty(PyObject * /*self*/, PyObject* args)
 	// - resting in inns: popup a GUISTORE error window with the reason
 	PyObject* dict = PyDict_New();
 	ieStrRef err = ieStrRef::INVALID;
-	bool cannotRest = !game->CanPartyRest(noareacheck, &err);
+	bool cannotRest = !game->CanPartyRest(flags, &err);
 	// fall back to the generic: you may not rest at this time
 	if (err == ieStrRef::INVALID) {
 		if (core->HasFeature(GFFlags::AREA_OVERRIDE)) {
@@ -11966,7 +11966,7 @@ static PyObject* GemRB_RestParty(PyObject * /*self*/, PyObject* args)
 	} else {
 		PyDict_SetItemString(dict, "ErrorMsg", PyLong_FromLong(-1));
 		// all is well, so do the actual resting
-		PyDict_SetItemString(dict, "Cutscene", PyBool_FromLong(game->RestParty(0, dream, hp)));
+		PyDict_SetItemString(dict, "Cutscene", PyBool_FromLong(game->RestParty(flags & REST_AREA, dream, hp)));
 	}
 
 	return dict;