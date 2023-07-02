1a66b585e69f8ab31f8a26770dedc3f8b47eef11
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2023-07-02 17:18:01 +0200

    Map::GetBlockedInLine: no need to recalc the factor all the time

--- gemrb/core/Map.cpp
+++ gemrb/core/Map.cpp
@@ -2453,10 +2453,10 @@ PathMapFlags Map::GetBlockedInLine(const Point &s, const Point &d, bool stopOnIm
 	PathMapFlags ret = PathMapFlags::IMPASSABLE;
 	Point p = s;
 	const SearchmapPoint sms = ConvertCoordToTile(s);
+	double factor = caller && caller->GetSpeed() ? double(gamedata->GetStepTime()) / double(caller->GetSpeed()) : 1;
 	while (p != d) {
 		double dx = d.x - p.x;
 		double dy = d.y - p.y;
-		double factor = caller && caller->GetSpeed() ? double(gamedata->GetStepTime()) / double(caller->GetSpeed()) : 1;
 		NormalizeDeltas(dx, dy, factor);
 		p.x += dx;
 		p.y += dy;
