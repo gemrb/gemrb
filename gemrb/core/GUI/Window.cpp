586d9abe04de6098cf744c0cf5661d5efae706f9
Brad Allred <bradallred@me.com>
2023-10-01 11:34:11 -0700

    ActionResponder: allow responders to be invoked during execution of other responders
    we ought to be able to call Window.Focus() in a response handler for a button click for example

--- gemrb/core/GUI/Window.cpp
+++ gemrb/core/GUI/Window.cpp
@@ -587,7 +587,7 @@ bool Window::InActionHandler() const
 		}
 	}
 	
-	return executingResponseHandler;
+	return !responderStack.empty();
 }
 
 bool Window::RegisterHotKeyCallback(EventMgr::EventCallback cb, KeyboardKey key)
