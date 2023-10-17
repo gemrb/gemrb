6bdf1aab9cf0f11b29b4ae66bb433631c082046f
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2023-10-17 19:38:42 +0200

    vita: add change forgotten in fb0c82e

--- platforms/vita/GemRB.cpp
+++ platforms/vita/GemRB.cpp
@@ -109,7 +109,7 @@ int main(int argc, char* argv[])
 		vitaDestRect.w = width;
 		vitaDestRect.h = height;
 
-		if (core->GetVideoDriver()->GetFullscreenMode()) {
+		if (VideoDriver->GetFullscreenMode()) {
 			const char* optstr = config->GetValueForKey("VitaKeepAspectRatio");
 			if (optstr && atoi(optstr) > 0) {
 				if ((static_cast<float>(VITA_FULLSCREEN_WIDTH) / VITA_FULLSCREEN_HEIGHT) >= (static_cast<float>(width) / height)) {
