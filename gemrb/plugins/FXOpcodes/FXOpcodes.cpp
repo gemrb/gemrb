gemrb/gemrb

commit 0c64bbfc3c8646f621ab0f41c93a895521a9def1

don't stack the same kind of poison

gemrb/plugins/FXOpcodes/FXOpcodes.cpp
@@ -1599,6 +1599,12 @@ int fx_set_panic_state (Scriptable* /*Owner*/, Actor* target, Effect* fx)
 int fx_set_poisoned_state (Scriptable* /*Owner*/, Actor* target, Effect* fx)
 {
 	if (0) print( "fx_set_poisoned_state (%2d): Damage: %d, Type: %d\n", fx->Opcode, fx->Parameter1, fx->Parameter2 );
+	
+	int count = target->fxqueue.CountEffects(fx_poisoned_state_ref, fx->Parameter1, fx->Parameter2, fx->Resource);
+	if (count > 1) {
+		return FX_NOT_APPLIED;
+	}
+
 	STATE_SET( STATE_POISONED );
 
 	ieDword damage;

commit 94a88ec30b693de1ab48d3b67c721d7d87a34a19

fx_set_poisoned_state: fixed regression from 0c64bbfc
we were blocking all copies, instead of leaving one to run

gemrb/plugins/FXOpcodes/FXOpcodes.cpp
@@ -1936,8 +1936,9 @@ int fx_set_poisoned_state (Scriptable* /*Owner*/, Actor* target, Effect* fx)
 		return FX_ABORT;
 	}
 
-	int count = target->fxqueue.CountEffects(fx_poisoned_state_ref, fx->Parameter1, fx->Parameter2, fx->Resource);
-	if (count > 1) {
+	// don't stack, only run one of the same at a time
+	ieDword count = target->fxqueue.CountEffects(fx_poisoned_state_ref, fx->Parameter1, fx->Parameter2, fx->Resource);
+	if (count > 1 && target->fxqueue.GetEffectOrder(fx_poisoned_state_ref, fx) < count) {
 		return FX_APPLIED;
 	}
 