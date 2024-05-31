532f440de5a27d0f60a61936f0c322e7417ba493
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2024-05-31 23:20:36 +0200

    Movable::DoStep: don't backoff as often

--- gemrb/core/Scriptable/Scriptable.cpp
+++ gemrb/core/Scriptable/Scriptable.cpp
@@ -2094,9 +2094,10 @@ void Movable::DoStep(unsigned int walkScale, ieDword time) {
 		// We can't use GetActorInRadius because we want to only check directly along the way
 		// and not be blocked by actors who are on the sides
 		int collisionLookaheadRadius = ((circleSize < 3 ? 3 : circleSize) - 1) * 3;
-		for (int r = collisionLookaheadRadius; r > 0 && !actorInTheWay; r--) {
+		int r = collisionLookaheadRadius;
+		for (; r > 0 && !actorInTheWay; r--) {
 			auto xCollision = Pos.x + dx * r;
-			auto yCollision = Pos.y + dy * r * 0.75;
+			auto yCollision = Pos.y + dy * r * 0.75; // FIXME: should this really be skewed?
 			Point nmptCollision(xCollision, yCollision);
 			actorInTheWay = area->GetActor(nmptCollision, GA_NO_DEAD|GA_NO_UNSCHEDULED|GA_NO_SELF, this);
 		}
@@ -2116,7 +2117,9 @@ void Movable::DoStep(unsigned int walkScale, ieDword time) {
 			}
 			if (actor && actor->ValidTarget(GA_CAN_BUMP) && actorInTheWay->ValidTarget(GA_ONLY_BUMPABLE)) {
 				actorInTheWay->BumpAway();
-			} else {
+			} else if (r == 1 || actorInTheWay->GetPath()) {
+				// only back off if the immediate step is blocked or if the blocker is moving
+				// it's better to make a single step if possible, to avoid backoff loops
 				Backoff();
 				return;
 			}
