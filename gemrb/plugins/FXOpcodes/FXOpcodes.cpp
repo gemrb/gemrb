gemrb/gemrb

commit 6b81371a1c538a23ee39e36145b66d2106fa3464

fx_add_effects_list: don't discard the position
fixes intro cornugon #2109

gemrb/plugins/FXOpcodes/FXOpcodes.cpp
@@ -8216,7 +8216,11 @@ int fx_add_effects_list(Scriptable* Owner, Actor* target, Effect* fx)
 	if (!EffectQueue::CheckIWDTargeting(Owner, target, fx->Parameter1, fx->Parameter2, fx)) {
 		return FX_NOT_APPLIED;
 	}
-	core->ApplySpell(fx->Resource, target, Owner, fx->Power);
+	if (fx->Pos.IsInvalid()) {
+		core->ApplySpell(fx->Resource, target, Owner, fx->Power);
+	} else {
+		core->ApplySpellPoint(fx->Resource, target->GetCurrentArea(), fx->Pos, Owner, fx->Power);
+	}
 	return FX_NOT_APPLIED;
 }
 

commit 8604ca7311faff5a8e542d25ebe91339a1734806

fx_add_effects_list: fixed application for everyone
regression from 6b81371a
fixes eg. Snilloc's snowball storm only affecting the caster

gemrb/plugins/FXOpcodes/FXOpcodes.cpp
@@ -8217,9 +8217,9 @@ int fx_add_effects_list(Scriptable* Owner, Actor* target, Effect* fx)
 	if (!EffectQueue::CheckIWDTargeting(Owner, target, fx->Parameter1, fx->Parameter2, fx)) {
 		return FX_NOT_APPLIED;
 	}
-	if (fx->Pos.IsInvalid()) {
+	if (fx->Target != FX_TARGET_SELF) {
 		core->ApplySpell(fx->Resource, target, Owner, fx->Power);
-	} else {
+	} else { // demon summoning spells
 		core->ApplySpellPoint(fx->Resource, target->GetCurrentArea(), fx->Pos, Owner, fx->Power);
 	}
 	return FX_NOT_APPLIED;