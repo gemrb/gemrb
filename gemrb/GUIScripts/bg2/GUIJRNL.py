fd2245604b200444aad58378809a94bad3f561d0
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2024-10-15 19:52:00 +0200

    GUIJRNL: use the tooltip background for user entries
    it looks less blocky with the parchment blending

--- gemrb/GUIScripts/bg2/GUIJRNL.py
+++ gemrb/GUIScripts/bg2/GUIJRNL.py
@@ -161,7 +161,7 @@ def AddUserEntry ():
 	Text = Window.ReplaceSubview (0, IE_GUI_TEXTAREA, "NORMAL")
 	Text.SetFlags (IE_GUI_TEXTAREA_EDITABLE, OP_OR)
 	Text.SetColor (ColorBlackish, TA_COLOR_NORMAL)
-	Text.SetColor (ColorGray, TA_COLOR_BACKGROUND)
+	Text.SetColor ({'r' : 0xf0, 'g' : 0xb0, 'b' : 0x80, 'a' : 255}, TA_COLOR_BACKGROUND)
 	Text.Focus ()
 
 	# title
