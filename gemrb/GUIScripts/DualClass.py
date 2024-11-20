2af4beb88b515fb394f0bfc018e569ddd1701abc
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2024-11-20 21:36:02 +0100

    Revert "bg1 doesn't have a separate dc mage window"
    This reverts commit 432cb4fe000d742963d46906813dd9ab745c36a4.

--- gemrb/GUIScripts/DualClass.py
+++ gemrb/GUIScripts/DualClass.py
@@ -529,7 +529,7 @@ def DCProfsDonePress ():
 	ClericTable = CommonTables.ClassSkills.GetValue (ClassName, "CLERICSPELL")
 	DruidTable = CommonTables.ClassSkills.GetValue (ClassName, "DRUIDSPELL")
 	if SpellTable != "*":
-		LUSpellSelection.OpenSpellsWindow (pc, SpellTable, 1, 1, 0, 0, False)
+		LUSpellSelection.OpenSpellsWindow (pc, SpellTable, 1, 1, 0)
 		SpellTable = GemRB.LoadTable (SpellTable)
 		GemRB.SetMemorizableSpellsCount (pc, SpellTable.GetValue (0, 0), IE_SPELL_TYPE_WIZARD, 0)
 		NewMageSpells = 1
--- gemrb/GUIScripts/LUSpellSelection.py
+++ gemrb/GUIScripts/LUSpellSelection.py
@@ -108,7 +108,7 @@ def OpenSpellsWindow (actor, table, level, diff, kit=0, gen=0, recommend=True, b
 		return
 
 	# load our window
-	if chargen or GameCheck.IsBG1 ():
+	if chargen:
 		SpellsWindow = GemRB.LoadWindow (7, "GUICG")
 
 		if GameCheck.IsBG2():
@@ -704,7 +704,7 @@ def RowIndex ():
 def ExtraSpellButtons ():
 	"""Determines if extra spell slots are available. """
 
-	if EnhanceGUI and not chargen and not GameCheck.IsBG1 ():
+	if EnhanceGUI and (not chargen):
 		return 1
 	else:
 		return 0
