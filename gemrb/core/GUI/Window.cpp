b6a66250240585f9e179aca9c24366efdf995c4b
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2025-01-13 21:27:04 +0100

    Window::DispatchEvent: don't assert on unknown event types #2268
    no need to be so strict

--- gemrb/core/GUI/Window.cpp
+++ gemrb/core/GUI/Window.cpp
@@ -568,7 +568,8 @@ bool Window::DispatchEvent(const Event& event)
 		default:
 #ifdef USE_SDL_CONTROLLER_API
 			// If controller api is not used, this maybe reached if a controller is used.
-			assert(false); // others should be handled above
+			// others should be handled above
+			Log(ERROR, "Window", "Unhandled event type generated: {}", event.type);
 #endif
 			break;
 	}
