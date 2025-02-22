6cc7142d0eb5e503f20685fadfd95b89f95296a6
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2025-02-22 15:25:55 +0100

    BeginDialog: increase dialog range further in pst
    a completely empirical guess

--- gemrb/core/GameScript/GSUtils.cpp
+++ gemrb/core/GameScript/GSUtils.cpp
@@ -1185,6 +1185,7 @@ void BeginDialog(Scriptable* Sender, const Action* parameters, int Flags)
 		if (Flags & BD_CHECKDIST) {
 			//DialogueRange is set in IWD
 			ieDword range = MAX_OPERATING_DISTANCE + speaker->GetBase(IE_DIALOGRANGE);
+			if (core->HasFeature(GFFlags::PST_STATE_FLAGS)) range += 160; // approx value to make the FFG range in 1201csg3 match
 			if (scr->GetCurrentArea() != target->GetCurrentArea() ||
 			    PersonalDistance(scr, target) > range) {
 				MoveNearerTo(Sender, target, MAX_OPERATING_DISTANCE);
