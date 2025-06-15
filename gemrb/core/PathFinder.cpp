4b81167e7db8a201b6eedcd21e3e01eceb448277
Dariusz Pyś <dariusz.pys@intel.com>
2025-06-15 19:28:47 +0200

    put back FindPath at its place, part 2

--- gemrb/core/PathFinder.cpp
+++ gemrb/core/PathFinder.cpp
@@ -232,7 +232,6 @@ PathNode Map::GetLineEnd(const Point& p, int steps, orient_t orient) const
 Path Map::FindPath(const Point& s, const Point& d, unsigned int size, unsigned int minDistance, int flags, const Actor* caller) const
 {
 	TRACY(ZoneScoped);
-
 	if (InDebugMode(DebugMode::PATHFINDER))
 		Log(DEBUG, "FindPath", "s = {}, d = {}, caller = {}, dist = {}, size = {}",
 		    s, d,
@@ -267,224 +266,197 @@ Path Map::FindPath(const Point& s, const Point& d, unsigned int size, unsigned i
 	if (!mapSize.PointInside(smptSource)) return {};
 
 	// Initialize data structures
-	{
-		TRACY(TracyCZoneN(tCtxInit, "Init", true));
 
-		FibonacciHeap<PQNode> open;
-		std::vector<bool> isClosed(mapSize.Area(), false);
-		std::vector<NavmapPoint> parents(mapSize.Area(), Point(0, 0));
-		std::vector<unsigned short> distFromStart(mapSize.Area(), std::numeric_limits<unsigned short>::max());
-		distFromStart[smptSource.y * mapSize.w + smptSource.x] = 0;
-		parents[smptSource.y * mapSize.w + smptSource.x] = nmptSource;
-		open.emplace(PQNode(nmptSource, 0));
-		bool foundPath = false;
-		static bool usePlainThetaStar = gamedata->GetMiscRule("LAZY_THETA_STAR") == 0;
-		unsigned int squaredMinDist = minDistance * minDistance;
+	FibonacciHeap<PQNode> open;
+	std::vector<bool> isClosed(mapSize.Area(), false);
+	std::vector<NavmapPoint> parents(mapSize.Area(), Point(0, 0));
+	std::vector<unsigned short> distFromStart(mapSize.Area(), std::numeric_limits<unsigned short>::max());
+	distFromStart[smptSource.y * mapSize.w + smptSource.x] = 0;
+	parents[smptSource.y * mapSize.w + smptSource.x] = nmptSource;
+	open.emplace(PQNode(nmptSource, 0));
+	bool foundPath = false;
+	static bool usePlainThetaStar = gamedata->GetMiscRule("LAZY_THETA_STAR") == 0;
+	unsigned int squaredMinDist = minDistance * minDistance;
 
-		// Weighted heuristic. Finds sub-optimal paths but should be quite a bit faster
-		constexpr float_t HEURISTIC_WEIGHT = 1.5;
-		auto getHeuristic = [&](const SearchmapPoint& smptChild, const int& smptChildIdx) {
-			// Calculate heuristic
-			int xDist = smptChild.x - smptDest.x;
-			int yDist = smptChild.y - smptDest.y;
-			// Tie-breaking used to smooth out the path
-			int dxCross = smptDest.x - smptSource.x;
-			int dyCross = smptDest.y - smptSource.y;
-			int crossProduct = std::abs(xDist * dyCross - yDist * dxCross) >> 3;
-			double distance = std::hypot(xDist, yDist);
-			double heuristic = HEURISTIC_WEIGHT * (distance + crossProduct);
-			double estDist = distFromStart[smptChildIdx] + heuristic;
-			return estDist;
-		};
-		TRACY(TracyCZoneEnd(tCtxInit));
+	// Weighted heuristic. Finds sub-optimal paths but should be quite a bit faster
+	constexpr float_t HEURISTIC_WEIGHT = 1.5;
+	auto getHeuristic = [&](const SearchmapPoint& smptChild, const int& smptChildIdx) {
+		// Calculate heuristic
+		int xDist = smptChild.x - smptDest.x;
+		int yDist = smptChild.y - smptDest.y;
+		// Tie-breaking used to smooth out the path
+		int dxCross = smptDest.x - smptSource.x;
+		int dyCross = smptDest.y - smptSource.y;
+		int crossProduct = std::abs(xDist * dyCross - yDist * dxCross) >> 3;
+		double distance = std::hypot(xDist, yDist);
+		double heuristic = HEURISTIC_WEIGHT * (distance + crossProduct);
+		double estDist = distFromStart[smptChildIdx] + heuristic;
+		return estDist;
+	};
 
-		while (!open.empty()) {
-			TRACY(ZoneScopedN("whileOpen"));
+	while (!open.empty()) {
+		NavmapPoint nmptCurrent = open.top().point;
+		open.pop();
+		SearchmapPoint smptCurrent { nmptCurrent };
+		int smptCurrentIdx = smptCurrent.y * mapSize.w + smptCurrent.x;
 
-			TRACY(TracyCZoneN(tCtxOpenTopPop, "openTopPop", true));
-			NavmapPoint nmptCurrent = open.top().point;
-			open.pop();
-			TRACY(TracyCZoneEnd(tCtxOpenTopPop));
+		TRACY(ZoneScopedN("firstChecks"));
+		if (parents[smptCurrentIdx].IsZero()) {
+			continue;
+		}
 
-			SearchmapPoint smptCurrent { nmptCurrent };
-			int smptCurrentIdx = smptCurrent.y * mapSize.w + smptCurrent.x;
+		if (smptCurrent == smptDest) {
+			nmptDest = nmptCurrent;
+			foundPath = true;
+			break;
+		} else if (minDistance &&
+			   parents[smptCurrentIdx] != nmptCurrent &&
+			   SquaredDistance(nmptCurrent, nmptDest) < squaredMinDist &&
+			   (!(flags & PF_SIGHT) ||
+			    IsVisibleLOS(smptCurrent, smptDest0, caller))) { // FIXME: should probably be smptDest
+			smptDest = smptCurrent;
+			nmptDest = nmptCurrent;
+			foundPath = true;
+			break;
+		}
+		isClosed[smptCurrentIdx] = true;
 
-			{
-				TRACY(ZoneScopedN("firstChecks"));
-				if (parents[smptCurrentIdx].IsZero()) {
-					continue;
-				}
+		for (size_t i = 0; i < DEGREES_OF_FREEDOM; i++) {
+			NavmapPoint nmptChild(nmptCurrent.x + 16 * dxAdjacent[i], nmptCurrent.y + 12 * dyAdjacent[i]);
+			SearchmapPoint smptChild { nmptChild };
+			// Outside map
+			if (smptChild.x < 0 || smptChild.y < 0 || smptChild.x >= mapSize.w || smptChild.y >= mapSize.h)
+				continue;
+			// Already visited
+			int smptChildIdx = smptChild.y * mapSize.w + smptChild.x;
+			if (isClosed[smptChildIdx]) continue;
 
-				if (smptCurrent == smptDest) {
-					nmptDest = nmptCurrent;
-					foundPath = true;
-					break;
-				} else if (minDistance &&
-					   parents[smptCurrentIdx] != nmptCurrent &&
-					   SquaredDistance(nmptCurrent, nmptDest) < squaredMinDist &&
-					   (!(flags & PF_SIGHT) ||
-					    IsVisibleLOS(smptCurrent, smptDest0, caller))) { // FIXME: should probably be smptDest
-					smptDest = smptCurrent;
-					nmptDest = nmptCurrent;
-					foundPath = true;
-					break;
-				}
+			PathMapFlags childBlockStatus;
+			if (size > 2) {
+				childBlockStatus = GetBlockedInRadiusTile(smptChild, size);
+			} else {
+				childBlockStatus = GetBlockedTile(smptChild);
 			}
-			isClosed[smptCurrentIdx] = true;
+			bool childBlocked = !(childBlockStatus & (PathMapFlags::PASSABLE | PathMapFlags::ACTOR));
+			if (childBlocked) continue;
 
-			for (size_t i = 0; i < DEGREES_OF_FREEDOM; i++) {
-				TRACY(ZoneScopedN("forDegreesOfFreedom"));
-				NavmapPoint nmptChild(nmptCurrent.x + 16 * dxAdjacent[i], nmptCurrent.y + 12 * dyAdjacent[i]);
-				SearchmapPoint smptChild { nmptChild };
-				// Outside map
-				if (smptChild.x < 0 || smptChild.y < 0 || smptChild.x >= mapSize.w || smptChild.y >= mapSize.h)
-					continue;
-				// Already visited
-				int smptChildIdx = smptChild.y * mapSize.w + smptChild.x;
-				if (isClosed[smptChildIdx]) continue;
+			// If there's an actor, check it can be bumped away
+			const Actor* childActor = GetActor(nmptChild, GA_NO_DEAD | GA_NO_UNSCHEDULED);
+			bool childIsUnbumpable = childActor && childActor != caller &&
+				(actorsAreBlocking || !childActor->ValidTarget(GA_ONLY_BUMPABLE));
+			if (childIsUnbumpable) continue;
 
-				TRACY(TracyCZoneN(tCtxchildBlockStatus, "childBlockStatus", true));
-				PathMapFlags childBlockStatus;
-				if (size > 2) {
-					childBlockStatus = GetBlockedInRadiusTile(smptChild, size);
-				} else {
-					childBlockStatus = GetBlockedTile(smptChild);
-				}
-				bool childBlocked = !(childBlockStatus & (PathMapFlags::PASSABLE | PathMapFlags::ACTOR));
-				TRACY(TracyCZoneEnd(tCtxchildBlockStatus));
-				if (childBlocked) continue;
+			SearchmapPoint smptCurrent2 { nmptCurrent };
+			NavmapPoint nmptParent = parents[smptCurrent2.y * mapSize.w + smptCurrent2.x];
+			SearchmapPoint smptParent { nmptParent };
+			unsigned short oldDist = distFromStart[smptChildIdx];
 
-				// If there's an actor, check it can be bumped away
-				TRACY(TracyCZoneN(tCtxActorChecks, "actorChecks", true));
-				const Actor* childActor = GetActor(nmptChild, GA_NO_DEAD | GA_NO_UNSCHEDULED);
-				bool childIsUnbumpable = childActor && childActor != caller &&
-					(actorsAreBlocking || !childActor->ValidTarget(GA_ONLY_BUMPABLE));
-				TRACY(TracyCZoneEnd(tCtxActorChecks));
-				if (childIsUnbumpable) continue;
-
-				SearchmapPoint smptCurrent2 { nmptCurrent };
-				NavmapPoint nmptParent = parents[smptCurrent2.y * mapSize.w + smptCurrent2.x];
-				SearchmapPoint smptParent { nmptParent };
-				unsigned short oldDist = distFromStart[smptChildIdx];
-
-				if (usePlainThetaStar) {
-					TRACY(ZoneScopedN("thetaStar"));
-					// Theta-star path if there is LOS
-					if (IsWalkableTo(nmptParent, nmptChild, actorsAreBlocking, caller)) {
-						TRACY(ZoneScopedN("thetaStarIsWalkableTo"));
-						unsigned short newDist = distFromStart[smptParent.y * mapSize.w + smptParent.x] +
-							Distance(smptParent, smptChild);
-						if (newDist < oldDist) {
-							parents[smptChildIdx] = nmptParent;
-							distFromStart[smptChildIdx] = newDist;
-						}
-						// Fall back to A-star path
-					} else {
-						TRACY(ZoneScopedN("AStarFallback!WalkableTo"));
-						unsigned short newDist = distFromStart[smptCurrent2.y * mapSize.w + smptCurrent2.x] +
-							Distance(smptCurrent2, smptChild);
-						if (newDist < oldDist) {
-							parents[smptChildIdx] = nmptCurrent;
-							distFromStart[smptChildIdx] = newDist;
-						}
-					}
-
-					if (distFromStart[smptChildIdx] < oldDist) {
-						{
-							PQNode newNode(nmptChild, getHeuristic(smptChild, smptChildIdx));
-							TRACY(ZoneScopedN("openEmplace1"));
-							open.emplace(newNode);
-						}
-					}
-				} else {
-					TRACY(ZoneScopedN("lazyThetaStar"));
-					// Lazy Theta star*
-					unsigned short newDist =
-						distFromStart[smptParent.y * mapSize.w + smptParent.x] + Distance(smptParent, smptChild);
+			if (usePlainThetaStar) {
+				// Theta-star path if there is LOS
+				if (IsWalkableTo(nmptParent, nmptChild, actorsAreBlocking, caller)) {
+					unsigned short newDist = distFromStart[smptParent.y * mapSize.w + smptParent.x] +
+						Distance(smptParent, smptChild);
 					if (newDist < oldDist) {
 						parents[smptChildIdx] = nmptParent;
 						distFromStart[smptChildIdx] = newDist;
 					}
-
-					if (distFromStart[smptChildIdx] < oldDist) {
-						TRACY(ZoneScopedN("if there is LOS"));
-						// Theta-star path if there is LOS
-						// so far the searchmap grid appears too coarse to play on, see #2261
-						//if (!IsWalkableTo(smptParent, smptChild, actorsAreBlocking, caller)) {
-						if (!IsWalkableTo(nmptParent, nmptChild, actorsAreBlocking, caller)) {
-							TRACY(ZoneScopedN("fallBackToAStar"));
-							// Fall back to A-star path
-							distFromStart[smptChildIdx] = std::numeric_limits<unsigned short>::max();
-							// Find already visited neighbour with shortest: path from start + path to child
-							for (size_t j = 0; j < DEGREES_OF_FREEDOM; j++) {
-								TRACY(ZoneScopedN("inner forDegreesOfFreedom"));
-								NavmapPoint nmptVis(nmptChild.x + 16 * dxAdjacent[j], nmptChild.y + 12 * dyAdjacent[j]);
-								SearchmapPoint smptVis { nmptVis };
-								// Outside map
-								if (smptVis.x < 0 || smptVis.y < 0 || smptVis.x >= mapSize.w ||
-								    smptVis.y >= mapSize.h)
-									continue;
-								// Only consider already visited
-								if (!isClosed[smptVis.y * mapSize.w + smptVis.x]) continue;
-
-								unsigned short oldVisDist = distFromStart[smptChildIdx];
-								newDist =
-									distFromStart[smptVis.y * mapSize.w + smptVis.x] + Distance(smptVis, smptChild);
-								if (newDist < oldVisDist) {
-									parents[smptChildIdx] = nmptVis;
-									distFromStart[smptChildIdx] = newDist;
-								}
-							}
-							if (distFromStart[smptChildIdx] >= oldDist) continue;
-						}
-
-						{
-							PQNode newNode(nmptChild, getHeuristic(smptChild, smptChildIdx));
-							TRACY(ZoneScopedN("openEmplace2"));
-							open.emplace(newNode);
-						}
+					// Fall back to A-star path
+				} else {
+					unsigned short newDist = distFromStart[smptCurrent2.y * mapSize.w + smptCurrent2.x] +
+						Distance(smptCurrent2, smptChild);
+					if (newDist < oldDist) {
+						parents[smptChildIdx] = nmptCurrent;
+						distFromStart[smptChildIdx] = newDist;
 					}
+				}
+
+				if (distFromStart[smptChildIdx] < oldDist) {
+					{
+						PQNode newNode(nmptChild, getHeuristic(smptChild, smptChildIdx));
+						open.emplace(newNode);
+					}
+				}
+			} else {
+				// Lazy Theta star*
+				unsigned short newDist =
+					distFromStart[smptParent.y * mapSize.w + smptParent.x] + Distance(smptParent, smptChild);
+				if (newDist < oldDist) {
+					parents[smptChildIdx] = nmptParent;
+					distFromStart[smptChildIdx] = newDist;
+				}
+
+				if (distFromStart[smptChildIdx] < oldDist) {
+					// Theta-star path if there is LOS
+					// so far the searchmap grid appears too coarse to play on, see #2261
+					//if (!IsWalkableTo(smptParent, smptChild, actorsAreBlocking, caller)) {
+					if (!IsWalkableTo(nmptParent, nmptChild, actorsAreBlocking, caller)) {
+						// Fall back to A-star path
+						distFromStart[smptChildIdx] = std::numeric_limits<unsigned short>::max();
+						// Find already visited neighbour with shortest: path from start + path to child
+						for (size_t j = 0; j < DEGREES_OF_FREEDOM; j++) {
+							NavmapPoint nmptVis(nmptChild.x + 16 * dxAdjacent[j], nmptChild.y + 12 * dyAdjacent[j]);
+							SearchmapPoint smptVis { nmptVis };
+							// Outside map
+							if (smptVis.x < 0 || smptVis.y < 0 || smptVis.x >= mapSize.w ||
+							    smptVis.y >= mapSize.h)
+								continue;
+							// Only consider already visited
+							if (!isClosed[smptVis.y * mapSize.w + smptVis.x]) continue;
+
+							unsigned short oldVisDist = distFromStart[smptChildIdx];
+							newDist =
+								distFromStart[smptVis.y * mapSize.w + smptVis.x] + Distance(smptVis, smptChild);
+							if (newDist < oldVisDist) {
+								parents[smptChildIdx] = nmptVis;
+								distFromStart[smptChildIdx] = newDist;
+							}
+						}
+						if (distFromStart[smptChildIdx] >= oldDist) continue;
+					}
+
+					PQNode newNode(nmptChild, getHeuristic(smptChild, smptChildIdx));
+					open.emplace(newNode);
 				}
 			}
 		}
+	}
 
-		if (foundPath) {
-			TRACY(ZoneScopedN("foundPath"));
-			Path resultPath;
-			NavmapPoint nmptCurrent = nmptDest;
-			NavmapPoint nmptParent;
-			SearchmapPoint smptCurrent { nmptCurrent };
-			while (!resultPath || nmptCurrent != parents[smptCurrent.y * mapSize.w + smptCurrent.x]) {
-				nmptParent = parents[smptCurrent.y * mapSize.w + smptCurrent.x];
-				PathNode newStep { nmptCurrent, S };
-				// movement in general allows characters to walk backwards given that
-				// the destination is behind the character (within a threshold), and
-				// that the distance isn't too far away
-				// we approximate that with a relaxed collinearity check and intentionally
-				// skip the first step, otherwise it doesn't help with iwd beetles in ar1015
-				if (flags & PF_BACKAWAY && resultPath &&
-				    std::abs(area2(nmptCurrent, resultPath.GetStep(0).point, nmptParent)) < 300) {
-					newStep.orient = GetOrient(nmptCurrent, nmptParent);
-				} else {
-					newStep.orient = GetOrient(nmptParent, nmptCurrent);
-				}
-
-				resultPath.PrependStep(newStep);
-				nmptCurrent = nmptParent;
-
-				smptCurrent = SearchmapPoint(nmptCurrent);
-			}
-			return resultPath;
-		} else if (InDebugMode(DebugMode::PATHFINDER)) {
-			if (caller) {
-				Log(DEBUG, "FindPath", "Pathing failed for {}", fmt::WideToChar { caller->GetShortName() });
+	if (foundPath) {
+		Path resultPath;
+		NavmapPoint nmptCurrent = nmptDest;
+		NavmapPoint nmptParent;
+		SearchmapPoint smptCurrent { nmptCurrent };
+		while (!resultPath || nmptCurrent != parents[smptCurrent.y * mapSize.w + smptCurrent.x]) {
+			nmptParent = parents[smptCurrent.y * mapSize.w + smptCurrent.x];
+			PathNode newStep { nmptCurrent, S };
+			// movement in general allows characters to walk backwards given that
+			// the destination is behind the character (within a threshold), and
+			// that the distance isn't too far away
+			// we approximate that with a relaxed collinearity check and intentionally
+			// skip the first step, otherwise it doesn't help with iwd beetles in ar1015
+			if (flags & PF_BACKAWAY && resultPath &&
+			    std::abs(area2(nmptCurrent, resultPath.GetStep(0).point, nmptParent)) < 300) {
+				newStep.orient = GetOrient(nmptCurrent, nmptParent);
 			} else {
-				Log(DEBUG, "FindPath", "Pathing failed");
+				newStep.orient = GetOrient(nmptParent, nmptCurrent);
 			}
-        }
 
-        return {};
+			resultPath.PrependStep(newStep);
+			nmptCurrent = nmptParent;
+
+			smptCurrent = SearchmapPoint(nmptCurrent);
+		}
+		return resultPath;
+	} else if (InDebugMode(DebugMode::PATHFINDER)) {
+		if (caller) {
+			Log(DEBUG, "FindPath", "Pathing failed for {}", fmt::WideToChar { caller->GetShortName() });
+		} else {
+			Log(DEBUG, "FindPath", "Pathing failed");
+		}
     }
+
+    return {};
+
 }
 
 Path Map::RunFindPath(const Point& s, const Point& d, unsigned int size, unsigned int minDistance, int flags, const Actor* caller)
