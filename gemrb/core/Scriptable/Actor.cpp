2eb2bc870c126e0be4f4e77c299e0f475ac3b46f
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2025-06-24 18:36:15 +0200

    Actor::ValidTarget: document the only-bumpable relation to the TraversabilityCache

--- gemrb/core/Scriptable/Actor.cpp
+++ gemrb/core/Scriptable/Actor.cpp
@@ -6202,11 +6202,12 @@ bool Actor::ValidTarget(int ga_flags, const Scriptable* checker) const
 		}
 	}
 	if (ga_flags & GA_ONLY_BUMPABLE) {
-		if (core->GetGame()->CombatCounter) return false;
-		if (GetStat(IE_EA) >= EA_EVILCUTOFF) return false;
+		// NOTE: if you add new conditions, make sure TraversabilityCache gets updated accordingly
+		if (core->GetGame()->CombatCounter) return false; // handled when TraversabilityCache is used (e.g. FindPath)
+		if (GetStat(IE_EA) >= EA_EVILCUTOFF) return false; // handled in pcf_ea
 		// Skip sitting patrons
-		if (GetStat(IE_ANIMATION_ID) >= 0x4000 && GetStat(IE_ANIMATION_ID) <= 0x4112) return false;
-		if (IsMoving()) return false;
+		if (GetStat(IE_ANIMATION_ID) >= 0x4000 && GetStat(IE_ANIMATION_ID) <= 0x4112) return false; // ignored
+		if (IsMoving()) return false; // handled in Movable::SetStanceDirect
 	}
 	if (ga_flags & GA_CAN_BUMP) {
 		if (core->GetGame()->CombatCounter) return false;
