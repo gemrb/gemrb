996200c0df8d085c1788022d93b0ddc4416d2d8e
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2025-07-21 18:52:15 +0200

    cmake: fix msvc vs new fmt assert for real
    the define is not strictly necessary and is also a cmake flag upstream

--- cmake/Helpers.cmake
+++ cmake/Helpers.cmake
@@ -126,7 +126,8 @@ FUNCTION(CONFIGURE_COMPILER)
 	IF(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
 		# this subtly breaks things, HoW crashes on first area entry
 		#STRING(APPEND CMAKE_CXX_FLAGS " /fp:fast")
-		ADD_COMPILE_OPTIONS(/MP2) # manually enable some file-level parallelism
+		# manually enable some file-level parallelism + unicode for fmt
+		ADD_COMPILE_OPTIONS(/MP2 /utf-8)
 	ENDIF()
 
 	SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" PARENT_SCOPE)
