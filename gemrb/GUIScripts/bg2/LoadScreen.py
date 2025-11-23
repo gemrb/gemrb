e0244c9ec50d7e29e8102bac842f4adcf72304dc
MarcelHB <marscel@googlemail.com>
2025-11-23 14:22:15 +0100

    GUI::BG2: adds load screen support for BG1 EE

--- gemrb/GUIScripts/bg2/LoadScreen.py
+++ gemrb/GUIScripts/bg2/LoadScreen.py
@@ -34,7 +34,24 @@ def StartLoadScreen ():
 
 	LoadScreen = GemRB.LoadWindow (0, "guils")
 	LoadScreen.AddAlias("LOADWIN")
-	
+
+	if GameCheck.HasTOB():
+		Table = GemRB.LoadTable ("loadh25")
+	else:
+		Table = GemRB.LoadTable ("loadhint")
+	tmp = Table.GetRowCount ()
+	tmp = GemRB.Roll (1, tmp-1, 0)
+	HintStr = Table.GetValue (tmp, 0)
+
+	Bar = LoadScreen.GetControl (0)
+	Bar.AddAlias("LOAD_PROG")
+	Bar.SetVarAssoc ("Progress", 0)
+	Bar.OnEndReached (lambda: EndLoadScreen(HintStr))
+
+	# original has no load screen, just use own simple one instead
+	if GameCheck.IsBGEE():
+		return
+
 	Middle = LoadScreen.GetControl (3)
 	Progress = GemRB.GetVar ("Progress")
 
@@ -54,33 +71,21 @@ def StartLoadScreen ():
 		else:
 			Middle.SetBAM ("COADCNTR", 0, 0)
 
-	if GameCheck.HasTOB():
-		Table = GemRB.LoadTable ("loadh25")
-	else:
-		Table = GemRB.LoadTable ("loadhint")
-	tmp = Table.GetRowCount ()
-	tmp = GemRB.Roll (1, tmp-1, 0)
-	HintStr = Table.GetValue (tmp, 0)
-
 	Label = LoadScreen.GetControl (2)
 	Label.SetText(HintStr)
 
-	def EndLoadScreen ():
-		TMessageTA = GemRB.GetView("MsgSys", 0)
-		TMessageTA.Append("[p][color=f1f28d]" + GemRB.GetString (HintStr) + "[/color][/p]\n")
-		
-		if GameCheck.IsBG2Demo():
-			Middle = LoadScreen.GetControl (3)
-			Middle.SetBAM ("COADCNTR", 1, 0)
-	
-		LoadScreen.OnClose (lambda win: GemRB.GamePause(0, 0))
-		GemRB.SetTimer(LoadScreen.Close, 500, 0)
-		return
-
-	Bar = LoadScreen.GetControl (0)
-	Bar.AddAlias("LOAD_PROG")
-	Bar.SetVarAssoc ("Progress", 0)
-	Bar.OnEndReached (EndLoadScreen)
 	LoadScreen.ShowModal(MODAL_SHADOW_NONE)
+
+def EndLoadScreen (HintStr):
+	TMessageTA = GemRB.GetView("MsgSys", 0)
+	TMessageTA.Append("[p][color=f1f28d]" + GemRB.GetString (HintStr) + "[/color][/p]\n")
+	
+	if GameCheck.IsBG2Demo():
+		Middle = LoadScreen.GetControl (3)
+		Middle.SetBAM ("COADCNTR", 1, 0)
+
+	LoadScreen.OnClose (lambda win: GemRB.GamePause(0, 0))
+	GemRB.SetTimer(LoadScreen.Close, 500, 0)
+	return
 
 
