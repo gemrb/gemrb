11e77047286b26724546019b6ff05cbe346b6380
MarcelHB <marscel@googlemail.com>
2025-12-10 18:40:44 +0100

    SDL: drops use of `SDL_GetColorKey`
    We have all the information we need at this point,
    and calling this on hot path is quite expensive.

--- gemrb/plugins/SDLVideo/SDLSurfaceSprite2D.cpp
+++ gemrb/plugins/SDLVideo/SDLSurfaceSprite2D.cpp
@@ -116,7 +116,7 @@ bool SDLSurfaceSprite2D::HasTransparency() const noexcept
 {
 	const SDL_PixelFormat* fmt = surface->format;
 #if SDL_VERSION_ATLEAST(1, 3, 0)
-	return SDL_ISPIXELFORMAT_ALPHA(fmt->format) || SDL_GetColorKey(surface, NULL) != -1;
+	return SDL_ISPIXELFORMAT_ALPHA(fmt->format) || format.HasColorKey;
 #else
 	return fmt->Amask > 0 || ((surface)->flags & SDL_SRCCOLORKEY);
 #endif
