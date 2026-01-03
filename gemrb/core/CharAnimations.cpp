gemrb/gemrb

commit 1ffea37481db88d836cbc65f88142eac0942217e

SDL: refactoring of palette handling
see #2146

gemrb/core/CharAnimations.cpp
@@ -543,7 +543,7 @@ void CharAnimations::SetupColors(PaletteType type)
 		*/
 		for (int i = 0; i < colorcount; i++) {
 			const auto& pal32 = core->GetPalette32(static_cast<uint8_t>(Colors[i]));
-			PartPalettes[PAL_MAIN]->CopyColorRange(&pal32[0], &pal32[32], static_cast<uint8_t>(dest));
+			PartPalettes[PAL_MAIN]->CopyColors(dest, &pal32[0], &pal32[32]);
 			dest +=size;
 		}
 
@@ -2885,66 +2885,73 @@ Palette SetupRGBModification(const Holder<Palette>& src, const RGBModifier* mods
 	unsigned int type) noexcept
 {
 	Palette pal;
+	Palette::Colors buffer;
 
 	const RGBModifier* tmods = mods+(8*type);
 	int i;
 
-	for (i = 0; i < 4; ++i)
-		pal.col[i] = src->col[i];
+	std::copy(src->cbegin(), src->cbegin() + 4, buffer.begin());
+	auto srcData = src->ColorData();
 
 	for (i = 0; i < 12; ++i)
-		applyMod(src->col[0x04+i], pal.col[0x04+i], tmods[0]);
+		applyMod(srcData[0x04+i], buffer[0x04+i], tmods[0]);
 	for (i = 0; i < 12; ++i)
-		applyMod(src->col[0x10+i], pal.col[0x10+i], tmods[1]);
+		applyMod(srcData[0x10+i], buffer[0x10+i], tmods[1]);
 	for (i = 0; i < 12; ++i)
-		applyMod(src->col[0x1c+i], pal.col[0x1c+i], tmods[2]);
+		applyMod(srcData[0x1c+i], buffer[0x1c+i], tmods[2]);
 	for (i = 0; i < 12; ++i)
-		applyMod(src->col[0x28+i], pal.col[0x28+i], tmods[3]);
+		applyMod(srcData[0x28+i], buffer[0x28+i], tmods[3]);
 	for (i = 0; i < 12; ++i)
-		applyMod(src->col[0x34+i], pal.col[0x34+i], tmods[4]);
+		applyMod(srcData[0x34+i], buffer[0x34+i], tmods[4]);
 	for (i = 0; i < 12; ++i)
-		applyMod(src->col[0x40+i], pal.col[0x40+i], tmods[5]);
+		applyMod(srcData[0x40+i], buffer[0x40+i], tmods[5]);
 	for (i = 0; i < 12; ++i)
-		applyMod(src->col[0x4c+i], pal.col[0x4c+i], tmods[6]);
+		applyMod(srcData[0x4c+i], buffer[0x4c+i], tmods[6]);
 	for (i = 0; i < 8; ++i)
-		applyMod(src->col[0x58+i], pal.col[0x58+i], tmods[1]);
+		applyMod(srcData[0x58+i], buffer[0x58+i], tmods[1]);
 	for (i = 0; i < 8; ++i)
-		applyMod(src->col[0x60+i], pal.col[0x60+i], tmods[2]);
+		applyMod(srcData[0x60+i], buffer[0x60+i], tmods[2]);
 	for (i = 0; i < 8; ++i)
-		applyMod(src->col[0x68+i], pal.col[0x68+i], tmods[1]);
+		applyMod(srcData[0x68+i], buffer[0x68+i], tmods[1]);
 	for (i = 0; i < 8; ++i)
-		applyMod(src->col[0x70+i], pal.col[0x70+i], tmods[0]);
+		applyMod(srcData[0x70+i], buffer[0x70+i], tmods[0]);
 	for (i = 0; i < 8; ++i)
-		applyMod(src->col[0x78+i], pal.col[0x78+i], tmods[4]);
+		applyMod(srcData[0x78+i], buffer[0x78+i], tmods[4]);
 	for (i = 0; i < 8; ++i)
-		applyMod(src->col[0x80+i], pal.col[0x80+i], tmods[4]);
+		applyMod(srcData[0x80+i], buffer[0x80+i], tmods[4]);
 	for (i = 0; i < 8; ++i)
-		applyMod(src->col[0x88+i], pal.col[0x88+i], tmods[1]);
+		applyMod(srcData[0x88+i], buffer[0x88+i], tmods[1]);
 	for (i = 0; i < 24; ++i)
-		applyMod(src->col[0x90+i], pal.col[0x90+i], tmods[4]);
+		applyMod(srcData[0x90+i], buffer[0x90+i], tmods[4]);
+
+	std::copy(src->cbegin() + 0xA8, src->cbegin() + 0xA8 + 8, buffer.begin() + 0xA8);
 
 	for (i = 0; i < 8; ++i)
-		pal.col[0xA8+i] = src->col[0xA8+i];
-
-	for (i = 0; i < 8; ++i)
-		applyMod(src->col[0xB0+i], pal.col[0xB0+i], tmods[3]);
+		applyMod(srcData[0xB0+i], buffer[0xB0+i], tmods[3]);
 	for (i = 0; i < 72; ++i)
-		applyMod(src->col[0xB8+i], pal.col[0xB8+i], tmods[4]);
-	
+		applyMod(srcData[0xB8+i], buffer[0xB8+i], tmods[4]);
+
+	pal.CopyColors(0, buffer.cbegin(), buffer.cend());
+
 	return pal;
 }
 
 Palette SetupGlobalRGBModification(const Holder<Palette>& src, const RGBModifier& mod) noexcept
 {
 	Palette pal;
+	Palette::Colors buffer;
+
+	auto srcData = src->ColorData();
 	// don't modify the transparency and shadow colour
 	for (int i = 0; i < 2; ++i) {
-		pal.col[i] = src->col[i];
+		buffer[i] = srcData[i];
 	}
 
 	for (int i = 2; i < 256; ++i) {
-		applyMod(src->col[i], pal.col[i], mod);
+		applyMod(srcData[i], buffer[i], mod);
 	}
+
+	pal.CopyColors(0, buffer.cbegin(), buffer.cend());
 
 	return pal;
 }
@@ -2954,6 +2961,7 @@ Palette SetupPaperdollColours(const ieDword* colors, unsigned int type) noexcept
 	unsigned int s = Clamp<ieDword>(8*type, 0, 8*sizeof(ieDword)-1);
 	constexpr uint8_t numCols = 12;
 	Palette pal;
+	Palette::Colors buffer;
 
 	enum PALETTES : uint8_t
 	{
@@ -2963,38 +2971,40 @@ Palette SetupPaperdollColours(const ieDword* colors, unsigned int type) noexcept
 
 	for (uint8_t idx = METAL; idx < END; ++idx) {
 		const auto& pal16 = core->GetPalette16(colors[idx]>>s);
-		pal.CopyColorRange(&pal16[0], &pal16[numCols], 0x04 + (idx * 12));
+		std::copy(&pal16[0], &pal16[numCols], buffer.begin() + 0x04 + (idx * 12));
 	}
 	
 	//minor
-	memcpy(&pal.col[0x58], &pal.col[0x11], 8 * sizeof(Color));
+	memcpy(&buffer[0x58], &buffer[0x11], 8 * sizeof(Color));
 	//major
-	memcpy(&pal.col[0x60], &pal.col[0x1d], 8 * sizeof(Color));
+	memcpy(&buffer[0x60], &buffer[0x1d], 8 * sizeof(Color));
 	//minor
-	memcpy(&pal.col[0x68], &pal.col[0x11], 8 * sizeof(Color));
+	memcpy(&buffer[0x68], &buffer[0x11], 8 * sizeof(Color));
 	//metal
-	memcpy(&pal.col[0x70], &pal.col[0x05], 8 * sizeof(Color));
+	memcpy(&buffer[0x70], &buffer[0x05], 8 * sizeof(Color));
 	//leather
-	memcpy(&pal.col[0x78], &pal.col[0x35], 8 * sizeof(Color));
+	memcpy(&buffer[0x78], &buffer[0x35], 8 * sizeof(Color));
 	//leather
-	memcpy(&pal.col[0x80], &pal.col[0x35], 8 * sizeof(Color));
+	memcpy(&buffer[0x80], &buffer[0x35], 8 * sizeof(Color));
 	//minor
-	memcpy(&pal.col[0x88], &pal.col[0x11], 8 * sizeof(Color));
+	memcpy(&buffer[0x88], &buffer[0x11], 8 * sizeof(Color));
 
 	for (int i = 0x90; i < 0xA8; i += 0x08) {
 		//leather
-		memcpy(&pal.col[i], &pal.col[0x35], 8 * sizeof(Color));
+		memcpy(&buffer[i], &buffer[0x35], 8 * sizeof(Color));
 	}
 
 	//skin
-	memcpy(&pal.col[0xB0], &pal.col[0x29], 8 * sizeof(Color));
+	memcpy(&buffer[0xB0], &buffer[0x29], 8 * sizeof(Color));
 
 	for (int i = 0xB8; i < 0xFF; i += 0x08) {
 		//leather
-		memcpy(&pal.col[i], &pal.col[0x35], 8 * sizeof(Color));
+		memcpy(&buffer[i], &buffer[0x35], 8 * sizeof(Color));
 	}
 
-	pal.col[1] = Color(0, 0, 0, 128); // shadows are always half trans black
+	buffer[1] = Color(0, 0, 0, 255); // shadows, will be half-trans'ed when required
+
+	pal.CopyColors(0, buffer.cbegin(), buffer.cend());
 	return pal;
 }
 

gemrb/CMakeLists.txt
@@ -176,7 +176,9 @@ ENDIF(VITA)
 # Tests
 IF (BUILD_TESTING)
   ADD_EXECUTABLE(Test_gemrb_core
+    tests/core/Test_MurmurHash.cpp
     tests/core/Test_Orient.cpp
+    tests/core/Test_Palette.cpp
     tests/core/Streams/Test_DataStream.cpp
     tests/core/Strings/Test_CString.cpp
     tests/core/Strings/Test_String.cpp

gemrb/core/CMakeLists.txt
@@ -36,6 +36,7 @@ FILE(GLOB gemrb_core_LIB_SRCS
 	Map.cpp
 	MapReverb.cpp
 	MoviePlayer.cpp
+	MurmurHash.cpp
 	Palette.cpp
 	PalettedImageMgr.cpp
 	Particles.cpp

gemrb/core/GUI/TextSystem/Font.cpp
@@ -146,11 +146,16 @@ void Font::GlyphAtlasPage::Draw(ieWord chr, const Region& dest, const PrintColor
 		if (font->background) {
 			invertedSheet = Sheet->copy();
 			auto invertedPalette = MakeHolder<Palette>(*font->palette);
-			for (auto& c : invertedPalette->col) {
+
+			Palette::Colors buffer;
+			for (size_t i = 0; i < buffer.size(); ++i) {
+				auto c = invertedPalette->GetColorAt(i);
 				c.r = 255 - c.r;
 				c.g = 255 - c.g;
 				c.b = 255 - c.b;
+				buffer[i] = c;
 			}
+			invertedPalette->CopyColors(0, buffer.cbegin(), buffer.cend());
 			invertedSheet->SetPalette(invertedPalette);
 		}
 	}

gemrb/core/GameData.cpp
@@ -151,9 +151,8 @@ Holder<Palette> GameData::GetPalette(const ResRef& resname)
 		return NULL;
 	}
 
-	Holder<Palette> palette = MakeHolder<Palette>();
-	im->GetPalette(256,palette->col);
-	palette->named=true;
+	Holder<Palette> palette = MakeHolder<Palette>(true);
+	im->GetPalette(256, *palette);
 	PaletteCache[resname] = palette;
 	return palette;
 }

gemrb/core/ImageMgr.cpp
@@ -26,7 +26,7 @@ namespace GemRB {
 
 const TypeID ImageMgr::ID = { "ImageMgr" };
 
-int ImageMgr::GetPalette(int /*colors*/, Color* /*pal*/)
+int ImageMgr::GetPalette(int /*colors*/, Palette& /*pal*/)
 {
 	return -1;
 }

gemrb/core/ImageMgr.h
@@ -47,11 +47,11 @@ public:
 	 * Returns image palette.
 	 *
 	 * @param[in] colors Number of colors to return.
-	 * @param[out] pal Array to fill with colors.
+	 * @param[out] pal Palette to fill with colors.
 	 *
 	 * This does nothing if there is no palette.
 	 */
-	virtual int GetPalette(int colors, Color* pal);
+	virtual int GetPalette(int colors, Palette& pal);
 	
 	Size GetSize() const { return size; }
 

gemrb/core/Map.cpp
@@ -162,7 +162,7 @@ int TileProps::QueryElevation(const Point& p) const noexcept
 Color TileProps::QueryLighting(const Point& p) const noexcept
 {
 	uint8_t val = QueryTileProp(p, Property::LIGHTING);
-	return propImage->GetPalette()->col[val];
+	return propImage->GetPalette()->GetColorAt(val);
 }
 
 void TileProps::PaintSearchMap(const Point& p, PathMapFlags value) const noexcept
@@ -1668,6 +1668,8 @@ BlitFlags Map::SetDrawingStencilForProjectile(const Projectile* pro, const Regio
 void Map::DrawDebugOverlay(const Region &vp, uint32_t dFlags) const
 {
 	const static struct DebugPalettes {
+		Palette::Colors buffer;
+
 		Holder<Palette> searchMapPal;
 		Holder<Palette> materialMapPal;
 		Holder<Palette> heightMapPal;
@@ -1675,42 +1677,45 @@ void Map::DrawDebugOverlay(const Region &vp, uint32_t dFlags) const
 		
 		DebugPalettes() noexcept {
 			searchMapPal = MakeHolder<Palette>();
-			std::fill_n(&searchMapPal->col[0], 256, Color()); // passable is transparent
-			searchMapPal->col[0] = Color(128, 64, 64, 128); // IMPASSABLE, red-ish
+			std::fill_n(buffer.begin(), 256, Color()); // passable is transparent
+			buffer[0] = Color(128, 64, 64, 128); // IMPASSABLE, red-ish
 			
 			for (uint8_t i = 1; i < 255; ++i) {
 				if (i & uint8_t(PathMapFlags::SIDEWALL)) {
-					searchMapPal->col[uint8_t(PathMapFlags::SIDEWALL)] = Color(64, 64, 128, 128); // blues-ish
+					buffer[uint8_t(PathMapFlags::SIDEWALL)] = Color(64, 64, 128, 128); // blues-ish
 				} else if (i & uint8_t(PathMapFlags::ACTOR)) {
-					searchMapPal->col[i] = Color(128, 64, 128, 128); // actor, purple-ish
+					buffer[i] = Color(128, 64, 128, 128); // actor, purple-ish
 				} else if ((i & uint8_t(PathMapFlags::PASSABLE)) == 0) {
 					// anything else that isnt PASSABLE
-					searchMapPal->col[i] = ColorGray;
+					buffer[i] = ColorGray;
 				}
 			}
-			
+			searchMapPal->CopyColors(0, buffer.cbegin(), buffer.cbegin() + 16);
+
 			materialMapPal = MakeHolder<Palette>();
-			materialMapPal->col[0] = ColorBlack; // impassable, light blocking
-			materialMapPal->col[1] = Color(0xB9, 0xAB, 0x79, 128); // sand
-			materialMapPal->col[2] = Color(0x6C, 0x4D, 0x2E, 128); // wood
-			materialMapPal->col[3] = Color(0x6C, 0x4D, 0x2E, 128); // wood
-			materialMapPal->col[4] = Color(0x84, 0x86, 0x80, 128); // stone
-			materialMapPal->col[5] = Color(0, 0xFF, 0, 128); // grass
-			materialMapPal->col[6] = ColorBlue; // water
-			materialMapPal->col[7] = Color(0x84, 0x86, 0x80, 128); // stone
-			materialMapPal->col[8] = ColorWhite; // obstacle, non light blocking
-			materialMapPal->col[9] = Color(0x6C, 0x4D, 0x2E, 128); // wood
-			materialMapPal->col[10] = ColorGray; // wall, impassable
-			materialMapPal->col[11] = ColorBlue; // water
-			materialMapPal->col[12] = ColorBlueDark; // water, impassable
-			materialMapPal->col[13] = Color(0xFF, 0x00, 0xFF, 128); // roof
-			materialMapPal->col[14] = Color(128, 0, 128, 128); // exit
-			materialMapPal->col[15] = Color(0, 0xFF, 0, 128); // grass
-			
+			buffer[0] = ColorBlack; // impassable, light blocking
+			buffer[1] = Color(0xB9, 0xAB, 0x79, 128); // sand
+			buffer[2] = Color(0x6C, 0x4D, 0x2E, 128); // wood
+			buffer[3] = Color(0x6C, 0x4D, 0x2E, 128); // wood
+			buffer[4] = Color(0x84, 0x86, 0x80, 128); // stone
+			buffer[5] = Color(0, 0xFF, 0, 128); // grass
+			buffer[6] = ColorBlue; // water
+			buffer[7] = Color(0x84, 0x86, 0x80, 128); // stone
+			buffer[8] = ColorWhite; // obstacle, non light blocking
+			buffer[9] = Color(0x6C, 0x4D, 0x2E, 128); // wood
+			buffer[10] = ColorGray; // wall, impassable
+			buffer[11] = ColorBlue; // water
+			buffer[12] = ColorBlueDark; // water, impassable
+			buffer[13] = Color(0xFF, 0x00, 0xFF, 128); // roof
+			buffer[14] = Color(128, 0, 128, 128); // exit
+			buffer[15] = Color(0, 0xFF, 0, 128); // grass
+			materialMapPal->CopyColors(0, buffer.cbegin(), buffer.cbegin() + 16);
+
 			heightMapPal = MakeHolder<Palette>();
 			for (uint8_t i = 0; i < 255; ++i) {
-				heightMapPal->col[i] = Color(i, i, i, 128);
+				buffer[i] = Color(i, i, i, 128);
 			}
+			heightMapPal->CopyColors(0, buffer.cbegin(), buffer.cend());
 		}
 	} debugPalettes;
 	
@@ -1734,13 +1739,13 @@ void Map::DrawDebugOverlay(const Region &vp, uint32_t dFlags) const
 			Color col;
 			if (dFlags & DEBUG_SHOW_SEARCHMAP) {
 				auto val = tileProps.QueryTileProp(p, TileProps::Property::SEARCH_MAP);
-				col = debugPalettes.searchMapPal->col[val];
+				col = debugPalettes.searchMapPal->GetColorAt(val);
 			} else if (dFlags & DEBUG_SHOW_MATERIALMAP) {
 				auto val = tileProps.QueryMaterial(p);
-				col = debugPalettes.materialMapPal->col[val];
+				col = debugPalettes.materialMapPal->GetColorAt(val);
 			} else if (dFlags & DEBUG_SHOW_HEIGHTMAP) {
 				auto val = tileProps.QueryTileProp(p, TileProps::Property::ELEVATION);
-				col = debugPalettes.heightMapPal->col[val];
+				col = debugPalettes.heightMapPal->GetColorAt(val);
 			} else if (dFlags & DEBUG_SHOW_LIGHTMAP) {
 				col = tileProps.QueryLighting(p);
 			}

commit 3d9c19fd58f7e11379ea6b21ce85cb68009ede4a

GetPaperdollImage: readd half-transing of shadows
regression from 1ffea37

gemrb/core/CharAnimations.cpp
@@ -3038,6 +3038,9 @@ Holder<Sprite2D> GetPaperdollImage(const ResRef& resref, const ieDword* colors, 
 	if (colors) {
 		Holder<Palette> pal = MakeHolder<Palette>(*spr->GetPalette());
 		*pal = SetupPaperdollColours(colors, type);
+		auto shadowColor = pal->GetColorAt(1);
+		shadowColor.a = core->GetDictionary().Get("Translucent Shadows", 0) ? 128 : 255;
+		pal->SetColor(1, shadowColor);
 		spr->SetPalette(pal);
 		picture2->SetPalette(pal);
 	}