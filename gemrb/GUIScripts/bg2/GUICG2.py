gemrb/gemrb

commit 2d2134defdb8ea51fd08af68cc1d407104dbe80a

bgee: fixed cg blockers

gemrb/GUIScripts/bg2/GUICG2.py
@@ -46,10 +46,11 @@ def OnLoad():
 
 	j = 0
 	#radiobutton groups must be set up before doing anything else to them
-	for i in range(1,ClassCount):
+	for i in range(1, min(ClassCount, 12)):
 		ClassName = CommonTables.Classes.GetRowName (i-1)
 		if CommonTables.Classes.GetValue (ClassName, "MULTI"):
 			continue
+		# bgee ids 2-9, 15-18, bg2ee is missing 18
 		if j>7:
 			Button = ClassWindow.GetControl(j+7)
 		else:
@@ -72,7 +73,7 @@ def OnLoad():
 	j = 0
 	GemRB.SetVar("MAGESCHOOL",0) 
 	HasMulti = 0
-	for i in range(1,ClassCount):
+	for i in range(1, min(ClassCount, 12)):
 		ClassName = CommonTables.Classes.GetRowName(i-1)
 		Allowed = CommonTables.Classes.GetValue(ClassName, RaceName)
 		if CommonTables.Classes.GetValue (ClassName, "MULTI"):

gemrb/GUIScripts/GameCheck.py
@@ -44,6 +44,9 @@ def IsIWD2 ():
 def IsBG1 ():
 	return GemRB.GameType == "bg1"
 
+def IsBG1OrEE ():
+	return GemRB.GameType in ["bg1", "bgee"]
+
 def IsBG2 ():
 	return GemRB.GameType == "bg2"
 

gemrb/GUIScripts/LUProfsSelection.py
@@ -87,7 +87,7 @@ def SetupProfsWindow (pc, proftype, window, callback, level1=[0,0,0], level2=[1,
 	ProfsType = proftype
 	ProfsScrollBar = None
 
-	if proftype == LUPROFS_TYPE_CHARGEN and (GameCheck.IsBG1() or GameCheck.IsBG2OrEE ()):
+	if proftype == LUPROFS_TYPE_CHARGEN and (GameCheck.IsBG1OrEE () or GameCheck.IsBG2OrEE ()):
 		ProfsOffsetSum = 9
 		ProfsOffsetButton1 = 11
 		ProfsOffsetStar = 27
@@ -99,7 +99,7 @@ def SetupProfsWindow (pc, proftype, window, callback, level1=[0,0,0], level2=[1,
 		if (scroll):
 			ProfsScrollBar = ProfsWindow.GetControl (78)
 
-		if GameCheck.IsBG2OrEE () or GameCheck.IsIWD2 ():
+		if GameCheck.IsBG2OrEE () or GameCheck.IsIWD2 () or GameCheck.IsBGEE ():
 			import CharGenCommon
 			CharGenCommon.PositionCharGenWin (ProfsWindow)
 	elif proftype == LUPROFS_TYPE_LEVELUP and GameCheck.IsBG2OrEE ():

gemrb/GUIScripts/TextScreen.py
@@ -114,8 +114,8 @@ def StartTextScreen ():
 	GameWin = GemRB.GetView("GAMEWIN")
 	GameWin.SetDisabled(True)
 
-	if GameCheck.IsBG1():
-		#these suckers couldn't use a fix row
+	if GameCheck.IsBG1OrEE ():
+		# these suckers couldn't use a fixed row
 		FindTextRow (Table)
 	elif GameCheck.IsBG2OrEE ():
 		FindTextRow (Table)

gemrb/GUIScripts/bg2/CharGenEnd.py
@@ -108,7 +108,7 @@ def RunGame(MyChar):
 		# avoid briefly showing the GUI before the bg2 intro cutscene starts
 		# however BGT and TUTU users can start bg1 through the same code and
 		# that definitely should not hide the GUI
-		if not GameCheck.IsBGT ("bg1"):
+		if not GameCheck.IsBGT ("bg1") and not GameCheck.IsBGEE ():
 			CommonWindow.SetGameGUIHidden (True)
 
 		GemRB.EnterGame()

commit 19c154a808f4dfbfd105fa70ea891c020f1a4aac

bg2/GUICG2: fixed regression from 2d2134d, fixes #2422

gemrb/GUIScripts/bg2/GUICG2.py
@@ -46,7 +46,9 @@ def OnLoad():
 
 	j = 0
 	#radiobutton groups must be set up before doing anything else to them
-	for i in range(1, min(ClassCount, 12)):
+	for i in range(1, ClassCount):
+		if j == 11: # reached the last control
+			break
 		ClassName = CommonTables.Classes.GetRowName (i-1)
 		if CommonTables.Classes.GetValue (ClassName, "MULTI"):
 			continue
@@ -73,7 +75,9 @@ def OnLoad():
 	j = 0
 	GemRB.SetVar("MAGESCHOOL",0) 
 	HasMulti = 0
-	for i in range(1, min(ClassCount, 12)):
+	for i in range(1, ClassCount):
+		if j == 11: # reached the last control
+			break
 		ClassName = CommonTables.Classes.GetRowName(i-1)
 		Allowed = CommonTables.Classes.GetValue(ClassName, RaceName)
 		if CommonTables.Classes.GetValue (ClassName, "MULTI"):