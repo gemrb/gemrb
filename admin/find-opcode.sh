9d1d51dde6f9b832482ea926c8b732c8a29199fc
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2026-01-25 18:11:10 +0100

    admin/find-opcode.sh: added lame aide for swapping opcodes
    couldn't make bbe to do this trivial task, so xxd + sed it is

--- admin/find-opcode.sh
+++ admin/find-opcode.sh
@@ -16,3 +16,17 @@ find gemrb/unhardcoded gemrb/override demo/override \
 while read file; do
   "$IELISTER" "$file" | grep -iq "Opcode   *$opcode" && echo "$file"
 done | sort
+
+# a bit flaky opcode replacer
+# takes two dwords, which need to be aligned to the 4-byte boundary or xxd will split them
+# use the log and hexdump if necessary to understand the structure and get the starting value
+# example run:
+# replaceDW gemrb/unhardcoded/iwd2/turn.spl 0189 0025
+function replaceDW() {
+	f=$1
+	indw=$(sed 's,\(..\)\(..\),\2\1,' <<< "$2")
+	outdw=$(sed 's,\(..\)\(..\),\2\1,' <<< "$3")
+	xxd "$f" | sed "s/$indw/$outdw/" | xxd -r > tmtmtmt3gfvxdfgfd
+	mv tmtmtmt3gfvxdfgfd "$f"
+	git diff "$f"
+}
