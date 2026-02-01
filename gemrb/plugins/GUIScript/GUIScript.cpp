gemrb/gemrb

commit 0c456b76faa7c623ccdfa149529af2cb98d0cf8a

CREVersion: converted to class enum and shared

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -5316,7 +5316,7 @@ static PyObject* GemRB_CreatePlayer(PyObject* /*self*/, PyObject* args)
 
 	ResRef CreResRef = ResRefFromPy(pystr);
 	if (!CreResRef.IsEmpty()) {
-		PlayerSlot = gamedata->LoadCreature(CreResRef, Slot, (bool) Import, VersionOverride);
+		PlayerSlot = gamedata->LoadCreature(CreResRef, Slot, (bool) Import, static_cast<CREVersion>(VersionOverride));
 	} else {
 		//just destroyed the previous actor, not going to create one
 		PlayerSlot = 0;

gemrb/core/GameData.cpp
@@ -73,7 +73,7 @@ Actor* GameData::GetCreature(const ResRef& creature, unsigned int PartySlot)
 	return actor;
 }
 
-int GameData::LoadCreature(const ResRef& creature, unsigned int PartySlot, bool character, int VersionOverride)
+int GameData::LoadCreature(const ResRef& creature, unsigned int PartySlot, bool character, CREVersion versionOverride)
 {
 	DataStream* stream;
 
@@ -101,8 +101,8 @@ int GameData::LoadCreature(const ResRef& creature, unsigned int PartySlot, bool 
 		return -1;
 	}
 
-	if (VersionOverride != -1) {
-		actor->creVersion = VersionOverride;
+	if (versionOverride != CREVersion::GemRB) {
+		actor->creVersion = versionOverride;
 	}
 
 	actor->AreaName = core->GetGame()->CurrentArea;

gemrb/core/GameData.h
@@ -24,6 +24,7 @@
 #include "SClassID.h"
 #include "exports.h"
 #include "ie_types.h"
+#include "versions.h"
 
 #include "Cache.h"
 #include "CharAnimations.h"
@@ -99,7 +100,7 @@ public:
 	/** Returns actor */
 	Actor* GetCreature(const ResRef& creature, unsigned int PartySlot = 0);
 	/** Returns a PC index, by loading a creature */
-	int LoadCreature(const ResRef& creature, unsigned int PartySlot, bool character = false, int VersionOverride = -1);
+	int LoadCreature(const ResRef& creature, unsigned int PartySlot, bool character = false, CREVersion versionOverride = CREVersion::GemRB);
 
 
 	// 2DA table functions.

gemrb/core/Scriptable/Actor.h
@@ -24,6 +24,7 @@
 #include "exports.h"
 #include "ie_types.h"
 #include "strrefs.h"
+#include "versions.h"
 
 #include "CombatInfo.h"
 #include "EffectQueue.h"
@@ -49,15 +50,6 @@ class Map;
 class ScriptedAnimation;
 struct PolymorphCache;
 enum class Feat : uint8_t;
-
-enum CREVersion {
-	GemRB,
-	V1_0 = 10, // bg1
-	V1_1 = 11, // bg2 (still V1.0)
-	V1_2 = 12, // pst
-	V2_2 = 22, // iwd2
-	V9_0 = 90 // iwd
-};
 
 #define MAX_STATS 256
 #define MAX_LEVEL 128
@@ -478,7 +470,7 @@ public:
 	Inventory inventory;
 	Spellbook spellbook;
 	//savefile version (creatures embedded in area)
-	int creVersion = 0;
+	CREVersion creVersion = CREVersion::GemRB;
 	//in game or area actor header
 	ieDword TalkCount = 0;
 	//FIXME: this is definitely not the same in bg2, in bg2 there are joinable npcs

gemrb/includes/versions.h
@@ -31,4 +31,13 @@ enum class GAMVersion {
 	IWD2 = 22
 };
 
+enum class CREVersion {
+	GemRB = 0,
+	V1_0 = 10, // bg1
+	V1_1 = 11, // bg2 (still V1.0)
+	V1_2 = 12, // pst
+	V2_2 = 22, // iwd2
+	V9_0 = 90 // iwd
+};
+
 #endif

gemrb/plugins/CREImporter/CREImporter.cpp
@@ -463,23 +463,23 @@ bool CREImporter::Import(DataStream* str)
 		CREOffset = 0;
 	}
 	if (strncmp(Signature, "CRE V1.0", 8) == 0) {
-		CREVersion = CREVersion::V1_0;
+		version = CREVersion::V1_0;
 		return true;
 	}
 	if (strncmp(Signature, "CRE V1.2", 8) == 0) {
-		CREVersion = CREVersion::V1_2;
+		version = CREVersion::V1_2;
 		return true;
 	}
 	if (strncmp(Signature, "CRE V2.2", 8) == 0) {
-		CREVersion = CREVersion::V2_2;
+		version = CREVersion::V2_2;
 		return true;
 	}
 	if (strncmp(Signature, "CRE V9.0", 8) == 0) {
-		CREVersion = CREVersion::V9_0;
+		version = CREVersion::V9_0;
 		return true;
 	}
 	if (strncmp(Signature, "CRE V0.0", 8) == 0) {
-		CREVersion = CREVersion::GemRB;
+		version = CREVersion::GemRB;
 		return true;
 	}
 
@@ -489,7 +489,7 @@ bool CREImporter::Import(DataStream* str)
 
 void CREImporter::SetupSlotCounts()
 {
-	switch (CREVersion) {
+	switch (version) {
 		case CREVersion::V1_2: // pst
 			QWPCount = 4;
 			QSPCount = 3;
@@ -520,7 +520,7 @@ void CREImporter::WriteChrHeader(DataStream* stream, const Actor* act)
 	ieDword hdrSize = 0;
 
 	CRESize = GetStoredFileSize(act);
-	switch (CREVersion) {
+	switch (version) {
 		case CREVersion::V9_0: // iwd/HoW
 			memcpy(Signature, "CHR V1.0", 8);
 			hdrSize = 0x64; //headersize
@@ -590,7 +590,7 @@ void CREImporter::WriteChrHeader(DataStream* stream, const Actor* act)
 	for (int i = 0; i < QITCount; i++) {
 		stream->WriteWord(act->PCStats->QuickItemHeaders[i]);
 	}
-	switch (CREVersion) {
+	switch (version) {
 		case CREVersion::V2_2:
 			//gemrb format doesn't save these redundantly
 			for (int i = 0; i < QSPCount; i++) {
@@ -667,7 +667,7 @@ void CREImporter::ReadChrHeader(Actor* act)
 
 	ResRef spell;
 	//here comes the version specific read
-	switch (CREVersion) {
+	switch (version) {
 		case CREVersion::V2_2:
 			//gemrb format doesn't save these redundantly
 			// quick innates and quick songs
@@ -860,13 +860,13 @@ Actor* CREImporter::GetActor(unsigned char is_in_party)
 	}
 
 	str->Read(&TotSCEFF, 1);
-	if (CREVersion == CREVersion::V1_0 && TotSCEFF) {
-		CREVersion = CREVersion::V1_1;
+	if (version == CREVersion::V1_0 && TotSCEFF) {
+		version = CREVersion::V1_1;
 	}
 	// saving in original version requires the original version
 	// otherwise it is set to 0 at construction time
 	if (core->config.SaveAsOriginal) {
-		act->creVersion = CREVersion;
+		act->creVersion = version;
 	}
 	str->ReadResRef(act->SmallPortrait);
 	if (act->SmallPortrait.IsEmpty()) {
@@ -882,7 +882,7 @@ Actor* CREImporter::GetActor(unsigned char is_in_party)
 	}
 
 	size_t inventorySize;
-	switch (CREVersion) {
+	switch (version) {
 		case CREVersion::GemRB:
 			inventorySize = GetActorGemRB(act);
 			break;
@@ -904,7 +904,7 @@ Actor* CREImporter::GetActor(unsigned char is_in_party)
 			GetActorIWD1(act);
 			break;
 		default:
-			Log(ERROR, "CREImporter", "Unknown creature signature: {}\n", CREVersion);
+			Log(ERROR, "CREImporter", "Unknown creature signature: {}\n", static_cast<int>(version));
 			delete act;
 			return NULL;
 	}
@@ -1795,8 +1795,8 @@ int CREImporter::GetStoredFileSize(const Actor* actor)
 	int headersize;
 	unsigned int Inventory_Size;
 
-	CREVersion = static_cast<unsigned char>(actor->creVersion);
-	switch (CREVersion) {
+	version = actor->creVersion;
+	switch (version) {
 		case CREVersion::GemRB:
 			headersize = 0x2d4;
 			//minus fist
@@ -1943,9 +1943,9 @@ int CREImporter::PutHeader(DataStream* stream, const Actor* actor) const
 	char Signature[8];
 
 	memcpy(Signature, "CRE V0.0", 8);
-	Signature[5] += CREVersion / 10;
+	Signature[5] += static_cast<int>(version) / 10;
 	if (actor->creVersion != CREVersion::V1_1) {
-		Signature[7] += CREVersion % 10;
+		Signature[7] += static_cast<int>(version) % 10;
 	}
 	stream->Write(Signature, 8);
 	stream->WriteStrRef(actor->LongStrRef);
@@ -2508,7 +2508,7 @@ int CREImporter::PutActor(DataStream* stream, const Actor* actor, bool chr)
 	//here comes the fuzzy part
 	ieDword Inventory_Size;
 
-	switch (CREVersion) {
+	switch (version) {
 		case CREVersion::GemRB:
 			//don't add fist
 			Inventory_Size = (ieDword) actor->inventory.GetSlotCount() - 1;

gemrb/plugins/CREImporter/CREImporter.h
@@ -21,6 +21,8 @@
 #ifndef CREIMPORTER_H
 #define CREIMPORTER_H
 
+#include "versions.h"
+
 #include "ActorMgr.h"
 #include "Spellbook.h"
 
@@ -31,7 +33,7 @@ struct Effect;
 
 class CREImporter : public ActorMgr {
 private:
-	unsigned char CREVersion = 0xff;
+	CREVersion version = CREVersion::GemRB;
 	ieDword KnownSpellsOffset = 0;
 	ieDword KnownSpellsCount = 0;
 	ieDword SpellMemorizationOffset = 0;

commit a8abb422f68b80fd3ac984e2f2f9ae40f69ac946

GemRB_CreatePlayer: fixed regression from 0c456b7

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -5290,7 +5290,7 @@ static PyObject* GemRB_CreatePlayer(PyObject* /*self*/, PyObject* args)
 	PyObject* pystr = nullptr;
 	int PlayerSlot, Slot;
 	int Import = 0;
-	int VersionOverride = -1;
+	int VersionOverride = 0;
 	PARSE_ARGS(args, "Oi|ii", &pystr, &PlayerSlot, &Import, &VersionOverride);
 	//PlayerSlot is zero based
 	Slot = (PlayerSlot & 0x7fff);