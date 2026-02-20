gemrb/gemrb

commit 21927a1cf6e0c7646eaf0853615a121466f74a13

shared orientation maps into Orientation.h

gemrb/core/ScriptedAnimation.cpp
@@ -66,17 +66,6 @@ static const std::array<CycleType, 16> cTypes = {
 	CycleType::Three | CycleType::Five,
 };
 
-static const ieByte SixteenToNine[3 * MAX_ORIENT] = {
-	0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1,
-	9, 10, 11, 12, 13, 14, 15, 16, 17, 16, 15, 14, 13, 12, 11, 10,
-	18, 19, 20, 21, 22, 23, 24, 25, 26, 25, 24, 23, 22, 21, 20, 19
-};
-static const ieByte SixteenToFive[3 * MAX_ORIENT] = {
-	0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 3, 3, 2, 2, 1, 1,
-	5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 8, 8, 7, 7, 6, 6,
-	10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 13, 13, 12, 12, 11, 11
-};
-
 static ieDword TranslucentShadows = 0;
 
 Animation* ScriptedAnimation::PrepareAnimation(const AnimationFactory& af, Animation::index_t cycle, Animation::index_t i, bool loop) const
@@ -86,9 +75,9 @@ Animation* ScriptedAnimation::PrepareAnimation(const AnimationFactory& af, Anima
 	if (NumOrientations == 16 || OrientationFlags & IE_VVC_FACE_FIXED) {
 		if (af.GetCycleCount() > i) c = i;
 	} else if (NumOrientations == 5) {
-		c = SixteenToFive[i];
+		c = SixteenToFive[i % MAX_ORIENT] + 5 * i / MAX_ORIENT;
 	} else if (NumOrientations == 9) {
-		c = SixteenToNine[i];
+		c = SixteenToNine[i % MAX_ORIENT] + 9 * i / MAX_ORIENT;
 	}
 
 	Animation* anim = af.GetCycle(c);
@@ -165,10 +154,10 @@ void ScriptedAnimation::LoadAnimationFactory(const AnimationFactory& af, int get
 			assert(p < 3);
 			p *= MAX_ORIENT;
 		} else if (type & CycleType::Five) {
-			c = SixteenToFive[c];
+			c = SixteenToFive[c % MAX_ORIENT] + 5 * c / MAX_ORIENT;
 			if ((i & 15) >= 5) mirrorFlags = BlitFlags::MIRRORX;
 		} else if (type & CycleType::Nine) {
-			c = SixteenToNine[c];
+			c = SixteenToNine[c % MAX_ORIENT] + 9 * c / MAX_ORIENT;
 			if ((i & 15) >= 9) mirrorFlags = BlitFlags::MIRRORX;
 		} else if (!(type & CycleType::SevenEyes2)) {
 			assert(p < 3);

gemrb/core/CharAnimations.cpp
@@ -33,9 +33,6 @@
 
 namespace GemRB {
 
-static const ieByte SixteenToNine[16] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1 };
-static const ieByte SixteenToFive[16] = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 3, 3, 2, 2, 1, 1 };
-
 static const int zOrder_TwoPiece[2] = { 1, 0 };
 
 static const int zOrder_Mirror16[16][4] = {

gemrb/core/Orientation.h
@@ -68,6 +68,9 @@ enum orient_t : uint8_t {
 /////maximum animation orientation count (used in many places)
 #define MAX_ORIENT 0x10 // uint8_t(orient_t::MAX)
 
+static const ieByte SixteenToNine[MAX_ORIENT] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1 };
+static const ieByte SixteenToFive[MAX_ORIENT] = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 3, 3, 2, 2, 1, 1 };
+
 inline orient_t RandomOrientation()
 {
 	return orient_t(RAND(0, MAX_ORIENT - 1));

gemrb/core/Projectile.cpp
@@ -42,8 +42,8 @@ namespace GemRB {
 
 constexpr uint8_t PALSIZE = 32;
 
-static const ieByte SixteenToNine[MAX_ORIENT] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1 };
-static const ieByte SixteenToFive[MAX_ORIENT] = { 0, 1, 2, 3, 4, 3, 2, 1, 0, 1, 2, 3, 4, 3, 2, 1 };
+// different conversion than in Orientation.h
+static const ieByte SixteenToFive2[MAX_ORIENT] = { 0, 1, 2, 3, 4, 3, 2, 1, 0, 1, 2, 3, 4, 3, 2, 1 };
 
 static ProjectileServer* server = NULL;
 
@@ -119,7 +119,7 @@ Projectile::AnimArray Projectile::CreateOrientedAnimations(const AnimationFactor
 		AnimationFactory::index_t c;
 		switch (Aim) {
 			case 5:
-				c = SixteenToFive[cycle];
+				c = SixteenToFive2[cycle];
 				// orientations go counter-clockwise, starting south
 				if (cycle > 4 && cycle <= 8) {
 					// top-left quadrant

commit 26e3793c9ccc79bd6a300849537f77c8dffdda3d

ScriptedAnimation: fixed regression from 21927a1c
for example caused missing cycles on wall of fire

gemrb/core/ScriptedAnimation.cpp
@@ -74,9 +74,9 @@ Animation* ScriptedAnimation::PrepareAnimation(const AnimationFactory& af, Anima
 	if (NumOrientations == 16 || OrientationFlags & IE_VVC_FACE_FIXED) {
 		if (af.GetCycleCount() > i) c = i;
 	} else if (NumOrientations == 5) {
-		c = SixteenToFive[i % MAX_ORIENT] + 5 * i / MAX_ORIENT;
+		c = SixteenToFive[i % MAX_ORIENT] + 5 * (i / MAX_ORIENT);
 	} else if (NumOrientations == 9) {
-		c = SixteenToNine[i % MAX_ORIENT] + 9 * i / MAX_ORIENT;
+		c = SixteenToNine[i % MAX_ORIENT] + 9 * (i / MAX_ORIENT);
 	}
 
 	Animation* anim = af.GetCycle(c);
@@ -155,10 +155,10 @@ void ScriptedAnimation::LoadAnimationFactory(const AnimationFactory& af, int get
 			assert(p < 3);
 			p *= MAX_ORIENT;
 		} else if (type & CycleType::Five) {
-			c = SixteenToFive[c % MAX_ORIENT] + 5 * c / MAX_ORIENT;
+			c = SixteenToFive[c % MAX_ORIENT] + 5 * (c / MAX_ORIENT);
 			if ((i & 15) >= 5) mirrorFlags = BlitFlags::MIRRORX;
 		} else if (type & CycleType::Nine) {
-			c = SixteenToNine[c % MAX_ORIENT] + 9 * c / MAX_ORIENT;
+			c = SixteenToNine[c % MAX_ORIENT] + 9 * (c / MAX_ORIENT);
 			if ((i & 15) >= 9) mirrorFlags = BlitFlags::MIRRORX;
 		} else if (!(type & CycleType::SevenEyes2)) {
 			assert(p < 3);