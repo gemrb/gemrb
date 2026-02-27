gemrb/gemrb

commit 9b58a7f337c384625e7923eaab285cae86316f61

GetPlayerStat, GetCreatureStat: fixed type size, so wild mages get recognized again

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -216,7 +216,7 @@ static bool StatIsASkill(unsigned int StatID)
 	return false;
 }
 
-static int GetCreatureStat(const Actor* actor, unsigned int StatID, int Mod)
+static int64_t GetCreatureStat(const Actor* actor, unsigned int StatID, int Mod)
 {
 	//this is a hack, if more PCStats fields are needed, improve it
 	if (StatID & EXTRASETTINGS) {
@@ -6098,18 +6098,18 @@ listed in ie_stats.py. For IWD2 skills, it takes all bonuses into account.\n\
 
 static PyObject* GemRB_GetPlayerStat(PyObject* /*self*/, PyObject* args)
 {
-	int globalID, StatID, StatValue, BaseStat;
-
-	BaseStat = 0;
+	int globalID;
+	int StatID;
+	int BaseStat = 0;
 	PARSE_ARGS(args, "ii|i", &globalID, &StatID, &BaseStat);
 	GET_GAME();
 	GET_ACTOR_GLOBAL();
 
 	//returning the modified stat if BaseStat was 0 (default)
-	StatValue = GetCreatureStat(actor, StatID, !BaseStat);
+	int64_t StatValue = GetCreatureStat(actor, StatID, !BaseStat);
 
 	// special handling for the hidden hp
-	if ((unsigned) StatValue == 0xdadadada) {
+	if (StatValue == 0xdadadada) {
 		return PyString_FromString("?");
 	} else {
 		return PyLong_FromLong(StatValue);

commit 8a4784e2b1d59b2c492c33d9e82271c585519ade

GemRB_SetPlayerStat: fixed regression from 9b58a7f
longs can be bigger that 4 bytes, so python was silently corrupting the next
read. Fixes for example TNO weapon switching not updating the avatar's weapon

found by nerifuture using claude

gemrb/plugins/GUIScript/GUIScript.cpp
@@ -6144,14 +6144,14 @@ static PyObject* GemRB_SetPlayerStat(PyObject* /*self*/, PyObject* args)
 {
 	int globalID;
 	unsigned int StatID;
-	stat_t StatValue;
+	long StatValue;
 	int pcf = 1;
 	PARSE_ARGS(args, "iIl|i", &globalID, &StatID, &StatValue, &pcf);
 	GET_GAME();
 	GET_ACTOR_GLOBAL();
 
 	//Setting the creature's base stat
-	SetCreatureStat(actor, StatID, StatValue, pcf);
+	SetCreatureStat(actor, StatID, static_cast<stat_t>(StatValue), pcf);
 	Py_RETURN_NONE;
 }
 