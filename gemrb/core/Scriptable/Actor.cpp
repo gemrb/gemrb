gemrb/gemrb

commit e4fb7bcd8565a4b021fd1559eb0215932b0a88ef

Actor::UpdateActorState: avoid invisible actors if the stance has no data
fix by nerifuture

gemrb/core/Scriptable/Actor.cpp
@@ -7833,6 +7833,13 @@ void Actor::UpdateActorState()
 	}
 
 	if (anim.empty()) {
+		// try auto-switching to a valid stance or a fall back instead of staying invisible
+		const CharAnimations* ca = GetAnims();
+		if (ca && ca->autoSwitchOnEnd) {
+			HandleActorStance();
+		} else {
+			SetStance(IE_ANI_AWAKE);
+		}
 		UpdateModalState(game->GameTime);
 		return;
 	}

commit ba9da52cf01cc808db304901b93de37620e8bc97

pst: fixed TNO not playing his intro stance
a regression since e4fb7bc; before the timing worked out

gemrb/core/Scriptable/Actor.cpp
@@ -9956,7 +9956,7 @@ void Actor::SetUsedWeapon(AnimRef AnimationType, const std::array<ieWord, 3>& me
 
 	anims->SetWeaponRef(AnimationType);
 	anims->SetWeaponType(wt);
-	ClearCurrentStanceAnims();
+	AdvanceAnimations(); // clear and recreate stance anims (the latter is needed for TNO to start on the table)
 	SetAttackMoveChances(meleeAnimation);
 	if (InParty) {
 		//update the paperdoll weapon animation
@@ -11420,7 +11420,7 @@ int Actor::UpdateAnimationID(bool derived)
 	}
 	if (!derived) {
 		SetAnimationID(newAnimID);
-		//setting PST's starting stance to 18
+		// setting PST's starting stance to 18 (IE_ANI_PST_START)
 		if (avStance != -1) {
 			SetStance(avStance);
 		}