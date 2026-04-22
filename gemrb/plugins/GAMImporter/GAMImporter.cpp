12709614dc06e8c2895c8aa9e471cfba21b85445
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2026-04-22 20:03:55 +0200

    GAMImporter::PutActor: bg2 shares the name saving behaviour with bg1

--- gemrb/plugins/GAMImporter/GAMImporter.cpp
+++ gemrb/plugins/GAMImporter/GAMImporter.cpp
@@ -964,7 +964,7 @@ int GAMImporter::PutActor(DataStream* stream, const Actor* ac, ieDword CRESize, 
 	if (ac->LongStrRef == ieStrRef::INVALID) {
 		std::string tmpstr = TLKStringFromString(ac->GetLongName());
 		stream->WriteVariable(ieVariable(tmpstr));
-	} else if (newVersion == GAMVersion::BG) {
+	} else if (newVersion == GAMVersion::BG || newVersion == GAMVersion::BG2) {
 		// at least bg1, if not more, only saved it for the protagonist
 		stream->WriteFilling(32);
 	} else {
