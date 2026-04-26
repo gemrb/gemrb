d3cbb3285fa8cfd6f75f3c4bcb0af0f243c4fa14
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2026-04-26 09:05:25 +0200

    MakeVariable: don't truncate full-length vars
    for example iwd2 has _deadritual_pureblood_fighter_02

--- gemrb/includes/ie_types.h
+++ gemrb/includes/ie_types.h
@@ -198,7 +198,7 @@ inline bool IsStar(const STR& str)
 inline ieVariable MakeVariable(const StringView& sv)
 {
 	ieVariable var;
-	uint8_t count = ieVariable::Size - 1;
+	uint8_t count = ieVariable::Size;
 	auto dest = var.begin();
 	auto source = sv.begin();
 	while (count-- && source != sv.end()) {
