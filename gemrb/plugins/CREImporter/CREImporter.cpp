9fa531bbc0779a0e465fa38fff7d09146c04a3a9
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2026-04-30 17:45:02 +0200

    CREImporter::PutSpellPages: more ugly special-casing
    the how resave difference is gone with this

--- gemrb/plugins/CREImporter/CREImporter.cpp
+++ gemrb/plugins/CREImporter/CREImporter.cpp
@@ -2452,8 +2452,9 @@ int CREImporter::PutSpellPages(DataStream* stream, const Actor* actor) const
 			stream->WriteScalar<int, ieWord>(tmp);
 			// bg1 apparently counted only the wisdom bonus, not anything extra from effects (Edwin or the ring of holiness)
 			// we always reconstruct it on load, so it doesn't matter to us
-			if (actor->creVersion == CREVersion::V1_2) {
+			if (actor->creVersion == CREVersion::V1_2 || (actor->creVersion == CREVersion::V9_0 && core->config.GameType == "how")) {
 				// pst didn't use the table, even though it kept a copy from bg1
+				// iwd1:how didn't store the bonus, iwd1 did??
 			} else if (tmp && i == IE_SPELL_TYPE_PRIEST && !bonuses.empty() && (actor->GetClericLevel() || actor->GetDruidLevel())) {
 				tmp += bonuses[j];
 			}
