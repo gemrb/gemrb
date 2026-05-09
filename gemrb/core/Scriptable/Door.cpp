dd5a0dbe4ea4099acba77f677289fca75f0184d6
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2026-05-09 09:45:26 +0200

    Door::SetDoorOpen: don't unlock on load

--- gemrb/core/Scriptable/Door.cpp
+++ gemrb/core/Scriptable/Door.cpp
@@ -253,7 +253,8 @@ void Door::SetDoorOpen(int Open, int playsound, ieDword openerID, bool addTrigge
 		// in PS:T, opening a door does not unlock it
 		// iwd2 ar6051 pit traps (eg. the lava switch door) also show it's not true there
 		// except perhaps sometimes on closing?
-		if (!core->HasFeature(GFFlags::REVERSE_DOOR) && !core->HasFeature(GFFlags::RULES_3ED)) {
+		// maybe the Ticks check is enough, we shouldn't touch it on load anywhere
+		if (!core->HasFeature(GFFlags::REVERSE_DOOR) && !core->HasFeature(GFFlags::RULES_3ED) && Ticks > 0) {
 			SetDoorLocked(false, playsound);
 		}
 	} else if (addTrigger) {
