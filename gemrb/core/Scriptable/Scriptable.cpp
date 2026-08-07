gemrb/gemrb

commit affa1e5d7e9bd0c2f4a5097c7d0163fae7e0d6b5

Scriptable::SetDialog: no need to condition on UseAsLibrary

gemrb/core/Scriptable/Scriptable.cpp
@@ -96,7 +96,7 @@ void Scriptable::SetDialog(const ResRef& resref, bool embedded)
 {
 	if (!resref.IsEmpty() && gamedata->Exists(resref, IE_DLG_CLASS_ID, true)) {
 		Dialog = resref;
-	} else if (core->config.UseAsLibrary && !embedded) {
+	} else if (!embedded) {
 		Dialog = resref;
 	}
 }

commit a18e3f278092dcbeb998a707152fab63153961ad

Scriptable::SetDialog: fixed regression from affa1e5, fixes #2507

gemrb/core/Scriptable/Scriptable.cpp
@@ -92,11 +92,9 @@ const ieVariable& Scriptable::GetScriptName(void) const
 	return scriptName;
 }
 
-void Scriptable::SetDialog(const ResRef& resref, bool embedded)
+void Scriptable::SetDialog(const ResRef& resref)
 {
-	if (!resref.IsEmpty() && gamedata->Exists(resref, IE_DLG_CLASS_ID, true)) {
-		Dialog = resref;
-	} else if (!embedded) {
+	if (!resref.IsEmpty()) {
 		Dialog = resref;
 	}
 }

gemrb/core/Scriptable/Scriptable.h
@@ -327,7 +327,7 @@ public:
 	{
 		return Dialog;
 	}
-	void SetDialog(const ResRef& resref, bool embedded = false);
+	void SetDialog(const ResRef& resref);
 	void SetFloatingText(char*);
 	void SetScript(const ResRef& aScript, int idx, bool ai = false);
 	void SetSpellResRef(const ResRef& resref);

gemrb/plugins/AREImporter/AREImporter.cpp
@@ -1176,7 +1176,7 @@ bool AREImporter::GetActor(DataStream* str, PluginHolder<ActorMgr> actorMgr, Map
 
 	// if the creature is already embedded in the area, leave its data intact
 	// if we just loaded the CRE, apply the override from the area entry
-	act->SetDialog(dialog, embedded);
+	act->SetDialog(dialog);
 
 	for (int j = 0; j < 8; j++) {
 		if (!scripts[j].IsEmpty()) {