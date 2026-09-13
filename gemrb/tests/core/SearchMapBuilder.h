3a880e2b839f8c1095a46126a551658e18a278fb
Dariusz Pyś <dariusz.pys@intel.com>
2026-09-13 23:55:02 +0200

    Use proper path walking algo for test's drawing

--- gemrb/tests/core/SearchMapBuilder.h
+++ gemrb/tests/core/SearchMapBuilder.h
@@ -535,17 +535,12 @@ namespace test {
 		Point p = from;
 		appendIfNotPresent(SearchmapPoint { p });
 		for (size_t i = 0; i < path.Size(); ++i) {
-			PathFinder::LineStepper<NavmapPoint> walk { p, path.GetStep(i).point };
+			const Point to = path.GetStep(i).point;
+			PathFinder::GridRayCast walk { NavmapPoint(p), NavmapPoint(to) };
 			while (walk.Step()) {
-				if (walk.Current() == p) {
-					// a step should always move, bail out if that ever stops holding
-					ADD_FAILURE() << "LineStepper makes no progress for point " << i << "  at ("
-						      << p.x << ',' << p.y << ')';
-					return tiles;
-				}
-				p = walk.Current();
-				appendIfNotPresent(SearchmapPoint { p });
+				appendIfNotPresent(walk.Current());
 			}
+			p = to;
 		}
 		return tiles;
 	}
